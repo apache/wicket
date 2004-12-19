519b7ab0cd47dea7e03edb1f356d39ad677669cd
Eelco Hillenius <ehillenius@apache.org>
2004-12-19 12:32:53 +0000

    +header
    git-svn-id: https://svn.apache.org/repos/asf/incubator/wicket/trunk@455071 13f79535-47bb-0310-9956-ffa450edef68

--- wicket-examples/src/java/displaytag/export/Export.java
+++ wicket-examples/src/java/displaytag/export/Export.java
@@ -1,8 +1,20 @@
 /*
- * Created on 29.10.2004
+ * $Id$
+ * $Revision$
+ * $Date$
  *
- * TODO To change the template for this generated file go to
- * Window - Preferences - Java - Code Style - Code Templates
+ * ====================================================================
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 package displaytag.export;
 
@@ -19,10 +31,6 @@ import com.voicetribe.wicket.RequestCycle;
 import com.voicetribe.wicket.protocol.http.HttpResponse;
 
 /**
- * @author pz65n8
- *
- * TODO To change the template for this generated type comment go to
- * Window - Preferences - Java - Code Style - Code Templates
  */
 public class Export
 {
--- wicket-examples/src/java/displaytag/utils/PagedTableWithAlternatingRowStyle.java
+++ wicket-examples/src/java/displaytag/utils/PagedTableWithAlternatingRowStyle.java
@@ -1,8 +1,20 @@
 /*
- * Created on 27.10.2004
+ * $Id$
+ * $Revision$
+ * $Date$
  *
- * TODO To change the template for this generated file go to
- * Window - Preferences - Java - Code Style - Code Templates
+ * ====================================================================
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 package displaytag.utils;
 
