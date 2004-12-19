68175a5ee204297aa1c5ce6dfe6acddebc1ec720
Martijn Dashorst <dashorst@apache.org>
2004-12-19 23:25:34 +0000

    Initial release
    git-svn-id: https://svn.apache.org/repos/asf/incubator/wicket/trunk@455084 13f79535-47bb-0310-9956-ffa450edef68

---
+++ wicket-quickstart/src/main/java/wicket/quickstart/Index.java
@@ -0,0 +1,24 @@
+/*
+ * Created on Dec 2, 2004
+ *
+ * TODO To change the template for this generated file go to
+ * Window - Preferences - Java - Code Style - Code Templates
+ */
+package wicket.quickstart;
+
+import wicket.PageParameters;
+import wicket.markup.html.HtmlPage;
+
+/**
+ */
+public class Index extends HtmlPage {
+	/**
+	 * Constructor that is invoked when page is invoked without a session.
+	 * 
+	 * @param parameters
+	 *            Page parameters
+	 */
+	public Index(final PageParameters parameters) {
+		super();
+	}
+}
---
+++ wicket-quickstart/src/main/java/wicket/quickstart/QuickStart.java
@@ -0,0 +1,71 @@
+/*
+ * Created on Dec 2, 2004
+ *
+ * TODO To change the template for this generated file go to
+ * Window - Preferences - Java - Code Style - Code Templates
+ */
+package wicket.quickstart;
+
+import java.net.URL;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.mortbay.jetty.Server;
+
+import wicket.util.time.Duration;
+import wicket.ApplicationSettings;
+import wicket.WebApplication;
+
+/**
+ */
+public class QuickStart extends WebApplication {
+	/**
+	 * Used for logging.
+	 */
+	private static final Log log = LogFactory.getLog(QuickStart.class);
+
+	/**
+	 * The Jetty server.
+	 */
+	private static Server jettyServer;
+
+	/**
+	 * Main function, starts the jetty server.
+	 * 
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		try {
+			URL jettyConfig = new URL(
+					"file:src/main/resources/jetty-config.xml");
+			if (jettyConfig == null) {
+				log
+						.fatal("Unable to locate jetty-test-config.xml on the classpath");
+			}
+			jettyServer = new Server(jettyConfig);
+
+			jettyServer.start();
+		} catch (Exception e) {
+			log.fatal("Could not start the Jetty server: " + e);
+			if (jettyServer != null) {
+				try {
+					jettyServer.stop();
+				} catch (InterruptedException e1) {
+					log.fatal("Unable to stop the jetty server: " + e1);
+				}
+			}
+		}
+	}
+
+	public QuickStart() {
+		ApplicationSettings settings = getSettings();
+		settings.setHomePage(Index.class);
+		if (!Boolean.getBoolean("cache-templates")) {
+			Duration pollFreq = Duration.ONE_SECOND;
+			settings.setResourcePollFrequency(pollFreq);
+			log.info("template caching is INACTIVE");
+		} else {
+			log.info("template caching is ACTIVE");
+		}
+	}
+}
