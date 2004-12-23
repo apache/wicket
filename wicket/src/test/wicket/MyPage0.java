cf9c5f6256d44c3f7310c5e426c956add1363243
Juegen Donnerstag <jdonnerstag@apache.org>
2004-12-23 16:36:39 +0000

    PageFactory test cases
    git-svn-id: https://svn.apache.org/repos/asf/incubator/wicket/trunk@455131 13f79535-47bb-0310-9956-ffa450edef68

---
+++ wicket/src/test/wicket/MyPage0.java
@@ -0,0 +1,29 @@
+/*
+ * $Id$
+ * $Revision$
+ * $Date$
+ *
+ * ====================================================================
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package wicket;
+
+import wicket.markup.html.HtmlPage;
+
+/**
+ * Dummy Page with no constructor at all
+ * Rememeber: Page must have at least a default constructor
+ */
+public class MyPage0 extends HtmlPage
+{
+}
