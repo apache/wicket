64c54adbbb619adddfee60558d0f0e29e2df5ca3
Eelco Hillenius <ehillenius@apache.org>
2004-12-23 10:04:05 +0000

    refactored: AbstractLink is now the (minimal) baseclass for links, and Link is its extension. AbstractLink is needed, as we want only one place to register the linkClicked listener interface
    git-svn-id: https://svn.apache.org/repos/asf/incubator/wicket/trunk/wicket@455128 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/java/wicket/markup/html/link/AbstractLink.java
@@ -0,0 +1,119 @@
+/*
+ * $Id$
+ * $Revision$
+ * $Date$
+ *
+ * ====================================================================
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package wicket.markup.html.link;
+
+import java.io.Serializable;
+
+import wicket.IModel;
+import wicket.RequestCycle;
+import wicket.markup.html.HtmlContainer;
+
+
+/**
+ * Base class for links.
+ *
+ * @author Jonathan Locke
+ * @author Eelco Hillenius
+ */
+abstract class AbstractLink extends HtmlContainer implements ILinkListener
+{
+    static
+    {
+        // Allow calls through the ILinkListener interface
+        RequestCycle.registerListenerInterface(ILinkListener.class);
+    }
+
+    /**
+     * Construct.
+     * @param componentName the name of the component
+     */
+    public AbstractLink(String componentName)
+    {
+        super(componentName);
+    }
+
+    /**
+     * Constructor that uses the provided {@link IModel} as its model.
+     * @param name The non-null name of this component
+     * @param model the model
+     * @throws wicket.RenderException Thrown if the component has been given a null name.
+     */
+    public AbstractLink(String name, IModel model)
+    {
+        super(name, model);
+    }
+
+    /**
+     * Constructor that uses the provided instance of {@link IModel} as a dynamic model.
+     * This model will be wrapped in an instance of {@link wicket.PropertyModel}
+     * using the provided expression.
+     *
+     * @param name The non-null name of this component
+     * @param model the instance of {@link IModel} from which the model object will be
+     *            used as the subject for the given expression
+     * @param expression the OGNL expression that works on the given object
+     * @throws wicket.RenderException Thrown if the component has been given a null name.
+     */
+    public AbstractLink(String name, IModel model, String expression)
+    {
+        super(name, model, expression);
+    }
+
+    /**
+     * Constructor that uses the provided object as a simple model. This object will be
+     * wrapped in an instance of {@link wicket.Model}.
+     * @param name The non-null name of this component
+     * @param object the object that will be used as a simple model
+     * @throws wicket.RenderException Thrown if the component has been given a null name.
+     */
+    public AbstractLink(String name, Serializable object)
+    {
+        super(name, object);
+    }
+
+    /**
+     * Constructor that uses the provided object as a dynamic model. This object will be
+     * wrapped in an instance of {@link wicket.Model} that will be wrapped in an instance of
+     * {@link wicket.PropertyModel} using the provided expression.
+     * @param name The non-null name of this component
+     * @param object the object that will be used as the subject for the given expression
+     * @param expression the OGNL expression that works on the given object
+     * @throws wicket.RenderException Thrown if the component has been given a null name.
+     */
+    public AbstractLink(String name, Serializable object, String expression)
+    {
+        super(name, object, expression);
+    }
+
+    /**
+     * Called when a link is clicked.
+     * @see ILinkListener
+     * @param cycle The cycle object
+     */
+    public abstract void linkClicked(final RequestCycle cycle);
+
+    /**
+     * @param cycle Request cycle
+     * @return The URL that this link links to
+     */
+    protected String getURL(final RequestCycle cycle)
+    {
+        return cycle.urlFor(AbstractLink.this, ILinkListener.class);
+    }
+}
