f2baba403ed4afe295a4219fb443e876062e8775
Jonathan Locke <jonl@apache.org>
2005-01-13 22:54:37 +0000

    javadoc
    git-svn-id: https://svn.apache.org/repos/asf/incubator/wicket/trunk/wicket@455317 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/wicket/util/string/Strings.java
+++ src/java/wicket/util/string/Strings.java
@@ -24,11 +24,30 @@ import java.io.PrintWriter;
 import java.io.StringWriter;
 
 /**
- * A variety of string utilities
+ * A variety of static String utility methods.
+ * <p>
+ * The escapeMarkup() and toMultilineMarkup() methods are useful for turning 
+ * normal Java Strings into HTML strings.
+ * <p>
+ * The lastPathComponent(), firstPathComponent(), afterFirstPathComponent()
+ * and beforeLastPathComponent() methods can chop up a String into path 
+ * components using a separator character.  If the separator cannot be found
+ * the original String is returned.
+ * <p>
+ * Similarly, the beforeLast(), beforeFirst(), afterFirst() and afterLast()
+ * methods return sections before and after a separator character.  But if
+ * the separator cannot be found, an empty string is returned.
+ * <p>
+ * Some other miscellaneous methods will strip a given ending off a String 
+ * if it can be found (stripEnding()), replace all occurrences of one String
+ * with another (replaceAll), do type conversions (toBoolean(), toChar(), 
+ * toString()), check a String for emptiness (isEmpty()), convert a Throwable
+ * to a String (toString(Throwable)) or capitalize a String (capitalize()).
+ * 
  * @author Jonathan Locke
  */
 public final class Strings
-{ // TODO finalize javadoc
+{
     /**
      * Private constructor prevents construction
      */
@@ -37,9 +56,11 @@ public final class Strings
     }
 
     /**
+     * Converts a Java String to an HTML markup string, but does not convert 
+     * normal spaces to non-breaking space entities (&lt;nbsp&gt;).
      * @param s The string to escape
      * @see Strings#escapeMarkup(String, boolean)
-     * @return Escaped markup
+     * @return The escaped string
      */
     public static String escapeMarkup(final String s)
     {
@@ -47,8 +68,12 @@ public final class Strings
     }
 
     /**
-     * Encodes a markup string using HTML entities where appropriate
-     * @param s The string to remove HTML from
+     * Converts a Java String to an HTML markup String by replacing illegal 
+     * characters with HTML entities where appropriate.  Spaces are converted 
+     * to non-breaking spaces (&lt;nbsp&gt;) if escapeSpaces is true, tabs are converted 
+     * to four non-breaking spaces, less than signs are converted to &amp;lt; entities
+     * and greater than signs to &amp;gt; entities.
+     * @param s The string to escape
      * @param escapeSpaces True to replace ' ' with nonbreaking space
      * @return The escaped string
      */
@@ -61,7 +86,7 @@ public final class Strings
         else
         {
             final StringBuffer buffer = new StringBuffer();
-
+            
             for (int i = 0; i < s.length(); i++)
             {
                 final char c = s.charAt(i);
@@ -69,7 +94,6 @@ public final class Strings
                 switch (c)
                 {
                     case '\t':
-
                         if (escapeSpaces)
                         {
                             // Assumption is four space tabs (sorry, but that's just
@@ -80,11 +104,9 @@ public final class Strings
                         {
                             buffer.append(c);
                         }
-
                         break;
 
                     case ' ':
-
                         if (escapeSpaces)
                         {
                             buffer.append("&nbsp;");
@@ -93,22 +115,18 @@ public final class Strings
                         {
                             buffer.append(c);
                         }
-
                         break;
 
                     case '<':
                         buffer.append("&lt;");
-
                         break;
 
                     case '>':
                         buffer.append("&gt;");
-
                         break;
 
                     default:
                         buffer.append(c);
-
                         break;
                 }
             }
@@ -118,9 +136,12 @@ public final class Strings
     }
 
     /**
+     * Converts a String to multiline HTML markup by replacing newlines with line
+     * break entities (&lt;br&gt;) and multiple occurrences of newline with paragraph break
+     * entities (&lt;p&gt;). 
      * @param s String to transform
-     * @return String with all single occurrences of newline replaced with &lt;BR&gt; and
-     *         all multiple occurrences of newline replaced with &lt;P&gt;.
+     * @return String with all single occurrences of newline replaced with &lt;br&gt; 
+     * and all multiple occurrences of newline replaced with &lt;p&gt;.
      */
     public static String toMultilineMarkup(final String s)
     {
@@ -135,14 +156,12 @@ public final class Strings
             {
                 case '\n':
                     newlineCount++;
-
                     break;
 
                 case '\r':
                     break;
 
                 default:
-
                     if (newlineCount == 1)
                     {
                         buffer.append("<br>");
@@ -154,7 +173,6 @@ public final class Strings
 
                     buffer.append(c);
                     newlineCount = 0;
-
                     break;
             }
         }
@@ -163,9 +181,15 @@ public final class Strings
     }
 
     /**
-     * @param path A path
-     * @param separator The path separator
-     * @return The first component in the path
+     * Gets the last path component of a path using a given separator.  If the 
+     * separator cannot be found, the path itself is returned.  
+     * <p>
+     * For example, lastPathComponent("foo.bar", '.') would return "bar" and 
+     * lastPathComponent("foo", '.') would return "foo".
+     * @param path The path to parse
+     * @param separator The path separator character
+     * @return The last component in the path or path itself if no separator
+     * characters exist.
      */
     public static String lastPathComponent(final String path, final char separator)
     {
@@ -180,9 +204,15 @@ public final class Strings
     }
 
     /**
-     * @param path A path
-     * @param separator The path separator
-     * @return The first component in the path
+     * Gets the first path component of a path using a given separator.  If the 
+     * separator cannot be found, the path itself is returned.
+     * <p>
+     * For example, firstPathComponent("foo.bar", '.') would return "foo" and 
+     * firstPathComponent("foo", '.') would return "foo".
+     * @param path The path to parse
+     * @param separator The path separator character
+     * @return The first component in the path or path itself if no separator
+     * characters exist.
      */
     public static String firstPathComponent(final String path, final char separator)
     {
@@ -197,110 +227,107 @@ public final class Strings
     }
 
     /**
-     * @param path A path
-     * @param separator The path separator
+     * Gets everything after the first path component of a path using a 
+     * given separator.  If the separator cannot be found, an empty String
+     * is returned.
+     * <p>
+     * For example, afterFirstPathComponent("foo.bar.baz", '.') would return 
+     * "bar.baz" and afterFirstPathComponent("foo", '.') would return "".
+     * @param path The path to parse
+     * @param separator The path separator character
      * @return Everything after the first component in the path
      */
     public static String afterFirstPathComponent(final String path, final char separator)
     {
-        final int index = path.indexOf(separator);
-
-        if (index == -1)
-        {
-            return "";
-        }
-
-        return path.substring(index + 1);
+        return afterFirst(path, separator);
     }
 
     /**
-     * @param path The path
-     * @param c The character
-     * @return Everything before the last occurrence of the given character in text. If
-     *         the character cannot be found, the path itself is returned.
+     * Gets everything before the last path component of a path using a 
+     * given separator.  If the separator cannot be found, the path itself
+     * is returned.
+     * <p>
+     * For example, beforeLastPathComponent("foo.bar.baz", '.') would return 
+     * "foo.bar" and beforeLastPathComponent("foo", '.') would return "".
+     * @param path The path to parse
+     * @param separator The path separator character
+     * @return Everything before the last component in the path
      */
-    public static String beforeLastPathComponent(final String path, final char c)
+    public static String beforeLastPathComponent(final String path, final char separator)
     {
-        final int index = path.lastIndexOf(c);
-
-        if (index == -1)
-        {
-            return path;
-        }
-
-        return path.substring(0, index);
+        return beforeLast(path, separator);
     }
 
     /**
-     * @param text The string
+     * @param s The string
      * @param c The character
-     * @return Everything before the last occurrence of the given character in text. If
+     * @return Everything before the last occurrence of the given character in s. If
      *         the character cannot be found, an empty string is returned.
      */
-    public static String beforeLast(final String text, final char c)
+    public static String beforeLast(final String s, final char c)
     {
-        final int index = text.lastIndexOf(c);
+        final int index = s.lastIndexOf(c);
 
         if (index == -1)
         {
             return "";
         }
 
-        return text.substring(0, index);
+        return s.substring(0, index);
     }
 
     /**
-     * @param text The string
+     * @param s The string
      * @param c The character
-     * @return Everything before the first occurrence of the given character in text. If
+     * @return Everything before the first occurrence of the given character in s. If
      *         the character cannot be found, an empty string is returned.
      */
-    public static String beforeFirst(final String text, final char c)
+    public static String beforeFirst(final String s, final char c)
     {
-        final int index = text.indexOf(c);
+        final int index = s.indexOf(c);
 
         if (index == -1)
         {
             return "";
         }
 
-        return text.substring(0, index);
+        return s.substring(0, index);
     }
 
     /**
-     * @param text The string
+     * @param s The string
      * @param c The character
-     * @return Everything after the first occurrence of the given character in text. If
+     * @return Everything after the first occurrence of the given character in s. If
      *         the character cannot be found, an empty string is returned.
      */
-    public static String afterFirst(final String text, final char c)
+    public static String afterFirst(final String s, final char c)
     {
-        final int index = text.indexOf(c);
+        final int index = s.indexOf(c);
 
         if (index == -1)
         {
             return "";
         }
 
-        return text.substring(index + 1);
+        return s.substring(index + 1);
     }
 
     /**
-     * @param text The string
+     * @param s The string
      * @param c The character
-     * @return Everything after the last occurrence of the given character in text. If the
+     * @return Everything after the last occurrence of the given character in s. If the
      *         character cannot be found, an empty string is returned.
      */
-    public static String afterLast(final String text, final char c)
+    public static String afterLast(final String s, final char c)
     {
-        final int index = text.lastIndexOf(c);
+        final int index = s.lastIndexOf(c);
 
         if (index == -1)
         {
             return "";
         }
 
-        return text.substring(index + 1);
+        return s.substring(index + 1);
     }
 
     /**
@@ -323,15 +350,15 @@ public final class Strings
 
     /**
      * Replace all occurrences of one string replaceWith another string
-     * @param text The string
+     * @param s The string
      * @param searchFor The value to search for
      * @param replaceWith The value to searchFor replaceWith
      * @return The resulting string with searchFor replaced with replaceWith
      */
-    public static String replaceAll(final String text, final String searchFor,
+    public static String replaceAll(final String s, final String searchFor,
             final String replaceWith)
     {
-        if (text != null)
+        if (s != null)
         {
             // Go through the string
             final StringBuffer buf = new StringBuffer();
@@ -341,20 +368,20 @@ public final class Strings
             {
                 // Get the next index of the string to searchFor
                 // starting from the position pos
-                final int matchIndex = text.indexOf(searchFor, pos);
+                final int matchIndex = s.indexOf(searchFor, pos);
 
                 // If there's no match
                 if (matchIndex == -1)
                 {
                     // Append rest
-                    buf.append(text.substring(pos));
+                    buf.append(s.substring(pos));
 
                     break;
                 }
                 else
                 {
                     // Found a match. Append up to the match
-                    buf.append(text.substring(pos, matchIndex));
+                    buf.append(s.substring(pos, matchIndex));
 
                     // Move the forward past the searchFor string
                     pos = matchIndex + searchFor.length();
@@ -371,20 +398,20 @@ public final class Strings
     }
 
     /**
-     * @param text String
+     * @param s String
      * @return Boolean value
      * @throws StringValueConversionException
      */
-    public static boolean toBoolean(final String text) throws StringValueConversionException
+    public static boolean toBoolean(final String s) throws StringValueConversionException
     {
-        if (text != null)
+        if (s != null)
         {
-            if (text.equalsIgnoreCase("true"))
+            if (s.equalsIgnoreCase("true"))
             {
                 return true;
             }
 
-            if (text.equalsIgnoreCase("false"))
+            if (s.equalsIgnoreCase("false"))
             {
                 return false;
             }
@@ -396,22 +423,22 @@ public final class Strings
     }
 
     /**
-     * @param text String
+     * @param s String
      * @return Character value
      * @throws StringValueConversionException
      */
-    public static char toChar(final String text) throws StringValueConversionException
+    public static char toChar(final String s) throws StringValueConversionException
     {
-        if (text != null)
+        if (s != null)
         {
-            if (text.length() == 1)
+            if (s.length() == 1)
             {
-                return text.charAt(0);
+                return s.charAt(0);
             }
             else
             {
-                throw new StringValueConversionException(
-                        "Expected single character value rather than '" + text + "'");
+                throw new StringValueConversionException
+                    ("Expected single character, not \"" + s + "\"");
             }
         }
 
@@ -427,11 +454,11 @@ public final class Strings
     {
         if (object instanceof Throwable)
         {
-            return toString((Throwable) object);
+            return toString((Throwable)object);
         }
         else if (object instanceof Node)
         {
-            return ((Node) object).getText();
+            return ((Node)object).getText();
         }
         else if (object == null)
         {
@@ -449,20 +476,18 @@ public final class Strings
      */
     public static boolean isEmpty(final String string)
     {
-        return (string == null) || string.trim().equals("");
+        return string == null || string.trim().equals("");
     }
 
     /**
-     * Converts a throwable to a string
+     * Converts a Throwable to a string
      * @param throwable The throwable
      * @return The string
      */
     public static String toString(final Throwable throwable)
     {
         final StringWriter stringWriter = new StringWriter();
-
         throwable.printStackTrace(new PrintWriter(stringWriter));
-
         return stringWriter.toString().replaceAll("\t", "    ");
     }
 
