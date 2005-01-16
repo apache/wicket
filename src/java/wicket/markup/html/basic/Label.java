e9734c55906dcf0149e157da931371a2509a737f
Jonathan Locke <jonl@apache.org>
2005-01-16 20:34:01 +0000

    javadoc
    git-svn-id: https://svn.apache.org/repos/asf/incubator/wicket/trunk/wicket@455388 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/wicket/markup/html/basic/Label.java
+++ src/java/wicket/markup/html/basic/Label.java
@@ -1,20 +1,19 @@
 /*
- * $Id$
- * $Revision$
+ * $Id$ $Revision$
  * $Date$
- *
- * ====================================================================
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *  http://www.apache.org/licenses/LICENSE-2.0
- *
+ * 
+ * ==================================================================== Licensed
+ * under the Apache License, Version 2.0 (the "License"); you may not use this
+ * file except in compliance with the License. You may obtain a copy of the
+ * License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
  */
 package wicket.markup.html.basic;
 
@@ -26,23 +25,30 @@ import wicket.markup.html.HtmlComponent;
 import wicket.model.IModel;
 
 /**
- * A Label component must be attached to a SPAN tag, and replaces its body with the model
- * object.
+ * A Label component replaces its body with the String version of its model
+ * object returned by getModelObjectAsString().
  * <p>
- * Exactly what is displayed as the body, depends on the model. The simples case is a
- * static string, that can be constructed like:
+ * Exactly what is displayed as the body, depends on the model. The simplest
+ * case is a Label with a static String model, which can be constructed like:
  * 
  * <pre>
  * add(new Label(&quot;myLabel&quot;, &quot;the string to display&quot;))
  * </pre>
  * 
- * A dynamic body could for instance be created like: Using class:
+ * A Label with a dynamic model could be created like:
  * 
  * <pre>
  * 
+ *     add(new Label(&quot;myLabel&quot;, person, &quot;name&quot;);
+ *  
+ * </pre>
+ * 
+ * This component will replace the body of the tag it is attached to with the
+ * 'name' property of the given person object, where person might look like:
+ * 
+ * <pre>
  * public class Person
  * {
- * 
  *     private String name;
  * 
  *     public String getName()
@@ -54,38 +60,26 @@ import wicket.model.IModel;
  *     {
  *         this.name = name;
  *     }
- * 
  * }
  * </pre>
  * 
- * with label:
- * 
- * <pre>
- * 
- * 
- * 
- *    add(new Label(&quot;myLabel&quot;, person, &quot;name&quot;);
- * 
- * 
- *  
- * </pre>
- * 
- * will replace the span body with the 'name' property of the given person object.
- * </p>
  * @author Jonathan Locke
  */
 public class Label extends HtmlComponent
-{ // TODO finalize javadoc
+{
     /** Serial Version ID */
-	private static final long serialVersionUID = -2180588252471379004L;
+    private static final long serialVersionUID = -2180588252471379004L;
 
-	/**
-     * Constructor that uses the provided {@link IModel} as its model. All components have
-     * names. A component's name cannot be null.
-     * @param name The non-null name of this component
-     * @param model the model
-     * @throws wicket.RenderException Thrown if the component has
-     * been given a null name.
+    /**
+     * Constructor that uses the provided {@link IModel}as its model. All
+     * components have names. A component's name cannot be null.
+     * 
+     * @param name
+     *            The non-null name of this component
+     * @param model
+     *            the model
+     * @throws wicket.RenderException
+     *             Thrown if the component has been given a null name.
      */
     public Label(String name, IModel model)
     {
@@ -93,21 +87,26 @@ public class Label extends HtmlComponent
     }
 
     /**
-     * Constructor that uses the provided instance of {@link IModel} as a dynamic model.
-     * This model will be wrapped in an instance of {@link wicket.model.PropertyModel}
-     * using the provided expression. Thus, using this constructor is a short-hand for:
+     * Constructor that uses the provided instance of {@link IModel}as a
+     * dynamic model. This model will be wrapped in an instance of
+     * {@link wicket.model.PropertyModel}using the provided expression. Thus,
+     * using this constructor is a short-hand for:
      * 
      * <pre>
      * new MyComponent(name, new PropertyModel(myIModel, expression));
      * </pre>
      * 
      * All components have names. A component's name cannot be null.
-     * @param name The non-null name of this component
-     * @param model the instance of {@link IModel}from which the model object will be
-     *            used as the subject for the given expression
-     * @param expression the OGNL expression that works on the given object
-     * @throws wicket.RenderException Thrown if the component has
-     * been given a null name.
+     * 
+     * @param name
+     *            The non-null name of this component
+     * @param model
+     *            The instance of {@link IModel}from which the model object
+     *            will be used as the subject for the given expression
+     * @param expression
+     *            The OGNL expression that works on the given object
+     * @throws wicket.RenderException
+     *             Thrown if the component has been given a null name.
      */
     public Label(String name, IModel model, String expression)
     {
@@ -115,13 +114,16 @@ public class Label extends HtmlComponent
     }
 
     /**
-     * Constructor that uses the provided object as a simple model. This object will be
-     * wrapped in an instance of {@link wicket.model.Model}. All components have
-     * names. A component's name cannot be null.
-     * @param name The non-null name of this component
-     * @param object the object that will be used as a simple model
-     * @throws wicket.RenderException Thrown if the component has
-     * been given a null name.
+     * Constructor that uses the provided object as a simple model. This object
+     * will be wrapped in an instance of {@link wicket.model.Model}. All
+     * components have names. A component's name cannot be null.
+     * 
+     * @param name
+     *            The non-null name of this component
+     * @param object
+     *            the object that will be used as a simple model
+     * @throws wicket.RenderException
+     *             Thrown if the component has been given a null name.
      */
     public Label(String name, Serializable object)
     {
@@ -129,36 +131,28 @@ public class Label extends HtmlComponent
     }
 
     /**
-     * Constructor that uses the provided object as a dynamic model. This object will be
-     * wrapped in an instance of {@link wicket.model.Model} that will be wrapped
-     * in an instance of {@link wicket.model.PropertyModel} using the provided
-     * expression. Thus, using this constructor is a short-hand for:
+     * Constructor that uses the provided object as a dynamic model. This object
+     * will be wrapped in an instance of {@link wicket.model.Model}that will be
+     * wrapped in an instance of {@link wicket.model.PropertyModel}using the
+     * provided expression. Thus, using this constructor is a short-hand for:
      * 
      * <pre>
      * new MyComponent(name, new PropertyModel(new Model(object), expression));
      * </pre>
      * 
-     * All components have names. A component's name cannot be null.
-     * @param name The non-null name of this component
-     * @param object the object that will be used as the subject for the given expression
-     * @param expression the OGNL expression that works on the given object
-     * @throws wicket.RenderException Thrown if the component has been
-     * given a null name.
+     * @param name
+     *            The non-null name of this component
+     * @param object
+     *            The object that will be used as the subject for the given
+     *            expression
+     * @param expression
+     *            The OGNL expression that works on the given object
+     * @throws wicket.RenderException
+     *             Thrown if the component has been given a null name.
      */
     public Label(String name, Serializable object, String expression)
     {
         super(name, object, expression);
-    }
-
-    /**
-     * Allows modification of component tag.
-     * @param tag The tag to modify
-     * @see wicket.Component#handleComponentTag(wicket.markup.ComponentTag)
-     */
-    protected final void handleComponentTag(final ComponentTag tag)
-    {
-        checkTag(tag, "span");
-        super.handleComponentTag(tag);
     }
 
     /**
@@ -171,5 +165,3 @@ public class Label extends HtmlComponent
         replaceBody(markupStream, openTag, getModelObjectAsString());
     }
 }
-
-
