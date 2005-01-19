8b2618883c051407cab2bc41b12924863454200c
Jonathan Locke <jonl@apache.org>
2005-01-19 00:43:06 +0000

    Formatting
    git-svn-id: https://svn.apache.org/repos/asf/incubator/wicket/trunk/wicket@455423 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/wicket/ApplicationPages.java
+++ src/java/wicket/ApplicationPages.java
@@ -1,30 +1,29 @@
 /*
  * $Id$
- * $Revision$
- * $Date$
- *
- * ====================================================================
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *  http://www.apache.org/licenses/LICENSE-2.0
- *
+ * $Revision$ $Date$
+ * 
+ * ==================================================================== Licensed
+ * under the Apache License, Version 2.0 (the "License"); you may not use this
+ * file except in compliance with the License. You may obtain a copy of the
+ * License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
  */
 package wicket;
 
 /**
- * Holder for Wicket pages with special meanings. 
- *
+ * Holder for Wicket pages with special meanings.
+ * 
  * @author Jonathan Locke
  */
 public class ApplicationPages
-{    
+{
     /** Home page class */
     private Class homePageClass;
 
@@ -36,9 +35,10 @@ public class ApplicationPages
 
     /** Error page to show when stale markup renderings are encountered */
     private Class staleDataErrorPageClass;
-    
+
     /**
      * Gets home page class.
+     * 
      * @return Returns the homePage.
      * @see ApplicationPages#setHomePage(Class)
      */
@@ -48,12 +48,13 @@ public class ApplicationPages
         if (homePageClass == null)
         {
             // give up with an exception
-            throw new IllegalStateException("No home page was specified in application settings");
+            throw new IllegalStateException(
+                    "No home page was specified in application settings");
         }
-        
+
         return homePageClass;
     }
-    
+
     /**
      * Gets internal error page class.
      * 
@@ -78,6 +79,7 @@ public class ApplicationPages
 
     /**
      * Gets the stale data error page class.
+     * 
      * @return Returns the staleDataErrorPage.
      * @see ApplicationPages#setStaleDataErrorPage(Class)
      */
@@ -87,29 +89,35 @@ public class ApplicationPages
     }
 
     /**
-     * Sets home page class. The class must be external / bookmarkable and therefore must
-     * extend Page.
-     * @param homePage The home page class
+     * Sets home page class. The class must be external / bookmarkable and
+     * therefore must extend Page.
+     * 
+     * @param homePage
+     *            The home page class
      * @return This
      */
     public final ApplicationPages setHomePage(final Class homePage)
     {
         this.homePageClass = homePage;
         return this;
-    }    
+    }
 
     /**
-     * Sets internal error page class. The class must be external / bookmarkable and
-     * therefore must extend Page and must be able to construct from PageParameters.
-     * @param internalErrorPage The internalErrorPage to set.
+     * Sets internal error page class. The class must be external / bookmarkable
+     * and therefore must extend Page and must be able to construct from
+     * PageParameters.
+     * 
+     * @param internalErrorPage
+     *            The internalErrorPage to set.
      * @return This
      */
-    public final ApplicationPages setInternalErrorPage(final Class internalErrorPage)
+    public final ApplicationPages setInternalErrorPage(
+            final Class internalErrorPage)
     {
         this.internalErrorPageClass = internalErrorPage;
         return this;
     }
-    
+
     /**
      * Sets the page expired page class. The class must be external /
      * bookmarkable and therefore must extend Page and must be able to construct
@@ -127,12 +135,16 @@ public class ApplicationPages
     }
 
     /**
-     * Sets the stale data error page class. The class must be external / bookmarkable and
-     * therefore must extend Page and must be able to construct from PageParameters.
-     * @param staleDataErrorPage The staleDataErrorPage to set
+     * Sets the stale data error page class. The class must be external /
+     * bookmarkable and therefore must extend Page and must be able to construct
+     * from PageParameters.
+     * 
+     * @param staleDataErrorPage
+     *            The staleDataErrorPage to set
      * @return This
      */
-    public final ApplicationPages setStaleDataErrorPage(final Class staleDataErrorPage)
+    public final ApplicationPages setStaleDataErrorPage(
+            final Class staleDataErrorPage)
     {
         this.staleDataErrorPageClass = staleDataErrorPage;
         return this;
