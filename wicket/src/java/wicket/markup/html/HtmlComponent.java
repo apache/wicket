d968626875b3d4f11454ac77ade2ab00634ee100
Jonathan Locke <jonl@apache.org>
2005-01-20 22:21:23 +0000

    javadoc
    git-svn-id: https://svn.apache.org/repos/asf/incubator/wicket/trunk@455494 13f79535-47bb-0310-9956-ffa450edef68

--- wicket/src/java/wicket/markup/html/HtmlComponent.java
+++ wicket/src/java/wicket/markup/html/HtmlComponent.java
@@ -1,20 +1,19 @@
 /*
  * $Id$
- * $Revision$
- * $Date$
- *
- * ====================================================================
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *  http://www.apache.org/licenses/LICENSE-2.0
- *
+ * $Revision$ $Date$
+ * 
+ * ==================================================================== Licensed
+ * under the Apache License, Version 2.0 (the "License"); you may not use this
+ * file except in compliance with the License. You may obtain a copy of the
+ * License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
  */
 package wicket.markup.html;
 
@@ -24,95 +23,60 @@ import wicket.Component;
 import wicket.model.IModel;
 
 /**
- * Base class for simple HTML components which do not hold nested components. If you need
- * to support nested components, see HtmlContainer.
- *
+ * Base class for simple HTML components which do not hold nested components. If
+ * you need to support nested components, see HtmlContainer or use Panel if the
+ * component will have its own associated markup.
+ * 
  * @see wicket.markup.html.HtmlContainer
  * @author Jonathan Locke
  */
 public abstract class HtmlComponent extends Component
 {
-    /**
-     * Constructor.
-     * @param name Component name
-     */
-    public HtmlComponent(final String name)
-    {
-        super(name);
-    }
+	/**
+	 * @see Component#Component(String)
+	 */
+	public HtmlComponent(final String name)
+	{
+		super(name);
+	}
 
-    /**
-     * Constructor that uses the provided {@link IModel}as its model. All components have
-     * names. A component's name cannot be null.
-     * @param name The non-null name of this component
-     * @param model the model
-     * @throws wicket.WicketRuntimeException Thrown if the component has been given a null name.
-     */
-    public HtmlComponent(String name, IModel model)
-    {
-        super(name, model);
-    }
+	/**
+	 * @see Component#Component(String, IModel)
+	 */
+	public HtmlComponent(final String name, final IModel model)
+	{
+		super(name, model);
+	}
 
-    /**
-     * Constructor that uses the provided instance of {@link IModel}as a dynamic model.
-     * This model will be wrapped in an instance of {@link wicket.model.PropertyModel}
-     * using the provided expression. Thus, using this constructor is a short-hand for:
-     * 
-     * <pre>
-     * new MyComponent(name, new PropertyModel(myIModel, expression));
-     * </pre>
-     * 
-     * All components have names. A component's name cannot be null.
-     * @param name The non-null name of this component
-     * @param model the instance of {@link IModel}from which the model object will be
-     *            used as the subject for the given expression
-     * @param expression the OGNL expression that works on the given object
-     * @throws wicket.WicketRuntimeException Thrown if the component has been given a null name.
-     */
-    public HtmlComponent(String name, IModel model, String expression)
-    {
-        super(name, model, expression);
-    }
+	/**
+	 * @see Component#Component(String, IModel, String)
+	 */
+	public HtmlComponent(final String name, final IModel model, final String expression)
+	{
+		super(name, model, expression);
+	}
 
-    /**
-     * Constructor that uses the provided object as a simple model. This object will be
-     * wrapped in an instance of {@link wicket.model.Model}. All components have names. A component's
-     * name cannot be null.
-     * @param name The non-null name of this component
-     * @param object the object that will be used as a simple model
-     * @throws wicket.WicketRuntimeException Thrown if the component has been given a null name.
-     */
-    public HtmlComponent(String name, Serializable object)
-    {
-        super(name, object);
-    }
+	/**
+	 * @see Component#Component(String, Serializable)
+	 */
+	public HtmlComponent(final String name, final Serializable object)
+	{
+		super(name, object);
+	}
 
-    /**
-     * Constructor that uses the provided object as a dynamic model. This object will be
-     * wrapped in an instance of {@link wicket.model.Model}that will be wrapped in an instance of
-     * {@link wicket.model.PropertyModel}using the provided expression. Thus, using this constructor
-     * is a short-hand for:
-     * 
-     * <pre>
-     * new MyComponent(name, new PropertyModel(new Model(object), expression));
-     * </pre>
-     * 
-     * All components have names. A component's name cannot be null.
-     * @param name The non-null name of this component
-     * @param object the object that will be used as the subject for the given expression
-     * @param expression the OGNL expression that works on the given object
-     * @throws wicket.WicketRuntimeException Thrown if the component has been given a null name.
-     */
-    public HtmlComponent(String name, Serializable object, String expression)
-    {
-        super(name, object, expression);
-    }
+	/**
+	 * @see Component#Component(String, Serializable, String)
+	 */
+	public HtmlComponent(final String name, final Serializable object, final String expression)
+	{
+		super(name, object, expression);
+	}
 
-    /**
-     * Renders this component.
-     */
-    protected void handleRender()
-    {
-        renderComponent(findMarkupStream());
-    }
+	/**
+	 * Renders this component.
+	 */
+	protected void handleRender()
+	{
+		renderComponent(findMarkupStream());
+	}
 }
--- wicket/src/java/wicket/markup/html/HtmlPage.java
+++ wicket/src/java/wicket/markup/html/HtmlPage.java
@@ -1,61 +1,64 @@
 /*
- * $Id$
- * $Revision$
- * $Date$
- *
- * ====================================================================
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *  http://www.apache.org/licenses/LICENSE-2.0
- *
+ * $Id$ $Revision:
+ * 1.8 $ $Date$
+ * 
+ * ==================================================================== Licensed
+ * under the Apache License, Version 2.0 (the "License"); you may not use this
+ * file except in compliance with the License. You may obtain a copy of the
+ * License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
  */
 package wicket.markup.html;
 
 import wicket.Page;
 import wicket.markup.html.link.BookmarkablePageLink;
 
-
 /**
- * Base class for HTML pages.
- *
+ * Base class for HTML pages. This subclass of Page simply returns HTML when
+ * asked for its markup type. It also has a method which subclasses can use to
+ * retrieve a bookmarkable link to the application's home page.
+ * 
  * @author Jonathan Locke
  */
 public class HtmlPage extends Page
 {
-    /** Serial Version ID. */
+	/** Serial Version ID. */
 	private static final long serialVersionUID = 3986701406378811908L;
 
 	/**
-     * Constructor.
-     */
-    protected HtmlPage()
-    {
-        super();
-    }
+	 * Constructor.
+	 */
+	protected HtmlPage()
+	{
+		super();
+	}
 
-    /**
-     * Gets the markup type for this component.
-     * @return Markup type for HTML
-     */
-    public final String getMarkupType()
-    {
-        return "html";
-    }
+	/**
+	 * Gets the markup type for this component.
+	 * 
+	 * @return Markup type for HTML
+	 */
+	public final String getMarkupType()
+	{
+		return "html";
+	}
 
-    /**
-     * Creates and returns a bookmarkable link to this application's home page.
-     * @param componentName Name of link
-     * @return Link to home page for this application
-     */
-    protected final BookmarkablePageLink homePageLink(final String componentName)
-    {
-        return new BookmarkablePageLink(componentName, getApplicationPages().getHomePage());
-    }
+	/**
+	 * Creates and returns a bookmarkable link to this application's home page.
+	 * 
+	 * @param componentName
+	 *            Name of link
+	 * @return Link to home page for this application
+	 */
+	protected final BookmarkablePageLink homePageLink(final String componentName)
+	{
+		return new BookmarkablePageLink(componentName, getApplicationPages().getHomePage());
+	}
 }
