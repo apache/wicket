83474c5360094cfeaa9c88856f3db5db06ac9c45
Jonathan Locke <jonl@apache.org>
2005-01-20 21:10:18 +0000

    javadoc
    git-svn-id: https://svn.apache.org/repos/asf/incubator/wicket/trunk/wicket@455488 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/wicket/markup/html/basic/Label.java
+++ src/java/wicket/markup/html/basic/Label.java
@@ -29,37 +29,37 @@ import wicket.model.IModel;
  * object returned by getModelObjectAsString().
  * <p>
  * Exactly what is displayed as the body, depends on the model. The simplest
- * case is a Label with a static String model, which can be constructed like:
+ * case is a Label with a static String model, which can be constructed like
+ * this:
  * 
  * <pre>
  * add(new Label(&quot;myLabel&quot;, &quot;the string to display&quot;))
  * </pre>
  * 
- * A Label with a dynamic model could be created like:
+ * A Label with a dynamic model can be created like this:
  * 
  * <pre>
- * 
- *     add(new Label(&quot;myLabel&quot;, person, &quot;name&quot;);
- *  
+ *      add(new Label(&quot;myLabel&quot;, person, &quot;name&quot;);
  * </pre>
  * 
- * This component will replace the body of the tag it is attached to with the
- * 'name' property of the given person object, where person might look like:
+ * In this case, the Label component will replace the body of the tag it is 
+ * attached to with the 'name' property of the given Person object, where 
+ * Person might look like:
  * 
  * <pre>
  * public class Person
  * {
- *     private String name;
+ * 	   private String name;
  * 
- *     public String getName()
+ * 	   public String getName()
  *     {
- *         return name;
- *     }
+ * 	       return name;
+ * 	   }
  * 
  *     public void setName(String name)
- *     {
- *         this.name = name;
- *     }
+ * 	   {
+ * 	       this.name = name;
+ * 	   }
  * }
  * </pre>
  * 
@@ -67,101 +67,47 @@ import wicket.model.IModel;
  */
 public class Label extends HtmlComponent
 {
-    /** Serial Version ID */
-    private static final long serialVersionUID = -2180588252471379004L;
+	/** Serial Version ID */
+	private static final long serialVersionUID = -2180588252471379004L;
 
-    /**
-     * Constructor that uses the provided {@link IModel}as its model. All
-     * components have names. A component's name cannot be null.
-     * 
-     * @param name
-     *            The non-null name of this component
-     * @param model
-     *            the model
-     * @throws wicket.WicketRuntimeException
-     *             Thrown if the component has been given a null name.
-     */
-    public Label(String name, IModel model)
-    {
-        super(name, model);
-    }
+	/**
+     * @see wicket.Component#Component(String, IModel)
+	 */
+	public Label(String name, IModel model)
+	{
+		super(name, model);
+	}
 
-    /**
-     * Constructor that uses the provided instance of {@link IModel}as a
-     * dynamic model. This model will be wrapped in an instance of
-     * {@link wicket.model.PropertyModel}using the provided expression. Thus,
-     * using this constructor is a short-hand for:
-     * 
-     * <pre>
-     * new MyComponent(name, new PropertyModel(myIModel, expression));
-     * </pre>
-     * 
-     * All components have names. A component's name cannot be null.
-     * 
-     * @param name
-     *            The non-null name of this component
-     * @param model
-     *            The instance of {@link IModel}from which the model object
-     *            will be used as the subject for the given expression
-     * @param expression
-     *            The OGNL expression that works on the given object
-     * @throws wicket.WicketRuntimeException
-     *             Thrown if the component has been given a null name.
-     */
-    public Label(String name, IModel model, String expression)
-    {
-        super(name, model, expression);
-    }
+	/**
+     * @see wicket.Component#Component(String, IModel, String)
+	 */
+	public Label(String name, IModel model, String expression)
+	{
+		super(name, model, expression);
+	}
 
-    /**
-     * Constructor that uses the provided object as a simple model. This object
-     * will be wrapped in an instance of {@link wicket.model.Model}. All
-     * components have names. A component's name cannot be null.
-     * 
-     * @param name
-     *            The non-null name of this component
-     * @param object
-     *            the object that will be used as a simple model
-     * @throws wicket.WicketRuntimeException
-     *             Thrown if the component has been given a null name.
-     */
-    public Label(String name, Serializable object)
-    {
-        super(name, object);
-    }
+	/**
+     * @see wicket.Component#Component(String, Serializable)
+	 */
+	public Label(String name, Serializable object)
+	{
+		super(name, object);
+	}
 
-    /**
-     * Constructor that uses the provided object as a dynamic model. This object
-     * will be wrapped in an instance of {@link wicket.model.Model}that will be
-     * wrapped in an instance of {@link wicket.model.PropertyModel}using the
-     * provided expression. Thus, using this constructor is a short-hand for:
-     * 
-     * <pre>
-     * new MyComponent(name, new PropertyModel(new Model(object), expression));
-     * </pre>
-     * 
-     * @param name
-     *            The non-null name of this component
-     * @param object
-     *            The object that will be used as the subject for the given
-     *            expression
-     * @param expression
-     *            The OGNL expression that works on the given object
-     * @throws wicket.WicketRuntimeException
-     *             Thrown if the component has been given a null name.
-     */
-    public Label(String name, Serializable object, String expression)
-    {
-        super(name, object, expression);
-    }
+	/**
+     * @see wicket.Component#Component(String, Serializable, String)
+	 */
+	public Label(String name, Serializable object, String expression)
+	{
+		super(name, object, expression);
+	}
 
-    /**
-     * @see wicket.Component#handleBody(wicket.markup.MarkupStream,
-     *      wicket.markup.ComponentTag)
-     */
-    protected void handleBody(final MarkupStream markupStream,
-            final ComponentTag openTag)
-    {
-        replaceBody(markupStream, openTag, getModelObjectAsString());
-    }
+	/**
+	 * @see wicket.Component#handleBody(wicket.markup.MarkupStream,
+	 *      wicket.markup.ComponentTag)
+	 */
+	protected void handleBody(final MarkupStream markupStream, final ComponentTag openTag)
+	{
+		replaceBody(markupStream, openTag, getModelObjectAsString());
+	}
 }
