ea94b0c6d770115cc5b4247513a7404f65cebc27
Martijn Dashorst <dashorst@apache.org>
2005-01-20 23:23:26 +0000

    Removed TODO tags.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/wicket/trunk@455504 13f79535-47bb-0310-9956-ffa450edef68

--- wicket-quickstart/src/main/java/wicket/quickstart/Index.java
+++ wicket-quickstart/src/main/java/wicket/quickstart/Index.java
@@ -1,9 +1,3 @@
-/*
- * Created on Dec 2, 2004
- *
- * TODO To change the template for this generated file go to
- * Window - Preferences - Java - Code Style - Code Templates
- */
 package wicket.quickstart;
 
 import wicket.PageParameters;
@@ -12,6 +6,11 @@ import wicket.markup.html.HtmlPage;
 /**
  */
 public class Index extends HtmlPage {
+	/**
+	 * <code>serialVersionUID</code>
+	 */
+	private static final long serialVersionUID = 1L;
+
 	/**
 	 * Constructor that is invoked when page is invoked without a session.
 	 * 
--- wicket-quickstart/src/main/java/wicket/quickstart/QuickStart.java
+++ wicket-quickstart/src/main/java/wicket/quickstart/QuickStart.java
@@ -1,9 +1,3 @@
-/*
- * Created on Dec 2, 2004
- * 
- * TODO To change the template for this generated file go to Window -
- * Preferences - Java - Code Style - Code Templates
- */
 package wicket.quickstart;
 
 import java.net.URL;
