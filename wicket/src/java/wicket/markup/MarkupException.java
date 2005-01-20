602cc0e45dacd6c9fbd72e361b12479bac80c351
Jonathan Locke <jonl@apache.org>
2005-01-20 23:04:21 +0000

    javadoc
    git-svn-id: https://svn.apache.org/repos/asf/incubator/wicket/trunk@455501 13f79535-47bb-0310-9956-ffa450edef68

--- wicket/src/java/wicket/markup/MarkupException.java
+++ wicket/src/java/wicket/markup/MarkupException.java
@@ -1,86 +1,93 @@
 /*
  * $Id$
- * $Revision$
- * $Date$
- *
- * ====================================================================
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *  http://www.apache.org/licenses/LICENSE-2.0
- *
+ * $Revision$ $Date$
+ * 
+ * ==================================================================== Licensed
+ * under the Apache License, Version 2.0 (the "License"); you may not use this
+ * file except in compliance with the License. You may obtain a copy of the
+ * License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
  */
 package wicket.markup;
 
 import wicket.WicketRuntimeException;
 import wicket.util.resource.Resource;
 
-
 /**
- * Exception thrown when markup parsing fails
+ * Runtime exception that is thrown when markup parsing fails.
+ * 
  * @author Jonathan Locke
  */
 public final class MarkupException extends WicketRuntimeException
-{ // TODO finalize javadoc
-    /** Serial Version ID */
+{
+	/** Serial Version ID */
 	private static final long serialVersionUID = 8431706931407810523L;
-	
+
+	/** The markup stream that was being parsed when the exception was thrown */
 	private final MarkupStream markupStream;
 
-    /**
-     * Constructor
-     * @param message The problem description
-     */
-    public MarkupException(final String message)
-    {
-        super(message);
-        markupStream = null;
-    }
+	/**
+	 * Constructor
+	 * 
+	 * @param message
+	 *            The problem description
+	 */
+	public MarkupException(final String message)
+	{
+		super(message);
+		markupStream = null;
+	}
 
-    /**
-     * @param resource The markup resource where this exception occurred
-     * @param message The message
-     */
-    public MarkupException(final Resource resource, final String message)
-    {
-        super(resource.toString() + ": " + message);
-        markupStream = null;
-    }
+	/**
+	 * @param resource
+	 *            The markup resource where this exception occurred
+	 * @param message
+	 *            The message
+	 */
+	public MarkupException(final Resource resource, final String message)
+	{
+		super(resource.toString() + ": " + message);
+		markupStream = null;
+	}
 
-    /**
-     * @param resource The markup where this exception occurred
-     * @param message The message
-     * @param cause The causing exception
-     */
-    public MarkupException(final Resource resource, final String message, final Throwable cause)
-    {
-        super(resource.toString() + ": " + message, cause);
-        markupStream = null;
-    }
+	/**
+	 * @param resource
+	 *            The markup where this exception occurred
+	 * @param message
+	 *            The message
+	 * @param cause
+	 *            The causing exception
+	 */
+	public MarkupException(final Resource resource, final String message, final Throwable cause)
+	{
+		super(resource.toString() + ": " + message, cause);
+		markupStream = null;
+	}
 
-    /**
-     * @param markupStream The markup stream where this exception occurred
-     * @param message The message
-     */
-    public MarkupException(final MarkupStream markupStream, final String message)
-    {
-        super(message + "\n" + markupStream.toString());
-        this.markupStream = markupStream;
-    }
+	/**
+	 * @param markupStream
+	 *            The markup stream where this exception occurred
+	 * @param message
+	 *            The message
+	 */
+	public MarkupException(final MarkupStream markupStream, final String message)
+	{
+		super(message + "\n" + markupStream.toString());
+		this.markupStream = markupStream;
+	}
 
-    /**
-     * @return Returns the markupStream.
-     */
-    public MarkupStream getMarkupStream()
-    {
-        return markupStream;
-    }
+	/**
+	 * @return Returns the MarkupStream.
+	 */
+	public MarkupStream getMarkupStream()
+	{
+		return markupStream;
+	}
 }
-
-
