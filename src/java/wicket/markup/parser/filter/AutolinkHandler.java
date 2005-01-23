63ee5d54fcde77edcff8c25cf4e96a032d0e6523
Martijn Dashorst <dashorst@apache.org>
2005-01-23 12:28:40 +0000

    Removed compilation problem.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/wicket/trunk/wicket@455530 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/wicket/markup/parser/filter/AutolinkHandler.java
+++ src/java/wicket/markup/parser/filter/AutolinkHandler.java
@@ -28,137 +28,140 @@ import wicket.markup.parser.IMarkupFilter;
 
 /**
  * This is a markup inline filter. It identifies xml tags which include a href
- * attribute and which are not Wicket specific components and flags these
- * tags (ComponentTag) as autolink enabled. A component resolver will later
- * only resolve the href and assign a BookmarkablePageLink to it (automatically).
+ * attribute and which are not Wicket specific components and flags these tags
+ * (ComponentTag) as autolink enabled. A component resolver will later only
+ * resolve the href and assign a BookmarkablePageLink to it (automatically).
  * <p>
- * An application settings is used as default value, which might be modified
- * for specific regions. These regions are identified by &lt;wicket:link&gt; 
- * tags with an optional 'autolink' attribute. The default value for the 
- * attribute is true, thus enabling autolinking. An open-close &lt;wicket:link/&gt
- * tag will change the autolink status until the end of the markup document
- * or the next &lt;wicket:link&gt; tag respectively.
- * &lt;wicket:link&gt; regions may be nested as well.
+ * An application settings is used as default value, which might be modified for
+ * specific regions. These regions are identified by &lt;wicket:link&gt; tags
+ * with an optional 'autolink' attribute. The default value for the attribute is
+ * true, thus enabling autolinking. An open-close &lt;wicket:link/&gt tag will
+ * change the autolink status until the end of the markup document or the next
+ * &lt;wicket:link&gt; tag respectively. &lt;wicket:link&gt; regions may be
+ * nested as well.
  * 
  * @author Juergen Donnerstag
  */
 public class AutolinkHandler implements IMarkupFilter
 {
-    /** The next MarkupFilter in the chain */
-    private final IMarkupFilter parent;
+	/** The next MarkupFilter in the chain */
+	private final IMarkupFilter parent;
 
-    /** Allow to have link regions within link regions */
-    private Stack autolinkStatus;
+	/** Allow to have link regions within link regions */
+	private Stack autolinkStatus;
 
-    /** Current status */
-    private boolean autolinking = true;
+	/** Current status */
+	private boolean autolinking = true;
 
-    /**
-     * Construct.
-     * @param nextInChain The next element in the chain. 
-     */
-    public AutolinkHandler(final IMarkupFilter nextInChain)
-    {
-        parent = nextInChain;
-    }
+	/**
+	 * Construct.
+	 * 
+	 * @param nextInChain
+	 *           The next element in the chain.
+	 */
+	public AutolinkHandler(final IMarkupFilter nextInChain)
+	{
+		parent = nextInChain;
+	}
 
-    /**
-     * @return The next MarkupFilter in the chain
-     */
-    public final IMarkupFilter getParent()
-    {
-        return parent;
-    }
+	/**
+	 * @return The next MarkupFilter in the chain
+	 */
+	public final IMarkupFilter getParent()
+	{
+		return parent;
+	}
 
-    /**
-     * Set the default value for autolinking
-     * @param enable if true, autolinks are enabled
-     */
-    public void setAutomaticLinking(final boolean enable)
-    {
-        this.autolinking = enable;
-    }
-    
-    /**
-     * Get the next MarkupElement from the parent MarkupFilter and handles it
-     * if the specific filter criteria are met. Depending on the filter, it 
-     * may return the MarkupElement unchanged, modified or it remove by
-     * asking the parent handler for the next tag.
-     *  
-     * @see wicket.util.xml.IMarkupFilter#nextTag()
-     * @return Return the next eligible MarkupElement
-     */
-    public MarkupElement nextTag() throws ParseException
-    {
-        // Get next tag. Null, if no more tag available
-        final ComponentTag tag = (ComponentTag) parent.nextTag();
-        if (tag == null)
-        {
-            return tag;
-        }
+	/**
+	 * Set the default value for autolinking
+	 * 
+	 * @param enable
+	 *           if true, autolinks are enabled
+	 */
+	public void setAutomaticLinking(final boolean enable)
+	{
+		this.autolinking = enable;
+	}
 
-        // Only xml tags not already identified as Wicket components will be considered
-        // for autolinking. This is because it is assumed that Wicket components
-        // like images, or all other kind of Wicket Links will handle it
-        // themselves.
-        final String href = tag.getAttributes().getString("href");
-        if ((autolinking == true) && (tag.getComponentName() == null) 
-                && (href != null) && (href.endsWith(".html") || href.contains(".html?")) 
-                && !href.startsWith("/") && (href.indexOf(":") == -1))
-        {
-            // Mark it as autolink enabled
-            tag.enableAutolink(true);
+	/**
+	 * Get the next MarkupElement from the parent MarkupFilter and handles it if
+	 * the specific filter criteria are met. Depending on the filter, it may
+	 * return the MarkupElement unchanged, modified or it remove by asking the
+	 * parent handler for the next tag.
+	 * 
+	 * @see wicket.util.xml.IMarkupFilter#nextTag()
+	 * @return Return the next eligible MarkupElement
+	 */
+	public MarkupElement nextTag() throws ParseException
+	{
+		// Get next tag. Null, if no more tag available
+		final ComponentTag tag = (ComponentTag)parent.nextTag();
+		if (tag == null)
+		{
+			return tag;
+		}
 
-            // Just a dummy name. The ComponentTag will not be forwarded.
-            tag.setComponentName("_autolink_");
-            return tag;
-        }
-        
-        // For all <wicket:link ..> tags which probably change the 
-        // current autolink status.
-        if (tag instanceof ComponentWicketTag)
-        {
-            final ComponentWicketTag wtag = (ComponentWicketTag) tag;
-            if (wtag.isLinkTag())
-            {
-                // Beginning of the region
-    	        if (tag.isOpen() || tag.isOpenClose())
-    	        {
-    	            if (tag.isOpen())
-    	            {
-	    	            if (autolinkStatus == null)
-	    	            {
-	    	                autolinkStatus = new Stack();
-	    	            }
-	    	            
-	    		        // remember the current setting to be reset after the region
-	    		        autolinkStatus.push(new Boolean(autolinking));
-    	            }
-    	            
-    		        // html allows to represent true in different ways
-    		        final String autolink = tag.getAttributes().getString("autolink");
-		            if ((autolink == null) 
-		                    || "".equals(autolink) 
-		                    || "true".equalsIgnoreCase(autolink) 
-		                    || "1".equals(autolink))
-		            {
-		                autolinking = true;
-		            }
-		            else 
-		            {
-		                autolinking = false;
-    		        }
-    	        } 
-    	        else if (tag.isClose())
-    	        {
-    	            // restore the autolink setting from before the region
-    	            autolinking = ((Boolean)autolinkStatus.pop()).booleanValue();
-    	        }
-    	        
-    	        return nextTag();
-            }
-        }
-        
-        return tag;
-    }
+		// Only xml tags not already identified as Wicket components will be
+		// considered
+		// for autolinking. This is because it is assumed that Wicket components
+		// like images, or all other kind of Wicket Links will handle it
+		// themselves.
+		final String href = tag.getAttributes().getString("href");
+		if ((autolinking == true) && (tag.getComponentName() == null) && (href != null)
+				&& (href.endsWith(".html") || (href.indexOf(".html?") != -1)) && !href.startsWith("/")
+				&& (href.indexOf(":") == -1))
+		{
+			// Mark it as autolink enabled
+			tag.enableAutolink(true);
+
+			// Just a dummy name. The ComponentTag will not be forwarded.
+			tag.setComponentName("_autolink_");
+			return tag;
+		}
+
+		// For all <wicket:link ..> tags which probably change the
+		// current autolink status.
+		if (tag instanceof ComponentWicketTag)
+		{
+			final ComponentWicketTag wtag = (ComponentWicketTag)tag;
+			if (wtag.isLinkTag())
+			{
+				// Beginning of the region
+				if (tag.isOpen() || tag.isOpenClose())
+				{
+					if (tag.isOpen())
+					{
+						if (autolinkStatus == null)
+						{
+							autolinkStatus = new Stack();
+						}
+
+						// remember the current setting to be reset after the region
+						autolinkStatus.push(new Boolean(autolinking));
+					}
+
+					// html allows to represent true in different ways
+					final String autolink = tag.getAttributes().getString("autolink");
+					if ((autolink == null) || "".equals(autolink) || "true".equalsIgnoreCase(autolink)
+							|| "1".equals(autolink))
+					{
+						autolinking = true;
+					}
+					else
+					{
+						autolinking = false;
+					}
+				}
+				else if (tag.isClose())
+				{
+					// restore the autolink setting from before the region
+					autolinking = ((Boolean)autolinkStatus.pop()).booleanValue();
+				}
+
+				return nextTag();
+			}
+		}
+
+		return tag;
+	}
 }
