d42c608b82fbcc3c3269de26cf5df4cbee828da8
Juegen Donnerstag <jdonnerstag@apache.org>
2005-01-23 11:32:28 +0000

    rename method
    git-svn-id: https://svn.apache.org/repos/asf/incubator/wicket/trunk@455529 13f79535-47bb-0310-9956-ffa450edef68

--- wicket/src/java/wicket/Container.java
+++ wicket/src/java/wicket/Container.java
@@ -748,7 +748,7 @@ public abstract class Container extends Component
 			final Resource markupResource) throws ParseException, IOException,
 			ResourceNotFoundException
 	{
-		final Markup markup = application.getMarkupParser().read(markupResource);
+		final Markup markup = application.getMarkupParser().readAndParse(markupResource);
 		markupCache.put(key, markup);
 		return markup;
 	}
