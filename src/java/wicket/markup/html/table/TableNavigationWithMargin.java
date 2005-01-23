74a74479ee09048d77b1204a4144c7b697fe31de
Juegen Donnerstag <jdonnerstag@apache.org>
2005-01-23 18:47:10 +0000

    javadoc
    git-svn-id: https://svn.apache.org/repos/asf/incubator/wicket/trunk/wicket@455544 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/wicket/markup/html/table/TableNavigationWithMargin.java
+++ src/java/wicket/markup/html/table/TableNavigationWithMargin.java
@@ -19,22 +19,31 @@
 package wicket.markup.html.table;
 
 /**
- *
+ * Assuming a table has 1000 entries and not more than 10 lines shall be
+ * printed per page. A standard navigation bar would have 100 entries.
+ * Because this is not feasible. TableNavigationWithMargin provides a 
+ * pageable navigation bar displaying only some page links.<p>
+ * The page links displayed are automatically adjusted based on 
+ * the number of page links to be displayed and a margin. The margin
+ * makes sure that the page link pointing to the current page is not
+ * at the left or right end of the page links currently printed and
+ * thus provinding a better user experience.
+ *  
  * @author Juergen Donnerstag
  */
 public class TableNavigationWithMargin extends TableNavigation
-{ // TODO finalize javadoc
-    /** Number of navigation cells left and/or right to keep the current
-      * cell somewhere near the middle.
-      */
+{
+    /** Number of navigation cells on the left and/or right 
+     * to keep the current cell somewhere near the middle.
+     */
     private int margin;
 
-    /** Default separator between page numbers */
+    /** Default separator between page numbers. Null: no separator */
     private String separator = null;
 
     /**
-     * Constructor.
-     * @param componentName The name of this component
+     * Construct.
+     * @param componentName The name of the component
      * @param table The table to navigate
      */
     public TableNavigationWithMargin(final String componentName, final Table table)
@@ -43,25 +52,30 @@ public class TableNavigationWithMargin extends TableNavigation
     }
 
     /**
-     * TableNavigation itself (not table) may have pages.
+     * Render the component
+     * 
+     * @see wicket.markup.html.table.ListView#handleRender()
      */
     protected void handleRender()
     {
-        // Set window based on table's current page
+        // TableNavigation itself (as well as the table) may have pages.
+
+        // The index of the first page link depends on the table's page
+        // current printed. 
         this.setStartIndex(); 
         
-        // default 
+        // continue with default 
         super.handleRender();
     }
 
     /**
-     * Render the cell. Add the separator if not last cell
-     * @param listItem
-     * @param lastItem
+     * Render the page link. Add the separator if not the last page link 
+     * @param listItem The current page link to render
+     * @param lastItem True, if last page link to render
      */
     protected void renderItem(final ListItem listItem, final boolean lastItem)
     {
-        // default
+        // call default implementation
         super.renderItem(listItem, lastItem);
         
         // add separator if not last page
@@ -72,16 +86,22 @@ public class TableNavigationWithMargin extends TableNavigation
     }
 
     /**
-     * Gets the first cell in the current page of this table. If the last page
-     * would only be partially listed, then recalculate firstListItem to show
-     * pageSizeInListItems cells up to the last one.
+     * Get the first page link to render. Adjust the first page link 
+     * based on the current table page displayed. 
      */
     protected void setStartIndex()
     {
+        // Which startIndex are we currently using
         int firstListItem = this.getStartIndex();
+        
+        // How many page links shall be displayed
         int viewSize = this.getViewSize();
+        
+        // What is the table's page index to be displayed
         int currentPage = table.getCurrentPage();
         
+        // Make sure the current page link index is within the current
+        // window taking the left and right margin into account
         if (currentPage < (firstListItem + this.margin))
         {
             firstListItem = currentPage - viewSize + margin;
@@ -91,6 +111,8 @@ public class TableNavigationWithMargin extends TableNavigation
             firstListItem = currentPage - margin;
         }
         
+        // Make sure the first index is >= 0 and the last index is <=
+        // than the last page link index.
         if ((firstListItem + viewSize) >= table.getPageCount())
         {
             firstListItem = table.getPageCount() - viewSize; 
@@ -101,6 +123,7 @@ public class TableNavigationWithMargin extends TableNavigation
             firstListItem = 0;
         }
         
+        // Tell the ListView what the new start index shall be
         this.setStartIndex(firstListItem);
     }
 
@@ -132,7 +155,7 @@ public class TableNavigationWithMargin extends TableNavigation
     }
     
     /**
-     * Sets the seperator.
+     * Sets the seperator. Null meaning, no separator at all.
      * @param separator the seperator
      */
     public void setSeparator(String separator)
@@ -140,5 +163,3 @@ public class TableNavigationWithMargin extends TableNavigation
         this.separator = separator;
     }
 }
-
-
