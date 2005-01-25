9979f077562966a98ec7b1a6af855dd459ed6179
Jonathan Locke <jonl@apache.org>
2005-01-25 09:12:51 +0000

    formatting
    git-svn-id: https://svn.apache.org/repos/asf/incubator/wicket/trunk@455550 13f79535-47bb-0310-9956-ffa450edef68

--- wicket/src/java/wicket/markup/html/form/DropDownChoice.java
+++ wicket/src/java/wicket/markup/html/form/DropDownChoice.java
@@ -38,239 +38,236 @@ import wicket.model.IModel;
  */
 public class DropDownChoice extends AbstractDropDownChoice
 {
-    /** serial UID. */
-    private static final long serialVersionUID = 122777360064586107L;
+	/** serial UID. */
+	private static final long serialVersionUID = 122777360064586107L;
 
-    static
-    {
-        // allow optional use of the IOnChangeListener interface
-        RequestCycle.registerRequestListenerInterface(IOnChangeListener.class);
-    }
+	/**
+	 * Constructor that uses the provided {@link IModel}as its model. All
+	 * components have names. A component's name cannot be null.
+	 * 
+	 * @param name
+	 *            The non-null name of this component
+	 * @param model
+	 *            the model
+	 * @param values
+	 *            the drop down values
+	 * @throws wicket.WicketRuntimeException
+	 *             Thrown if the component has been given a null name.
+	 */
+	public DropDownChoice(String name, IModel model, final Collection values)
+	{
+		super(name, model, values);
+	}
 
-    /**
-     * Constructor that uses the provided {@link IModel}as its model. All
-     * components have names. A component's name cannot be null.
-     * 
-     * @param name
-     *            The non-null name of this component
-     * @param model
-     *            the model
-     * @param values
-     *            the drop down values
-     * @throws wicket.WicketRuntimeException
-     *             Thrown if the component has been given a null name.
-     */
-    public DropDownChoice(String name, IModel model, final Collection values)
-    {
-        super(name, model, values);
-    }
+	/**
+	 * Constructor that uses the provided instance of {@link IModel}as a
+	 * dynamic model. This model will be wrapped in an instance of
+	 * {@link wicket.model.PropertyModel}using the provided expression. Thus,
+	 * using this constructor is a short-hand for:
+	 * 
+	 * <pre>
+	 * new MyComponent(name, new PropertyModel(myIModel, expression));
+	 * </pre>
+	 * 
+	 * All components have names. A component's name cannot be null.
+	 * 
+	 * @param name
+	 *            The non-null name of this component
+	 * @param model
+	 *            the instance of {@link IModel}from which the model object
+	 *            will be used as the subject for the given expression
+	 * @param values
+	 *            the drop down values
+	 * @param expression
+	 *            the OGNL expression that works on the given object
+	 * @throws wicket.WicketRuntimeException
+	 *             Thrown if the component has been given a null name.
+	 */
+	public DropDownChoice(String name, IModel model, String expression, final Collection values)
+	{
+		super(name, model, expression, values);
+	}
 
-    /**
-     * Constructor that uses the provided instance of {@link IModel}as a
-     * dynamic model. This model will be wrapped in an instance of
-     * {@link wicket.model.PropertyModel}using the provided expression. Thus,
-     * using this constructor is a short-hand for:
-     * 
-     * <pre>
-     * new MyComponent(name, new PropertyModel(myIModel, expression));
-     * </pre>
-     * 
-     * All components have names. A component's name cannot be null.
-     * 
-     * @param name
-     *            The non-null name of this component
-     * @param model
-     *            the instance of {@link IModel}from which the model object
-     *            will be used as the subject for the given expression
-     * @param values
-     *            the drop down values
-     * @param expression
-     *            the OGNL expression that works on the given object
-     * @throws wicket.WicketRuntimeException
-     *             Thrown if the component has been given a null name.
-     */
-    public DropDownChoice(String name, IModel model, String expression,
-            final Collection values)
-    {
-        super(name, model, expression, values);
-    }
+	/**
+	 * Constructor that uses the provided object as a simple model. This object
+	 * will be wrapped in an instance of {@link wicket.model.Model}. All
+	 * components have names. A component's name cannot be null.
+	 * 
+	 * @param name
+	 *            The non-null name of this component
+	 * @param object
+	 *            the object that will be used as a simple model
+	 * @param values
+	 *            the drop down values
+	 * @throws wicket.WicketRuntimeException
+	 *             Thrown if the component has been given a null name.
+	 */
+	public DropDownChoice(String name, Serializable object, final Collection values)
+	{
+		super(name, object, values);
+	}
 
-    /**
-     * Constructor that uses the provided object as a simple model. This object
-     * will be wrapped in an instance of {@link wicket.model.Model}. All
-     * components have names. A component's name cannot be null.
-     * 
-     * @param name
-     *            The non-null name of this component
-     * @param object
-     *            the object that will be used as a simple model
-     * @param values
-     *            the drop down values
-     * @throws wicket.WicketRuntimeException
-     *             Thrown if the component has been given a null name.
-     */
-    public DropDownChoice(String name, Serializable object,
-            final Collection values)
-    {
-        super(name, object, values);
-    }
+	/**
+	 * Constructor that uses the provided object as a dynamic model. This object
+	 * will be wrapped in an instance of {@link wicket.model.Model}that will be
+	 * wrapped in an instance of {@link wicket.model.PropertyModel}using the
+	 * provided expression. Thus, using this constructor is a short-hand for:
+	 * 
+	 * <pre>
+	 * new MyComponent(name, new PropertyModel(new Model(object), expression));
+	 * </pre>
+	 * 
+	 * All components have names. A component's name cannot be null.
+	 * 
+	 * @param name
+	 *            The non-null name of this component
+	 * @param object
+	 *            the object that will be used as the subject for the given
+	 *            expression
+	 * @param expression
+	 *            the OGNL expression that works on the given object
+	 * @param values
+	 *            the drop down values
+	 * @throws wicket.WicketRuntimeException
+	 *             Thrown if the component has been given a null name.
+	 */
+	public DropDownChoice(String name, Serializable object, String expression,
+			final Collection values)
+	{
+		super(name, object, expression, values);
+	}
 
-    /**
-     * Constructor that uses the provided object as a dynamic model. This object
-     * will be wrapped in an instance of {@link wicket.model.Model}that will be
-     * wrapped in an instance of {@link wicket.model.PropertyModel}using the
-     * provided expression. Thus, using this constructor is a short-hand for:
-     * 
-     * <pre>
-     * new MyComponent(name, new PropertyModel(new Model(object), expression));
-     * </pre>
-     * 
-     * All components have names. A component's name cannot be null.
-     * 
-     * @param name
-     *            The non-null name of this component
-     * @param object
-     *            the object that will be used as the subject for the given
-     *            expression
-     * @param expression
-     *            the OGNL expression that works on the given object
-     * @param values
-     *            the drop down values
-     * @throws wicket.WicketRuntimeException
-     *             Thrown if the component has been given a null name.
-     */
-    public DropDownChoice(String name, Serializable object, String expression,
-            final Collection values)
-    {
-        super(name, object, expression, values);
-    }
+	/**
+	 * Gets the cookie value for this component.
+	 * 
+	 * @return the cookie value for this component
+	 * @see wicket.markup.html.form.FormComponent.ICookieValue#getCookieValue()
+	 */
+	public final String getCookieValue()
+	{
+		final List list = getValues();
+		if (list instanceof IIdList)
+		{
+			final int index = list.indexOf(getModelObject());
+			if (index != -1)
+			{
+				return ((IIdList)list).getIdValue(index);
+			}
+			return "-1";
+		}
+		else
+		{
+			return Integer.toString(list.indexOf(getModelObject()));
+		}
+	}
 
-    /**
-     * Processes the component tag.
-     * 
-     * @param tag
-     *            Tag to modify
-     * @see wicket.Component#handleComponentTag(wicket.markup.ComponentTag)
-     */
-    protected void handleComponentTag(final ComponentTag tag)
-    {
-        if (this instanceof IOnChangeListener)
-        {
-            // if a user subclasses this class and implements IOnChangeListener
-            // an onChange scriptlet is added
-            String url = getRequestCycle()
-                    .urlFor(this, IOnChangeListener.class);
-            url = url.replaceAll("&", "&amp;");
-            tag.put("onChange", "location.href='" + url + "&amp;" + getPath()
-                    + "=' + this.options[this.selectedIndex].value;");
-        }
-        super.handleComponentTag(tag);
-    }
+	/**
+	 * Called when a selection changes.
+	 */
+	public final void selectionChanged()
+	{
+		selectionChanged(internalUpdateModel());
+	}
 
-    /**
-     * Updates this components' model from the request.
-     * 
-     * @see wicket.markup.html.form.AbstractDropDownChoice#updateModel()
-     */
-    public final void updateModel()
-    {
-        internalUpdateModel();
-    }
+	/**
+	 * Template method that can be overriden by clients that implement
+	 * IOnChangeListener to be notified by onChange events of a select element.
+	 * This method does nothing by default.
+	 * <p>
+	 * Called when a option is selected of a dropdown list that wants to be
+	 * notified of this event. This method is to be implemented by clients that
+	 * want to be notified of selection events.
+	 * 
+	 * @param newSelection
+	 *            The newly selected object of the backing model NOTE this is
+	 *            the same as you would get by calling getModelObject() if the
+	 *            new selection were current
+	 */
+	public void selectionChanged(final Object newSelection)
+	{
+	}
 
-    /**
-     * Update model and return the object.
-     * 
-     * @return the object
-     */
-    private Object internalUpdateModel()
-    {
-        final String indexOrId = getRequestString();
-        Object object = null;
-        final List list = getValues();
-        if (indexOrId == null || "".equals(indexOrId))
-        {
-            setModelObject(null);
-        }
-        else if (list instanceof IIdList)
-        {
-            object = ((IIdList) list).getObjectById(indexOrId);
-            setModelObject(object);
-        }
-        else
-        {
-				object = list.get(Integer.parseInt(indexOrId));
-				setModelObject(object);
-        }
-        return object;
-    }
+	/**
+	 * Sets the cookie value for this component
+	 * 
+	 * @param value
+	 *            the cookie value for this component
+	 * @see wicket.markup.html.form.FormComponent.ICookieValue#setCookieValue(java.lang.String)
+	 */
+	public final void setCookieValue(final String value)
+	{
+		final List list = getValues();
+		if (list instanceof IIdList)
+		{
+			setModelObject(((IIdList)list).getObjectById(value));
+		}
+		else
+		{
+			setModelObject(list.get(Integer.parseInt(value)));
+		}
+	}
 
-    /**
-     * Gets the cookie value for this component.
-     * 
-     * @return the cookie value for this component
-     * @see wicket.markup.html.form.FormComponent.ICookieValue#getCookieValue()
-     */
-    public final String getCookieValue()
-    {
-        final List list = getValues();
-        if (list instanceof IIdList)
-        {
-            final int index = list.indexOf(getModelObject());
-            if (index != -1)
-            {
-                return ((IIdList) list).getIdValue(index);
-            }
-            return "-1";
-        }
-        else
-        {
-            return Integer.toString(list.indexOf(getModelObject()));
-        }
-    }
+	/**
+	 * Updates this components' model from the request.
+	 * 
+	 * @see wicket.markup.html.form.AbstractDropDownChoice#updateModel()
+	 */
+	public final void updateModel()
+	{
+		internalUpdateModel();
+	}
 
-    /**
-     * Sets the cookie value for this component
-     * 
-     * @param value
-     *            the cookie value for this component
-     * @see wicket.markup.html.form.FormComponent.ICookieValue#setCookieValue(java.lang.String)
-     */
-    public final void setCookieValue(final String value)
-    {
-        final List list = getValues();
-        if (list instanceof IIdList)
-        {
-            setModelObject(((IIdList) list).getObjectById(value));
-        }
-        else
-        {
-            setModelObject(list.get(Integer.parseInt(value)));
-        }
-    }
+	/**
+	 * Processes the component tag.
+	 * 
+	 * @param tag
+	 *            Tag to modify
+	 * @see wicket.Component#handleComponentTag(wicket.markup.ComponentTag)
+	 */
+	protected void handleComponentTag(final ComponentTag tag)
+	{
+		if (this instanceof IOnChangeListener)
+		{
+			// if a user subclasses this class and implements IOnChangeListener
+			// an onChange scriptlet is added
+			String url = getRequestCycle().urlFor(this, IOnChangeListener.class);
+			url = url.replaceAll("&", "&amp;");
+			tag.put("onChange", "location.href='" + url + "&amp;" + getPath()
+					+ "=' + this.options[this.selectedIndex].value;");
+		}
+		super.handleComponentTag(tag);
+	}
 
-    /**
-     * Called when a selection changes.
-     */
-    public final void selectionChanged()
-    {
-        selectionChanged(internalUpdateModel());
-    }
+	/**
+	 * Update model and return the object.
+	 * 
+	 * @return the object
+	 */
+	private Object internalUpdateModel()
+	{
+		final String indexOrId = getRequestString();
+		Object object = null;
+		final List list = getValues();
+		if (indexOrId == null || "".equals(indexOrId))
+		{
+			setModelObject(null);
+		}
+		else if (list instanceof IIdList)
+		{
+			object = ((IIdList)list).getObjectById(indexOrId);
+			setModelObject(object);
+		}
+		else
+		{
+			object = list.get(Integer.parseInt(indexOrId));
+			setModelObject(object);
+		}
+		return object;
+	}
 
-    /**
-     * Template method that can be overriden by clients that implement
-     * IOnChangeListener to be notified by onChange events of a select element.
-     * This method does nothing by default.
-     * <p>
-     * Called when a option is selected of a dropdown list that wants to be
-     * notified of this event. This method is to be implemented by clients that
-     * want to be notified of selection events.
-     * 
-     * @param newSelection
-     *            The newly selected object of the backing model NOTE this is
-     *            the same as you would get by calling getModelObject() if the
-     *            new selection were current
-     */
-    public void selectionChanged(final Object newSelection)
-    {
-    }
+	static
+	{
+		// allow optional use of the IOnChangeListener interface
+		RequestCycle.registerRequestListenerInterface(IOnChangeListener.class);
+	}
 }
