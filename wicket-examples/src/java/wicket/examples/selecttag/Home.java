f42f91b786eb73e5eddf46c05316b5c9a98c6e29
Johan Compagner <jcompagner@apache.org>
2005-01-26 10:51:43 +0000

    small example for the select tag with IIdList implementation
    git-svn-id: https://svn.apache.org/repos/asf/incubator/wicket/trunk@455587 13f79535-47bb-0310-9956-ffa450edef68

---
+++ wicket-examples/src/java/wicket/examples/selecttag/Home.java
@@ -0,0 +1,133 @@
+package wicket.examples.selecttag;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+
+import wicket.PageParameters;
+import wicket.markup.html.HtmlPage;
+import wicket.markup.html.basic.Label;
+import wicket.markup.html.form.DropDownChoice;
+import wicket.markup.html.form.Form;
+import wicket.markup.html.form.IIdList;
+import wicket.model.IModel;
+
+/**
+ * @author jcompagner
+ * @version $Id$
+ */
+public class Home extends HtmlPage
+{
+	/**
+	 * Constructor
+	 * 
+	 * @param parameters
+	 *          Page parameters (ignored since this is the home page)
+	 */
+	public Home(final PageParameters parameters)
+	{
+		add(new SelectForm("selectform"));
+	}
+	
+	class SelectForm extends Form
+	{
+		SelectModel model;
+		Label label;
+		
+		public SelectForm(String name)
+		{
+			super(name,null);
+			model = new SelectModel();
+			label = new Label("label",model,"name");
+			add(label);
+			DropDownChoice choice = new DropDownChoice("users",model,new UserIdList());
+			add(choice);
+		}
+
+		/*
+		 * @see wicket.markup.html.form.Form#handleSubmit()
+		 */
+		public void handleSubmit()
+		{
+			getRequestCycle().setRedirect(true);
+			getRequestCycle().setPage(Home.this);
+		}
+	}
+	
+	class SelectModel implements IModel
+	{
+		private Object selection; 
+		/*
+		 * @see wicket.model.IModel#getObject()
+		 */
+		public Object getObject()
+		{
+			return selection;
+		}
+
+		/*
+		 * @see wicket.model.IModel#setObject(java.lang.Object)
+		 */
+		public void setObject(Object object)
+		{
+			selection = object;
+		}
+	}
+	
+	class UserIdList extends ArrayList implements IIdList
+	{
+		/*
+		 * @see wicket.markup.html.form.IIdList#detach()
+		 */
+		public void detach()
+		{
+			this.clear();
+		}
+
+		/*
+		 * @see wicket.markup.html.form.IIdList#attach()
+		 */
+		public void attach()
+		{
+			if(size() == 0)
+			{
+				add(new User(new Long(1),"Foo"));
+				add(new User(new Long(2),"Bar"));
+				add(new User(new Long(3),"FooBar"));
+			}
+		}
+
+		/*
+		 * @see wicket.markup.html.form.IIdList#getDisplayValue(int)
+		 */
+		public String getDisplayValue(int row)
+		{
+			return ((User)get(row)).getName();
+		}
+
+		/*
+		 * @see wicket.markup.html.form.IIdList#getIdValue(int)
+		 */
+		public String getIdValue(int row)
+		{
+			return ((User)get(row)).getId().toString();
+		}
+
+		/*
+		 * @see wicket.markup.html.form.IIdList#getObjectById(java.lang.String)
+		 */
+		public Object getObjectById(String id)
+		{
+			Long longId = new Long(id);
+			Iterator it = iterator();
+			while(it.hasNext())
+			{
+				User user = (User)it.next();
+				if(user.getId().equals(longId))
+				{
+					return user;
+				}
+			}
+			return null;
+		}
+	}
+}
---
+++ wicket-examples/src/java/wicket/examples/selecttag/User.java
@@ -0,0 +1,34 @@
+package wicket.examples.selecttag;
+
+/**
+ * @author jcompagner
+ * @version $Id$
+ */
+public class User
+{
+	private Long id;
+	private String name;
+	
+	public User(Long id, String name)
+	{
+		this.id= id;
+		this.name= name;
+	}
+	
+	public Long getId()
+	{
+		return id;
+	}
+	public void setId(Long id)
+	{
+		this.id = id;
+	}
+	public String getName()
+	{
+		return name;
+	}
+	public void setName(String name)
+	{
+		this.name = name;
+	}
+}
