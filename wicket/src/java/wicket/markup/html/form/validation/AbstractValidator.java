6fe79fa2bb30487bfa23e48f8a695948cba051cb
Eelco Hillenius <ehillenius@apache.org>
2005-01-26 15:29:08 +0000

    don't fail on null/ empty string
    git-svn-id: https://svn.apache.org/repos/asf/incubator/wicket/trunk@455591 13f79535-47bb-0310-9956-ffa450edef68

--- wicket/src/java/wicket/markup/html/form/validation/AbstractValidator.java
+++ wicket/src/java/wicket/markup/html/form/validation/AbstractValidator.java
@@ -31,166 +31,148 @@ import wicket.util.lang.Classes;
 
 /**
  * Base class for form component validators.
- * 
  * @author Jonathan Locke
  * @author Eelco Hillenius
  */
 public abstract class AbstractValidator implements IValidator
 {
-    /**
-     * Returns a formatted validation error message for a given component. The
-     * error message is retrieved from a message bundle associated with the page
-     * in which this validator is contained. The resource key must be of the
-     * form: [form-name].[component-name].[validator-class]. For example, in the
-     * SignIn page's SignIn.properties file, you might find an entry:
-     * signInForm.password.RequiredValidator=A password is required Entries can
-     * contain optional ognl variable interpolations from the component, such
-     * as: editBook.name.LengthValidator='${input}' is too short a name.
-     * <p>
-     * Available variables for interpolation are:
-     * <ul>
-     * <li>${input}: the user's input</li>
-     * <li>${name}: the name of the component</li>
-     * </ul>
-     * </p>
-     * 
-     * @param input
-     *            the input (that caused the error)
-     * @param component
-     *            The component where the error occurred
-     * @return The validation error message
-     */
-    public ValidationErrorMessage errorMessage(final String input,
-            final FormComponent component)
-    {
-        return errorMessage(getResourceKey(component), input, component);
-    }
+	/**
+	 * Returns a formatted validation error message for a given component. The error
+	 * message is retrieved from a message bundle associated with the page in which this
+	 * validator is contained. The resource key must be of the form:
+	 * [form-name].[component-name].[validator-class]. For example, in the SignIn page's
+	 * SignIn.properties file, you might find an entry:
+	 * signInForm.password.RequiredValidator=A password is required Entries can contain
+	 * optional ognl variable interpolations from the component, such as:
+	 * editBook.name.LengthValidator='${input}' is too short a name.
+	 * <p>
+	 * Available variables for interpolation are:
+	 * <ul>
+	 * <li>${input}: the user's input</li>
+	 * <li>${name}: the name of the component</li>
+	 * </ul>
+	 * </p>
+	 * @param input the input (that caused the error)
+	 * @param component The component where the error occurred
+	 * @return The validation error message
+	 */
+	public ValidationErrorMessage errorMessage(final String input, final FormComponent component)
+	{
+		return errorMessage(getResourceKey(component), input, component);
+	}
 
-    /**
-     * Returns a formatted validation error message for a given component. The
-     * error message is retrieved from a message bundle associated with the page
-     * in which this validator is contained using the given resource key. The
-     * resourceModel is used for variable interpolation.
-     * 
-     * @param resourceKey
-     *            the resource key to be used for the message
-     * @param resourceModel
-     *            the model for variable interpolation
-     * @param input
-     *            the input (that caused the error)
-     * @param component
-     *            The component where the error occurred
-     * @return The validation error message
-     */
-    public ValidationErrorMessage errorMessage(final String resourceKey,
-            final IModel resourceModel, final String input,
-            final FormComponent component)
-    {
-        // Return formatted error message
-        Localizer localizer = component.getLocalizer();
-        String message = localizer.getString(resourceKey, component,
-                resourceModel);
-        return new ValidationErrorMessage(component, message);
-    }
+	/**
+	 * Returns a formatted validation error message for a given component. The error
+	 * message is retrieved from a message bundle associated with the page in which this
+	 * validator is contained using the given resource key. The resourceModel is used for
+	 * variable interpolation.
+	 * @param resourceKey the resource key to be used for the message
+	 * @param resourceModel the model for variable interpolation
+	 * @param input the input (that caused the error)
+	 * @param component The component where the error occurred
+	 * @return The validation error message
+	 */
+	public ValidationErrorMessage errorMessage(final String resourceKey, final IModel resourceModel,
+			final String input, final FormComponent component)
+	{
+		// Return formatted error message
+		Localizer localizer = component.getLocalizer();
+		String message = localizer.getString(resourceKey, component, resourceModel);
+		return new ValidationErrorMessage(component, message);
+	}
 
-    /**
-     * Returns a formatted validation error message for a given component. The
-     * error message is retrieved from a message bundle associated with the page
-     * in which this validator is contained using the given resource key. The
-     * resourceModel is used for variable interpolation.
-     * 
-     * @param resourceKey
-     *            the resource key to be used for the message
-     * @param map
-     *            the model for variable interpolation
-     * @param input
-     *            the input (that caused the error)
-     * @param component
-     *            The component where the error occurred
-     * @return The validation error message
-     */
-    public ValidationErrorMessage errorMessage(final String resourceKey,
-            final Map map, final String input, final FormComponent component)
-    {
-        return errorMessage(resourceKey, MapModel.valueOf(map), input,
-                component);
-    }
+	/**
+	 * Returns a formatted validation error message for a given component. The error
+	 * message is retrieved from a message bundle associated with the page in which this
+	 * validator is contained using the given resource key. The resourceModel is used for
+	 * variable interpolation.
+	 * @param resourceKey the resource key to be used for the message
+	 * @param map the model for variable interpolation
+	 * @param input the input (that caused the error)
+	 * @param component The component where the error occurred
+	 * @return The validation error message
+	 */
+	public ValidationErrorMessage errorMessage(final String resourceKey, final Map map,
+			final String input, final FormComponent component)
+	{
+		return errorMessage(resourceKey, MapModel.valueOf(map), input, component);
+	}
 
-    /**
-     * Returns a formatted validation error message for a given component. The
-     * error message is retrieved from a message bundle associated with the page
-     * in which this validator is contained using the given resource key.
-     * <p>
-     * The available variables for interpolation are by default:
-     * <ul>
-     * <li>${input}: the user's input</li>
-     * <li>${name}: the name of the component</li>
-     * </ul>
-     * Optionally, you can either override getMessageContextVariables, or
-     * provide a model or a map with those variables yourself by using one of
-     * the other errorMessage methods.
-     * </p>
-     * 
-     * @param resourceKey
-     *            the resource key to be used for the message
-     * @param input
-     *            the input (that caused the error)
-     * @param component
-     *            The component where the error occurred
-     * @return The validation error message
-     */
-    public ValidationErrorMessage errorMessage(final String resourceKey,
-            final String input, final FormComponent component)
-    {
-        final Map resourceModel = getMessageContextVariables(input, component);
-        return errorMessage(resourceKey, resourceModel, input, component);
-    }
+	/**
+	 * Returns a formatted validation error message for a given component. The error
+	 * message is retrieved from a message bundle associated with the page in which this
+	 * validator is contained using the given resource key.
+	 * <p>
+	 * The available variables for interpolation are by default:
+	 * <ul>
+	 * <li>${input}: the user's input</li>
+	 * <li>${name}: the name of the component</li>
+	 * </ul>
+	 * Optionally, you can either override getMessageContextVariables, or provide a model
+	 * or a map with those variables yourself by using one of the other errorMessage
+	 * methods.
+	 * </p>
+	 * @param resourceKey the resource key to be used for the message
+	 * @param input the input (that caused the error)
+	 * @param component The component where the error occurred
+	 * @return The validation error message
+	 */
+	public ValidationErrorMessage errorMessage(final String resourceKey, final String input,
+			final FormComponent component)
+	{
+		final Map resourceModel = getMessageContextVariables(input, component);
+		return errorMessage(resourceKey, resourceModel, input, component);
+	}
 
-    /**
-     * Gets the default variables for interpolation. These are:
-     * <ul>
-     * <li>${input}: the user's input</li>
-     * <li>${name}: the name of the component</li>
-     * </ul>
-     * 
-     * @param input
-     *            the user's input
-     * @param component
-     *            the component
-     * @return a map with the variables for interpolation
-     */
-    protected Map getMessageContextVariables(final String input,
-            final FormComponent component)
-    {
-        final Map resourceModel = new HashMap(4);
-        resourceModel.put("input", input);
-        resourceModel.put("name", component.getName());
-        return resourceModel;
-    }
+	/**
+	 * Gets the default variables for interpolation. These are:
+	 * <ul>
+	 * <li>${input}: the user's input</li>
+	 * <li>${name}: the name of the component</li>
+	 * </ul>
+	 * @param input the user's input
+	 * @param component the component
+	 * @return a map with the variables for interpolation
+	 */
+	protected Map getMessageContextVariables(final String input, final FormComponent component)
+	{
+		final Map resourceModel = new HashMap(4);
+		resourceModel.put("input", input);
+		resourceModel.put("name", component.getName());
+		return resourceModel;
+	}
 
-    /**
-     * Gets the resource key based on the form component. It will have the form:
-     * <code>[form-name].[component-name].[validator-class]</code>
-     * 
-     * @param component
-     *            the form component
-     * @return the resource key based on the form component
-     */
-    protected String getResourceKey(final FormComponent component)
-    {
-        // Resource key must be <form-name>.<component-name>.<validator-class>
-        final Component parentForm = component.findParent(Form.class);
-        if (parentForm != null)
-        {
-            return parentForm.getName() + "." + component.getName() + "."
-                    + Classes.name(getClass());
-        }
-        else
-        {
-            throw new WicketRuntimeException(
-                    "Unable to find Form parent for FormComponent " + component);
-        }
-    }
+	/**
+	 * Gets the resource key based on the form component. It will have the form:
+	 * <code>[form-name].[component-name].[validator-class]</code>
+	 * @param component the form component
+	 * @return the resource key based on the form component
+	 */
+	protected String getResourceKey(final FormComponent component)
+	{
+		// Resource key must be <form-name>.<component-name>.<validator-class>
+		final Component parentForm = component.findParent(Form.class);
+		if (parentForm != null)
+		{
+			return parentForm.getName() + "." + component.getName() + "." + Classes.name(getClass());
+		}
+		else
+		{
+			throw new WicketRuntimeException("Unable to find Form parent for FormComponent "
+					+ component);
+		}
+	}
+
+
+	/**
+	 * Gets whether the provided argument is null or an empty string.
+	 * @param toCheck the string to check
+	 * @return whether the provided argument is null or an empty string
+	 */
+	protected final boolean isNullOrEmpty(String toCheck)
+	{
+		if (toCheck == null)
+			return true;
+		return (toCheck.trim().equals(""));
+	}
 }
-
-
--- wicket/src/java/wicket/markup/html/form/validation/TypeValidator.java
+++ wicket/src/java/wicket/markup/html/form/validation/TypeValidator.java
@@ -31,210 +31,185 @@ import wicket.util.convert.ConverterRegistry;
 
 /**
  * Validates input by trying it to convert to the given type using the
- * {@link wicket.util.convert.ConverterRegistry}instance of that can be found
- * in the application settings.
- * 
+ * {@link wicket.util.convert.ConverterRegistry}instance of that can be found in the
+ * application settings.
  * @author Eelco Hillenius
  */
 public class TypeValidator extends AbstractValidator
 {
-    /** Log. */
-    private static Log log = LogFactory.getLog(TypeValidator.class);
+	/** Log. */
+	private static Log log = LogFactory.getLog(TypeValidator.class);
 
-    /**
-     * The locale to use. If null and useLocaled == true, the session's locale
-     * will be used.
-     */
-    private Locale locale = null;
+	/**
+	 * The locale to use. If null and useLocaled == true, the session's locale will be
+	 * used.
+	 */
+	private Locale locale = null;
 
-    /** The type to use for checking. */
-    private Class type;
+	/** The type to use for checking. */
+	private Class type;
 
-    /** Whether to use either the set locale or the session's locale. */
-    private boolean useLocalized = true;
+	/** Whether to use either the set locale or the session's locale. */
+	private boolean useLocalized = true;
 
-    /**
-     * Construct. The current session's locale will be used for conversion.
-     * 
-     * @param type
-     *            the type to use for checking
-     */
-    public TypeValidator(final Class type)
-    {
-        this(type, null);
-    }
+	/**
+	 * Construct. The current session's locale will be used for conversion.
+	 * @param type the type to use for checking
+	 */
+	public TypeValidator(final Class type)
+	{
+		this(type, null);
+	}
 
-    /**
-     * Construct. If useLocalized is true, the current session's locale will be
-     * used for conversion. If useLocalized is false, no localization will be
-     * used for checking.
-     * 
-     * @param type
-     *            the type to use for checking
-     * @param useLocalized
-     *            whether localization (using the current session's locale)
-     *            should be used
-     */
-    public TypeValidator(final Class type, final boolean useLocalized)
-    {
-        this(type, null);
-        this.useLocalized = useLocalized;
-    }
+	/**
+	 * Construct. If useLocalized is true, the current session's locale will be used for
+	 * conversion. If useLocalized is false, no localization will be used for checking.
+	 * @param type the type to use for checking
+	 * @param useLocalized whether localization (using the current session's locale) should
+	 *           be used
+	 */
+	public TypeValidator(final Class type, final boolean useLocalized)
+	{
+		this(type, null);
+		this.useLocalized = useLocalized;
+	}
 
-    /**
-     * Construct. If not-null, the given locale will be used for conversion.
-     * Otherwise the session's locale will be used for conversion.
-     * 
-     * @param type
-     *            the type to use for checking
-     * @param locale
-     *            the locale to use
-     */
-    public TypeValidator(final Class type, final Locale locale)
-    {
-        this.type = type;
-        this.locale = locale;
-    }
+	/**
+	 * Construct. If not-null, the given locale will be used for conversion. Otherwise the
+	 * session's locale will be used for conversion.
+	 * @param type the type to use for checking
+	 * @param locale the locale to use
+	 */
+	public TypeValidator(final Class type, final Locale locale)
+	{
+		this.type = type;
+		this.locale = locale;
+	}
 
-    /**
-     * Validates input by trying it to convert to the given type using the
-     * {@link wicket.util.convert.ConverterRegistry}instance of that can be
-     * found in the application settings.
-     * 
-     * @param component
-     *            the component that wants to validate its input
-     * @return validation error message
-     * @see wicket.markup.html.form.validation.IValidator#validate(wicket.markup.html.form.FormComponent)
-     */
-    public final ValidationErrorMessage validate(FormComponent component)
-    {
-        // Get component value
-        final String value = component.getRequestString();
+	/**
+	 * Validates input by trying it to convert to the given type using the
+	 * {@link wicket.util.convert.ConverterRegistry}instance of that can be found in the
+	 * application settings.
+	 * @param component the component that wants to validate its input
+	 * @return validation error message
+	 * @see wicket.markup.html.form.validation.IValidator#validate(wicket.markup.html.form.FormComponent)
+	 */
+	public final ValidationErrorMessage validate(FormComponent component)
+	{
+		// Get component value
+		final String value = component.getRequestString();
+		if (isNullOrEmpty(value))
+			return ValidationErrorMessage.NO_MESSAGE;
+		// Check value by attempting to convert it
+		final ConversionUtils conversionUtils = getConversionUtils();
+		final Locale localeForValidation = getLocaleForValidation();
+		try
+		{
+			conversionUtils.convert(component.getRequestString(), type, localeForValidation);
+			return ValidationErrorMessage.NO_MESSAGE;
+		}
+		catch (ConversionException e)
+		{
+			return getError(value, component, e);
+		}
+	}
 
-        // Check value by attempting to convert it
-        final ConversionUtils conversionUtils = getConversionUtils();
-        final Locale localeForValidation = getLocaleForValidation();
-        try
-        {
-            conversionUtils.convert(component.getRequestString(), type, localeForValidation);
-            return ValidationErrorMessage.NO_MESSAGE;
-        }
-        catch (ConversionException e)
-        {
-            return getError(value, component, e);
-        }
-    }
+	/**
+	 * Gets the conversion utilities.
+	 * @return the conversion utilities
+	 */
+	protected final ConversionUtils getConversionUtils()
+	{
+		RequestCycle requestCycle = RequestCycle.get();
+		if (requestCycle != null)
+		{
+			ConverterRegistry converterRegistry = requestCycle.getApplication().getConverterRegistry();
+			return converterRegistry.getConversionUtils();
+		}
+		else
+		// we must be in a test case; just give a dummy
+		{
+			log.error("no current request cycle found; using a dummy converter registry");
+			return new ConversionUtils(new ConverterRegistry());
+		}
+	}
 
-    /**
-     * Gets the conversion utilities.
-     * 
-     * @return the conversion utilities
-     */
-    protected final ConversionUtils getConversionUtils()
-    {
-        RequestCycle requestCycle = RequestCycle.get();
-        if (requestCycle != null)
-        {
-            ConverterRegistry converterRegistry = requestCycle.getApplication()
-                    .getConverterRegistry();
-            return converterRegistry.getConversionUtils();
-        }
-        else
-        // we must be in a test case; just give a dummy
-        {
-            log.error("no current request cycle found; using a dummy converter registry");
-            return new ConversionUtils(new ConverterRegistry());
-        }
-    }
+	/**
+	 * Gets the error message.
+	 * @param input The input
+	 * @param component the component
+	 * @param e the conversion exception
+	 * @return the validation error message
+	 */
+	protected ValidationErrorMessage getError(String input, FormComponent component,
+			ConversionException e)
+	{
+		Map ctx = getMessageContext(input, component, e);
+		return errorMessage(getResourceKey(component), ctx, input, component);
+	}
 
-    /**
-     * Gets the error message.
-     * 
-     * @param input
-     *            The input
-     * @param component
-     *            the component
-     * @param e
-     *            the conversion exception
-     * @return the validation error message
-     */
-    protected ValidationErrorMessage getError(String input, FormComponent component,
-            ConversionException e)
-    {
-        Map ctx = getMessageContext(input, component, e);
-        return errorMessage(getResourceKey(component), ctx, input, component);
-    }
+	/**
+	 * Gets the locale to use. if null and useLocaled == true, the session's locale will be
+	 * used..
+	 * @return the locale to use
+	 */
+	protected final Locale getLocale()
+	{
+		return locale;
+	}
 
-    /**
-     * Gets the locale to use. if null and useLocaled == true, the session's
-     * locale will be used..
-     * 
-     * @return the locale to use
-     */
-    protected final Locale getLocale()
-    {
-        return locale;
-    }
+	/**
+	 * Gets the message context.
+	 * @param input The input
+	 * @param component the component
+	 * @param e the conversion exception
+	 * @return a map with variables for interpolation
+	 */
+	protected Map getMessageContext(String input, FormComponent component, ConversionException e)
+	{
+		Map ctx = super.getMessageContextVariables(input, component);
+		ctx.put("type", type);
+		Locale loc = e.getLocale();
+		if (loc != null)
+			ctx.put("locale", loc);
+		ctx.put("exception", e.getMessage());
+		ctx.put("pattern", e.getPattern());
+		return ctx;
+	}
 
-    /**
-     * Gets the message context.
-     * 
-     * @param input
-     *            The input
-     * @param component
-     *            the component
-     * @param e
-     *            the conversion exception
-     * @return a map with variables for interpolation
-     */
-    protected Map getMessageContext(String input, FormComponent component, ConversionException e)
-    {
-        Map ctx = super.getMessageContextVariables(input, component);
-        ctx.put("type", type);
-        Locale loc = e.getLocale();
-        if (loc != null)
-            ctx.put("locale", loc);
-        ctx.put("exception", e.getMessage());
-        ctx.put("pattern", e.getPattern());
-        return ctx;
-    }
+	/**
+	 * Gets the type to use for checking.
+	 * @return the type to use for checking
+	 */
+	protected final Class getType()
+	{
+		return type;
+	}
 
-    /**
-     * Gets the type to use for checking.
-     * 
-     * @return the type to use for checking
-     */
-    protected final Class getType()
-    {
-        return type;
-    }
+	/**
+	 * Gets whether to use either the set locale or the session's locale.
+	 * @return whether to use either the set locale or the session's locale.
+	 */
+	protected final boolean isUseLocalized()
+	{
+		return useLocalized;
+	}
 
-    /**
-     * Gets whether to use either the set locale or the session's locale.
-     * 
-     * @return whether to use either the set locale or the session's locale.
-     */
-    protected final boolean isUseLocalized()
-    {
-        return useLocalized;
-    }
-
-    /**
-     * Gets the locale that should be used for the current validation.
-     * 
-     * @return the locale that should be used for the current validation
-     */
-    private final Locale getLocaleForValidation()
-    {
-        Locale localeForValidation = null;
-        if (isUseLocalized())
-        {
-            localeForValidation = getLocale();
-            if (localeForValidation == null)
-            {
-                localeForValidation = RequestCycle.get().getSession().getLocale();
-            }
-        }
-        return localeForValidation;
-    }
+	/**
+	 * Gets the locale that should be used for the current validation.
+	 * @return the locale that should be used for the current validation
+	 */
+	private final Locale getLocaleForValidation()
+	{
+		Locale localeForValidation = null;
+		if (isUseLocalized())
+		{
+			localeForValidation = getLocale();
+			if (localeForValidation == null)
+			{
+				localeForValidation = RequestCycle.get().getSession().getLocale();
+			}
+		}
+		return localeForValidation;
+	}
 }
