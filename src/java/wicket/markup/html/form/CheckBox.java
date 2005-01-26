9a84541a90eea64a5d9a175c6a90dacdcbacf5b5
Eelco Hillenius <ehillenius@apache.org>
2005-01-26 20:23:27 +0000

    fixes
    git-svn-id: https://svn.apache.org/repos/asf/incubator/wicket/trunk/wicket@455593 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/wicket/markup/html/form/CheckBox.java
+++ src/java/wicket/markup/html/form/CheckBox.java
@@ -23,6 +23,8 @@ import java.io.Serializable;
 import wicket.WicketRuntimeException;
 import wicket.markup.ComponentTag;
 import wicket.model.IModel;
+import wicket.model.Model;
+import wicket.model.PropertyModel;
 
 /**
  * HTML checkbox input component.
@@ -31,128 +33,136 @@ import wicket.model.IModel;
  */
 public final class CheckBox extends FormComponent
 {
-    /** Serial Version ID. */
+	/** Serial Version ID. */
 	private static final long serialVersionUID = 7559827519977114184L;
 
 	/**
-     * Constructor that uses the provided {@link IModel} as its model. All components have
-     * names. A component's name cannot be null.
-     * @param name The non-null name of this component
-     * @param model the model
-     * @throws wicket.WicketRuntimeException Thrown if the component has been given a null name.
-     */
-    public CheckBox(String name, IModel model)
-    {
-        super(name, model);
-    }
+	 * Constructor that uses the provided {@link IModel} as its model. All components have
+	 * names. A component's name cannot be null.
+	 * @param name The non-null name of this component
+	 * @param model the model
+	 * @throws wicket.WicketRuntimeException Thrown if the component has been given a null name.
+	 */
+	public CheckBox(String name, IModel model)
+	{
+		super(name, model);
+	}
 
-    /**
-     * Constructor that uses the provided instance of {@link IModel} as a dynamic model.
-     * This model will be wrapped in an instance of {@link wicket.model.PropertyModel}using the
-     * provided expression. Thus, using this constructor is a short-hand for:
-     * 
-     * <pre>
-     * new MyComponent(name, new PropertyModel(myIModel, expression));
-     * </pre>
-     * 
-     * All components have names. A component's name cannot be null.
-     * @param name The non-null name of this component
-     * @param model the instance of {@link IModel} from which the model object will be
-     *            used as the subject for the given expression
-     * @param expression the OGNL expression that works on the given object
-     * @throws wicket.WicketRuntimeException Thrown if the component has been given a null name.
-     */
-    public CheckBox(String name, IModel model, String expression)
-    {
-        super(name, model, expression);
-    }
+	/**
+	 * Constructor that uses the provided instance of {@link IModel} as a dynamic model.
+	 * This model will be wrapped in an instance of {@link wicket.model.PropertyModel}using the
+	 * provided expression. Thus, using this constructor is a short-hand for:
+	 * 
+	 * <pre>
+	 * new MyComponent(name, new PropertyModel(myIModel, expression));
+	 * </pre>
+	 * 
+	 * All components have names. A component's name cannot be null.
+	 * @param name The non-null name of this component
+	 * @param model the instance of {@link IModel} from which the model object will be
+	 *            used as the subject for the given expression
+	 * @param expression the OGNL expression that works on the given object
+	 * @throws wicket.WicketRuntimeException Thrown if the component has been given a null name.
+	 */
+	public CheckBox(String name, IModel model, String expression)
+	{
+		super(name);
+		// construct model without formatting as we want the model object as-is
+		setModel(new PropertyModel(model, expression, false));
+	}
 
-    /**
-     * Constructor that uses the provided object as a simple model. This object will be
-     * wrapped in an instance of {@link wicket.model.Model}. All components have names. A component's
-     * name cannot be null.
-     * @param name The non-null name of this component
-     * @param object the object that will be used as a simple model
-     * @throws wicket.WicketRuntimeException Thrown if the component has been given a null name.
-     */
-    public CheckBox(String name, Serializable object)
-    {
-        super(name, object);
-    }
+	/**
+	 * Constructor that uses the provided object as a simple model. This object will be
+	 * wrapped in an instance of {@link wicket.model.Model}. All components have names. A component's
+	 * name cannot be null.
+	 * @param name The non-null name of this component
+	 * @param object the object that will be used as a simple model
+	 * @throws wicket.WicketRuntimeException Thrown if the component has been given a null name.
+	 */
+	public CheckBox(String name, Serializable object)
+	{
+		super(name, object);
+	}
 
-    /**
-     * Constructor that uses the provided object as a dynamic model. This object will be
-     * wrapped in an instance of {@link wicket.model.Model} that will be wrapped in an instance of
-     * {@link wicket.model.PropertyModel} using the provided expression. Thus, using this constructor
-     * is a short-hand for:
-     * 
-     * <pre>
-     * new MyComponent(name, new PropertyModel(new Model(object), expression));
-     * </pre>
-     * 
-     * All components have names. A component's name cannot be null.
-     * @param name The non-null name of this component
-     * @param object the object that will be used as the subject for the given expression
-     * @param expression the OGNL expression that works on the given object
-     * @throws wicket.WicketRuntimeException Thrown if the component has been given a null name.
-     */
-    public CheckBox(String name, Serializable object, String expression)
-    {
-        super(name, object, expression);
-    }
+	/**
+	 * Constructor that uses the provided object as a dynamic model. This object will be
+	 * wrapped in an instance of {@link wicket.model.Model} that will be wrapped in an instance of
+	 * {@link wicket.model.PropertyModel} using the provided expression. Thus, using this constructor
+	 * is a short-hand for:
+	 * 
+	 * <pre>
+	 * new MyComponent(name, new PropertyModel(new Model(object), expression));
+	 * </pre>
+	 * 
+	 * All components have names. A component's name cannot be null.
+	 * @param name The non-null name of this component
+	 * @param object the object that will be used as the subject for the given expression
+	 * @param expression the OGNL expression that works on the given object
+	 * @throws wicket.WicketRuntimeException Thrown if the component has been given a null name.
+	 */
+	public CheckBox(String name, Serializable object, String expression)
+	{
+		super(name, object, expression);
+		// construct model without formatting as we want the model object as-is
+		setModel(new PropertyModel(new Model(object), expression, false));
+	}
 
-    /**
-     * @see FormComponent#supportsPersistence()
-     */
-    public boolean supportsPersistence()
-    {
-        return true;
-    }
+	/**
+	 * @see FormComponent#supportsPersistence()
+	 */
+	public boolean supportsPersistence()
+	{
+		return true;
+	}
 
-    /**
-     * @see FormComponent#setValue(java.lang.String)
-     */
-    public void setValue(String value)
-    {
-        setModelObject(Boolean.valueOf(value));
-    }
+	/**
+	 * @see FormComponent#setValue(java.lang.String)
+	 */
+	public void setValue(String value)
+	{
+		setModelObject(Boolean.valueOf(value));
+	}
 
-    /**
-     * Updates this components' model from the request.
-     * @see wicket.markup.html.form.FormComponent#updateModel()
-     */
-    public void updateModel()
-    {
-        setModelObject(getRequestBoolean());
-    }
+	/**
+	 * Updates this components' model from the request.
+	 * @see wicket.markup.html.form.FormComponent#updateModel()
+	 */
+	public void updateModel()
+	{
+		setModelObject(getRequestBoolean());
+	}
 
-    /**
+	/**
 	 * Processes the component tag.
 	 * @param tag Tag to modify
-     * @see wicket.Component#handleComponentTag(ComponentTag)
-     */
-    protected void handleComponentTag(final ComponentTag tag)
-    {
-        checkTag(tag, "input");
-        checkAttribute(tag, "type", "checkbox");
-        super.handleComponentTag(tag);
+	 * @see wicket.Component#handleComponentTag(ComponentTag)
+	 */
+	protected void handleComponentTag(final ComponentTag tag)
+	{
+		checkTag(tag, "input");
+		checkAttribute(tag, "type", "checkbox");
+		super.handleComponentTag(tag);
 
-        Object value = getModelObject();
+		Object value = getModelObject();
 
-        if (value != null)
-        {
-            if (!(value instanceof Boolean))
-            {
-                throw new WicketRuntimeException(
-                        "Model objects for checkboxes have to be of type Boolean");
-            }
-
-            if (((Boolean)value).booleanValue())
-            {
-                tag.put("checked", "true");
-            }
-        }
-    }
+		if (value != null)
+		{
+			final boolean tagValue;
+			if(value instanceof String) // probably was formatted or straight from request
+			{
+				tagValue = Boolean.parseBoolean((String)value);
+			}
+			else if(value instanceof Boolean)
+			{
+				tagValue = ((Boolean)value).booleanValue();
+			}
+			else
+			{
+				throw new WicketRuntimeException(
+						"Model objects for checkboxes have to be of type Boolean");
+			}
+			if(tagValue) tag.put("checked", "checked");
+			else tag.remove("checked"); // in case the was a design time attrib
+		}
+	}
 }
-
-
