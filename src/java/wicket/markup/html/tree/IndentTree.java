475a72a407b06071ebcf6e90d16d30091c9690c8
Eelco Hillenius <ehillenius@apache.org>
2005-01-28 10:37:09 +0000

    small efficiency tweak
    git-svn-id: https://svn.apache.org/repos/asf/incubator/wicket/trunk/wicket@455606 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/wicket/markup/html/tree/IndentTree.java
+++ src/java/wicket/markup/html/tree/IndentTree.java
@@ -37,8 +37,8 @@ import wicket.model.IModel;
 import wicket.model.Model;
 
 /**
- * A Tree that renders as a flat (not-nested) list, using spacers for indentation
- * and nodes at the end of one row.
+ * A Tree that renders as a flat (not-nested) list, using spacers for indentation and
+ * nodes at the end of one row.
  * <p>
  * The visible tree rows are put in one flat list. For each row, a list is constructed
  * with fillers, that can be used to create indentation. After the fillers, the actual
@@ -46,105 +46,112 @@ import wicket.model.Model;
  * </p>
  * <p>
  * For example:
+ * 
  * <pre>
- * &lt;span id="spacers"&gt;&lt;/span&gt;&lt;span id=" spacers"&gt;&lt;/span&gt;
- * &lt;span id ="node"&gt;&lt;span id="label"&gt;foo&lt;/span&gt;&lt;/span&gt;
+ * 
+ *  &lt;span id=&quot;spacers&quot;&gt;&lt;/span&gt;&lt;span id=&quot; spacers&quot;&gt;&lt;/span&gt;
+ *  &lt;span id =&quot;node&quot;&gt;&lt;span id=&quot;label&quot;&gt;foo&lt;/span&gt;&lt;/span&gt;
+ *  
  * </pre>
+ * 
  * Could be one row, where the node is on level two (hence the two spacer elements).
  * </p>
  * <p>
  * If you combine this with CSS like:
+ * 
  * <pre>
- *	#spacers {
- *		padding-left: 16px;
- *	}
+ * 
+ * 	#spacers {
+ * 		padding-left: 16px;
+ * 	}
+ *  
  * </pre>
+ * 
  * and you have an indented tree.
  * </p>
- *
  * @author Eelco Hillenius
  */
 public abstract class IndentTree extends Tree
 {
-    /** table for the current visible tree paths. */
-    private ListView visibleTreePathTable;
-
-    /**
-     * Constructor.
-     * @param componentName The name of this container
-     * @param model the underlying tree model
-     */
-    public IndentTree(final String componentName, final TreeModel model)
-    {
-        super(componentName, model);
-    }
-
-    /**
-     * Constructor.
-     * @param componentName The name of this container
-     * @param model the underlying tree model
-     * @param makeTreeModelUnique whether to make the user objects of the tree model
-     * unique. If true, the default implementation will wrapp all user objects in
-     * instances of {@link IdWrappedUserObject}. If false, users must ensure that the
-     * user objects are unique within the tree in order to have the tree working properly
-     */
-    public IndentTree(final String componentName, final TreeModel model,
-    		final boolean makeTreeModelUnique)
-    {
-        super(componentName, model, makeTreeModelUnique);
-    }
-
-    /**
-     * Constructor using the given tree state. This tree state holds the tree model and
-     * the currently visible paths.
-     * @param componentName The name of this container
-     * @param treeState the tree state that holds the tree model and the currently visible
-     * paths
-     */
-    public IndentTree(final String componentName, TreeStateCache treeState)
-    {
-        super(componentName, treeState);
-    }
-
-    /**
-     * Builds the structures needed to display the currently visible tree paths.
-     * @param treeState the current tree state
-     */
-    protected final void applySelectedPaths(TreeStateCache treeState)
-    {
-        Enumeration e = treeState.getVisiblePathsFromRoot();
-        List visiblePathsList = new ArrayList();
-        boolean skip = (!treeState.isRootVisible());
-        while (e.hasMoreElements())
-        {
-        	TreePath path = (TreePath)e.nextElement();
-        	if(skip) // skip root if not visible
-        	{
-        		skip = false;
-        		continue;
-        	}
-            DefaultMutableTreeNode treeNode =
-                (DefaultMutableTreeNode)path.getLastPathComponent();
-            TreeNodeModel treeNodeModel = new TreeNodeModel(treeNode, treeState, path);
-            visiblePathsList.add(treeNodeModel);
-        }
-        Model model = new Model((Serializable) visiblePathsList);
-
-        if (visibleTreePathTable == null)
-        {
-        	visibleTreePathTable = newVisibleTreePathListView(model);
-            add(visibleTreePathTable);
-        }
-        else
-        {
-            visibleTreePathTable.removeAll();
-            visibleTreePathTable.setModel(model);
-        }
-    }
+	/** table for the current visible tree paths. */
+	private ListView visibleTreePathTable;
 
 	/**
-	 * Creates a {@link ListView} to use for rendering the visible tree rows.
-	 * The model parameter should be used as the model for the <code>ListView</code>.
+	 * Constructor.
+	 * @param componentName The name of this container
+	 * @param model the underlying tree model
+	 */
+	public IndentTree(final String componentName, final TreeModel model)
+	{
+		super(componentName, model);
+	}
+
+	/**
+	 * Constructor.
+	 * @param componentName The name of this container
+	 * @param model the underlying tree model
+	 * @param makeTreeModelUnique whether to make the user objects of the tree model
+	 *           unique. If true, the default implementation will wrapp all user objects in
+	 *           instances of {@link IdWrappedUserObject}. If false, users must ensure
+	 *           that the user objects are unique within the tree in order to have the tree
+	 *           working properly
+	 */
+	public IndentTree(final String componentName, final TreeModel model,
+			final boolean makeTreeModelUnique)
+	{
+		super(componentName, model, makeTreeModelUnique);
+	}
+
+	/**
+	 * Constructor using the given tree state. This tree state holds the tree model and the
+	 * currently visible paths.
+	 * @param componentName The name of this container
+	 * @param treeState the tree state that holds the tree model and the currently visible
+	 *           paths
+	 */
+	public IndentTree(final String componentName, TreeStateCache treeState)
+	{
+		super(componentName, treeState);
+	}
+
+	/**
+	 * Builds the structures needed to display the currently visible tree paths.
+	 * @param treeState the current tree state
+	 */
+	protected final void applySelectedPaths(TreeStateCache treeState)
+	{
+		Enumeration e = treeState.getVisiblePathsFromRoot();
+		List visiblePathsList = new ArrayList();
+		boolean skip = (!treeState.isRootVisible());
+		while (e.hasMoreElements())
+		{
+			TreePath path = (TreePath)e.nextElement();
+			if (skip) // skip root if not visible
+			{
+				skip = false;
+				continue;
+			}
+			DefaultMutableTreeNode treeNode = (DefaultMutableTreeNode)path.getLastPathComponent();
+			TreeNodeModel treeNodeModel = new TreeNodeModel(treeNode, treeState, path);
+			visiblePathsList.add(treeNodeModel);
+		}
+		Model model = new Model((Serializable)visiblePathsList);
+
+		if (visibleTreePathTable == null)
+		{
+			visibleTreePathTable = newVisibleTreePathListView(model);
+			add(visibleTreePathTable);
+		}
+		else
+		{
+			visibleTreePathTable.removeAll();
+			visibleTreePathTable.setModel(model);
+		}
+	}
+
+	/**
+	 * Creates a {@link ListView}to use for rendering the visible tree rows. The model
+	 * parameter should be used as the model for the <code>ListView</code>.
 	 * @param model a model that contains a list of {@link TreeNodeModel}s
 	 * @return a new <code>ListView</code> that uses the provided model
 	 */
@@ -160,36 +167,36 @@ public abstract class IndentTree extends Tree
 	 */
 	private final TreeNodeLink createJunctionLink(TreeNodeModel node)
 	{
-        TreeNodeLink junctionLink = new TreeNodeLink(
-        		"junctionLink", this, node){
+		TreeNodeLink junctionLink = new TreeNodeLink("junctionLink", this, node)
+		{
 
-            public void linkClicked(TreeNodeModel node)
-            {
-            	junctionLinkClicked(node);
-            }   
-        };
+			public void linkClicked(TreeNodeModel node)
+			{
+				junctionLinkClicked(node);
+			}
+		};
 		HtmlContainer junctionImg = new HtmlContainer("junctionImg");
-		junctionImg.add(new ComponentTagAttributeModifier(
-        		"src", true, new Model(getJunctionImageName(node))));
+		junctionImg.add(new ComponentTagAttributeModifier("src", true, new Model(
+				getJunctionImageName(node))));
 		junctionLink.add(junctionImg);
 		return junctionLink;
 	}
 
-    /**
-     * Get image name for junction; used by method createExpandCollapseLink.
-     * @param node the model with the current node
-     * @return image name
-     */
-    protected abstract String getJunctionImageName(TreeNodeModel node);
+	/**
+	 * Get image name for junction; used by method createExpandCollapseLink.
+	 * @param node the model with the current node
+	 * @return image name
+	 */
+	protected abstract String getJunctionImageName(TreeNodeModel node);
 
 	/**
-	 * Handler that is called when a junction link is clicked; this implementation
-	 * sets the expanded state to one that corresponds with the node selection.
+	 * Handler that is called when a junction link is clicked; this implementation sets the
+	 * expanded state to one that corresponds with the node selection.
 	 * @param node the tree node model
 	 */
 	protected void junctionLinkClicked(TreeNodeModel node)
 	{
-        setExpandedState(node);
+		setExpandedState(node);
 	}
 
 	/**
@@ -200,16 +207,17 @@ public abstract class IndentTree extends Tree
 	private final TreeNodeLink createNodeLink(TreeNodeModel node)
 	{
 		Object userObject = node.getTreeNode().getUserObject();
-        TreeNodeLink nodeLink = new TreeNodeLink("nodeLink", this, node){
+		TreeNodeLink nodeLink = new TreeNodeLink("nodeLink", this, node)
+		{
 
-            public void linkClicked(TreeNodeModel node)
-            {
-            	nodeLinkClicked(node);
-            }   
-        };
+			public void linkClicked(TreeNodeModel node)
+			{
+				nodeLinkClicked(node);
+			}
+		};
 		HtmlContainer nodeImg = new HtmlContainer("nodeImg");
-		nodeImg.add(new ComponentTagAttributeModifier(
-        		"src", true, new Model(getNodeImageName(node))));
+		nodeImg
+				.add(new ComponentTagAttributeModifier("src", true, new Model(getNodeImageName(node))));
 		nodeLink.add(nodeImg);
 		String userObjectAsString = getNodeLabel(node);
 		nodeLink.add(new Label("label", userObjectAsString));
@@ -217,25 +225,24 @@ public abstract class IndentTree extends Tree
 	}
 
 	/**
-	 * Gets the label of the node that is used for the node link.
-	 * Defaults to treeNodeModel.getUserObject().toString(); override to provide
-	 * a custom label
+	 * Gets the label of the node that is used for the node link. Defaults to
+	 * treeNodeModel.getUserObject().toString(); override to provide a custom label
 	 * @param node the tree node model
 	 * @return the label of the node that is used for the node link
 	 */
 	protected abstract String getNodeLabel(TreeNodeModel node);
 
-    /**
-     * Get image name for node; used by method createExpandCollapseLink.
-     * @param node the model with the current node
-     * @return image name
-     */
-    protected abstract String getNodeImageName(TreeNodeModel node);
+	/**
+	 * Get image name for node; used by method createExpandCollapseLink.
+	 * @param node the model with the current node
+	 * @return image name
+	 */
+	protected abstract String getNodeImageName(TreeNodeModel node);
 
 	/**
-	 * Handler that is called when a node link is clicked; this implementation
-	 * sets the expanded state just as a click on a junction would do.
-	 * Override this for custom behaviour.
+	 * Handler that is called when a node link is clicked; this implementation sets the
+	 * expanded state just as a click on a junction would do. Override this for custom
+	 * behaviour.
 	 * @param node the tree node model
 	 */
 	protected void nodeLinkClicked(TreeNodeModel node)
@@ -244,9 +251,9 @@ public abstract class IndentTree extends Tree
 	}
 
 	/**
-	 * Returns whether the path and the selected path are equal.
-	 * This method is used by the {@link ComponentTagAttributeModifier} that is used
-	 * for setting the CSS class for the selected row.
+	 * Returns whether the path and the selected path are equal. This method is used by the
+	 * {@link ComponentTagAttributeModifier}that is used for setting the CSS class for the
+	 * selected row.
 	 * @param path the path
 	 * @param selectedPath the selected path
 	 * @return true if the path and the selected are equal, false otherwise
@@ -256,8 +263,7 @@ public abstract class IndentTree extends Tree
 		boolean equals;
 		Object pathNode = path.getLastPathComponent();
 		Object selectedPathNode = selectedPath.getLastPathComponent();
-		equals = (pathNode != null && selectedPathNode != null &&
-				pathNode.equals(selectedPathNode));
+		equals = (pathNode != null && selectedPathNode != null && pathNode.equals(selectedPathNode));
 		return equals;
 	}
 
@@ -279,73 +285,68 @@ public abstract class IndentTree extends Tree
 		return "treerow";
 	}
 
-    /**
-     * Table for visible tree paths.
-     */
-    private final class VisibleTreePathListView extends ListView
-    {
-        /**
-         * Construct.
-         * @param name name of the component
-         * @param model the model
-         */
-        public VisibleTreePathListView(String name, IModel model)
-        {
-            super(name, model);
-        }
+	/**
+	 * Table for visible tree paths.
+	 */
+	private final class VisibleTreePathListView extends ListView
+	{
+		/**
+		 * Construct.
+		 * @param name name of the component
+		 * @param model the model
+		 */
+		public VisibleTreePathListView(String name, IModel model)
+		{
+			super(name, model);
+		}
 
-        /**
-         * @see wicket.markup.html.list.ListView#populateItem(wicket.markup.html.list.ListItem)
-         */
-        protected void populateItem(ListItem listItem)
-        {
-            TreeNodeModel treeNodeModel = (TreeNodeModel)listItem.getModelObject();
+		/**
+		 * @see wicket.markup.html.list.ListView#populateItem(wicket.markup.html.list.ListItem)
+		 */
+		protected void populateItem(ListItem listItem)
+		{
+			TreeNodeModel treeNodeModel = (TreeNodeModel)listItem.getModelObject();
 
-            // add spacers
-            int level = treeNodeModel.getLevel();
-            List spacerList = new ArrayList(level);
-            for(int i = 0; i < level; i++)
-            {
-                spacerList.add(treeNodeModel);
-            }
-            listItem.add(new SpacerList("spacers", spacerList));
+			// add spacers
+			int level = treeNodeModel.getLevel();
+			listItem.add(new SpacerList("spacers", level));
 
-            // add node
-            HtmlContainer nodeContainer = new HtmlContainer("node");
-            Serializable userObject = treeNodeModel.getUserObject();
+			// add node
+			HtmlContainer nodeContainer = new HtmlContainer("node");
+			Serializable userObject = treeNodeModel.getUserObject();
 
-            if (userObject == null)
-            {
-                throw new WicketRuntimeException("User object cannot be null");
-            }
-            TreeNodeLink expandCollapsLink = IndentTree.this.createJunctionLink(treeNodeModel);
-            nodeContainer.add(expandCollapsLink);
+			if (userObject == null)
+			{
+				throw new WicketRuntimeException("User object cannot be null");
+			}
+			TreeNodeLink expandCollapsLink = IndentTree.this.createJunctionLink(treeNodeModel);
+			nodeContainer.add(expandCollapsLink);
 
-            TreeNodeLink selectLink = IndentTree.this.createNodeLink(treeNodeModel);
-            nodeContainer.add(selectLink);
-            listItem.add(nodeContainer);
+			TreeNodeLink selectLink = IndentTree.this.createNodeLink(treeNodeModel);
+			nodeContainer.add(selectLink);
+			listItem.add(nodeContainer);
 
-            listItem.add(new ComponentTagAttributeModifier(
-            		"class", true, new SelectedPathReplacementModel(treeNodeModel)));
-        }
-    }
+			listItem.add(new ComponentTagAttributeModifier("class", true,
+					new SelectedPathReplacementModel(treeNodeModel)));
+		}
+	}
 
-    /**
-     * Replacement model that looks up whether the current row is the active one.
-     */
-    private final class SelectedPathReplacementModel extends Model
-    {
-    	/** the tree node model. */
-    	private final TreeNodeModel treeNodeModel;
+	/**
+	 * Replacement model that looks up whether the current row is the active one.
+	 */
+	private final class SelectedPathReplacementModel extends Model
+	{
+		/** the tree node model. */
+		private final TreeNodeModel treeNodeModel;
 
-    	/**
-    	 * Construct.
-    	 * @param treeNodeModel tree node model
-    	 */
-    	public SelectedPathReplacementModel(TreeNodeModel treeNodeModel)
-    	{
-    		this.treeNodeModel = treeNodeModel;
-    	}
+		/**
+		 * Construct.
+		 * @param treeNodeModel tree node model
+		 */
+		public SelectedPathReplacementModel(TreeNodeModel treeNodeModel)
+		{
+			this.treeNodeModel = treeNodeModel;
+		}
 
 		/**
 		 * @see wicket.model.IModel#getObject()
@@ -354,40 +355,41 @@ public abstract class IndentTree extends Tree
 		{
 			TreePath path = treeNodeModel.getPath();
 			TreePath selectedPath = treeNodeModel.getTreeState().getSelectedPath();
-			if(selectedPath != null)
+			if (selectedPath != null)
 			{
 				boolean equals = IndentTree.this.equals(path, selectedPath);
-				
-				if(equals)
+
+				if (equals)
 				{
 					return IndentTree.this.getCSSClassForSelectedRow();
 				}
 			}
 			return IndentTree.this.getCSSClassForRow();
 		}
-    }
+	}
 
-    /**
-     * Renders spacer items.
-     */
-    private final class SpacerList extends ListView
-    {
-        /**
-         * Construct.
-         * @param componentName name of the component
-         * @param list list
-         */
-        public SpacerList(String componentName, List list)
-        {
-            super(componentName, list);
-        }
+	/**
+	 * Renders spacer items.
+	 */
+	private final class SpacerList extends ListView
+	{
+		/**
+		 * Construct.
+		 * @param componentName name of the component
+		 * @param size number of spacer elements to create
+		 */
+		public SpacerList(String componentName, int size)
+		{
+			super(componentName, (IModel)null);
+			setViewSize(size);
+		}
 
-        /**
-         * @see wicket.markup.html.list.ListView#populateItem(wicket.markup.html.list.ListItem)
-         */
-        protected void populateItem(ListItem listItem)
-        {
-        	// nothing needed; we just render the tags and use CSS to indent
-        }
-    }
+		/**
+		 * @see wicket.markup.html.list.ListView#populateItem(wicket.markup.html.list.ListItem)
+		 */
+		protected void populateItem(ListItem listItem)
+		{
+			// nothing needed; we just render the tags and use CSS to indent
+		}
+	}
 }
