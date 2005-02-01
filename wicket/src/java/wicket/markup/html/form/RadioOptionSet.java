da8295a42dec2de56a06a14439e658bd29e1b05c
Jonathan Locke <jonl@apache.org>
2005-02-01 06:25:54 +0000

    *** empty log message ***
    git-svn-id: https://svn.apache.org/repos/asf/incubator/wicket/trunk@455639 13f79535-47bb-0310-9956-ffa450edef68

--- wicket/src/java/wicket/markup/html/form/RadioOptionSet.java
+++ wicket/src/java/wicket/markup/html/form/RadioOptionSet.java
@@ -1,20 +1,19 @@
 /*
  * $Id$
- * $Revision$
- * $Date$
- *
- * ====================================================================
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *  http://www.apache.org/licenses/LICENSE-2.0
- *
+ * $Revision$ $Date$
+ * 
+ * ==================================================================== Licensed
+ * under the Apache License, Version 2.0 (the "License"); you may not use this
+ * file except in compliance with the License. You may obtain a copy of the
+ * License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
  */
 package wicket.markup.html.form;
 
@@ -28,9 +27,9 @@ import wicket.markup.MarkupStream;
 import wicket.util.lang.EnumeratedType;
 
 /**
- * Produces a set of radio input options for a collection of values. When the form is
- * submitted, the RadioOptionSet object updates the parent RadioChoice component with the
- * selected value.
+ * Produces a set of radio input options for a collection of values. When the
+ * form is submitted, the RadioOptionSet object updates the parent RadioChoice
+ * component with the selected value.
  * 
  * @author Jonathan Locke
  */
@@ -40,110 +39,117 @@ public final class RadioOptionSet extends FormComponent
 	public static final Style LINE_BREAK = new Style("<br>");
 
 	/** Paragraph markup. */
-    public static final Style PARAGRAPH = new Style("<p>");
+	public static final Style PARAGRAPH = new Style("<p>");
 
-    /** Space markup. */
-    public static final Style SPACE = new Style(" ");
- // TODO finalize javadoc
-    /** Serial Version ID */
+	/** Space markup. */
+	public static final Style SPACE = new Style(" ");
+
+	/** Serial Version ID */
 	private static final long serialVersionUID = 2552126944567296644L;
 
-    /** The style to render in */
-    private final Style style;
+	/** The style to render in */
+	private final Style style;
 
-    /**
-     * Typesafe enum for layout of radio options.
-     */
-    public static final class Style extends EnumeratedType
-    {
-        /**
-         * Construct.
-         * @param name style name
-         */
-        Style(final String name)
-        {
-            super(name);
-        }
-    }
+	/**
+	 * Typesafe enum for layout of radio options.
+	 */
+	public static final class Style extends EnumeratedType
+	{
+		/**
+		 * Construct.
+		 * 
+		 * @param name
+		 *            style name
+		 */
+		Style(final String name)
+		{
+			super(name);
+		}
+	}
 
-    /**
-     * Constructor.
-     * @param componentName The component name
-     * @param values The option values
-     */
-    public RadioOptionSet(final String componentName, final Collection values)
-    {
-        this(componentName, values, LINE_BREAK);
-    }
+	/**
+	 * Constructor.
+	 * 
+	 * @param componentName
+	 *            The component name
+	 * @param values
+	 *            The option values
+	 */
+	public RadioOptionSet(final String componentName, final Collection values)
+	{
+		this(componentName, values, LINE_BREAK);
+	}
 
-    /**
-     * Constructor.
-     * @param componentName The component name
-     * @param values The option values
-     * @param style The style of layout
-     */
-    public RadioOptionSet(final String componentName, final Collection values, final Style style)
-    {
-        super(componentName, new ArrayList(values));
-        this.style = style;
-    }
+	/**
+	 * Constructor.
+	 * 
+	 * @param componentName
+	 *            The component name
+	 * @param values
+	 *            The option values
+	 * @param style
+	 *            The style of layout
+	 */
+	public RadioOptionSet(final String componentName, final Collection values, final Style style)
+	{
+		super(componentName, new ArrayList(values));
+		this.style = style;
+	}
 
-    /**
-     * @see wicket.markup.html.form.FormComponent#updateModel()
-     */
-    public void updateModel()
-    {
-    }
+	/**
+	 * @see wicket.markup.html.form.FormComponent#updateModel()
+	 */
+	public void updateModel()
+	{
+	}
 
-    /**
-     * @see wicket.Component#handleBody(MarkupStream, ComponentTag)
-     */
-    protected void handleBody(final MarkupStream markupStream, final ComponentTag openTag)
-    {
-        // Buffer to hold generated body
-        final StringBuffer options = new StringBuffer();
+	/**
+	 * @see wicket.Component#handleBody(MarkupStream, ComponentTag)
+	 */
+	protected void handleBody(final MarkupStream markupStream, final ComponentTag openTag)
+	{
+		// Buffer to hold generated body
+		final StringBuffer options = new StringBuffer();
 
-        // Get the parent RadioChoice
-        final RadioChoice parent = (RadioChoice) findParent(RadioChoice.class);
+		// Get the parent RadioChoice
+		final RadioChoice parent = (RadioChoice)findParent(RadioChoice.class);
 
-        // Get currently seleced value of radio choice
-        final Object selected = parent.getModelObject();
+		// Get currently seleced value of radio choice
+		final Object selected = parent.getModelObject();
 
-        // Iterate through values
-        List values = (List) getModelObject();
+		// Iterate through values
+		List values = (List)getModelObject();
 
-        for (final Iterator iterator = values.iterator(); iterator.hasNext();)
-        {
-            // Get next value
-            final Object value = iterator.next();
+		for (final Iterator iterator = values.iterator(); iterator.hasNext();)
+		{
+			// Get next value
+			final Object value = iterator.next();
 
-            // Add choice to parent
-            final int index = parent.addRadioOption(value);
+			// Add choice to parent
+			final int index = parent.addRadioOption(value);
 
-            options.append("<input name=\""
-                    + parent.getPath() + "\"" + " type=\"radio\""
-                    + ((selected == value) ? " checked" : "") + " value=\"" + index + "\">");
+			options.append("<input name=\"" + parent.getPath() + "\"" + " type=\"radio\""
+					+ ((selected == value) ? " checked" : "") + " value=\"" + index + "\">");
 
-            // Add label for radio button
-            //TODO support custom labels in future
-            final String label = String.valueOf(value.toString());
+			// Add label for radio button
+			//TODO support custom labels in future
+			final String label = String.valueOf(value.toString());
 
-            options.append(getLocalizer().getString(
-                    getName() + "." + label, this, label));
+			options.append(getLocalizer().getString(getName() + "." + label, this, label));
 
-            // Append separator
-            if (iterator.hasNext())
-            {
-                options.append(style.toString());
-            }
+			// Append separator
+			if (iterator.hasNext())
+			{
+				options.append(style.toString());
+			}
 
-            // For HTML readability
-            options.append('\n');
-        }
+			// For HTML readability
+			options.append('\n');
+		}
 
-        // Replace body
-        replaceBody(markupStream, openTag, options.toString());
-    }
+		// Replace body
+		replaceBody(markupStream, openTag, options.toString());
+	}
 }
 
 
