d21ed50c212bf225722cccd3db795dae2f76e7d3
Jonathan Locke <jonl@apache.org>
2005-02-06 19:50:50 +0000

    Trivial: formatting
    git-svn-id: https://svn.apache.org/repos/asf/incubator/wicket/trunk@455739 13f79535-47bb-0310-9956-ffa450edef68

--- wicket/src/java/wicket/markup/html/link/AbstractLink.java
+++ wicket/src/java/wicket/markup/html/link/AbstractLink.java
@@ -1,20 +1,19 @@
 /*
  * $Id$
- * $Revision$
- * $Date$
- *
- * ====================================================================
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *  http://www.apache.org/licenses/LICENSE-2.0
- *
+ * $Revision$ $Date$
+ * 
+ * ==================================================================== Licensed
+ * under the Apache License, Version 2.0 (the "License"); you may not use this
+ * file except in compliance with the License. You may obtain a copy of the
+ * License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
  */
 package wicket.markup.html.link;
 
@@ -26,120 +25,149 @@ import wicket.model.IModel;
 
 /**
  * Base class for links.
- *
+ * 
  * @author Jonathan Locke
  * @author Eelco Hillenius
  */
 public abstract class AbstractLink extends HtmlContainer implements ILinkListener
 {
-    /**
-     * The popup specification. If not-null, a javascript on-click event handler will be
-     * generated that opens a new window using the popup properties.
-     */
-    private PopupSettings popupSettings = null;
+	/**
+	 * The popup specification. If not-null, a javascript on-click event handler
+	 * will be generated that opens a new window using the popup properties.
+	 */
+	private PopupSettings popupSettings = null;
 
-    /**
-     * Construct.
-     * @param componentName the name of the component
-     */
-    public AbstractLink(String componentName)
-    {
-        super(componentName);
-    }
+	/**
+	 * Construct.
+	 * 
+	 * @param componentName
+	 *            the name of the component
+	 */
+	public AbstractLink(String componentName)
+	{
+		super(componentName);
+	}
 
-    /**
-     * Constructor that uses the provided {@link IModel} as its model.
-     * @param name The non-null name of this component
-     * @param model the model
-     * @throws wicket.WicketRuntimeException Thrown if the component has been given a null name.
-     */
-    public AbstractLink(String name, IModel model)
-    {
-        super(name, model);
-    }
+	/**
+	 * Constructor that uses the provided {@link IModel}as its model.
+	 * 
+	 * @param name
+	 *            The non-null name of this component
+	 * @param model
+	 *            the model
+	 * @throws wicket.WicketRuntimeException
+	 *             Thrown if the component has been given a null name.
+	 */
+	public AbstractLink(String name, IModel model)
+	{
+		super(name, model);
+	}
 
-    /**
-     * Constructor that uses the provided instance of {@link IModel} as a dynamic model.
-     * This model will be wrapped in an instance of {@link wicket.model.PropertyModel}
-     * using the provided expression.
-     *
-     * @param name The non-null name of this component
-     * @param model the instance of {@link IModel} from which the model object will be
-     *            used as the subject for the given expression
-     * @param expression the OGNL expression that works on the given object
-     * @throws wicket.WicketRuntimeException Thrown if the component has been given a null name.
-     */
-    public AbstractLink(String name, IModel model, String expression)
-    {
-        super(name, model, expression);
-    }
+	/**
+	 * Constructor that uses the provided instance of {@link IModel}as a
+	 * dynamic model. This model will be wrapped in an instance of
+	 * {@link wicket.model.PropertyModel}using the provided expression.
+	 * 
+	 * @param name
+	 *            The non-null name of this component
+	 * @param model
+	 *            the instance of {@link IModel}from which the model object
+	 *            will be used as the subject for the given expression
+	 * @param expression
+	 *            the OGNL expression that works on the given object
+	 * @throws wicket.WicketRuntimeException
+	 *             Thrown if the component has been given a null name.
+	 */
+	public AbstractLink(String name, IModel model, String expression)
+	{
+		super(name, model, expression);
+	}
 
-    /**
-     * Constructor that uses the provided object as a simple model. This object will be
-     * wrapped in an instance of {@link wicket.model.Model}.
-     * @param name The non-null name of this component
-     * @param object the object that will be used as a simple model
-     * @throws wicket.WicketRuntimeException Thrown if the component has been given a null name.
-     */
-    public AbstractLink(String name, Serializable object)
-    {
-        super(name, object);
-    }
+	/**
+	 * Constructor that uses the provided object as a simple model. This object
+	 * will be wrapped in an instance of {@link wicket.model.Model}.
+	 * 
+	 * @param name
+	 *            The non-null name of this component
+	 * @param object
+	 *            the object that will be used as a simple model
+	 * @throws wicket.WicketRuntimeException
+	 *             Thrown if the component has been given a null name.
+	 */
+	public AbstractLink(String name, Serializable object)
+	{
+		super(name, object);
+	}
 
-    /**
-     * Constructor that uses the provided object as a dynamic model. This object will be
-     * wrapped in an instance of {@link wicket.model.Model} that will be wrapped in an instance of
-     * {@link wicket.model.PropertyModel} using the provided expression.
-     * @param name The non-null name of this component
-     * @param object the object that will be used as the subject for the given expression
-     * @param expression the OGNL expression that works on the given object
-     * @throws wicket.WicketRuntimeException Thrown if the component has been given a null name.
-     */
-    public AbstractLink(String name, Serializable object, String expression)
-    {
-        super(name, object, expression);
-    }
+	/**
+	 * Constructor that uses the provided object as a dynamic model. This object
+	 * will be wrapped in an instance of {@link wicket.model.Model}that will be
+	 * wrapped in an instance of {@link wicket.model.PropertyModel}using the
+	 * provided expression.
+	 * 
+	 * @param name
+	 *            The non-null name of this component
+	 * @param object
+	 *            the object that will be used as the subject for the given
+	 *            expression
+	 * @param expression
+	 *            the OGNL expression that works on the given object
+	 * @throws wicket.WicketRuntimeException
+	 *             Thrown if the component has been given a null name.
+	 */
+	public AbstractLink(String name, Serializable object, String expression)
+	{
+		super(name, object, expression);
+	}
 
 
-    /**
-     * Gets the popup specification. If not-null, a javascript on-click event handler
-     * will be generated that opens a new window using the popup properties.
-     * @return the popup specification.
-     */
-    public final PopupSettings getPopupSettings()
-    {
-        return popupSettings;
-    }
+	/**
+	 * Gets the popup specification. If not-null, a javascript on-click event
+	 * handler will be generated that opens a new window using the popup
+	 * properties.
+	 * 
+	 * @return the popup specification.
+	 */
+	public final PopupSettings getPopupSettings()
+	{
+		return popupSettings;
+	}
 
-    /**
-     * Called when a link is clicked.
-     * @see ILinkListener
-     */
-    public abstract void linkClicked();
+	/**
+	 * Called when a link is clicked.
+	 * 
+	 * @see ILinkListener
+	 */
+	public abstract void linkClicked();
 
-    /**
-     * Sets the popup specification. If not-null, a javascript on-click event handler
-     * will be generated that opens a new window using the popup properties.
-     * @param popupSettings the popup specification.
-     * @return This
-     */
-    public final AbstractLink setPopupSettings(PopupSettings popupSettings)
-    {
-        this.popupSettings = popupSettings;
-        return this;
-    }
+	/**
+	 * Sets the popup specification. If not-null, a javascript on-click event
+	 * handler will be generated that opens a new window using the popup
+	 * properties.
+	 * 
+	 * @param popupSettings
+	 *            the popup specification.
+	 * @return This
+	 */
+	public final AbstractLink setPopupSettings(PopupSettings popupSettings)
+	{
+		this.popupSettings = popupSettings;
+		return this;
+	}
 
-    /**
-     * Gets the url to use for this link.
-     * @return The URL that this link links to
-     */
-    protected String getURL()
-    {
-        return getRequestCycle().urlFor(AbstractLink.this, ILinkListener.class);
-    }
-    static
-    {
-        // Allow calls through the ILinkListener interface
-        RequestCycle.registerRequestListenerInterface(ILinkListener.class);
-    }
+	/**
+	 * Gets the url to use for this link.
+	 * 
+	 * @return The URL that this link links to
+	 */
+	protected String getURL()
+	{
+		return getRequestCycle().urlFor(AbstractLink.this, ILinkListener.class);
+	}
+
+	static
+	{
+		// Allow calls through the ILinkListener interface
+		RequestCycle.registerRequestListenerInterface(ILinkListener.class);
+	}
 }
