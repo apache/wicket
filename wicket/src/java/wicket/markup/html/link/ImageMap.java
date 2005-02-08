ed8484937fe944fcc4b30a797f7bc70076a428f4
sourceforge-skipoles <sourceforge-skipoles@apache.org>
2005-02-08 09:26:28 +0000

    Made shapelink class Serializable so that image maps can be clustered
    git-svn-id: https://svn.apache.org/repos/asf/incubator/wicket/trunk@455776 13f79535-47bb-0310-9956-ffa450edef68

--- wicket/src/java/wicket/markup/html/link/ImageMap.java
+++ wicket/src/java/wicket/markup/html/link/ImageMap.java
@@ -22,6 +22,7 @@ package wicket.markup.html.link;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
+import java.io.Serializable;
 
 import wicket.markup.ComponentTag;
 import wicket.markup.MarkupStream;
@@ -34,301 +35,301 @@ import wicket.markup.html.HtmlComponent;
  */
 public final class ImageMap extends HtmlComponent
 {
-    /** Serial Version ID. */
+	/** Serial Version ID. */
 	private static final long serialVersionUID = 209001445308790198L;
 
 	/** list of shape links. */
 	private final List shapeLinks = new ArrayList();
 
-    /**
-     * Constructor.
-     * @param name component name
-     */
-    public ImageMap(final String name)
-    {
-        super(name);
-    }
+	/**
+	 * Constructor.
+	 * @param name component name
+	 */
+	public ImageMap(final String name)
+	{
+		super(name);
+	}
 
-    /**
-     * Renders this component.
-     * @see wicket.Component#handleRender()
-     */
-    protected void handleRender()
-    {
-        // Get markup stream
-        final MarkupStream markupStream = findMarkupStream();
+	/**
+	 * Renders this component.
+	 * @see wicket.Component#handleRender()
+	 */
+	protected void handleRender()
+	{
+		// Get markup stream
+		final MarkupStream markupStream = findMarkupStream();
 
-        // Get mutable copy of next tag
-        final ComponentTag tag = markupStream.getTag().mutable();
+		// Get mutable copy of next tag
+		final ComponentTag tag = markupStream.getTag().mutable();
 
-        // Must be an img tag
-        checkComponentTag(tag, "img");
+		// Must be an img tag
+		checkComponentTag(tag, "img");
 
-        // Set map name to path
-        tag.put("usemap", "#" + getPath());
+		// Set map name to path
+		tag.put("usemap", "#" + getPath());
 
-        // Write out the tag
-        renderComponentTag(tag);
-        markupStream.next();
+		// Write out the tag
+		renderComponentTag(tag);
+		markupStream.next();
 
-        // Write out the image map
-        final StringBuffer imageMap = new StringBuffer();
+		// Write out the image map
+		final StringBuffer imageMap = new StringBuffer();
 
-        imageMap.append("\n<map name=\"" + getPath() + "\"> ");
+		imageMap.append("\n<map name=\"" + getPath() + "\"> ");
 
-        for (Iterator iterator = shapeLinks.iterator(); iterator.hasNext();)
-        {
-            final ShapeLink shapeLink = (ShapeLink) iterator.next();
+		for (Iterator iterator = shapeLinks.iterator(); iterator.hasNext();)
+		{
+			final ShapeLink shapeLink = (ShapeLink) iterator.next();
 
-            imageMap.append('\n');
-            imageMap.append(shapeLink.toString());
-        }
+			imageMap.append('\n');
+			imageMap.append(shapeLink.toString());
+		}
 
-        imageMap.append("\n</map>");
-        getResponse().write(imageMap.toString());
-    }
+		imageMap.append("\n</map>");
+		getResponse().write(imageMap.toString());
+	}
 
-    /**
-     * Adds a polygon link.
-     * @param coordinates the coordinates for the polygon
-     * @param link the link
-     * @return This
-     */
-    public ImageMap addPolygonLink(final int[] coordinates, final Link link)
-    {
-        shapeLinks.add(new PolygonLink(coordinates, link));
+	/**
+	 * Adds a polygon link.
+	 * @param coordinates the coordinates for the polygon
+	 * @param link the link
+	 * @return This
+	 */
+	public ImageMap addPolygonLink(final int[] coordinates, final Link link)
+	{
+		shapeLinks.add(new PolygonLink(coordinates, link));
 
-        return this;
-    }
+		return this;
+	}
 
-    /**
-     * Adds a rectangular link.
-     * @param x1 top left x
-     * @param y1 top left y
-     * @param x2 bottom right x
-     * @param y2 bottom right y
-     * @param link
-     * @return This
-     */
-    public ImageMap addRectangleLink(final int x1, final int y1, final int x2, final int y2,
-            final Link link)
-    {
-        shapeLinks.add(new RectangleLink(x1, y1, x2, y2, link));
+	/**
+	 * Adds a rectangular link.
+	 * @param x1 top left x
+	 * @param y1 top left y
+	 * @param x2 bottom right x
+	 * @param y2 bottom right y
+	 * @param link
+	 * @return This
+	 */
+	public ImageMap addRectangleLink(final int x1, final int y1, final int x2, final int y2,
+			final Link link)
+	{
+		shapeLinks.add(new RectangleLink(x1, y1, x2, y2, link));
 
-        return this;
-    }
+		return this;
+	}
 
-    /**
-     * Adds a circle link.
-     * @param x1 top left x
-     * @param y1 top left y
-     * @param radius the radius
-     * @param link the link
-     * @return This
-     */
-    public ImageMap addCircleLink(final int x1, final int y1, final int radius, final Link link)
-    {
-        shapeLinks.add(new CircleLink(x1, y1, radius, link));
+	/**
+	 * Adds a circle link.
+	 * @param x1 top left x
+	 * @param y1 top left y
+	 * @param radius the radius
+	 * @param link the link
+	 * @return This
+	 */
+	public ImageMap addCircleLink(final int x1, final int y1, final int radius, final Link link)
+	{
+		shapeLinks.add(new CircleLink(x1, y1, radius, link));
 
-        return this;
-    }
+		return this;
+	}
 
-    /**
-     * Base class for shaped links.
-     */
-    private static abstract class ShapeLink
-    {
-        /** the link. */
-        private final Link link;
+	/**
+	 * Base class for shaped links.
+	 */
+	private static abstract class ShapeLink implements Serializable
+	{
+		/** the link. */
+		private final Link link;
 
-        /**
-         * Constructor.
-         * @param link the link
-         */
-        public ShapeLink(final Link link)
-        {
-            this.link = link;
-        }
+		/**
+		 * Constructor.
+		 * @param link the link
+		 */
+		public ShapeLink(final Link link)
+		{
+			this.link = link;
+		}
 
-        /**
-         * Gets the shape type.
-         * @return the shape type
-         */
-        abstract String getType();
+		/**
+		 * Gets the shape type.
+		 * @return the shape type
+		 */
+		abstract String getType();
 
-        /**
-         * Gets the coordinates of the shape.
-         * @return the coordinates of the shape
-         */
-        abstract String getCoordinates();
+		/**
+		 * Gets the coordinates of the shape.
+		 * @return the coordinates of the shape
+		 */
+		abstract String getCoordinates();
 
-        /**
-         * The shape as a string using the given request cycle; will be used
-         * for rendering.
-         * @return The shape as a string
-         */
-        public String toString()
-        {
-            // Add any popup script
-            final String popupJavaScript;
+		/**
+		 * The shape as a string using the given request cycle; will be used
+		 * for rendering.
+		 * @return The shape as a string
+		 */
+		public String toString()
+		{
+			// Add any popup script
+			final String popupJavaScript;
 
-            if (link.getPopupSettings() != null)
-            {
-                popupJavaScript = link.getPopupSettings().getPopupJavaScript();
-            }
-            else
-            {
-                popupJavaScript = null;
-            }
+			if (link.getPopupSettings() != null)
+			{
+				popupJavaScript = link.getPopupSettings().getPopupJavaScript();
+			}
+			else
+			{
+				popupJavaScript = null;
+			}
 
-            return "<area shape=\""
-                    + getType() + "\"" + " coords=\"" + getCoordinates() + "\"" + " href=\""
-                    + link.getURL() + "\""
-                    + ((popupJavaScript == null) ? "" : (" onClick = \"" + popupJavaScript + "\""))
-                    + ">";
-        }
-    }
+			return "<area shape=\""
+					+ getType() + "\"" + " coords=\"" + getCoordinates() + "\"" + " href=\""
+					+ link.getURL() + "\""
+					+ ((popupJavaScript == null) ? "" : (" onClick = \"" + popupJavaScript + "\""))
+					+ ">";
+		}
+	}
 
-    /**
-     * A shape that has a free (polygon) form.
-     */
-    private static final class PolygonLink extends ShapeLink
-    {
-        /** Its coordinates. */
-        private final int[] coordinates;
+	/**
+	 * A shape that has a free (polygon) form.
+	 */
+	private static final class PolygonLink extends ShapeLink
+	{
+		/** Its coordinates. */
+		private final int[] coordinates;
 
-        /**
-         * Construct.
-         * @param coordinates the polygon coordinates
-         * @param link the link
-         */
-        public PolygonLink(final int[] coordinates, final Link link)
-        {
-            super(link);
-            this.coordinates = coordinates;
-        }
+		/**
+		 * Construct.
+		 * @param coordinates the polygon coordinates
+		 * @param link the link
+		 */
+		public PolygonLink(final int[] coordinates, final Link link)
+		{
+			super(link);
+			this.coordinates = coordinates;
+		}
 
-        /**
-         * @see wicket.markup.html.link.ImageMap.ShapeLink#getType()
-         */
-        String getType()
-        {
-            return "polygon";
-        }
+		/**
+		 * @see wicket.markup.html.link.ImageMap.ShapeLink#getType()
+		 */
+		String getType()
+		{
+			return "polygon";
+		}
 
-        /**
-         * @see wicket.markup.html.link.ImageMap.ShapeLink#getCoordinates()
-         */
-        String getCoordinates()
-        {
-            final StringBuffer buffer = new StringBuffer();
-            for (int i = 0; i < coordinates.length; i++)
-            {
-                buffer.append(coordinates[i]);
+		/**
+		 * @see wicket.markup.html.link.ImageMap.ShapeLink#getCoordinates()
+		 */
+		String getCoordinates()
+		{
+			final StringBuffer buffer = new StringBuffer();
+			for (int i = 0; i < coordinates.length; i++)
+			{
+				buffer.append(coordinates[i]);
 
-                if (i < (coordinates.length - 1))
-                {
-                    buffer.append(',');
-                }
-            }
-            return buffer.toString();
-        }
-    }
+				if (i < (coordinates.length - 1))
+				{
+					buffer.append(',');
+				}
+			}
+			return buffer.toString();
+		}
+	}
 
-    /**
-     * A shape that has a rectangular form.
-     */
-    private static final class RectangleLink extends ShapeLink
-    {
-        /** left upper x. */
-        private final int x1;
+	/**
+	 * A shape that has a rectangular form.
+	 */
+	private static final class RectangleLink extends ShapeLink
+	{
+		/** left upper x. */
+		private final int x1;
 
-        /** left upper y. */
-        private final int y1;
+		/** left upper y. */
+		private final int y1;
 
-        /** right bottom x. */
-        private final int x2;
+		/** right bottom x. */
+		private final int x2;
 
-        /** right bottom y. */
-        private final int y2;
+		/** right bottom y. */
+		private final int y2;
 
-        /**
-         * Construct.
-         * @param x1 left upper x
-         * @param y1 left upper y
-         * @param x2 right bottom x
-         * @param y2 right bottom y
-         * @param link the link
-         */
-        public RectangleLink(final int x1, final int y1, final int x2, final int y2, final Link link)
-        {
-            super(link);
-            this.x1 = x1;
-            this.y1 = y1;
-            this.x2 = x2;
-            this.y2 = y2;
-        }
+		/**
+		 * Construct.
+		 * @param x1 left upper x
+		 * @param y1 left upper y
+		 * @param x2 right bottom x
+		 * @param y2 right bottom y
+		 * @param link the link
+		 */
+		public RectangleLink(final int x1, final int y1, final int x2, final int y2, final Link link)
+		{
+			super(link);
+			this.x1 = x1;
+			this.y1 = y1;
+			this.x2 = x2;
+			this.y2 = y2;
+		}
 
-        /**
-         * @see wicket.markup.html.link.ImageMap.ShapeLink#getType()
-         */
-        String getType()
-        {
-            return "rectangle";
-        }
+		/**
+		 * @see wicket.markup.html.link.ImageMap.ShapeLink#getType()
+		 */
+		String getType()
+		{
+			return "rectangle";
+		}
 
-        /**
-         * @see wicket.markup.html.link.ImageMap.ShapeLink#getCoordinates()
-         */
-        String getCoordinates()
-        {
-            return x1 + "," + y1 + "," + x2 + "," + y2;
-        }
-    }
+		/**
+		 * @see wicket.markup.html.link.ImageMap.ShapeLink#getCoordinates()
+		 */
+		String getCoordinates()
+		{
+			return x1 + "," + y1 + "," + x2 + "," + y2;
+		}
+	}
 
-    /**
-     * A shape that has a circle form.
-     */
-    private static final class CircleLink extends ShapeLink
-    {
-        /** left upper x. */
-        private final int x1;
+	/**
+	 * A shape that has a circle form.
+	 */
+	private static final class CircleLink extends ShapeLink
+	{
+		/** left upper x. */
+		private final int x1;
 
-        /** left upper y. */
-        private final int y1;
+		/** left upper y. */
+		private final int y1;
 
-        /** the circles' radius. */
-        private final int radius;
+		/** the circles' radius. */
+		private final int radius;
 
-        /**
-         * Construct.
-         * @param x1 left upper x
-         * @param y1 left upper y
-         * @param radius the circles' radius
-         * @param link the link
-         */
-        public CircleLink(final int x1, final int y1, final int radius, final Link link)
-        {
-            super(link);
-            this.x1 = x1;
-            this.y1 = y1;
-            this.radius = radius;
-        }
+		/**
+		 * Construct.
+		 * @param x1 left upper x
+		 * @param y1 left upper y
+		 * @param radius the circles' radius
+		 * @param link the link
+		 */
+		public CircleLink(final int x1, final int y1, final int radius, final Link link)
+		{
+			super(link);
+			this.x1 = x1;
+			this.y1 = y1;
+			this.radius = radius;
+		}
 
-        /**
-         * @see wicket.markup.html.link.ImageMap.ShapeLink#getType()
-         */
-        String getType()
-        {
-            return "circle";
-        }
+		/**
+		 * @see wicket.markup.html.link.ImageMap.ShapeLink#getType()
+		 */
+		String getType()
+		{
+			return "circle";
+		}
 
-        /**
-         * @see wicket.markup.html.link.ImageMap.ShapeLink#getCoordinates()
-         */
-        String getCoordinates()
-        {
-            return x1 + "," + y1 + "," + radius;
-        }
-    }
+		/**
+		 * @see wicket.markup.html.link.ImageMap.ShapeLink#getCoordinates()
+		 */
+		String getCoordinates()
+		{
+			return x1 + "," + y1 + "," + radius;
+		}
+	}
 }
