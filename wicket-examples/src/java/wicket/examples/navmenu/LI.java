decc8feb313a59f6639565faf435de7a93f63036
Eelco Hillenius <ehillenius@apache.org>
2005-02-15 22:10:24 +0000

    work in progress: user menu based on UL/LI tree and CSS
    git-svn-id: https://svn.apache.org/repos/asf/incubator/wicket/trunk@455890 13f79535-47bb-0310-9956-ffa450edef68

---
+++ wicket-examples/src/java/wicket/examples/navmenu/LI.java
@@ -0,0 +1,70 @@
+/*
+ * $Id$
+ * $Revision$
+ * $Date$
+ * 
+ * ==================================================================== Licensed
+ * under the Apache License, Version 2.0 (the "License"); you may not use this
+ * file except in compliance with the License. You may obtain a copy of the
+ * License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package wicket.examples.navmenu;
+
+import javax.swing.tree.DefaultMutableTreeNode;
+
+import wicket.AttributeModifier;
+import wicket.markup.html.basic.Label;
+import wicket.markup.html.link.BookmarkablePageLink;
+import wicket.markup.html.panel.Panel;
+import wicket.model.Model;
+
+/**
+ * This example list knows how to display sublists. It expects a list where
+ * each element is either a string or another list.
+ *
+ * @author Eelco Hillenius
+ */
+final class LI extends Panel
+{
+	/** the level this view is on. */
+	private final int level;
+
+    /**
+     * Constructor.
+     * @param componentName The name of this component
+     * @param node tree node
+     * @param level the level this view is on (from 0..n-1)
+     * @param index the sibling index
+     */
+    public LI(final String componentName, final DefaultMutableTreeNode node,
+    		final int level, final int index)
+    {
+        super(componentName);
+        this.level = level;
+        // add the row (with the LI element attached, and the label with the
+        // row's actual value to display
+        MenuItem menuItem = (MenuItem)node.getUserObject();
+        final String label = menuItem.getLabel();
+        final BookmarkablePageLink pageLink = new BookmarkablePageLink(
+        		"link", menuItem.getPageClass(), menuItem.getPageParameters());
+        pageLink.setAutoEnable(false);
+        pageLink.add(new Label("label", label));
+        add(pageLink);
+        // TODO this works for one level, but what about nesting?
+		add(new AttributeModifier("class", true, new Model()
+		{
+			public Object getObject()
+			{
+				return (pageLink.linksTo(getPage())) ? "selectedTab" : null;
+			}
+		}));
+    }
+}
