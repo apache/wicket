4597baa6681bd583ed9e3567cb0cff96a7ecd070
Juegen Donnerstag <jdonnerstag@apache.org>
2005-02-15 21:50:42 +0000

    additional tests for <wicket:component>
    git-svn-id: https://svn.apache.org/repos/asf/incubator/wicket/trunk/wicket@455889 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/wicket/markup/WicketTagComponentResolver.java
+++ src/java/wicket/markup/WicketTagComponentResolver.java
@@ -30,6 +30,7 @@ import org.apache.commons.logging.LogFactory;
 import wicket.Component;
 import wicket.MarkupContainer;
 import wicket.WicketRuntimeException;
+import wicket.util.string.StringValue;
 import wicket.util.string.StringValueConversionException;
 
 /**
@@ -216,7 +217,7 @@ public class WicketTagComponentResolver implements IComponentResolver
         {
             final Map.Entry entry = (Map.Entry)iter.next();
             final String key = (String)entry.getKey();
-            final String value = (String)entry.getKey();
+            final String value = ((StringValue)entry.getValue()).toString();
 
             // Ignore attributes 'name' and 'class'
             if ("name".equalsIgnoreCase(key) || ("class".equalsIgnoreCase(key)))
