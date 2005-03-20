d62d62d33fb78775533bf230181d06d0fbee6230
Juegen Donnerstag <jdonnerstag@apache.org>
2005-03-20 11:25:30 +0000

    new components Loop and LoopItem
    git-svn-id: https://svn.apache.org/repos/asf/incubator/wicket/trunk@456262 13f79535-47bb-0310-9956-ffa450edef68

---
+++ wicket/src/java/wicket/markup/html/list/LoopItem.java
@@ -0,0 +1,54 @@
+/*
+ * $Id$ $Revision:
+ * 1.7 $ $Date$
+ * 
+ * ==============================================================================
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package wicket.markup.html.list;
+
+import wicket.markup.html.WebMarkupContainer;
+
+/**
+ * Items of the Loop.
+ * 
+ * @author Jonathan Locke
+ */
+public class LoopItem extends WebMarkupContainer
+{
+	/** The index of the ListItem in the parent listView */
+	private final int index;
+
+	/**
+	 * A constructor which uses the index and the list provided to create a
+	 * ListItem. This constructor is the default one.
+	 * 
+	 * @param index The index of the item
+	 */
+	protected LoopItem(final int index)
+	{
+		super(Integer.toString(index), null);
+		this.index = index;
+	}
+
+	/**
+	 * Gets the index of the listItem in the parent listView.
+	 * 
+	 * @return The index of this listItem in the parent listView
+	 */
+	public final int getIndex()
+	{
+		return index;
+	}
+}
+
