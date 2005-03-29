2941b4214cf384a05a1f26863fbde59b53327fc7
Jonathan Locke <jonl@apache.org>
2005-03-29 15:26:23 +0000

    Fixes for clustering problems?
    git-svn-id: https://svn.apache.org/repos/asf/incubator/wicket/trunk@456361 13f79535-47bb-0310-9956-ffa450edef68

--- wicket-examples/src/java/wicket/examples/images/Home.java
+++ wicket-examples/src/java/wicket/examples/images/Home.java
@@ -17,10 +17,18 @@
  */
 package wicket.examples.images;
 
+import java.awt.BasicStroke;
 import java.awt.Graphics2D;
+import java.awt.image.BufferedImage;
+import java.util.Random;
 
+import wicket.ISharedResourceFactory;
+import wicket.Resource;
+import wicket.SharedResource;
 import wicket.examples.WicketExamplePage;
 import wicket.markup.html.image.Image;
+import wicket.markup.html.image.resource.BufferedDynamicImageResource;
+import wicket.markup.html.image.resource.DefaultButtonImageResource;
 import wicket.markup.html.image.resource.RenderedDynamicImageResource;
 import wicket.model.Model;
 
@@ -31,6 +39,9 @@ import wicket.model.Model;
  */
 public final class Home extends WicketExamplePage
 {
+	static final DefaultButtonImageResource cancelButtonImageResource = new DefaultButtonImageResource(
+			30, 30, "Cancel!");
+
 	/**
 	 * Constructor
 	 */
@@ -38,16 +49,17 @@ public final class Home extends WicketExamplePage
 	{
 		// Get our custom application subclass
 		final ImagesApplication application = (ImagesApplication)getApplication();
-		
+
 		// Image as package resource
 		add(new Image("image2"));
 
-		// Dynamically created image.  Will re-render whenever resource is asked for.
+		// Dynamically created image. Will re-render whenever resource is asked
+		// for.
 		add(new Image("image3", new RenderedDynamicImageResource(100, 100)
 		{
 			protected boolean render(Graphics2D graphics)
 			{
-				application.drawCircle(graphics);
+				drawCircle(graphics);
 				return true;
 			}
 		}));
@@ -56,9 +68,50 @@ public final class Home extends WicketExamplePage
 		add(new Image("image4", new Model("Image2.gif")));
 
 		// Dynamically created buffered image
-		add(new Image("image5", application.getImage5Resource()));
-		
+		add(new Image("image5", getImage5Resource()));
+
 		// Add cancel button image
-		add(new Image("cancelButton", application.getCancelButtonImageResource()));
+		add(new Image("cancelButton", cancelButtonImageResource.getShared(getApplication(),
+				Home.class, "cancel")));
+	}
+
+	/**
+	 * @return Gets shared image component
+	 */
+	public SharedResource getImage5Resource()
+	{
+		return getApplication().getSharedResource(Home.class, "image5",
+				new ISharedResourceFactory()
+				{
+					public Resource newResource()
+					{
+						final BufferedDynamicImageResource resource = new BufferedDynamicImageResource();
+						final BufferedImage image = new BufferedImage(100, 100,
+								BufferedImage.TYPE_INT_RGB);
+						drawCircle((Graphics2D)image.getGraphics());
+						resource.setImage(image);
+						return resource;
+					}
+				});
+	}
+
+	/**
+	 * Draws a random circle on a graphics
+	 * 
+	 * @param graphics
+	 *            The graphics to draw on
+	 */
+	void drawCircle(Graphics2D graphics)
+	{
+		// Compute random size for circle
+		final Random random = new Random();
+		int dx = Math.abs(10 + random.nextInt(80));
+		int dy = Math.abs(10 + random.nextInt(80));
+		int x = Math.abs(random.nextInt(100 - dx));
+		int y = Math.abs(random.nextInt(100 - dy));
+
+		// Draw circle with thick stroke width
+		graphics.setStroke(new BasicStroke(5));
+		graphics.drawOval(x, y, dx, dy);
 	}
 }
--- wicket-examples/src/java/wicket/examples/images/ImagesApplication.java
+++ wicket-examples/src/java/wicket/examples/images/ImagesApplication.java
@@ -18,14 +18,7 @@
  */
 package wicket.examples.images;
 
-import java.awt.BasicStroke;
-import java.awt.Graphics2D;
-import java.awt.image.BufferedImage;
-import java.util.Random;
-
 import wicket.examples.WicketExampleApplication;
-import wicket.markup.html.image.resource.BufferedDynamicImageResource;
-import wicket.markup.html.image.resource.DefaultButtonImageResource;
 
 /**
  * WicketServlet class for wicket.examples.linkomatic example.
@@ -33,9 +26,6 @@ import wicket.markup.html.image.resource.DefaultButtonImageResource;
  */
 public class ImagesApplication extends WicketExampleApplication
 {
-	private BufferedDynamicImageResource image5Resource;
-	private DefaultButtonImageResource cancelButtonImageResource;
-	
 	/**
 	 * Constructor
 	 */
@@ -43,49 +33,7 @@ public class ImagesApplication extends WicketExampleApplication
     {
         getPages().setHomePage(Home.class);
         
-	    cancelButtonImageResource = new DefaultButtonImageResource("Cancel");
     }
-    
-    /**
-     * @return Gets shared image component
-     */
-    public BufferedDynamicImageResource getImage5Resource()
-    {
-    	if (image5Resource == null)
-    	{
-		    image5Resource = new BufferedDynamicImageResource();
-			final BufferedImage image = new BufferedImage(100, 100, BufferedImage.TYPE_INT_RGB);
-			drawCircle((Graphics2D)image.getGraphics());
-			image5Resource.setImage(image);
-    	}
-    	return image5Resource;
-    }
-    
-    /**
-     * @return Cancel button image
-     */
-    public DefaultButtonImageResource getCancelButtonImageResource()
-    {
-    	return cancelButtonImageResource;
-    }
-    
-	/**
-	 * Draws a random circle on a graphics
-	 * @param graphics The graphics to draw on
-	 */
-	void drawCircle(Graphics2D graphics)
-	{
-		// Compute random size for circle
-		final Random random = new Random();
-		int dx = Math.abs(10 + random.nextInt(80));
-		int dy = Math.abs(10 + random.nextInt(80));
-		int x = Math.abs(random.nextInt(100 - dx));
-		int y = Math.abs(random.nextInt(100 - dy));
-		
-		// Draw circle with thick stroke width
-		graphics.setStroke(new BasicStroke(5));
-		graphics.drawOval(x, y, dx, dy);
-	}
 }
 
 
