e61ccd98dc4cad153a5392dbf89d2502a923e42a
Juegen Donnerstag <jdonnerstag@apache.org>
2005-05-29 14:10:46 +0000

    revert back to previous version
    git-svn-id: https://svn.apache.org/repos/asf/incubator/wicket/trunk@456793 13f79535-47bb-0310-9956-ffa450edef68

--- wicket/src/java/wicket/protocol/http/WebApplication.java
+++ wicket/src/java/wicket/protocol/http/WebApplication.java
@@ -33,11 +33,8 @@ import wicket.AutoLinkResolver;
 import wicket.ISessionFactory;
 import wicket.Session;
 import wicket.WicketRuntimeException;
-import wicket.markup.MarkupParser;
 import wicket.markup.html.pages.InternalErrorPage;
 import wicket.markup.html.pages.PageExpiredErrorPage;
-import wicket.markup.parser.XmlPullParser;
-import wicket.markup.parser.filter.HtmlHeadTagHandler;
 import wicket.response.BufferedResponse;
 
 /**
@@ -307,21 +304,5 @@ public abstract class WebApplication extends Application
 			}
 		}
 		sessionMap.put(requestUri, renderedResponse);
-	}
-
-	/**
-	 * @see wicket.Application#newMarkupParser()
-	 */
-	public MarkupParser newMarkupParser()
-	{
-		final MarkupParser parser = new MarkupParser(new XmlPullParser())
-		{
-			public void initFilterChain()
-			{
-				appendMarkupFilter(new HtmlHeadTagHandler());
-			}
-		};
-		parser.configure(getSettings());
-		return parser;
 	}
 }
