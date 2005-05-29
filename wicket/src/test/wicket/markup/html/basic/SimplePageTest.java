178841cf063a9b38b69a2ca154063e32348e7444
Juegen Donnerstag <jdonnerstag@apache.org>
2005-05-29 09:40:54 +0000

    by means of Component.setRenderBodyOnly(boolean) it is now easily possible to print a component tag's body only. This is e.g. necessary with <span> tags and Panels where in cases you want to be strictly HTML compliant the <span> tag must be removed.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/wicket/branches/WICKET_1_0_0@456778 13f79535-47bb-0310-9956-ffa450edef68

---
+++ wicket/src/test/wicket/markup/html/basic/SimplePageTest.java
@@ -0,0 +1,82 @@
+/*
+ * $Id$
+ * $Revision$
+ * $Date$
+ *
+ * ====================================================================
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package wicket.markup.html.basic;
+
+import junit.framework.TestCase;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+import wicket.markup.html.list.DiffUtil;
+import wicket.protocol.http.MockWebApplication;
+
+/**
+ * Simple application that demonstrates the mock http application code (and
+ * checks that it is working)
+ * 
+ * @author Chris Turner
+ */
+public class SimplePageTest extends TestCase
+{
+	private static Log log = LogFactory.getLog(SimplePageTest.class);
+
+	private MockWebApplication application;
+
+	/**
+	 * Create the test.
+	 * 
+	 * @param name
+	 *            The test name
+	 */
+	public SimplePageTest(String name)
+	{
+		super(name);
+	}
+
+	/**
+	 * @throws Exception
+	 */
+	public void testRenderHomePage() throws Exception
+	{
+	    executeTest(SimplePage.class, "SimplePageExpectedResult.html");
+	}
+
+	/**
+	 * @param pageClass
+	 * @param filename
+	 * @throws Exception
+	 */
+	public void executeTest(final Class pageClass, final String filename) throws Exception
+	{
+		System.out.println("=== " + pageClass.getName() + " ===");
+		
+		application = new MockWebApplication(null);
+		application.getPages().setHomePage(pageClass);
+
+		// Do the processing
+		application.setupRequestAndResponse();
+		application.processRequestCycle();
+
+		// Validate the document
+		String document = application.getServletResponse().getDocument();
+		System.out.println(document);
+
+		assertTrue(DiffUtil.validatePage(document, this.getClass(), filename));
+	}
+}
