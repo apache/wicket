e854cf1712a4e09b75d696fd7ea4c60e607e23b6
Johan Compagner <jcompagner@apache.org>
2005-06-04 14:48:57 +0000

    ResourceStreamLocator should be a IResourceStreamLocator
    git-svn-id: https://svn.apache.org/repos/asf/incubator/wicket/trunk@456843 13f79535-47bb-0310-9956-ffa450edef68

--- wicket/src/java/wicket/util/resource/locator/ResourceStreamLocator.java
+++ wicket/src/java/wicket/util/resource/locator/ResourceStreamLocator.java
@@ -31,7 +31,7 @@ import wicket.util.string.Strings;
  * @author Juergen Donnerstag
  * @author Jonathan Locke
  */
-public class ResourceStreamLocator
+public class ResourceStreamLocator implements IResourceStreamLocator
 {
 	/** Logging */
 	private static Log log = LogFactory.getLog(ResourceStreamLocator.class);
@@ -80,7 +80,7 @@ public class ResourceStreamLocator
 	public IResourceStream locate(final Class c, final String style, final Locale locale,
 			final String extension)
 	{
-		return locate(c.getName(), style, locale, extension);
+		return locate(c.getName().replace('.','/'), style, locale, extension);
 	}
 
 	/**
@@ -118,11 +118,6 @@ public class ResourceStreamLocator
 			{
 				extensionString = "." + extension;
 			}
-		}
-		int index = path.indexOf("/");
-		if(index == -1)
-		{
-			path = path.replace('.', '/');
 		}
 
 		return locator.locate(path, style, locale, extensionString);
