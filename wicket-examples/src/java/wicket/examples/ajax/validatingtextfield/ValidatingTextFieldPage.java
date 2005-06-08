b9cefed8ea942a9ebdea352f995616fa8d36e0ef
Johan Compagner <jcompagner@apache.org>
2005-06-08 22:25:11 +0000

    ValidatingTextFieldExample
    git-svn-id: https://svn.apache.org/repos/asf/incubator/wicket/trunk@456934 13f79535-47bb-0310-9956-ffa450edef68

---
+++ wicket-examples/src/java/wicket/examples/ajax/validatingtextfield/ValidatingTextFieldPage.java
@@ -0,0 +1,31 @@
+package wicket.examples.ajax.validatingtextfield;
+
+import wicket.markup.html.WebPage;
+import wicket.markup.html.form.Form;
+import wicket.markup.html.form.ajax.AjaxValidationTextField;
+import wicket.markup.html.form.validation.LengthValidator;
+import wicket.markup.html.panel.FeedbackPanel;
+import wicket.model.Model;
+
+/**
+ * @author jcompagner
+ * @version $Id$
+ */
+public class ValidatingTextFieldPage extends WebPage
+{
+	/**
+	 * Simple pages with a form/feedback panel and a AjaxValidation textfield 
+	 */
+	public ValidatingTextFieldPage()
+	{
+		
+		FeedbackPanel feedback = new FeedbackPanel("feedback");
+		add(feedback);
+		Form form = new Form("form",feedback) {};
+		AjaxValidationTextField tf = new AjaxValidationTextField("validating",feedback,new Model());
+		tf.add(LengthValidator.range(2, 5));
+		form.add(tf);
+		add(form);
+		
+	}
+}
