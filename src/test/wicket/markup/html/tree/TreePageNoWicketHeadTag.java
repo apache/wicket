674d0009ddfaa6736b81fba17ca20ff409f733d6
Eelco Hillenius <ehillenius@apache.org>
2005-06-12 12:48:15 +0000

    some more tests with automatic header insertion
    git-svn-id: https://svn.apache.org/repos/asf/incubator/wicket/trunk/wicket@456945 13f79535-47bb-0310-9956-ffa450edef68

--- src/test/wicket/markup/html/tree/TreePageNoWicketHeadTag.java
+++ src/test/wicket/markup/html/tree/TreePageNoWicketHeadTag.java
@@ -21,13 +21,13 @@ import wicket.PageParameters;
 
 /**
  */
-public class TreePage extends AbstractTreePage
+public class TreePageNoWicketHeadTag extends AbstractTreePage
 {
 	/**
 	 * Constructor.
 	 * @param parameters Page parameters
 	 */
-	public TreePage(final PageParameters parameters)
+	public TreePageNoWicketHeadTag(final PageParameters parameters)
 	{
 		super(parameters);
 	}
