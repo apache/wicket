e32a2b2eb336ad4a5dc24d508aa1c678c9fd44b9
sourceforge-skipoles <sourceforge-skipoles@apache.org>
2005-07-01 07:54:32 +0000

    Added recursing the class hierarchy when looking for resources such that each component now looks at its own resource bundle plus the resource bundles of any of its superclasses.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/wicket/trunk@456993 13f79535-47bb-0310-9956-ffa450edef68

--- wicket/src/java/wicket/resource/ComponentStringResourceLoader.java
+++ wicket/src/java/wicket/resource/ComponentStringResourceLoader.java
@@ -1,14 +1,14 @@
 /*
  * $Id: ComponentStringResourceLoader.java,v 1.5 2005/01/19 08:07:57
  * jonathanlocke Exp $ $Revision$ $Date$
- * 
+ *
  * ==============================================================================
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not
  * use this file except in compliance with the License. You may obtain a copy of
  * the License at
- * 
+ *
  * http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
  * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
@@ -24,16 +24,19 @@ import java.util.Map;
 import java.util.Properties;
 import java.util.Stack;
 
+import EDU.oswego.cs.dl.util.concurrent.ConcurrentReaderHashMap;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 
 import wicket.Component;
 import wicket.MarkupContainer;
 import wicket.Page;
+import wicket.markup.html.WebComponent;
+import wicket.markup.html.WebMarkupContainer;
+import wicket.markup.html.WebPage;
 import wicket.util.resource.IResourceStream;
 import wicket.util.resource.ResourceStreamNotFoundException;
 import wicket.util.value.ValueMap;
-import EDU.oswego.cs.dl.util.concurrent.ConcurrentReaderHashMap;
 
 /**
  * This string resource loader attempts to find the resource from a bundle that
@@ -65,7 +68,17 @@ import EDU.oswego.cs.dl.util.concurrent.ConcurrentReaderHashMap;
  * <p>
  * This implementation is fully aware of both locale and style values when
  * trying to obtain the appropriate resources.
- * 
+ * <p>
+ * In addition to the above search order, each component that is being searched
+ * for a resource also includes the resources from any parent classes that it
+ * inherits from. For example, PageA extends CommonBasePage which in turn extends
+ * WebPage. When a resource lookup is requested on PageA, the resource bundle for
+ * PageA is first checked. If the resource is not found in this bundle then the
+ * resource bundle for CommonBasePage is checked. This allows designers of base
+ * pages and components to define default sets of string resources and then
+ * developers implementing subclasses to either override or extend these in their
+ * own resource bundle.
+ *
  * @author Chris Turner
  */
 public class ComponentStringResourceLoader implements IStringResourceLoader
@@ -93,7 +106,7 @@ public class ComponentStringResourceLoader implements IStringResourceLoader
 	 * component is not an instance of <code>Page</code> and has not been
 	 * previously added to a <code>Page</code> then an exception will be
 	 * thrown.
-	 * 
+	 *
 	 * @param component
 	 *            The component to use to find resources to be loaded
 	 * @param key
@@ -132,17 +145,27 @@ public class ComponentStringResourceLoader implements IStringResourceLoader
 			while (!searchStack.isEmpty())
 			{
 				Component c = (Component)searchStack.pop();
+				Class cc = c.getClass();
 
-				// Locate previously loaded resources from the cache
-				final String id = createCacheId(c, style, locale);
-				ValueMap strings = (ValueMap)resourceCache.get(id);
-				if (strings == null)
-				{
-					// No resources previously loaded, attempt to load them
-					strings = loadResources(c, style, locale, id);
+				while ( value == null ) {
+					// Locate previously loaded resources from the cache
+					final String id = createCacheId(cc, style, locale);
+					ValueMap strings = (ValueMap)resourceCache.get(id);
+					if (strings == null)
+					{
+						// No resources previously loaded, attempt to load them
+						strings = loadResources(c, cc, style, locale, id);
+					}
+
+					// Lookup value
+					value = strings.getString(key);
+					if (value != null)
+						break;
+
+					// Move to next superclass
+					cc = cc.getSuperclass();
+					if (isStopResourceSearch(cc)) break;
 				}
-
-				value = strings.getString(key);
 				if (value != null)
 					break;
 			}
@@ -154,10 +177,33 @@ public class ComponentStringResourceLoader implements IStringResourceLoader
 	}
 
 	/**
+	 * Check the supplied class to see if it is one that we shouldn't bother
+	 * further searches up the class hierarchy for properties.
+	 *
+	 * @param clazz
+	 *            The class to check
+	 * @return
+	 *            Whether to stop the search
+	 */
+	private boolean isStopResourceSearch(final Class clazz) {
+		if (clazz == null || clazz.equals(Object.class)) return true;
+
+		// Stop at all html markup base classes
+		if (clazz.equals(WebPage.class) || clazz.equals(WebMarkupContainer.class) ||
+				clazz.equals(WebComponent.class)) return true;
+
+		// Stop at all wicket base classes
+		return clazz.equals(Page.class) || clazz.equals(MarkupContainer.class) ||
+				clazz.equals(Component.class);
+	}
+
+	/**
 	 * Helper method to do the actual loading of resources if required.
-	 * 
+	 *
 	 * @param component
 	 *            The component that the resources are being loaded for
+	 * @param componentClass
+	 *            The class that resources are bring loaded for
 	 * @param style
 	 *            The style to load resources for (see {@link wicket.Session})
 	 * @param locale
@@ -166,8 +212,8 @@ public class ComponentStringResourceLoader implements IStringResourceLoader
 	 *            The cache id to use
 	 * @return The map of loaded resources
 	 */
-	private synchronized ValueMap loadResources(final Component component, final String style,
-			final Locale locale, final String id)
+	private synchronized ValueMap loadResources(final Component component, final Class componentClass,
+			final String style, final Locale locale, final String id)
 	{
 		// Make sure someone else didn't load our resources while we were
 		// waiting
@@ -181,7 +227,7 @@ public class ComponentStringResourceLoader implements IStringResourceLoader
 		// Do the resource load
 		final Properties properties = new Properties();
 		final IResourceStream resource = component.getApplication().getResourceStreamLocator().locate(
-				component.getClass(), style, locale, "properties");
+				componentClass, style, locale, "properties");
 		if (resource != null)
 		{
 			try
@@ -220,19 +266,19 @@ public class ComponentStringResourceLoader implements IStringResourceLoader
 	/**
 	 * Helper method to create a unique id for caching previously loaded
 	 * resources.
-	 * 
-	 * @param component
-	 *            The component that the resources are being loaded for
+	 *
+	 * @param componentClass
+	 *            The component class that the resources are being loaded for
 	 * @param style
 	 *            The style of the resources (see {@link wicket.Session})
 	 * @param locale
 	 *            The locale of the resources
 	 * @return The unique cache id
 	 */
-	private String createCacheId(final Component component, final String style, final Locale locale)
+	private String createCacheId(final Class componentClass, final String style, final Locale locale)
 	{
 		final StringBuffer buffer = new StringBuffer();
-		buffer.append(component.getClass().getName());
+		buffer.append(componentClass.getName());
 		if (style != null)
 		{
 			buffer.append('.');
