8815f831d9f5d3cae65a5d37b291d5e632fa67c7
Eelco Hillenius <ehillenius@apache.org>
2005-07-17 13:14:47 +0000

    seperated main method from application class
    git-svn-id: https://svn.apache.org/repos/asf/incubator/wicket/branches/WICKET_1_0_0@457091 13f79535-47bb-0310-9956-ffa450edef68

--- wicket-examples/src/java/wicket/examples/WicketExampleApplication.java
+++ wicket-examples/src/java/wicket/examples/WicketExampleApplication.java
@@ -18,11 +18,8 @@
  */
 package wicket.examples;
 
-import java.net.URL;
-
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
-import org.mortbay.jetty.Server;
 
 import wicket.protocol.http.WebApplication;
 import wicket.util.crypt.NoCrypt;
@@ -69,40 +66,5 @@ public abstract class WicketExampleApplication extends WebApplication
 	        // Use development settings 
 	        getSettings().configure("development");
 	    }
-	}
-    
-	/**
-	 * Main function, starts the jetty server.
-	 * 
-	 * @param args
-	 */
-	public static void main(String[] args)
-	{
-        Server jettyServer = null;
-		try
-		{
-			URL jettyConfig = new URL("file:src/etc/jetty-config.xml");
-			if (jettyConfig == null)
-			{
-				log.fatal("Unable to locate jetty-test-config.xml on the classpath");
-			}
-			jettyServer = new Server(jettyConfig);
-			jettyServer.start();
-		}
-		catch (Exception e)
-		{
-			log.fatal("Could not start the Jetty server: " + e);
-			if (jettyServer != null)
-			{
-				try
-				{
-					jettyServer.stop();
-				}
-				catch (InterruptedException e1)
-				{
-					log.fatal("Unable to stop the jetty server: " + e1);
-				}
-			}
-		}
 	}
 }
