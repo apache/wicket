3dd59e8424bb4acace6aa52ccc5fafd00a12c3f2
Eelco Hillenius <ehillenius@apache.org>
2005-08-01 17:25:26 +0000

    javadoc
    git-svn-id: https://svn.apache.org/repos/asf/incubator/wicket/trunk/wicket@457238 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/wicket/markup/html/form/RadioChoice.java
+++ src/java/wicket/markup/html/form/RadioChoice.java
@@ -26,7 +26,22 @@ import wicket.util.string.Strings;
 
 /**
  * A choice subclass that shows choices in radio style.
- * TODO elaborate with an example
+ * <p>
+ * Java:
+ * <pre>
+ * 	List SITES = Arrays.asList(new String[] { "The Server Side", "Java Lobby", "Java.Net" });
+ *	// Add a radio choice component that uses Input's 'site' property to designate the
+ *	// current selection, and that uses the SITES list for the available options.
+ *	form.add(new RadioChoice("site", SITES));
+ * </pre>
+ * HTML:
+ * <pre>
+ * &lt;span valign="top" wicket:id="site"&gt;
+ *	&lt;input type="radio"&gt;site 1&lt;/input&gt;
+ *	&lt;input type="radio"&gt;site 2&lt;/input&gt;
+ * &lt;/span&gt;
+ * </pre>
+ * </p>
  * 
  * <p>
  * You can can extend this class and override method wantOnSelectionChangedNotifications()
