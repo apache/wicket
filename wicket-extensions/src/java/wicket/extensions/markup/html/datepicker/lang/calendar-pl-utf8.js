67f495e498e8957c243c712ec7b34ac6f4137059
Eelco Hillenius <ehillenius@apache.org>
2005-08-15 11:50:50 +0000

    fix for Polish locale
    git-svn-id: https://svn.apache.org/repos/asf/incubator/wicket/trunk@457376 13f79535-47bb-0310-9956-ffa450edef68

--- wicket-extensions/src/java/wicket/extensions/markup/html/datepicker/lang/calendar-pl-utf8.js
+++ wicket-extensions/src/java/wicket/extensions/markup/html/datepicker/lang/calendar-pl-utf8.js
@@ -1,11 +1,16 @@
-﻿// ** I18N
+// ** I18N
 
 // Calendar PL language
-// Author: Dariusz Pietrzak, <eyck@ghost.anime.pl>
-// Author: Janusz Piwowarski, <jpiw@go2.pl>
+// Author: Mihai Bazon, <mihai_bazon@yahoo.com>
+// Polish translation by Dariusz Wojtas
 // Encoding: utf-8
 // Distributed under the same terms as the calendar itself.
 
+// For translators: please use UTF-8 if possible.  We strongly believe that
+// Unicode is the answer to a real internationalized world.  Also please
+// include your contact information in the header, as can be seen above.
+
+// full day names
 Calendar._DN = new Array
 ("Niedziela",
  "Poniedziałek",
@@ -15,15 +20,35 @@ Calendar._DN = new Array
  "Piątek",
  "Sobota",
  "Niedziela");
+
+// Please note that the following array of short day names (and the same goes
+// for short month names, _SMN) isn't absolutely necessary.  We give it here
+// for exemplification on how one can customize the short day names, but if
+// they are simply the first N letters of the full name you can simply say:
+//
+//   Calendar._SDN_len = N; // short day name length
+//   Calendar._SMN_len = N; // short month name length
+//
+// If N = 3 then this is not needed either since we assume a value of 3 if not
+// present, to be compatible with translation files that were written before
+// this feature.
+
+// short day names
 Calendar._SDN = new Array
 ("Nie",
- "Pn",
- "Wt",
- "Śr",
- "Cz",
- "Pt",
- "So",
+ "Pon",
+ "Wto",
+ "Śro",
+ "Czw",
+ "Pią",
+ "Sob",
  "Nie");
+
+// First day of the week. "0" means display Sunday first, "1" means display
+// Monday first, etc.
+Calendar._FD = 1;
+
+// full month names
 Calendar._MN = new Array
 ("Styczeń",
  "Luty",
@@ -37,6 +62,8 @@ Calendar._MN = new Array
  "Październik",
  "Listopad",
  "Grudzień");
+
+// short month names
 Calendar._SMN = new Array
 ("Sty",
  "Lut",
@@ -51,47 +78,52 @@ Calendar._SMN = new Array
  "Lis",
  "Gru");
 
-// First day of the week. "0" means display Sunday first, "1" means display
-// Monday first, etc.
-Calendar._FD = 1;
-
 // tooltips
 Calendar._TT = {};
 Calendar._TT["INFO"] = "O kalendarzu";
 
 Calendar._TT["ABOUT"] =
-"DHTML Date/Time Selector\n" +
+"DHTML Wybór daty i czasu\n" +
 "(c) dynarch.com 2002-2005 / Author: Mihai Bazon\n" + // don't translate this this ;-)
-"Aby pobrać najnowszą wersję, odwiedź: http://www.dynarch.com/projects/calendar/\n" +
-"Dostępny na licencji GNU LGPL. Zobacz szczegóły na http://gnu.org/licenses/lgpl.html." +
+"Po najnowszą wersję odwiedź: http://www.dynarch.com/projects/calendar/\n" +
+"Dystrybuowane na licencji GNU LGPL. Zobacz http://gnu.org/licenses/lgpl.html po szczegóły." +
 "\n\n" +
 "Wybór daty:\n" +
-"- Użyj przycisków \xab, \xbb by wybrać rok\n" +
-"- Użyj przycisków " + String.fromCharCode(0x2039) + ", " + String.fromCharCode(0x203a) + " by wybrać miesiąc\n" +
-"- Przytrzymaj klawisz myszy nad jednym z powyższych przycisków dla szybszego wyboru.";
+"- Użyj przycisków \xab, \xbb aby wybrać rok\n" +
+"- Użyj przycisków " + String.fromCharCode(0x2039) + ", " + String.fromCharCode(0x203a) + " aby wybrać miesiąc\n" +
+"- Przytrzymaj klawisz myszy na dowolnym z tych przycisków dla szybszego wyboru.";
 Calendar._TT["ABOUT_TIME"] = "\n\n" +
 "Wybór czasu:\n" +
-"- Kliknij na jednym z pól czasu by zwiększyć jego wartość\n" +
-"- lub kliknij trzymając Shift by zmiejszyć jego wartość\n" +
-"- lub kliknij i przeciągnij dla szybszego wyboru.";
+"- Kliknij na dowolnej części czasu aby ją zwiększyć\n" +
+"- lub kliknij z klawiszem SHIFT aby ją zmniejszyć\n" +
+"- lub kliknij i przeciągaj by wybrać szybciej.";
 
-//Calendar._TT["TOGGLE"] = "Zmień pierwszy dzień tygodnia";
 Calendar._TT["PREV_YEAR"] = "Poprzedni rok (przytrzymaj dla menu)";
 Calendar._TT["PREV_MONTH"] = "Poprzedni miesiąc (przytrzymaj dla menu)";
-Calendar._TT["GO_TODAY"] = "Idź do dzisiaj";
+Calendar._TT["GO_TODAY"] = "Dzisiejsza data";
 Calendar._TT["NEXT_MONTH"] = "Następny miesiąc (przytrzymaj dla menu)";
 Calendar._TT["NEXT_YEAR"] = "Następny rok (przytrzymaj dla menu)";
 Calendar._TT["SEL_DATE"] = "Wybierz datę";
-Calendar._TT["DRAG_TO_MOVE"] = "Przeciągnij by przesunąć";
-Calendar._TT["PART_TODAY"] = " (dzisiaj)";
-Calendar._TT["MON_FIRST"] = "Wyświetl poniedziałek jako pierwszy";
-Calendar._TT["SUN_FIRST"] = "Wyświetl niedzielę jako pierwszą";
+Calendar._TT["DRAG_TO_MOVE"] = "Przeciągnij by przenieść";
+Calendar._TT["PART_TODAY"] = " (dziś)";
+
+// the following is to inform that "%s" is to be the first day of week
+// %s will be replaced with the day name.
+Calendar._TT["DAY_FIRST"] = "Wyświetl %s najpierw";
+
+// This may be locale-dependent.  It specifies the week-end days, as an array
+// of comma-separated numbers.  The numbers are from 0 to 6: 0 means Sunday, 1
+// means Monday, etc.
+Calendar._TT["WEEKEND"] = "0,6";
+
 Calendar._TT["CLOSE"] = "Zamknij";
 Calendar._TT["TODAY"] = "Dzisiaj";
-Calendar._TT["TIME_PART"] = "(Shift-)Kliknij lub przeciągnij by zmienić wartość";
+Calendar._TT["TIME_PART"] = "(Shift+)Klik lub przenieś by zmienić";
 
 // date formats
 Calendar._TT["DEF_DATE_FORMAT"] = "%Y-%m-%d";
-Calendar._TT["TT_DATE_FORMAT"] = "%e %B, %A";
+Calendar._TT["TT_DATE_FORMAT"] = "%a, %e %b";
 
 Calendar._TT["WK"] = "ty";
+Calendar._TT["TIME"] = "Czas:";
+
