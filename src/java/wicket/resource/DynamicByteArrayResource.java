2449f905df9ad08ef51e8ea7a5de965ee034d74e
Johan Compagner <jcompagner@apache.org>
2005-08-27 21:11:46 +0000

    resource refactor, added byte resources
    git-svn-id: https://svn.apache.org/repos/asf/incubator/wicket/trunk/wicket@457520 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/java/wicket/resource/DynamicByteArrayResource.java
@@ -0,0 +1,171 @@
+/**
+ * 
+ */
+package wicket.resource;
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+import wicket.markup.html.WebResource;
+import wicket.util.resource.IResourceStream;
+import wicket.util.resource.ResourceStreamNotFoundException;
+import wicket.util.time.Duration;
+import wicket.util.time.Time;
+
+/**
+ * @author jcompagner
+ *
+ */
+public abstract class DynamicByteArrayResource extends WebResource
+{
+	/** The time this image resource was last modified */
+	private Time lastModifiedTime;
+
+	/** The maximum duration a resource can be idle before its cache is flushed */
+	private Duration cacheTimeout = Duration.NONE;
+
+	/** The content type of this byte array */
+	private String contentType;
+	
+	/**
+	 * @param contentType
+	 */
+	public DynamicByteArrayResource(String contentType)
+	{
+		this.contentType = contentType;
+	}
+	
+	/**
+	 * @return Gets the image resource to attach to the component.
+	 */
+	public IResourceStream getResourceStream()
+	{
+		return new IResourceStream()
+		{
+			/** Transient input stream to resource */
+			private transient InputStream inputStream = null;
+			
+			/** Transient byte array of the resources, will always be deleted in the close*/
+			private transient byte[] data = null; 
+
+			/**
+			 * @see wicket.util.resource.IResourceStream#close()
+			 */
+			public void close() throws IOException
+			{
+				if (inputStream != null)
+				{
+					inputStream.close();
+					inputStream = null;
+				}
+				data = null;
+			}
+
+			/**
+			 * @see wicket.util.resource.IResourceStream#getContentType()
+			 */
+			public String getContentType()
+			{
+				return contentType;
+			}
+
+			/**
+			 * @see wicket.util.resource.IResourceStream#getInputStream()
+			 */
+			public InputStream getInputStream() throws ResourceStreamNotFoundException
+			{
+				if(data == null)
+				{
+					data = getData();
+				}
+				if (inputStream == null)
+				{
+					inputStream = new ByteArrayInputStream(data);
+				}
+				return inputStream;
+			}
+
+			/**
+			 * @see wicket.util.watch.IModifiable#lastModifiedTime()
+			 */
+			public Time lastModifiedTime()
+			{
+				return DynamicByteArrayResource.this.lastModifiedTime();
+			}
+
+			public long length()
+			{
+				if(data == null)
+				{
+					data = getData();
+				}
+				return (data != null) ? data.length : 0;
+			}
+		};
+	}
+
+	/**
+	 * @return The last time this image resource was modified
+	 */
+	public final Time lastModifiedTime()
+	{
+		return lastModifiedTime;
+	}
+
+	/**
+	 * @param lastModifiedTime 
+	 */
+	public final void setLastModifiedTime(Time lastModifiedTime)
+	{
+		this.lastModifiedTime = lastModifiedTime;
+	}
+
+
+	/**
+	 * @return The content type of the byte array
+	 */
+	public final String getContentType()
+	{
+		return this.contentType;
+	}
+
+	/**
+	 * @param contentType The content type of the byte array
+	 */
+	public final void setContentType(String contentType)
+	{
+		this.contentType = contentType;
+	}
+	
+	
+	/**
+	 * Set the maximum duration the resource can be idle before its cache is flushed.
+	 * The cache might get flushed sooner if the JVM is low on memory.
+	 * 
+	 * @param value The cache timout 
+	 */
+	public final void setCacheTimeout(Duration value)
+	{
+		cacheTimeout = value;
+	}
+
+	/**
+	 * Returns the maximum duration the resource can be idle before its cache is flushed.
+	 * 
+	 * @return The cache timeout 
+	 */
+	public final Duration getCacheTimeout()
+	{
+		return cacheTimeout;
+	}
+
+	/**
+	 * Get byte array for our dynamic resource. If the subclass
+	 * regenerates the data, it should set the lastModifiedTime when it does so.
+	 * This ensures that image caching works correctly.
+	 *
+	 * @return The byte array for this dynamic resource.
+	 */
+	protected abstract byte[] getData();
+}
