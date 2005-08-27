7e4643e6f960c30ea6ed5242eaf57de1c62258ed
Johan Compagner <jcompagner@apache.org>
2005-08-27 21:11:46 +0000

    resource refactor, added byte resources
    git-svn-id: https://svn.apache.org/repos/asf/incubator/wicket/trunk@457520 13f79535-47bb-0310-9956-ffa450edef68

---
+++ wicket/src/java/wicket/resource/ByteArrayResource.java
@@ -0,0 +1,90 @@
+/**
+ * 
+ */
+package wicket.resource;
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+import wicket.markup.html.WebResource;
+import wicket.util.resource.IResourceStream;
+import wicket.util.resource.ResourceStreamNotFoundException;
+import wicket.util.time.Time;
+
+/**
+ * @author jcompagner
+ *
+ */
+public class ByteArrayResource extends WebResource
+{
+	private final String contentType;
+	private final byte[] array;
+	private final Time lastModified = Time.now();
+
+	/**
+	 * @param contentType
+	 * @param array
+	 */
+	public ByteArrayResource(String contentType, byte[] array)
+	{
+		this.contentType = contentType;
+		this.array = array;
+	}
+
+	/**
+	 * @see wicket.Resource#getResourceStream()
+	 */
+	public IResourceStream getResourceStream()
+	{
+		return new IResourceStream()
+		{
+			/** Transient input stream to resource */
+			private transient InputStream inputStream = null;
+			
+			/**
+			 * @see wicket.util.resource.IResourceStream#close()
+			 */
+			public void close() throws IOException
+			{
+				if (inputStream != null)
+				{
+					inputStream.close();
+					inputStream = null;
+				}
+			}
+	
+			/**
+			 * @see wicket.util.resource.IResourceStream#getContentType()
+			 */
+			public String getContentType()
+			{
+				return contentType;
+			}
+	
+			/**
+			 * @see wicket.util.resource.IResourceStream#getInputStream()
+			 */
+			public InputStream getInputStream() throws ResourceStreamNotFoundException
+			{
+				if (inputStream == null)
+					inputStream = new ByteArrayInputStream(array);
+
+				return inputStream;
+			}
+	
+			/**
+			 * @see wicket.util.watch.IModifiable#lastModifiedTime()
+			 */
+			public Time lastModifiedTime()
+			{
+				return lastModified;
+			}
+	
+			public long length()
+			{
+				return array.length;
+			}
+		};
+	}
+}
