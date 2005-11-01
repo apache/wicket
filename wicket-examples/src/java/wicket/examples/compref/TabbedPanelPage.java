6c01485e5f2f51b79dc0843f11dbe2a21e3b527b
Igor Vaynberg <ivaynberg@apache.org>
2005-11-01 05:26:47 +0000

    init checking of compref for the TabbedPanel extensions component
    git-svn-id: https://svn.apache.org/repos/asf/incubator/wicket/trunk@457977 13f79535-47bb-0310-9956-ffa450edef68

---
+++ wicket-examples/src/java/wicket/examples/compref/TabbedPanelPage.java
@@ -0,0 +1,234 @@
+package wicket.examples.compref;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import wicket.AttributeModifier;
+import wicket.examples.WicketExamplePage;
+import wicket.extensions.markup.html.tabs.AbstractTab;
+import wicket.extensions.markup.html.tabs.TabbedPanel;
+import wicket.markup.html.link.Link;
+import wicket.markup.html.panel.Panel;
+import wicket.model.Model;
+import wicket.model.PropertyModel;
+
+/**
+ * Reference page for TabbedPanel wicket-extensions component
+ * 
+ * @see wicket.extensions.markup.html.tabs.TabbedPanel
+ * 
+ * @author igor
+ * 
+ */
+public class TabbedPanelPage extends WicketExamplePage
+{
+	private String var = "tabpanel";
+
+	/**
+	 * @return css variation class name used to render tabs
+	 */
+	public String getVar()
+	{
+		return var;
+	}
+
+	/**
+	 * @param var
+	 *            set css variation class name used to render tabs
+	 */
+	public void setVar(String var)
+	{
+		this.var = var;
+	}
+
+	/**
+	 * Constructor
+	 */
+	public TabbedPanelPage()
+	{
+
+		// create links used to switch between css variations
+		addCssSwitchingLinks();
+
+		// create a list of ITab objects used to feed the tabbed panel
+		List tabs = new ArrayList();
+		tabs.add(new AbstractTab(new Model("first tab"))
+		{
+
+			public Panel getPanel(String panelId)
+			{
+				return new TabPanel1(panelId);
+			}
+
+		});
+
+		tabs.add(new AbstractTab(new Model("second tab"))
+		{
+
+			public Panel getPanel(String panelId)
+			{
+				return new TabPanel2(panelId);
+			}
+
+		});
+
+		tabs.add(new AbstractTab(new Model("third tab"))
+		{
+
+			public Panel getPanel(String panelId)
+			{
+				return new TabPanel3(panelId);
+			}
+
+		});
+
+		// add the new tabbed panel, attribute modifier only used to switch
+		// between different css variations
+		add(new TabbedPanel("tabs", tabs).add(new AttributeModifier("class", true,
+				new PropertyModel(this, "var"))));
+
+	}
+
+
+	private void addCssSwitchingLinks()
+	{
+		add(new Link("var0")
+		{
+			public void onClick()
+			{
+				setVar("tabpanel");
+			}
+
+			public boolean isEnabled()
+			{
+				return !getVar().equals("tabpanel");
+			}
+		});
+
+		add(new Link("var1")
+		{
+			public void onClick()
+			{
+				setVar("tabpanel1");
+			}
+
+			public boolean isEnabled()
+			{
+				return !getVar().equals("tabpanel1");
+			}
+		});
+
+		add(new Link("var2")
+		{
+			public void onClick()
+			{
+				setVar("tabpanel2");
+			}
+
+			public boolean isEnabled()
+			{
+				return !getVar().equals("tabpanel2");
+			}
+
+		});
+
+		add(new Link("var3")
+		{
+			public void onClick()
+			{
+				setVar("tabpanel3");
+			}
+
+			public boolean isEnabled()
+			{
+				return !getVar().equals("tabpanel3");
+			}
+
+		});
+	}
+
+
+	/**
+	 * Panel representing the content panel for the first tab
+	 * 
+	 * @author Igor Vaynberg (ivaynberg)
+	 * 
+	 */
+	private static class TabPanel1 extends Panel
+	{
+
+		/**
+		 * Constructor
+		 * 
+		 * @param id
+		 *            component id
+		 */
+		public TabPanel1(String id)
+		{
+			super(id);
+		}
+
+	};
+
+	/**
+	 * Panel representing the content panel for the second tab
+	 * 
+	 * @author Igor Vaynberg (ivaynberg)
+	 * 
+	 */
+	private static class TabPanel2 extends Panel
+	{
+
+		/**
+		 * Constructor
+		 * 
+		 * @param id
+		 *            component id
+		 */
+		public TabPanel2(String id)
+		{
+			super(id);
+		}
+
+	};
+
+	/**
+	 * Panel representing the content panel for the third tab
+	 * 
+	 * @author Igor Vaynberg (ivaynberg)
+	 * 
+	 */
+	private static class TabPanel3 extends Panel
+	{
+
+		/**
+		 * Constructor
+		 * 
+		 * @param id
+		 *            component id
+		 */
+		public TabPanel3(String id)
+		{
+			super(id);
+		}
+
+	};
+
+
+	protected void explain()
+	{
+		String html = "<span wicket:id=\"tabs\" class=\"tabpanel\">[tabbed panel will be here]</span>\n";
+		String code = "&nbsp;&nbsp;&nbsp;&nbsp;List tabs=new ArrayList();<br/>"
+				+ "&nbsp;&nbsp;&nbsp;&nbsp;tabs.add(new AbstractTab(new Model(\"first tab\")) {<br/>"
+				+ "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;public Panel getPanel(String panelId) { return new TabPanel1(panelId); }<br/>"
+				+ "&nbsp;&nbsp;&nbsp;&nbsp;});<br/>"
+				+ "&nbsp;&nbsp;&nbsp;&nbsp;tabs.add(new AbstractTab(new Model(\"second tab\")) {<br/>"
+				+ "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;public Panel getPanel(String panelId) { return new TabPanel2(panelId); }<br/>"
+				+ "&nbsp;&nbsp;&nbsp;&nbsp;});<br/>"
+				+ "&nbsp;&nbsp;&nbsp;&nbsp;tabs.add(new AbstractTab(new Model(\"third tab\")) {<br/>"
+				+ "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;public Panel getPanel(String panelId) { return new TabPanel3(panelId); }<br/>"
+				+ "&nbsp;&nbsp;&nbsp;&nbsp;});<br/>"
+				+ "&nbsp;&nbsp;&nbsp;&nbsp;add(new TabbedPanel(\"tabs\", tabs)<br/>";
+		add(new ExplainPanel(html, code));
+	}
+}
