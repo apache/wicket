103efb01ccad999d98cd5a781d363f7002296a33
Juegen Donnerstag <jdonnerstag@apache.org>
2005-12-03 10:57:18 +0000

    Eelco asked to have a means to automatically prefix all absolute href (and src) attr values (URL starts with "/") with the web apps context path. Here it is. Note: it is NOT automatically added to the list of markup filters, you have to do it manually. Please see the javadoc and the junit test case on how to do it. Note: The reason why it is not added by default is because it prevents access to all other web apps on the same server which your web apps addresses by "/myApp333/myPage.html" because it will be prefix like "/myApp/myApp333/myPage.html"
    git-svn-id: https://svn.apache.org/repos/asf/incubator/wicket/trunk@458202 13f79535-47bb-0310-9956-ffa450edef68

--- wicket/src/java/wicket/markup/parser/IMarkupFilter.java
+++ wicket/src/java/wicket/markup/parser/IMarkupFilter.java
@@ -48,9 +48,12 @@ public interface IMarkupFilter
 	public abstract void setParent(final IMarkupFilter parent);
 	
 	/**
-	 * Gets the next tag from the input string.
+	 * Get the next MarkupElement from the parent MarkupFilter and handle it if
+	 * the specific filter criteria are met. Depending on the filter, it may
+	 * return the MarkupElement unchanged, modified or it remove by asking the
+	 * parent handler for the next tag.
 	 * 
-	 * @return The extracted tag.
+	 * @return Return the next eligible MarkupElement
 	 * @throws ParseException
 	 */
 	public abstract MarkupElement nextTag() throws ParseException;
---
+++ wicket/src/java/wicket/markup/parser/filter/PrependContextPathHandler.java
@@ -0,0 +1,125 @@
+/*
+ * $Id$ $Revision:
+ * 1.5 $ $Date$
+ * 
+ * ==============================================================================
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package wicket.markup.parser.filter;
+
+import java.text.ParseException;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+import wicket.RequestCycle;
+import wicket.markup.ComponentTag;
+import wicket.markup.MarkupElement;
+import wicket.markup.parser.AbstractMarkupFilter;
+import wicket.protocol.http.WebRequestCycle;
+
+/**
+ * This is a markup inline filter which by default is not added to the list
+ * of markup filters. It can be added by means of subclassing 
+ * Application.newMarkupParser() like 
+ * <pre>
+ *     public class MyApplication extends Application
+ *     {
+ *         ...
+ * 	       public MarkupParser newMarkupParser()
+ *         {
+ *             final MarkupParser parser = new MarkupParser(new XmlPullParser())
+ *                 {
+ *                     public void initFilterChain()
+ *                     {
+ *                         appendMarkupFilter(new PrependContextPathHandler());
+ *                     }
+ *                 };
+ *             parser.configure(getSettings());
+ *             return parser;
+ *             }
+ * </pre>
+ * 
+ * The purpose of the filter is to prepend the servlets context path to all
+ * href and src attributes found in the markup, no matter whether the tag
+ * is a wicket tag or not.
+ * 
+ * @author Juergen Donnerstag
+ */
+public final class PrependContextPathHandler extends AbstractMarkupFilter
+{
+	/** Logging */
+	private static final Log log = LogFactory.getLog(PrependContextPathHandler.class);
+
+	/** List of attribute names considered */
+	private static final String attributeNames[] = new String[] { "href", "src" };
+
+	/** The application's servlet context path */
+	private String contextPath;
+	
+	/**
+	 * Construct.
+	 */
+	public PrependContextPathHandler()
+	{
+		super(null);
+	}
+
+	/**
+	 * Get the next MarkupElement from the parent MarkupFilter and handle it if
+	 * the specific filter criteria are met. Depending on the filter, it may
+	 * return the MarkupElement unchanged, modified or it remove by asking the
+	 * parent handler for the next tag.
+	 * 
+	 * @see wicket.markup.parser.IMarkupFilter#nextTag()
+	 * @return Return the next eligible MarkupElement
+	 */
+	public MarkupElement nextTag() throws ParseException
+	{
+		// Get the next tag. If null, no more tags are available
+		final ComponentTag tag = (ComponentTag)getParent().nextTag();
+		if (tag == null)
+		{
+			return tag;
+		}
+
+		// A new handler is created for each markup file. Hence it is
+		// sufficient to "create" the context path just once.
+		if (contextPath == null)
+		{
+			contextPath = ((WebRequestCycle)RequestCycle.get()).getWebRequest().getContextPath();
+			if (contextPath == null)
+			{
+				contextPath = "";
+			}
+		}
+
+		if (contextPath.length() > 0)
+		{
+			// Modify all relevant attributes 
+			for (int i=0; i < attributeNames.length; i++)
+			{
+				String attrName = attributeNames[i];
+				String attrValue = tag.getAttributes().getString(attrName);
+				if ((attrValue != null) && attrValue.startsWith("/"))
+				{
+					String url = contextPath + attrValue;
+					tag.getAttributes().put(attrName, url);
+					tag.setModified(true);
+				}
+			}
+		}
+		
+		return tag;
+	}
+}
