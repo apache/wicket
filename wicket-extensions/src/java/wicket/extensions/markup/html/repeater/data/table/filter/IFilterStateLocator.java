da05d8875eb638b9ac8298f9e8dc3aae44ec09b1
Igor Vaynberg <ivaynberg@apache.org>
2005-12-07 08:28:46 +0000

    added Serializable
    git-svn-id: https://svn.apache.org/repos/asf/incubator/wicket/trunk@458259 13f79535-47bb-0310-9956-ffa450edef68

--- wicket-extensions/src/java/wicket/extensions/markup/html/repeater/data/table/filter/IFilterStateLocator.java
+++ wicket-extensions/src/java/wicket/extensions/markup/html/repeater/data/table/filter/IFilterStateLocator.java
@@ -1,6 +1,8 @@
 package wicket.extensions.markup.html.repeater.data.table.filter;
 
-public interface IFilterStateLocator
+import java.io.Serializable;
+
+public interface IFilterStateLocator extends Serializable
 {
 	Object getFilterState();
 	void setFilterState(Object state);
