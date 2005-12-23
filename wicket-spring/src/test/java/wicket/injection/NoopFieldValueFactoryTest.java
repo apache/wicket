ebbbec1486d0024a0e0d9b6aa45a90509cf5d587
Igor Vaynberg <ivaynberg@apache.org>
2005-12-23 23:00:29 +0000

    init checkin
    git-svn-id: https://svn.apache.org/repos/asf/incubator/wicket/trunk@458351 13f79535-47bb-0310-9956-ffa450edef68

---
+++ wicket-spring/src/test/java/wicket/injection/NoopFieldValueFactoryTest.java
@@ -0,0 +1,35 @@
+package wicket.injection;
+
+import java.lang.reflect.Field;
+
+import wicket.injection.NoopFieldValueFactory;
+
+import junit.framework.TestCase;
+
+/**
+ * Test {@link NoopFieldValueFactory}
+ * 
+ * @author Igor Vaynberg (ivaynberg)
+ * 
+ */
+public class NoopFieldValueFactoryTest extends TestCase
+{
+	private Integer testField;
+
+	private Field field;
+
+	protected void setUp() throws Exception
+	{
+		Field field = NoopFieldValueFactoryTest.class.getDeclaredField("testField");
+	}
+
+	/**
+	 * make sure null is returned
+	 */
+	public void test()
+	{
+		NoopFieldValueFactory fact = new NoopFieldValueFactory();
+		assertNull(fact.getFieldValue(field, this));
+	}
+
+}
