6688247923483528034156b00d01f54b2b93bc57
Juegen Donnerstag <jdonnerstag@apache.org>
2005-12-31 10:09:31 +0000

    An abstract output transformer container which can be extended to post-process the rendered output with various technologies (e.g. xslt). See the junit test for an example.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/wicket/trunk/wicket@458416 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/test/wicket/markup/outputTransformer/Page_1.java
@@ -0,0 +1,99 @@
+/*
+ * $Id$
+ * $Revision$
+ * $Date$
+ *
+ * ====================================================================
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package wicket.markup.outputTransformer;
+
+import wicket.AttributeModifier;
+import wicket.MarkupContainer;
+import wicket.PageParameters;
+import wicket.markup.html.WebPage;
+import wicket.markup.html.basic.Label;
+import wicket.markup.html.basic.SimpleBorder;
+import wicket.markup.html.border.Border;
+import wicket.markup.html.panel.Panel;
+import wicket.model.Model;
+
+
+/**
+ * Mock page for testing.
+ *
+ * @author Chris Turner
+ */
+public class Page_1 extends WebPage 
+{
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * Construct.
+	 * @param parameters
+	 */
+	public Page_1(final PageParameters parameters) 
+	{
+		add(new Label("myLabel", "Test Label"));
+		
+	    MarkupContainer container = new OutputTransformerContainer("test")
+	    {
+			private static final long serialVersionUID = 1L;
+
+			protected String transform(String output)
+			{
+				// do nothing
+				return output;
+			}
+	    };
+	    
+	    add(container);
+	    container.add(new Label("myLabel2", "Test Label2"));
+
+	    MarkupContainer panelContainer = new OutputTransformerContainer("test2")
+	    {
+			private static final long serialVersionUID = 1L;
+
+			protected String transform(String output)
+			{
+				// replace the generated String
+				return "Whatever";
+			}
+	    };
+
+	    add(panelContainer);
+	    Panel panel = new Panel_1("myPanel");
+	    panel.setRenderBodyOnly(true);
+	    panelContainer.add(panel);
+
+	    MarkupContainer borderContainer = new OutputTransformerContainer("test3")
+	    {
+			private static final long serialVersionUID = 1L;
+
+			protected String transform(String output)
+			{
+				// Convert all text to uppercase
+				return output.toUpperCase();
+			}
+	    };
+
+	    add(borderContainer);
+	    Border border = new SimpleBorder("myBorder");
+	    borderContainer.add(border);
+
+	    Border border2 = new SimpleBorder("myBorder2");
+	    border2.setRenderBodyOnly(false);
+	    border2.add(new AttributeModifier("testAttr", true, new Model("myValue")));
+	    add(border2);
+    }
+}
