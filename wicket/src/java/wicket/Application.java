acaf0c2231655a7ed72bb6a7cee6b666e554d780
Jonathan Locke <jonl@apache.org>
2006-01-01 23:02:46 +0000

    Removed unused imports
    git-svn-id: https://svn.apache.org/repos/asf/incubator/wicket/trunk@458443 13f79535-47bb-0310-9956-ffa450edef68

--- wicket/src/java/wicket/Application.java
+++ wicket/src/java/wicket/Application.java
@@ -23,7 +23,6 @@ import java.net.URL;
 import java.util.ArrayList;
 import java.util.Enumeration;
 import java.util.HashMap;
-import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 import java.util.Properties;
