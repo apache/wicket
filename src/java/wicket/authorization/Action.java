349c0d47c91bce36f20aa091ba2ed222112289a4
Jonathan Locke <jonl@apache.org>
2006-01-12 21:46:03 +0000

    Javadoc
    git-svn-id: https://svn.apache.org/repos/asf/incubator/wicket/trunk/wicket@458607 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/wicket/authorization/Action.java
+++ src/java/wicket/authorization/Action.java
@@ -20,8 +20,9 @@ package wicket.authorization;
 import java.io.Serializable;
 
 /**
- * A token/key that represents a given component action that needs to be
- * authorized. For example, Component.RENDER or Component.ENABLE.
+ * A class for constructing singleton constants that represent a given component
+ * action that needs to be authorized. For example, Component.RENDER or
+ * Component.ENABLE.
  * 
  * @see wicket.Component#RENDER
  * @see wicket.Component#ENABLE
