9541186c366aa68e1faeb30262f55cc72b9be6c6
Jonathan Locke <jonl@apache.org>
2006-01-12 23:57:32 +0000

    Formatting
    git-svn-id: https://svn.apache.org/repos/asf/incubator/wicket/trunk@458615 13f79535-47bb-0310-9956-ffa450edef68

--- wicket/src/java/wicket/authorization/strategies/actionauthorizer/ActionAuthorizationStrategy.java
+++ wicket/src/java/wicket/authorization/strategies/actionauthorizer/ActionAuthorizationStrategy.java
@@ -15,7 +15,6 @@
  * License for the specific language governing permissions and limitations under
  * the License.
  */
-
 package wicket.authorization.strategies.actionauthorizer;
 
 import java.util.HashMap;
