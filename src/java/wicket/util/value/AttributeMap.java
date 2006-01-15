2fda81ce44809fcd4347361f5e405eb96c6bd6fe
Jonathan Locke <jonl@apache.org>
2006-01-15 10:26:37 +0000

    Hotspot in valuemap constructor reduced by making valuemap more efficient (now extends HashMap rather than aggregating one).
    git-svn-id: https://svn.apache.org/repos/asf/incubator/wicket/trunk/wicket@458667 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/wicket/util/value/AttributeMap.java
+++ src/java/wicket/util/value/AttributeMap.java
@@ -1,7 +1,6 @@
 /*
- * $Id$
- * $Revision$
- * $Date$
+ * $Id$ $Revision:
+ * 1.3 $ $Date$
  * 
  * ==============================================================================
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not
@@ -18,12 +17,11 @@
  */
 package wicket.util.value;
 
-import java.util.Iterator;
 import java.util.Map;
 
 /**
  * ValueMap for attribtues.
- *
+ * 
  * @author Eelco Hillenius
  */
 public final class AttributeMap extends ValueMap
@@ -40,47 +38,12 @@ public final class AttributeMap extends ValueMap
 
 	/**
 	 * Copy constructor.
-	 * @param map map to be copied
+	 * 
+	 * @param map
+	 *            Map to be copied
 	 */
 	public AttributeMap(Map map)
 	{
-		super(toMap(map));
-	}
-
-	/**
-	 * Copies all values into a value map.
-	 * @param map map to copy
-	 * @return new map
-	 */
-	private static Map toMap(Map map)
-	{
-		AttributeMap newMap = new AttributeMap();
-		if (map != null)
-		{
-			for (Iterator i = map.keySet().iterator(); i.hasNext();)
-			{
-				Object key = i.next();
-				newMap.put(key, map.get(key));
-			}
-		}
-		return newMap;
-	}
-
-	/**
-	 * Puts all pairs of the given map in this map, converting all keys to lower case
-	 * strings along the way.
-	 * @param map the map to add to this map
-	 * @see wicket.util.value.ValueMap#putAll(java.util.Map)
-	 */
-	public void putAll(Map map)
-	{
-		if (map != null)
-		{
-			for (Iterator i = map.keySet().iterator(); i.hasNext();)
-			{
-				Object key = i.next();
-				put(key, map.get(key));
-			}
-		}
+		super(map);
 	}
 }
