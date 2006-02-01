316eb424e1cd5c2611b6a5cbdaeb8339144b317f
Juegen Donnerstag <jdonnerstag@apache.org>
2006-02-01 22:28:13 +0000

    javadoc
    git-svn-id: https://svn.apache.org/repos/asf/incubator/wicket/trunk/wicket@458922 13f79535-47bb-0310-9956-ffa450edef68

--- src/test/wicket/WicketTestCase.java
+++ src/test/wicket/WicketTestCase.java
@@ -1,20 +1,19 @@
 /*
- * $Id$
- * $Revision$
- * $Date$
- *
- * ====================================================================
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *  http://www.apache.org/licenses/LICENSE-2.0
- *
+ * $Id$ $Revision:
+ * 1.5 $ $Date$
+ * 
+ * ==================================================================== Licensed
+ * under the Apache License, Version 2.0 (the "License"); you may not use this
+ * file except in compliance with the License. You may obtain a copy of the
+ * License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
  */
 package wicket;
 
@@ -23,8 +22,11 @@ import wicket.markup.html.list.DiffUtil;
 import wicket.protocol.http.MockWebApplication;
 
 /**
- * Base class for tests which require comparing wicket response
- * with a file
+ * Base class for tests which require comparing wicket response with a file.
+ * <p>
+ * To create/replace the expected result file with the new content, define the
+ * system property like -Dwicket.replace.expected.results=true
+ * 
  */
 public abstract class WicketTestCase extends TestCase
 {
@@ -46,7 +48,7 @@ public abstract class WicketTestCase extends TestCase
 	{
 		application = new MockWebApplication(null);
 	}
-	
+
 	/**
 	 * @param pageClass
 	 * @param filename
@@ -55,7 +57,7 @@ public abstract class WicketTestCase extends TestCase
 	protected void executeTest(final Class pageClass, final String filename) throws Exception
 	{
 		System.out.println("=== " + pageClass.getName() + " ===");
-		
+
 		application.setHomePage(pageClass);
 
 		// Do the processing
