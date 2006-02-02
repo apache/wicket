99392bd5c69fbcef86cc90ea7042556cffbbe0ab
Igor Vaynberg <ivaynberg@apache.org>
2006-02-02 19:10:08 +0000

    external js library integration example
    git-svn-id: https://svn.apache.org/repos/asf/incubator/wicket/trunk@458956 13f79535-47bb-0310-9956-ffa450edef68

---
+++ wicket-examples/src/java/wicket/examples/ajax/builtin/EffectsPage.java
@@ -0,0 +1,77 @@
+package wicket.examples.ajax.builtin;
+
+import wicket.ajax.AjaxRequestTarget;
+import wicket.ajax.markup.html.AjaxFallbackLink;
+import wicket.ajax.markup.html.AjaxLink;
+import wicket.behavior.MarkupIdSetter;
+import wicket.markup.html.basic.Label;
+import wicket.model.PropertyModel;
+
+public class EffectsPage extends BasePage
+{
+	private int counter1 = 0;
+	private int counter2 = 0;
+
+
+	public int getCounter1()
+	{
+		return counter1;
+	}
+
+
+	public void setCounter1(int counter1)
+	{
+		this.counter1 = counter1;
+	}
+
+
+	public int getCounter2()
+	{
+		return counter2;
+	}
+
+
+	public void setCounter2(int counter2)
+	{
+		this.counter2 = counter2;
+	}
+
+	public EffectsPage()
+	{
+
+		final Label c1 = new Label("c1", new PropertyModel(this, "counter1"));
+		add(c1.add(MarkupIdSetter.INSTANCE));
+
+		final Label c2 = new Label("c2", new PropertyModel(this, "counter2"));
+		add(c2.add(MarkupIdSetter.INSTANCE));
+
+		add(new AjaxLink("c1-link")
+		{
+
+			protected void onClick(AjaxRequestTarget target)
+			{
+				counter1++;
+				target.addComponent(c1);
+				target.addJavascript("new Effect.Shake($('"+c1.getMarkupId()+"'));");
+			}
+
+		});
+
+		add(new AjaxFallbackLink("c2-link")
+		{
+
+			protected void onClick(AjaxRequestTarget target)
+			{
+				counter2++;
+				if (target != null)
+				{
+					target.addComponent(c2);
+					target.addJavascript("new Effect.Highlight($('"+c2.getMarkupId()+"'));");
+				}
+			}
+
+		});
+
+	}
+
+}
