203c6528c4fef84f4ded2a83a3ad09911c368f42
Eelco Hillenius <ehillenius@apache.org>
2006-02-09 01:07:58 +0000

    use simple strat
    git-svn-id: https://svn.apache.org/repos/asf/incubator/wicket/trunk@459101 13f79535-47bb-0310-9956-ffa450edef68

--- wicket-examples/src/java/wicket/examples/library/LibraryApplication.java
+++ wicket-examples/src/java/wicket/examples/library/LibraryApplication.java
@@ -1,91 +1,83 @@
 /*
  * $Id$
- * $Revision$
- * $Date$
- *
- * ====================================================================
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *  http://www.apache.org/licenses/LICENSE-2.0
- *
+ * $Revision$ $Date$
+ * 
+ * ==================================================================== Licensed
+ * under the Apache License, Version 2.0 (the "License"); you may not use this
+ * file except in compliance with the License. You may obtain a copy of the
+ * License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
  */
 package wicket.examples.library;
 
-import wicket.Component;
 import wicket.ISessionFactory;
-import wicket.RestartResponseAtInterceptPageException;
 import wicket.Session;
-import wicket.authorization.Action;
-import wicket.authorization.IAuthorizationStrategy;
+import wicket.authorization.SimplePageAuthorizationStrategy;
 import wicket.examples.WicketExampleApplication;
 import wicket.settings.Settings;
 
 /**
  * WicketServlet class for example.
+ * 
  * @author Jonathan Locke
  */
 public final class LibraryApplication extends WicketExampleApplication
 {
-    /**
-     * Constructor.
-     */
-    public LibraryApplication()
-    {
-    }
-    
+	/**
+	 * Constructor.
+	 */
+	public LibraryApplication()
+	{
+	}
+
 	/**
 	 * @see wicket.examples.WicketExampleApplication#init()
 	 */
 	protected void init()
 	{
-        getExceptionSettings().setThrowExceptionOnMissingResource(false);
+		getExceptionSettings().setThrowExceptionOnMissingResource(false);
 		getRequestCycleSettings().setRenderStrategy(Settings.REDIRECT_TO_RENDER);
-		getSecuritySettings().setAuthorizationStrategy(new IAuthorizationStrategy()
+
+		// set the sign in page
+		getApplicationSettings().setSignInPage(SignIn.class);
+
+		// create a simple authorization strategy
+		SimplePageAuthorizationStrategy authorizationStrategy = new SimplePageAuthorizationStrategy()
 		{
-			public boolean authorizeAction(Component component, Action action)
+			protected boolean isAuthorized()
 			{
-				return true;
+				// check whether the user is logged on
+				return (((LibrarySession)Session.get()).isSignedIn());
 			}
-		
-			public boolean authorizeInstantiation(Class componentClass)
-			{
-				if(AuthenticatedWebPage.class.isAssignableFrom(componentClass))
-				{
-			        // Is user signed in?
-			        if (((LibrarySession)Session.get()).isSignedIn())
-			        {
-			            // okay to proceed
-			            return true;
-			        }
-			        
-			        // Force sign in
-			        throw new RestartResponseAtInterceptPageException(SignIn.class);
-				}
-				return true;
-			}
-		});
+		};
+		// all pages of type AuthenticatedWebPage must be checked for
+		// authorization
+		authorizationStrategy.add(AuthenticatedWebPage.class);
+
+		// set the strategy
+		getSecuritySettings().setAuthorizationStrategy(authorizationStrategy);
 	}
-    
-    
-    /**
+
+
+	/**
 	 * @see wicket.protocol.http.WebApplication#getSessionFactory()
 	 */
 	public ISessionFactory getSessionFactory()
 	{
-        return new ISessionFactory()
-        {
-            public Session newSession()
-            {
-                return new LibrarySession(LibraryApplication.this);
-            }
-        };
+		return new ISessionFactory()
+		{
+			public Session newSession()
+			{
+				return new LibrarySession(LibraryApplication.this);
+			}
+		};
 	}
 
 	/**
