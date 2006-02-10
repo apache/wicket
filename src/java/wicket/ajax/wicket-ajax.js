dcce2b3d774af014a10b8b55c17ea8414a0cab3c
Igor Vaynberg <ivaynberg@apache.org>
2006-02-10 18:15:55 +0000

    added form handling functions, courtesy of matej_k
    git-svn-id: https://svn.apache.org/repos/asf/incubator/wicket/trunk/wicket@459150 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/wicket/ajax/wicket-ajax.js
+++ src/java/wicket/ajax/wicket-ajax.js
@@ -79,3 +79,54 @@ function wicketAjaxProcessEvaluation(node) {
     var text=node.firstChild.nodeValue;
     eval(text);
 }
+
+//FORM SERIALIZATION FUNCTIONS
+function wicketEncode(text) {
+	if (encodeURIComponent) {
+	    return encodeURIComponent(text);
+	} else {
+	    return escape(text);
+	}
+}
+
+function wicketSerializeSelect(select) {
+	var result = "";
+	for (var i = 0; i < select.options.length; ++i) {
+		var option = select.options[i];
+		if (option.selected) {
+			result += wicketEncode(select.name) + "=" + wicketEncode(option.value) + "&";
+		}	
+	}
+	return result;
+}
+
+// this function intentionally ignores image and submit inputs
+function wicketSerializeInput(input) {
+	var type = input.type.toLowerCase();			
+	if ((type == "checkbox" || type == "radio") && input.checked)
+		return wicketEncode(input.name) + "=" + wicketEncode(input.value) + "&";
+	else if (type == "text" || type == "password" || type=="hidden")
+		return wicketEncode(input.name) + "=" + wicketEncode(input.value) + "&" ;
+	else return "";
+}
+
+// returns url/post-body fragment representing element (e) 
+function wicketSerialize(e) {
+	var tag = e.tagName.toLowerCase();
+	if (tag == "select") {
+		return wicketSerializeSelect(e);
+	} else if (tag == "input" || tag == "textarea") {
+		return wicketSerializeInput(e);
+	}
+}
+
+function wicketSerializeForm(form) {
+	var result = "";
+	for (var i = 0; i < form.elements.length; ++i) {
+		var e = form.elements[i];
+		if (!e.disabled) {
+            result+=wicketSerialize(e);
+		}
+	}
+	return result;
+}
