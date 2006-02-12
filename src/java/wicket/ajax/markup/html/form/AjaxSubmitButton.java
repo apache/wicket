d2ac26e16c38cc6d439fe20b5f4c3072b2729385
Igor Vaynberg <ivaynberg@apache.org>
2006-02-12 01:02:37 +0000

    more ajax behaviors regarding forms
    git-svn-id: https://svn.apache.org/repos/asf/incubator/wicket/trunk/wicket@459191 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/java/wicket/ajax/markup/html/form/AjaxSubmitButton.java
@@ -0,0 +1,69 @@
+/*
+ * $Id$
+ * $Revision$ $Date$
+ * 
+ * ==============================================================================
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package wicket.ajax.markup.html.form;
+
+import wicket.ajax.AjaxRequestTarget;
+import wicket.ajax.form.AjaxFormSubmitBehavior;
+import wicket.markup.html.WebComponent;
+import wicket.markup.html.form.Button;
+import wicket.markup.html.form.Form;
+
+/**
+ * A button that submits the form via ajax. Since this button takes the form as
+ * a constructor argument it does not need to be added to it unlike the
+ * {@link Button} component.
+ * 
+ * @author Igor Vaynberg (ivaynberg)
+ */
+public abstract class AjaxSubmitButton extends WebComponent
+{
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * Construct.
+	 * 
+	 * @param id
+	 * @param form
+	 */
+	public AjaxSubmitButton(String id, Form form)
+	{
+		super(id);
+
+		add(new AjaxFormSubmitBehavior(form, "onclick")
+		{
+
+			private static final long serialVersionUID = 1L;
+
+			protected void onSubmit(AjaxRequestTarget target)
+			{
+				AjaxSubmitButton.this.onSubmit(target);
+			}
+
+		});
+
+	}
+
+	/**
+	 * Listener method invoked on form submit
+	 * 
+	 * @param target
+	 */
+	protected abstract void onSubmit(AjaxRequestTarget target);
+
+
+}
