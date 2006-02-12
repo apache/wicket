12f1897c39b8f8715056ca661541101254709c16
Jonathan Locke <jonl@apache.org>
2006-02-12 08:27:25 +0000

    Trivial documentation to get rid of FIXME.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/wicket/trunk/wicket@459204 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/wicket/request/target/FormSubmitInterfaceRequestTarget.java
+++ src/java/wicket/request/target/FormSubmitInterfaceRequestTarget.java
@@ -1,15 +1,14 @@
 /*
- * $Id$
- * $Revision$
- * $Date$
- *
+ * $Id: FormSubmitInterfaceRequestTarget.java,v 1.5 2006/01/17 20:35:34
+ * jonathanlocke Exp $ $Revision$ $Date$
+ * 
  * ==============================================================================
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not
  * use this file except in compliance with the License. You may obtain a copy of
  * the License at
- *
+ * 
  * http://www.apache.org/licenses/LICENSE-2.0
- *
+ * 
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
  * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
@@ -25,7 +24,7 @@ import wicket.Page;
 import wicket.RequestCycle;
 
 /**
- * FIXME Documentation: Document this class!
+ * Interface request target for forms.
  * 
  * @author jcompagner
  */
@@ -33,9 +32,13 @@ public class FormSubmitInterfaceRequestTarget extends AbstractListenerInterfaceR
 {
 	/**
 	 * Construct.
+	 * 
 	 * @param page
+	 *            The Page
 	 * @param component
+	 *            The component
 	 * @param listenerMethod
+	 *            The listener method to invoke
 	 */
 	public FormSubmitInterfaceRequestTarget(Page page, Component component, Method listenerMethod)
 	{
