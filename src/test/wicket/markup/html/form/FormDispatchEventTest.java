92222f58b94cd9a15ea4b824054bf20c0484e28a
Johan Compagner <jcompagner@apache.org>
2006-02-17 23:44:25 +0000

    form dispatch to combo test
    git-svn-id: https://svn.apache.org/repos/asf/incubator/wicket/trunk/wicket@459420 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/test/wicket/markup/html/form/FormDispatchEventTest.java
@@ -0,0 +1,109 @@
+/*
+ * $Id$
+ * $Revision$
+ * $Date$
+ *
+ * ==============================================================================
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package wicket.markup.html.form;
+
+import wicket.IRedirectListener;
+import wicket.RequestCycle;
+import wicket.WicketRuntimeException;
+import wicket.WicketTestCase;
+import wicket.markup.html.form.DropDownChoice;
+import wicket.markup.html.form.Form;
+import wicket.protocol.http.MockPage;
+
+/**
+ * @author jcompagner
+ */
+public class FormDispatchEventTest extends WicketTestCase
+{
+	private boolean selection;
+
+	/**
+	 * Construct.
+	 * @param name
+	 */
+	public FormDispatchEventTest(String name)
+	{
+		super(name);
+	}
+	
+	/**
+	 * @throws Exception
+	 */
+	public void testDropDownEvent() throws Exception
+	{
+		Form form = new Form("form")
+		{
+			private static final long serialVersionUID = 1L;
+
+			protected void onSubmit() 
+			{
+				throw new WicketRuntimeException("on submit shouldn't be reached");
+			};
+		};
+		
+		DropDownChoice dropDown = new DropDownChoice("dropdown")
+		{
+			private static final long serialVersionUID = 1L;
+
+			protected void onSelectionChanged(Object newSelection) 
+			{
+				selection = true;
+			};
+			
+			/**
+			 * @see wicket.markup.html.form.DropDownChoice#wantOnSelectionChangedNotifications()
+			 */
+			protected boolean wantOnSelectionChangedNotifications()
+			{
+				return true;
+			}
+		};
+
+		
+		form.add(dropDown);
+		
+		MockPage page = new MockPage();
+		page.add(form);
+		
+		application.setupRequestAndResponse();
+		RequestCycle cycle = application.createRequestCycle();
+		
+		page.urlFor(IRedirectListener.INTERFACE);
+		cycle.getSession().touch(page);
+		
+		try
+		{
+			form.onFormSubmitted();
+			throw new Exception("form should throw an error");
+		} catch(WicketRuntimeException ex)
+		{
+			
+		}
+		
+		application.getServletRequest().setParameter(form.getHiddenFieldId(), dropDown.urlFor(IOnChangeListener.INTERFACE));
+		
+		form.onFormSubmitted();
+		
+		if(!selection)
+		{
+			throw new Exception("Selection should be called");
+		}
+	}
+
+}
