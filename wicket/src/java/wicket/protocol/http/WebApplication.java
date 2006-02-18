fb0ef7954fb1c968008532eb1f5ebba8b8842fc8
Johan Compagner <jcompagner@apache.org>
2006-02-18 12:06:47 +0000

    on a new session create set the context path in the applicaiton settings if not already set.. This have to be done here because ServletContext (servlet.init()) doesn't have the path available..
    git-svn-id: https://svn.apache.org/repos/asf/incubator/wicket/trunk@459429 13f79535-47bb-0310-9956-ffa450edef68

--- wicket/src/java/wicket/protocol/http/WebApplication.java
+++ wicket/src/java/wicket/protocol/http/WebApplication.java
@@ -542,6 +542,15 @@ public abstract class WebApplication extends Application
 				// Save this session in the HttpSession using the attribute name
 				httpSession.setAttribute(sessionAttribute, webSession);
 			}
+			
+			// Set the default context path if the context path is not already set
+			// (previous time or by the developer itself)
+			// This all to do missing api in the servlet spec.. You can't get a 
+			// context path from the servlet context, which is just stupid.
+			if(getApplicationSettings().getContextPath() == null)
+			{
+				getApplicationSettings().setContextPath(request.getContextPath());
+			}
 		}
 
 		// Set application on session
