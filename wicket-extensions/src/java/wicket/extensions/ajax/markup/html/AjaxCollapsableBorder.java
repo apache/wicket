6218cf553d0e9ecd7e27fc7293680f07717116a2
Igor Vaynberg <ivaynberg@apache.org>
2006-03-15 18:11:11 +0000

    wip: ajax collapsable border
    git-svn-id: https://svn.apache.org/repos/asf/incubator/wicket/trunk@459829 13f79535-47bb-0310-9956-ffa450edef68

--- wicket-extensions/src/java/wicket/extensions/ajax/markup/html/AjaxCollapsableBorder.java
+++ wicket-extensions/src/java/wicket/extensions/ajax/markup/html/AjaxCollapsableBorder.java
@@ -1,6 +1,8 @@
 package wicket.extensions.ajax.markup.html;
 
 import wicket.Component;
+import wicket.ajax.AjaxRequestTarget;
+import wicket.ajax.markup.html.AjaxFallbackLink;
 import wicket.markup.html.WebMarkupContainer;
 import wicket.markup.html.border.Border;
 import wicket.model.Model;
@@ -18,35 +20,30 @@ public class AjaxCollapsableBorder extends Border
 	{
 		super(id, new Model());
 		setCollapsed(true);
-		setTransparentResolver(true);
+		setOutputMarkupId(true);
+		
 		header = new WebMarkupContainer("header");
 		add(header);
-	}
+		header.add(new AjaxFallbackLink("toggle-link") {
 
-	protected void onBeginRequest()
-	{
-		visitChildren(new IVisitor()
-		{
+			private static final long serialVersionUID = 1L;
 
-			public Object component(Component component)
+			public void onClick(AjaxRequestTarget target)
 			{
-				System.out.println(component.getId());
-				if (component != header||true)
-				{
-					
-					component.setVisible(!isCollapsed());
+				setCollapsed(!isCollapsed());
+				if (target!=null) {
+					target.addComponent(AjaxCollapsableBorder.this);
 				}
-				return IVisitor.CONTINUE_TRAVERSAL_BUT_DONT_GO_DEEPER;
 			}
-
-
+			
 		});
 	}
 
+
 	public final void setCollapsed(boolean collapsed)
 	{
-
-		setModelObject(Boolean.TRUE);
+		setModelObject(Boolean.valueOf(collapsed));
+		setBorderBodyVisible(!collapsed);
 	}
 
 
@@ -56,4 +53,161 @@ public class AjaxCollapsableBorder extends Border
 	}
 
 
+	/**
+	 * User: nick
+	 * Date: Mar 8, 2006
+	 * Time: 2:51:17 PM
+	 */
+	public class AjaxCollapsableDiv extends WebMarkupContainer {
+
+		public AjaxCollapsableDiv(String id)
+		{
+			super(id);
+			// TODO Auto-generated constructor stub
+		}
+
+		/**
+		 * 
+		 */
+		private static final long serialVersionUID = 1L;
+
+	    /*	private static final long serialVersionUID = 1L;
+
+		static Log log = LogFactory.getLog(AjaxCollapsableDiv.class);
+
+	    private WebMarkupContainer toggleImage;
+	    private final AjaxPanel body;
+	    private boolean isExpanded = false;
+	    private String title;
+
+	    public AjaxCollapsableDiv(final String id, AjaxPanel ap, String title) {
+	        super(id);
+	        this.body = ap;
+	        this.title = title;
+	        setOutputMarkupId(true);
+	        init();
+	    }
+
+	    protected void init() {
+	        toggleImage = new WebMarkupContainer("toggleImage");
+	        toggleImage.setOutputMarkupId(true);
+	        add(toggleImage);
+
+	        body.setOutputMarkupId(true);
+	        add(body);
+
+	        AjaxLink link = new AjaxLink("expand") {
+	    		private static final long serialVersionUID = 1L;
+
+				public void onClick(AjaxRequestTarget target) {
+	                target.addComponent(AjaxCollapsableDiv.this);
+	                expanded(!isExpanded);
+	            }
+	        };
+	        link.add(new Label("name", title));
+	        add(link);
+
+	        final Cookie cookie = getCookie();
+	        boolean shouldExpand = false;
+	        String[] divs = getCookieValue(cookie).split(",");
+
+	        for (String div : divs) {
+	            shouldExpand = (getId().equals(div));
+	            if (shouldExpand) {
+	                break;
+	            }
+	        }
+
+	        expanded(shouldExpand);
+	    }
+
+	    protected void expanded(boolean shouldExpand) {
+	        if (shouldExpand) {
+	            toggleImage.add(new AttributeModifier("src", true,
+	                new Model(CollapsableDiv.EXPANDED_IMAGE)));
+	            body.addContent();
+	            body.setVisible(true);
+	            body.add(new AttributeModifier("class", true,
+	                new Model(CollapsableDiv.EXPANDED_STYLE)));
+	            isExpanded = true;
+	            addToCookie();
+	        }
+	        else {
+	            toggleImage.add(new AttributeModifier("src", true,
+	                new Model(CollapsableDiv.COLLAPSED_IMAGE)));
+	            body.setVisible(false);
+	            body.add(new AttributeModifier("class", true,
+	                new Model(CollapsableDiv.COLLAPSED_STYLE)));
+	            isExpanded = false;
+	            removeFromCookie();
+	        }
+	    }
+
+	    protected Cookie getCookie() {
+	        WebRequest webRequest = (WebRequest) getRequest();
+	        Cookie[] cookies = webRequest.getCookies();
+	        for (Cookie cookie : cookies) {
+	            if (cookie.getName().equals(CollapsableDiv.DIVS_TO_EXPAND)) {
+	                log.debug("Returning preexisting cookie: " + cookie.getMaxAge());
+	                return cookie;
+	            }
+	        }
+
+	        Cookie cookie = new Cookie(CollapsableDiv.DIVS_TO_EXPAND, "");
+	        cookie.setMaxAge(CollapsableDiv.EXPIRATION);
+
+	        setCookie(cookie);
+	        return cookie;
+	    }
+
+	    protected void addToCookie() {
+	        Cookie cookie = getCookie();
+	        String[] values = getCookieValue(cookie).split(",");
+	        boolean contains = false;
+	        for (String s : values) {
+	            if (s.equals(getId())) {
+	                contains = true;
+	            }
+	        }
+
+	        if (!contains) {
+	            StringBuilder b = new StringBuilder();
+	            for (String s : values) {
+	                b.append(s).append(",");
+	            }
+	            b.append(getId());
+	            cookie.setValue(URLEncoder.encode(b.toString()));
+	            setCookie(cookie);
+	        }
+	    }
+
+	    protected void removeFromCookie() {
+	        Cookie cookie = getCookie();
+	        String[] values = getCookieValue(cookie).split(",");
+	        StringBuilder b = new StringBuilder();
+	        for (int i = 0; i < values.length; i++) {
+	            if (!values[i].equals(getId())) {
+	                b.append(values[i]);
+	                if (i < (values.length -1)) {
+	                    b.append(",");
+	                }
+	            }
+	        }
+
+	        cookie.setValue(URLEncoder.encode(b.toString()));
+	        setCookie(cookie);
+	    }
+
+	    private String getCookieValue(Cookie c) {
+	        return URLDecoder.decode(c.getValue());
+	    }
+
+	    private void setCookie(Cookie c) {
+	        WebResponse webResponse = (WebResponse) getResponse();
+	        webResponse.addCookie(c);
+	    }
+	    
+	    */
+	}
+	
 }
