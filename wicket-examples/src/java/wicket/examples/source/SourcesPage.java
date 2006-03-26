c4b8f14db3895c3ee0d7184416f7976971227e57
Juegen Donnerstag <jdonnerstag@apache.org>
2006-03-26 10:20:53 +0000

    applied eclipse 3.2M5 source cleanUp
    git-svn-id: https://svn.apache.org/repos/asf/incubator/wicket/trunk@460005 13f79535-47bb-0310-9956-ffa450edef68

--- wicket-examples/src/java/wicket/examples/source/SourcesPage.java
+++ wicket-examples/src/java/wicket/examples/source/SourcesPage.java
@@ -2,7 +2,6 @@ package wicket.examples.source;
 
 import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.URL;
 import java.util.ArrayList;
