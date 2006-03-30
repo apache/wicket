444a54291d6628cf4c6fa3f5840f648ed33edd0e
Eelco Hillenius <ehillenius@apache.org>
2006-03-30 18:06:39 +0000

    wrong repo
    git-svn-id: https://svn.apache.org/repos/asf/incubator/wicket/trunk@460074 13f79535-47bb-0310-9956-ffa450edef68

--- wicket-contrib-yui/src/main/java/wicket/extensions/markup/html/yui/calendar/init.js
+++
@@ -1,7 +0,0 @@
-var ${javaScriptId};
-function init${javaScriptId}() {
-	${javaScriptId} = new YAHOO.widget.Calendar("${javaScriptId}","${elementId}");
-	${javaScriptId}.Options.NAV_ARROW_LEFT = "${navigationArrowLeft}";
-	${javaScriptId}.Options.NAV_ARROW_RIGHT = "${navigationArrowRight}";
-	${javaScriptId}.render();
-}
