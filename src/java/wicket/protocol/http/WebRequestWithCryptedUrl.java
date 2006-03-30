73b65796f18bdf5bd42808c9db1018a7ff14a051
Eelco Hillenius <ehillenius@apache.org>
2006-03-30 03:10:52 +0000

    made parameters mutable as decoding removes parameters. fixes failing junit tests in wicket-examples
    git-svn-id: https://svn.apache.org/repos/asf/incubator/wicket/trunk/wicket@460062 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/wicket/protocol/http/WebRequestWithCryptedUrl.java
+++ src/java/wicket/protocol/http/WebRequestWithCryptedUrl.java
@@ -1,6 +1,7 @@
 /*
- * $Id$ $Revision$
- * $Date$
+ * $Id: WebRequestWithCryptedUrl.java 4508 2006-02-16 15:14:18 -0800 (Thu, 16
+ * Feb 2006) jonathanlocke $ $Revision$ $Date: 2006-02-16 15:14:18 -0800
+ * (Thu, 16 Feb 2006) $
  * 
  * ==============================================================================
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not
@@ -19,7 +20,6 @@ package wicket.protocol.http;
 
 import java.io.UnsupportedEncodingException;
 import java.net.URLDecoder;
-import java.util.Collections;
 import java.util.Enumeration;
 import java.util.HashMap;
 import java.util.Iterator;
@@ -38,10 +38,9 @@ import wicket.util.string.Strings;
 import wicket.util.value.ValueMap;
 
 /**
- * It extends WebRequest and decodes URLs encoded by 
- * WebResponseWithCryptedUrl. One reason for obfuscating the URL's query string
- * might be, that you don't want the details to be visible to the user to
- * play around with.
+ * It extends WebRequest and decodes URLs encoded by WebResponseWithCryptedUrl.
+ * One reason for obfuscating the URL's query string might be, that you don't
+ * want the details to be visible to the user to play around with.
  * 
  * @author Juergen Donnerstag
  */
@@ -69,66 +68,69 @@ public class WebRequestWithCryptedUrl extends ServletWebRequest
 		{
 			// Get the crypt implementation from the application
 			ICrypt urlCrypt = Application.get().getSecuritySettings().getCryptFactory().newCrypt();
-			
-		    // Decrypt the query string
+
+			// Decrypt the query string
 			final String queryString = urlCrypt.decrypt(secureParam);
 
 			// The querystring might have been shortened (length reduced).
-			// In that case, lengthen the query string again. 
+			// In that case, lengthen the query string again.
 			this.queryString = rebuildUrl(queryString);
 
 			// extract parameter key/value pairs from the query string
-		    this.parameters = analyzeQueryString(this.queryString);
+			this.parameters = analyzeQueryString(this.queryString);
 		}
 		else
 		{
-		    // If "x" parameter does not exist, we assume the query string
-		    // is not encoded.
-		    // Note: You might want to throw an exception, if you don't want 
-		    // the automatic fallback.
+			// If "x" parameter does not exist, we assume the query string
+			// is not encoded.
+			// Note: You might want to throw an exception, if you don't want
+			// the automatic fallback.
 			this.queryString = null;
-		    this.parameters = new ValueMap();
+			this.parameters = new ValueMap();
 		}
 
 		// If available, add POST parameters as well. They are not encrypted.
-		// The parameters from HttpRequest 
+		// The parameters from HttpRequest
 		final Enumeration paramNames = request.getParameterNames();
-		
-	    // For all parameters (POST + URL query string)
-	    while (paramNames.hasMoreElements())
-	    {
-	    	String paramName = (String)paramNames.nextElement();
 
-	        // Ignore the "x" parameter
-	        if (!"x".equalsIgnoreCase(paramName))
-	        {
-	 	       	String[] values = request.getParameterValues(paramName);
-	            // add key/value to our parameter map
-	            this.parameters.put(paramName, values);
-	        }
-	    }
+		// For all parameters (POST + URL query string)
+		while (paramNames.hasMoreElements())
+		{
+			String paramName = (String)paramNames.nextElement();
+
+			// Ignore the "x" parameter
+			if (!"x".equalsIgnoreCase(paramName))
+			{
+				String[] values = request.getParameterValues(paramName);
+				// add key/value to our parameter map
+				this.parameters.put(paramName, values);
+			}
+		}
 	}
-	
+
 	/**
 	 * @see wicket.Request#decodeURL(java.lang.String)
 	 */
 	public String decodeURL(final String url)
 	{
 		int startIndex = url.indexOf("x=");
-		if(startIndex != -1)
+		if (startIndex != -1)
 		{
-			startIndex = startIndex +2;
+			startIndex = startIndex + 2;
 			final String secureParam;
 			final int endIndex = url.indexOf("&", startIndex);
 			try
 			{
-				if(endIndex == -1)
+				if (endIndex == -1)
 				{
-					secureParam = URLDecoder.decode(url.substring(startIndex),Application.get().getRequestCycleSettings().getResponseRequestEncoding());
+					secureParam = URLDecoder.decode(url.substring(startIndex), Application.get()
+							.getRequestCycleSettings().getResponseRequestEncoding());
 				}
 				else
 				{
-					secureParam = URLDecoder.decode(url.substring(startIndex, endIndex),Application.get().getRequestCycleSettings().getResponseRequestEncoding());
+					secureParam = URLDecoder.decode(url.substring(startIndex, endIndex),
+							Application.get().getRequestCycleSettings()
+									.getResponseRequestEncoding());
 				}
 			}
 			catch (UnsupportedEncodingException ex)
@@ -136,44 +138,54 @@ public class WebRequestWithCryptedUrl extends ServletWebRequest
 				// should never happen
 				throw new WicketRuntimeException(ex);
 			}
-			 
+
 			// Get the crypt implementation from the application
-			final ICrypt urlCrypt = Application.get().getSecuritySettings().getCryptFactory().newCrypt();
-			
-		    // Decrypt the query string
+			final ICrypt urlCrypt = Application.get().getSecuritySettings().getCryptFactory()
+					.newCrypt();
+
+			// Decrypt the query string
 			final String queryString = urlCrypt.decrypt(secureParam);
-	
+
 			// The querystring might have been shortened (length reduced).
-			// In that case, lengthen the query string again. 
+			// In that case, lengthen the query string again.
 			return rebuildUrl(queryString);
 		}
 		return url;
 	}
 
 	/**
-	 * In case the query string has been shortened prior to encryption,
-	 * than rebuild (lengthen) the query string now.
+	 * In case the query string has been shortened prior to encryption, than
+	 * rebuild (lengthen) the query string now.
 	 * 
-	 * @param queryString The URL's query string
+	 * @param queryString
+	 *            The URL's query string
 	 * @return The lengthened query string
 	 */
 	private String rebuildUrl(String queryString)
 	{
-	    queryString = Strings.replaceAll(queryString, "1=", WebRequestCodingStrategy.BEHAVIOR_ID_PARAMETER_NAME + "=");
-	    queryString = Strings.replaceAll(queryString, "2=", WebRequestCodingStrategy.INTERFACE_PARAMETER_NAME + "=IRedirectListener");
-	    queryString = Strings.replaceAll(queryString, "3=", WebRequestCodingStrategy.INTERFACE_PARAMETER_NAME + "=IFormSubmitListener");
-	    queryString = Strings.replaceAll(queryString, "4=", WebRequestCodingStrategy.INTERFACE_PARAMETER_NAME + "=IOnChangeListener");
-	    queryString = Strings.replaceAll(queryString, "5=", WebRequestCodingStrategy.INTERFACE_PARAMETER_NAME + "=ILinkListener");
-	    queryString = Strings.replaceAll(queryString, "6=", WebRequestCodingStrategy.INTERFACE_PARAMETER_NAME + "=");
-	    queryString = Strings.replaceAll(queryString, "7=", WebRequestCodingStrategy.BOOKMARKABLE_PAGE_PARAMETER_NAME + "=");
+		queryString = Strings.replaceAll(queryString, "1=",
+				WebRequestCodingStrategy.BEHAVIOR_ID_PARAMETER_NAME + "=");
+		queryString = Strings.replaceAll(queryString, "2=",
+				WebRequestCodingStrategy.INTERFACE_PARAMETER_NAME + "=IRedirectListener");
+		queryString = Strings.replaceAll(queryString, "3=",
+				WebRequestCodingStrategy.INTERFACE_PARAMETER_NAME + "=IFormSubmitListener");
+		queryString = Strings.replaceAll(queryString, "4=",
+				WebRequestCodingStrategy.INTERFACE_PARAMETER_NAME + "=IOnChangeListener");
+		queryString = Strings.replaceAll(queryString, "5=",
+				WebRequestCodingStrategy.INTERFACE_PARAMETER_NAME + "=ILinkListener");
+		queryString = Strings.replaceAll(queryString, "6=",
+				WebRequestCodingStrategy.INTERFACE_PARAMETER_NAME + "=");
+		queryString = Strings.replaceAll(queryString, "7=",
+				WebRequestCodingStrategy.BOOKMARKABLE_PAGE_PARAMETER_NAME + "=");
 
-	    return queryString;
+		return queryString;
 	}
 
 	/**
 	 * Extract key/value pairs from query string
 	 * 
-	 * @param queryString The query string
+	 * @param queryString
+	 *            The query string
 	 * @return A map of query string parameter keys and values
 	 */
 	private ValueMap analyzeQueryString(final String queryString)
@@ -196,33 +208,33 @@ public class WebRequestWithCryptedUrl extends ServletWebRequest
 				String[] prevValue = (String[])params.get(pair);
 				if (prevValue != null)
 				{
-					String[] newValue = new String[prevValue.length+1];
+					String[] newValue = new String[prevValue.length + 1];
 					System.arraycopy(prevValue, 0, newValue, 0, prevValue.length);
 					newValue[prevValue.length] = "";
-					params.put(pair,newValue);
+					params.put(pair, newValue);
 				}
 				else
 				{
-				    // Parameter without value
-					params.put(pair, new String[] {""});
+					// Parameter without value
+					params.put(pair, new String[] { "" });
 				}
 			}
 			else
 			{
-			    final String key = pair.substring(0, pos);
-			    final String value = pair.substring(pos + 1);
+				final String key = pair.substring(0, pos);
+				final String value = pair.substring(pos + 1);
 				String[] prevValue = (String[])params.get(key);
 				if (prevValue != null)
 				{
-					String[] newValue = new String[prevValue.length+1];
+					String[] newValue = new String[prevValue.length + 1];
 					System.arraycopy(prevValue, 0, newValue, 0, prevValue.length);
 					newValue[prevValue.length] = value;
-					params.put(key,newValue);
+					params.put(key, newValue);
 				}
 				else
 				{
-				    // Parameter without value
-					params.put(key, new String[] {value});
+					// Parameter without value
+					params.put(key, new String[] { value });
 				}
 			}
 		}
@@ -239,7 +251,7 @@ public class WebRequestWithCryptedUrl extends ServletWebRequest
 	 */
 	public String getParameter(final String key)
 	{
-		String[] value = (String[]) this.parameters.get(key);
+		String[] value = (String[])this.parameters.get(key);
 		if (value != null)
 		{
 			return value[0];
@@ -254,14 +266,14 @@ public class WebRequestWithCryptedUrl extends ServletWebRequest
 	 */
 	public Map getParameterMap()
 	{
-		Map map = new HashMap(parameters.size(),1);
+		Map map = new HashMap(parameters.size(), 1);
 		Iterator it = parameters.keySet().iterator();
 		while (it.hasNext())
 		{
 			String param = (String)it.next();
 			map.put(param, getParameter(param));
 		}
-		return Collections.unmodifiableMap(map);
+		return map;
 	}
 
 	/**
