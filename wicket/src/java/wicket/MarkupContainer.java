5237015cc8b097c3d62e2ec3b64958116b3b3f17
Igor Vaynberg <ivaynberg@apache.org>
2006-04-03 16:02:11 +0000

    fixed erroneous exception message
    git-svn-id: https://svn.apache.org/repos/asf/incubator/wicket/trunk@460117 13f79535-47bb-0310-9956-ffa450edef68

--- wicket/src/java/wicket/MarkupContainer.java
+++ wicket/src/java/wicket/MarkupContainer.java
@@ -789,7 +789,7 @@ public abstract class MarkupContainer extends Component
 							+ "' for component '"
 							+ getClass().getName()
 							+ "' not found."
-							+ " Enable debug messages for wicket.util.resource.Resource to get a list of all filenames tried"),
+							+ " Enable debug messages for wicket.util.resource to get a list of all filenames tried"),
 					ex);
 		}
 	}
