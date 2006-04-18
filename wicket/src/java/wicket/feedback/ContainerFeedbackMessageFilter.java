95c799006d34f6deb35c08f6169917a67102b813
Igor Vaynberg <ivaynberg@apache.org>
2006-04-18 17:08:08 +0000

    forgotten @author tag
    git-svn-id: https://svn.apache.org/repos/asf/incubator/wicket/trunk@460324 13f79535-47bb-0310-9956-ffa450edef68

--- wicket/src/java/wicket/feedback/ContainerFeedbackMessageFilter.java
+++ wicket/src/java/wicket/feedback/ContainerFeedbackMessageFilter.java
@@ -24,6 +24,7 @@ import wicket.MarkupContainer;
  * Filter for child-of relationship
  * 
  * @author Jonathan Locke
+ * @author Johan Compagner
  */
 public class ContainerFeedbackMessageFilter implements IFeedbackMessageFilter
 {
