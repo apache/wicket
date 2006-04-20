e52c706f39037ce3286f4b78edd6404c9b90d83c
Igor Vaynberg <ivaynberg@apache.org>
2006-04-20 05:25:53 +0000

    cleanup
    git-svn-id: https://svn.apache.org/repos/asf/incubator/wicket/trunk@460345 13f79535-47bb-0310-9956-ffa450edef68

--- wicket-extensions/src/java/wicket/extensions/markup/html/form/palette/component/Recorder.java
+++ wicket-extensions/src/java/wicket/extensions/markup/html/form/palette/component/Recorder.java
@@ -1,7 +1,6 @@
 package wicket.extensions.markup.html.form.palette.component;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.Iterator;
