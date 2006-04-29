dc909ac9cbfb336b3df127b42c158ebeb3032e9d
Jonathan Locke <jonl@apache.org>
2006-04-29 06:33:47 +0000

    Added java.io.File constructor to File for convenience in working with wicket.util.file.File.  Added file filtering to Folder for convenience so that returned files are wicket.util.file.Files.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/wicket/trunk/wicket@460436 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/wicket/util/file/Folder.java
+++ src/java/wicket/util/file/Folder.java
@@ -1,6 +1,6 @@
 /*
- * $Id$ $Revision$
- * $Date$
+ * $Id$ $Revision$ $Date:
+ * 2005-10-02 03:06:33 -0700 (Sun, 02 Oct 2005) $
  * 
  * ==============================================================================
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not
@@ -17,6 +17,7 @@
  */
 package wicket.util.file;
 
+import java.io.FileFilter;
 import java.net.URI;
 
 /**
@@ -75,5 +76,45 @@ public final class Folder extends File
 	public Folder(final URI uri)
 	{
 		super(uri);
+	}
+
+	/**
+	 * File filter for folders
+	 * 
+	 * @author Jonathan Locke
+	 */
+	public static interface Filter
+	{
+		/**
+		 * @param file
+		 *            The file to test
+		 * @return True if the file should be accepted
+		 */
+		public boolean accept(File file);
+	}
+
+	/**
+	 * @param filter
+	 *            Filename fiter
+	 * @return Files
+	 */
+	public File[] getFiles(final Filter filter)
+	{
+		final java.io.File[] files = listFiles(new FileFilter()
+		{
+			/**
+			 * @see java.io.FileFilter#accept(java.io.File)
+			 */
+			public boolean accept(java.io.File file)
+			{
+				return filter.accept(new File(file));
+			}
+		});
+		final File[] wicketFiles = new File[files.length];
+		for (int i = 0; i < files.length; i++)
+		{
+			wicketFiles[i] = new File(files[i]);
+		}
+		return wicketFiles;
 	}
 }
