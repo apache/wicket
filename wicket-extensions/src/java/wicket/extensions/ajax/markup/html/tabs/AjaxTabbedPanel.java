9cd2cf57242760c243a69d117eb56a73318684c1
Igor Vaynberg <ivaynberg@apache.org>
2006-04-29 16:48:57 +0000

    added method to let users postprocess ajax update
    git-svn-id: https://svn.apache.org/repos/asf/incubator/wicket/trunk@460437 13f79535-47bb-0310-9956-ffa450edef68

--- wicket-extensions/src/java/wicket/extensions/ajax/markup/html/tabs/AjaxTabbedPanel.java
+++ wicket-extensions/src/java/wicket/extensions/ajax/markup/html/tabs/AjaxTabbedPanel.java
@@ -44,10 +44,27 @@ public class AjaxTabbedPanel extends TabbedPanel
 				{
 					target.addComponent(AjaxTabbedPanel.this);
 				}
+				onAjaxUpdate(target);
 			}
 
 		};
 	}
 
+	/**
+	 * A template method that lets users add additional behavior when ajax
+	 * update occurs. This method is called after the current tab has been set
+	 * so access to it can be obtained via {@link #getSelectedTab()}.
+	 * <p>
+	 * <strong>Note</strong> Since an {@link AjaxFallbackLink} is used to back
+	 * the ajax update the <code>target</code> argument can be null when the client
+	 * browser does not support ajax and the fallback mode is used. See
+	 * {@link AjaxFallbackLink} for details.
+	 * 
+	 * @param target
+	 *            ajax target used to update this component
+	 */
+	protected void onAjaxUpdate(AjaxRequestTarget target)
+	{
+	}
 
 }
