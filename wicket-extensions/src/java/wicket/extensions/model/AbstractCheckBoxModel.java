01b96037e36559ee157bd09d6f969da167652c67
Eelco Hillenius <ehillenius@apache.org>
2006-05-27 19:56:38 +0000

    wip
    git-svn-id: https://svn.apache.org/repos/asf/incubator/wicket/trunk@460784 13f79535-47bb-0310-9956-ffa450edef68

--- wicket-extensions/src/java/wicket/extensions/model/AbstractCheckBoxModel.java
+++ wicket-extensions/src/java/wicket/extensions/model/AbstractCheckBoxModel.java
@@ -8,10 +8,18 @@ import wicket.model.IModel;
  * 
  * @author Igor Vaynberg (ivaynberg)
  * 
+ * @deprecated I think this model can be got rid of now we have generics, no?
  */
-public abstract class AbstractCheckBoxModel implements IModel
+public abstract class AbstractCheckBoxModel implements IModel<Boolean>
 {
 	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see wicket.model.IDetachable#detach()
+	 */
+	public void detach()
+	{
+	}
 
 	/**
 	 * @see wicket.model.IModel#getNestedModel()
@@ -24,19 +32,27 @@ public abstract class AbstractCheckBoxModel implements IModel
 	/**
 	 * @see wicket.model.IModel#getObject(wicket.Component)
 	 */
-	public Object getObject(Component component)
+	public Boolean getObject(Component component)
 	{
 		return isSelected(component) ? Boolean.TRUE : Boolean.FALSE;
 	}
 
+
+	/**
+	 * Returns model's value
+	 * 
+	 * @param component
+	 * @return true to indicate the checkbox should be selected, false otherwise
+	 */
+	public abstract boolean isSelected(Component component);
+
 	/**
 	 * @see wicket.model.IModel#setObject(wicket.Component, java.lang.Object)
 	 */
-	public void setObject(Component component, Object object)
+	public void setObject(Component component, Boolean object)
 	{
 		boolean sel = Boolean.TRUE.equals(object);
 		setSelected(component, sel);
-
 	}
 
 
@@ -48,21 +64,4 @@ public abstract class AbstractCheckBoxModel implements IModel
 	 *            true if the checkbox is selected, false otherwise
 	 */
 	public abstract void setSelected(Component component, boolean sel);
-
-	/**
-	 * Returns model's value
-	 * 
-	 * @param component
-	 * @return true to indicate the checkbox should be selected, false otherwise
-	 */
-	public abstract boolean isSelected(Component component);
-
-
-	/**
-	 * @see wicket.model.IDetachable#detach()
-	 */
-	public void detach()
-	{
-	}
-
 }
