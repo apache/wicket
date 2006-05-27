ace2e9c860eb3ee17c40b38af0eb48763e0a6e9c
Eelco Hillenius <ehillenius@apache.org>
2006-05-27 20:36:48 +0000

    wip
    git-svn-id: https://svn.apache.org/repos/asf/incubator/wicket/trunk@460788 13f79535-47bb-0310-9956-ffa450edef68

--- wicket/src/java/wicket/model/StringResourceModel.java
+++ wicket/src/java/wicket/model/StringResourceModel.java
@@ -223,7 +223,7 @@ import wicket.util.string.interpolator.PropertyVariableInterpolator;
  * 
  * @author Chris Turner
  */
-public class StringResourceModel extends AbstractReadOnlyDetachableModel<String>
+public class StringResourceModel extends AbstractReadOnlyDetachableModel<CharSequence>
 {
 	private static final long serialVersionUID = 1L;
 
@@ -483,7 +483,7 @@ public class StringResourceModel extends AbstractReadOnlyDetachableModel<String>
 	 * @see AbstractDetachableModel#onGetObject(Component)
 	 */
 	@Override
-	protected final String onGetObject(final Component component)
+	protected final CharSequence onGetObject(final Component component)
 	{
 		return getString();
 	}
