6fbba68e6ad748b96fcdb60a1c9a01e9878573df
Johan Compagner <jcompagner@apache.org>
2006-06-02 21:44:37 +0000

    give the cause exception
    git-svn-id: https://svn.apache.org/repos/asf/incubator/wicket/trunk/wicket@460882 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/wicket/application/DefaultClassResolver.java
+++ src/java/wicket/application/DefaultClassResolver.java
@@ -66,7 +66,7 @@ public final class DefaultClassResolver implements IClassResolver
 		}
 		catch (ClassNotFoundException ex)
 		{
-			throw new WicketRuntimeException("Unable to load class with name: " + classname);
+			throw new WicketRuntimeException("Unable to load class with name: " + classname, ex);
 		}
 	}
 }
