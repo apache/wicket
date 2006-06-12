0a0f710a197449479772fb3c0e25de863d2841e3
Eelco Hillenius <ehillenius@apache.org>
2006-06-12 07:53:50 +0000

    some additional doc/ explaination
    git-svn-id: https://svn.apache.org/repos/asf/incubator/wicket/trunk@461016 13f79535-47bb-0310-9956-ffa450edef68

--- wicket-examples/src/java/wicket/examples/template/TemplatePage.java
+++ wicket-examples/src/java/wicket/examples/template/TemplatePage.java
@@ -26,6 +26,48 @@ import wicket.model.PropertyModel;
  * Our base page that serves as a template for pages that inherit from it.
  * Doesn't have to be abstract, but was made abstract here to stress the fact
  * that this page is not meant for direct use.
+ * <p>
+ * Alternatively, instead of creating new instances of components whenever we
+ * want to replace others (Banner1/ Banner2), we can re-use components. This
+ * class could be re-written like this:
+ * 
+ * <pre>
+ *       public abstract class TemplatePage extends WicketExamplePage
+ *       {
+ *              private String pageTitle = &quot;(no title)&quot;;
+ *       
+ *              private Banner currentBanner;
+ *       
+ *              private Banner banner1;
+ *       
+ *              private Banner banner2;
+ *       
+ *              public TemplatePage()
+ *              {
+ *                      new Label(this, &quot;title&quot;, new PropertyModel(this, &quot;pageTitle&quot;));
+ *                      banner2 = new Banner2(this, &quot;ad&quot;);
+ *                      currentBanner = banner1 = new Banner1(this, &quot;ad&quot;);
+ *       
+ *                     new Link(this, &quot;changeAdLink&quot;)
+ *                     {
+ *                              public void onClick()
+ *                              {
+ *                                      if (currentBanner == banner1)
+ *                                      {
+ *                                              currentBanner = banner2;
+ *                                              banner2.reAttach();
+ *                                      }
+ *                                      else
+ *                                      {
+ *                                              currentBanner = banner1;
+ *                                              banner1.reAttach();
+ *                                      }
+ *                              }
+ *                      };
+ *       ...
+ * </pre>
+ * 
+ * </p>
  * 
  * @author Eelco Hillenius
  */
@@ -54,6 +96,19 @@ public abstract class TemplatePage extends WicketExamplePage
 			{
 				if (currentBanner.getClass() == Banner1.class)
 				{
+					// we already had a component directly under the page with
+					// id 'ad'. Creating a new one like this (same id, same
+					// hierarchy position) will have the effect that this new
+					// component will be set as the current one, and thus will
+					// be rendered instead of the previous child. In Wicket
+					// pre 2.0 (before you had to pass in the parent in the
+					// constructor to create the hierarchy, and had to use
+					// Component#add instead) you achieved the same by calling
+					// Component#replace. Now you either construct a new
+					// component with the same parent and same id, or - if
+					// you have a reference to a component that was previously
+					// created with that parent - you call Component#reAttach
+					// to set that component as the current one.
 					new Banner2(TemplatePage.this, "ad");
 				}
 				else
