3a1f4b35f0a6310275e2d1ff252ee2500900a091
Igor Vaynberg <ivaynberg@apache.org>
2006-06-23 15:48:50 +0000

    fixed tests
    git-svn-id: https://svn.apache.org/repos/asf/incubator/wicket/branches/WICKET_1_2@461127 13f79535-47bb-0310-9956-ffa450edef68

--- wicket/src/java/wicket/util/tester/FormTester.java
+++ wicket/src/java/wicket/util/tester/FormTester.java
@@ -427,10 +427,14 @@ public class FormTester
 						new SearchOptionByIndexVisitor(index));
 				if (foundCheck == null)
 				{
-					Assert.fail("CheckGroup " + formComponent.getPath() + " does not has index:"
+					Assert.fail("CheckGroup " + formComponent.getPath() + " does not have index:"
 							+ index);
 				}
-				assignValueToFormComponent(formComponent, foundCheck.getPath());
+				
+				String path=foundCheck.getPath();
+				path=path.substring(formComponent.getPath().length()+1, path.length());
+				
+				assignValueToFormComponent(formComponent, path);
 			}
 			else
 			{
