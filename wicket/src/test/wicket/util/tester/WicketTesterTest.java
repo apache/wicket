65b606c4861c2e356e5cb41c0976eba2b2cd721a
Eelco Hillenius <ehillenius@apache.org>
2006-06-26 22:36:15 +0000

    1512212
    git-svn-id: https://svn.apache.org/repos/asf/incubator/wicket/trunk@461166 13f79535-47bb-0310-9956-ffa450edef68

--- wicket/src/test/wicket/util/tester/WicketTesterTest.java
+++ wicket/src/test/wicket/util/tester/WicketTesterTest.java
@@ -23,10 +23,14 @@ import java.util.Locale;
 import junit.framework.TestCase;
 import wicket.Component;
 import wicket.MockPageWithLink;
+import wicket.MockPageWithOneComponent;
 import wicket.Page;
 import wicket.Session;
+import wicket.ajax.AjaxEventBehavior;
 import wicket.ajax.AjaxRequestTarget;
+import wicket.ajax.ClientEvent;
 import wicket.ajax.markup.html.AjaxLink;
+import wicket.markup.html.basic.Label;
 import wicket.markup.html.link.Link;
 import wicket.util.tester.apps_1.Book;
 import wicket.util.tester.apps_1.CreateBook;
@@ -34,12 +38,22 @@ import wicket.util.tester.apps_1.MyMockApplication;
 import wicket.util.tester.apps_1.SuccessPage;
 import wicket.util.tester.apps_1.ViewBook;
 
+
+
 /**
  * 
  * @author Juergen Donnerstag
  */
 public class WicketTesterTest extends TestCase
 {
+	private boolean eventExecuted;
+		
+	@Override
+	protected void setUp() throws Exception
+	{
+		eventExecuted = false;
+	}
+
 	/**
 	 * 
 	 * @throws Exception
@@ -212,5 +226,45 @@ public class WicketTesterTest extends TestCase
 		
 		// This must not fail
 		tester.assertComponentOnAjaxResponse(component);
+	}
+
+	/**
+	 * Test that the executeAjaxEvent on the WicketTester works.
+	 */
+	public void testExecuteAjaxEvent()
+	{
+		// Start the tester
+		WicketTester tester = new WicketTester();
+
+		// Setup mocks
+		final MockPageWithOneComponent page = new MockPageWithOneComponent();
+
+		Label label = new Label(page, "component", "Dblclick This To See Magick");
+		label.add(new AjaxEventBehavior(ClientEvent.DBLCLICK)
+		{
+			private static final long serialVersionUID = 1L;
+
+			@Override
+			protected void onEvent(AjaxRequestTarget target)
+			{
+				eventExecuted = true;
+			}
+		});
+
+		// Start the page
+		tester.startPage(new ITestPageSource()
+		{
+			private static final long serialVersionUID = 1L;
+
+			public Page getTestPage()
+			{
+				return page;
+			}
+		});
+
+		// Execute the event
+		tester.executeAjaxEvent(label, ClientEvent.DBLCLICK);
+
+		assertTrue(eventExecuted);
 	}
 }
