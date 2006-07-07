34e024a648b203a40c6804f4507c342339f1b6ab
Matej Knopp <knopp@apache.org>
2006-07-07 17:11:33 +0000

    Implemented executing scripts when replacing components and fixed javascript script contribution
    git-svn-id: https://svn.apache.org/repos/asf/incubator/wicket/trunk/wicket@461292 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/wicket/ajax/wicket-ajax-debug.js
+++ src/java/wicket/ajax/wicket-ajax-debug.js
@@ -41,47 +41,52 @@ var WicketAjaxDebug = {
     },
 
 	init : function() {
-        if ( wicketAjaxDebugEnabled() ) {
+
+        if ( wicketAjaxDebugEnabled()) {
         	var wad=WicketAjaxDebug;
         	var dwid=wad.debugWindowId;
         	var dwdhid=wad.debugWindowDragHandleId;
-        	
-            document.write(
-        	
-        	 "<table id='"+dwid+"' cellspacing='0' cellpadding='2'"
-        	+"    style='display:none; position: absolute; left:300px; top:300px; width: 600px; z-index:100;"
-        	+"    background-color: white; border: 2px solid #333; padding: 5px;' "
-        	+"    ><tr>"
-        	
-        	+"    <td id='"+dwdhid+"' "
-        	+"        style='cursor:move;"
-        	+"        color: white; background-color: navy;'"
-        	+"        >"
-        	
-        	+"        Wicket Ajax Debug Window (drag me here)"
-        	+"    </td>"
-        	+"    <td style='text-align: right;'>"
-        	+"        <a href='javascript:WicketAjaxDebug.clearLog()'>clear</a> | "
-        	+"        <a href='javascript:WicketAjaxDebug.hideDebugWindow()'>close</a>"
-        	+"     </td> </tr><td colspan='2'>"
-        	    
-        	+"    <form>"
-        	    
-        	+"        <textarea cols='60' rows='20' wrap='off' "
-        	+"            name='"+WicketAjaxDebug.debugWindowLogId+"' id='"+WicketAjaxDebug.debugWindowLogId+"' "
-        	+"            style='width:100%;' "
-        	+"        ></textarea>"
-        	    
-        	+"    </form>"
-        	+"</td></tr></table>");
+
+			var firstTime = document.getElementById(dwid) == null;
+
+			if (firstTime)
+	            document.write(
+	        	
+	        	 "<table id='"+dwid+"' cellspacing='0' cellpadding='2'"
+	        	+"    style='display:none; position: absolute; left:300px; top:300px; width: 600px; z-index:100;"
+	        	+"    background-color: white; border: 2px solid #333; padding: 5px;' "
+	        	+"    ><tr>"
+	        	
+	        	+"    <td id='"+dwdhid+"' "
+	        	+"        style='cursor:move;"
+	        	+"        color: white; background-color: navy;'"
+	        	+"        >"
+	        	
+	        	+"        Wicket Ajax Debug Window (drag me here)"
+	        	+"    </td>"
+	        	+"    <td style='text-align: right;'>"
+	        	+"        <a href='javascript:WicketAjaxDebug.clearLog()'>clear</a> | "
+	        	+"        <a href='javascript:WicketAjaxDebug.hideDebugWindow()'>close</a>"
+	        	+"     </td> </tr><td colspan='2'>"
+	        	    
+	        	+"    <form>"
+	        	    
+	        	+"        <textarea cols='60' rows='20' wrap='off' "
+	        	+"            name='"+WicketAjaxDebug.debugWindowLogId+"' id='"+WicketAjaxDebug.debugWindowLogId+"' "
+	        	+"            style='width:100%;' "
+	        	+"        ></textarea>"
+	        	    
+	        	+"    </form>"
+	        	+"</td></tr></table>");
             
             WicketDrag.init(wicketGet(dwdhid), wicketGet(dwid));
-            
-            document.write(
-            
-             "<div style='position:absolute; left:10px; top:95%; z-index:100;'>"
-            +"    <a href='javascript:WicketAjaxDebug.showDebugWindow()'>WICKET AJAX DEBUG</a>"
-            +"</div>");
+
+			if (firstTime)            
+	            document.write(
+	            
+	             "<div style='position:absolute; left:10px; top:95%; z-index:100;'>"
+	            +"    <a href='javascript:WicketAjaxDebug.showDebugWindow()'>WICKET AJAX DEBUG</a>"
+	            +"</div>");
         }
 	}
 
