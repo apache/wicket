52a305c8699a93cf48d4fcaa2755cf1f8705f885
Igor Vaynberg <ivaynberg@apache.org>
2006-07-10 20:41:57 +0000

    IDebugSettings.serializeSessionAttributes
    git-svn-id: https://svn.apache.org/repos/asf/incubator/wicket/trunk@461323 13f79535-47bb-0310-9956-ffa450edef68

--- wicket/src/java/wicket/settings/IDebugSettings.java
+++ wicket/src/java/wicket/settings/IDebugSettings.java
@@ -6,8 +6,11 @@ package wicket.settings;
  * <i>componentUseCheck </i> (defaults to true) - Causes the framework to do a
  * check after rendering each page to ensure that each component was used in
  * rendering the markup. If components are found that are not referenced in the
- * markup, an appropriate error will be displayed
+ * markup, an appropriate error will be displayed <i>serializeSessionAttributes</i>
+ * (defaults to true in devel mode) - Causes the framework to serialize any
+ * attribute put into session - this helps find Not Serializable errors early
  * <p>
+ * 
  * 
  * @author Igor Vaynberg (ivaynberg)
  */
@@ -41,5 +44,17 @@ public interface IDebugSettings
 	 * 
 	 */
 	boolean isAjaxDebugModeEnabled();
+
+	/**
+	 * Sets the seriaalize session attributes setting
+	 * 
+	 * @param serialize
+	 */
+	void setSerializeSessionAttributes(boolean serialize);
+
+	/**
+	 * @return true if serialize session attributes is enabled, false otherwise
+	 */
+	boolean getSerializeSessionAttributes();
 
 }
