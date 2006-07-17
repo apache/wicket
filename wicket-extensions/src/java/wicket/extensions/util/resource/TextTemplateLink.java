46fe0e914d2c591014f465948d3800a830bc019b
Eelco Hillenius <ehillenius@apache.org>
2006-07-17 10:19:55 +0000

    javadoc, format
    git-svn-id: https://svn.apache.org/repos/asf/incubator/wicket/branches/WICKET_1_2@461341 13f79535-47bb-0310-9956-ffa450edef68

--- wicket-extensions/src/java/wicket/extensions/util/resource/TextTemplateLink.java
+++ wicket-extensions/src/java/wicket/extensions/util/resource/TextTemplateLink.java
@@ -33,6 +33,8 @@ public class TextTemplateLink extends ResourceLink
 	private static final long serialVersionUID = 1L;
 
 	/**
+	 * Construct.
+	 * 
 	 * @param id
 	 *            Component id
 	 * @param factory
