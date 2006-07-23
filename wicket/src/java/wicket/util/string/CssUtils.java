aea36573929d4f5253e7674cf7b73c03261945d4
Eelco Hillenius <ehillenius@apache.org>
2006-07-23 11:42:43 +0000

    added css utils
    git-svn-id: https://svn.apache.org/repos/asf/incubator/wicket/branches/WICKET_1_2@461460 13f79535-47bb-0310-9956-ffa450edef68

---
+++ wicket/src/java/wicket/util/string/CssUtils.java
@@ -0,0 +1,40 @@
+/*
+ * $Id: org.eclipse.jdt.ui.prefs 5004 2006-03-17 20:47:08 -0800 (Fri, 17 Mar
+ * 2006) eelco12 $ $Revision: 5004 $ $Date: 2006-03-17 20:47:08 -0800 (Fri, 17
+ * Mar 2006) $
+ * 
+ * ==============================================================================
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package wicket.util.string;
+
+/**
+ * Utility methods for CSS.
+ * 
+ * @author eelcohillenius
+ */
+public final class CssUtils
+{
+	/** CSS inline open tag */
+	public final static String INLINE_OPEN_TAG = "<style type=\"text/css\"><!--\n";
+
+	/** CSS inline close tag */
+	public final static String INLINE_CLOSE_TAG = "--></style>\n";
+
+	/**
+	 * Hiden constructor.
+	 */
+	private CssUtils()
+	{
+	}
+}
