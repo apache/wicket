d1cae171f25193f2ba60b4390b72b56eb417e662
Matej Knopp <knopp@apache.org>
2006-07-28 20:39:31 +0000

    Improved debug console.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/wicket/trunk@461638 13f79535-47bb-0310-9956-ffa450edef68

--- wicket/src/java/wicket/ajax/wicket-ajax-debug.js
+++ wicket/src/java/wicket/ajax/wicket-ajax-debug.js
@@ -22,9 +22,17 @@ var WicketAjaxDebug = {
 	},
 
     log : function(msg) {
-        var ta=wicketGet(WicketAjaxDebug.debugWindowLogId);
-        ta.value=ta.value+"\n"+msg;
-        ta.scrollTop = ta.scrollHeight;
+        var d = wicketGet(WicketAjaxDebug.debugWindowLogId);
+        var c = document.createElement("div");
+        msg = msg.replace(/</g, "&lt;");
+        msg = msg.replace(/>/g, "&gt;");
+        msg = msg.replace(/\n/g, "<br/>");
+        msg = msg.replace(/ /g, "&nbsp;");  
+        msg = msg.replace(/\t/g, "&nbsp;&nbsp;&nbsp;&nbsp;");          
+        c.innerHTML = msg;
+        c.setAttribute("style","font-size: 82%; margin: 0px; padding:0px");        
+        d.appendChild(c);
+        d.scrollTop = d.scrollHeight;
     },
     
     logError : function(msg) {
@@ -36,8 +44,8 @@ var WicketAjaxDebug = {
     },
 
     clearLog : function() {
-        var ta=wicketGet(WicketAjaxDebug.debugWindowLogId);
-        ta.value=null;
+        var d = wicketGet(WicketAjaxDebug.debugWindowLogId);
+		d.innerHTML = "";
     },
 
 	init : function() {
@@ -50,35 +58,23 @@ var WicketAjaxDebug = {
 			var firstTime = document.getElementById(dwid) == null;
 
 			if (firstTime)
-	            document.write(
-	        	
-	        	 "<table id='"+dwid+"' cellspacing='0' cellpadding='2'"
-	        	+"    style='display:none; position: absolute; left:300px; top:300px; width: 600px; z-index:100;"
-	        	+"    background-color: white; border: 2px solid #333; padding: 5px;' "
-	        	+"    ><tr>"
-	        	
-	        	+"    <td id='"+dwdhid+"' "
-	        	+"        style='cursor:move;"
-	        	+"        color: white; background-color: navy;'"
-	        	+"        >"
-	        	
-	        	+"        Wicket Ajax Debug Window (drag me here)"
-	        	+"    </td>"
-	        	+"    <td style='text-align: right;'>"
-	        	+"        <a href='javascript:WicketAjaxDebug.clearLog()'>clear</a> | "
-	        	+"        <a href='javascript:WicketAjaxDebug.hideDebugWindow()'>close</a>"
-	        	+"     </td> </tr><td colspan='2'>"
-	        	    
-	        	+"    <form>"
-	        	    
-	        	+"        <textarea cols='60' rows='20' wrap='off' "
-	        	+"            name='"+WicketAjaxDebug.debugWindowLogId+"' id='"+WicketAjaxDebug.debugWindowLogId+"' "
-	        	+"            style='width:100%;' "
-	        	+"        ></textarea>"
-	        	    
-	        	+"    </form>"
-	        	+"</td></tr></table>");
-            
+	            document.write(	        	
+					"<div style='width: 50em; display: none; position: absolute; left: 200px; top: 300px; z-index: 1000;' id='"+dwid+"'>"+
+					"	<div style='border: 1px solid black; padding: 1px; background-color: #eee'>"+
+					"		<div style='overflow: auto; width: 100%'>"+
+					"			<div style='float: right; padding: 0.2em; padding-right: 1em;'>"+
+					"				<a href='javascript:WicketAjaxDebug.clearLog()' style='color:blue'>clear</a> | "+
+					"				<a href='javascript:WicketAjaxDebug.hideDebugWindow()' style='color:blue'>close</a>"+
+					"			</div>"+
+					"			<div id='"+dwdhid+"' style='padding: 0.2em; background-color: gray; color: white; padding-left: 1em; margin-right: 8em; cursor: move;'>"+
+					"				Wicket Ajax Debug Window (drag me here)"+
+					"			</div>"+
+					"			<div id='"+WicketAjaxDebug.debugWindowLogId+"' style='width: 100%; height: 30em; background-color: white; overflow: auto; white-space: nowrap'>"+
+					"			</div>"+
+					"		</div>"+					
+					"	</div>" +
+					"</div>" 
+	            );
             WicketDrag.init(wicketGet(dwdhid), wicketGet(dwid));
 
 			if (firstTime)            
