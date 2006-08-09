abe5fa9cc569612497984f387201c183ec0356c4
Igor Vaynberg <ivaynberg@apache.org>
2006-08-09 20:38:01 +0000

    spring injector for portlets support
    git-svn-id: https://svn.apache.org/repos/asf/incubator/wicket/trunk@461780 13f79535-47bb-0310-9956-ffa450edef68

--- wicket-spring/src/main/java/wicket/spring/injection/SpringComponentInjector.java
+++ wicket-spring/src/main/java/wicket/spring/injection/SpringComponentInjector.java
@@ -4,7 +4,12 @@ import java.io.Serializable;
 
 import javax.servlet.ServletContext;
 
+import org.springframework.beans.factory.xml.XmlBeanDefinitionReader;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.GenericApplicationContext;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.core.io.Resource;
 import org.springframework.web.context.support.WebApplicationContextUtils;
 
 import wicket.Application;
@@ -30,8 +35,8 @@ import wicket.spring.ISpringContextLocator;
  * <code>InjectorHolder.getInjector().inject(this)</code> in their
  * constructor.
  * 
- * @author ivaynberg
- * 
+ * @author Igor Vaynberg (ivaynberg)
+ * @author <a href="mailto:jlee@antwerkz.com">Justin Lee</a>
  */
 public class SpringComponentInjector extends ComponentInjector
 {
@@ -74,8 +79,25 @@ public class SpringComponentInjector extends ComponentInjector
 	 */
 	public SpringComponentInjector(PortletApplication portletapp)
 	{
-		throw new IllegalStateException("THIS IS NOT YET SUPPORTED");
-		// FIXME add support for resolving app context through the portlet app
+		// locate spring's application context ...
+		String configLocation = portletapp.getWicketPortlet().getInitParameter(
+				"contextConfigLocation");
+		GenericApplicationContext ctx = new GenericApplicationContext();
+		XmlBeanDefinitionReader xmlReader = new XmlBeanDefinitionReader(ctx);
+		Resource resource = new ClassPathResource(configLocation);
+		if (!resource.exists())
+		{
+			resource = new FileSystemResource(configLocation);
+		}
+		xmlReader.loadBeanDefinitions(resource);
+		ctx.refresh();
+
+		// ... stash the holder to the context in the application's metadata ...
+		portletapp.setMetaData(CONTEXT_KEY, new ApplicationContextHolder(ctx));
+
+		// ... and create and register the annotation aware injector
+		InjectorHolder.setInjector(new AnnotSpringInjector(new ContextLocator()));
+
 	}
 
 	/**
