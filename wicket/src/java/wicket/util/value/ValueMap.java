fc347fda10a8c91e7127ccf89359a8d76e0aa55d
Johan Compagner <jcompagner@apache.org>
2006-08-09 12:25:14 +0000

    fix from 1.2
    git-svn-id: https://svn.apache.org/repos/asf/incubator/wicket/trunk@461769 13f79535-47bb-0310-9956-ffa450edef68

--- wicket/src/java/wicket/util/value/ValueMap.java
+++ wicket/src/java/wicket/util/value/ValueMap.java
@@ -23,9 +23,6 @@ import java.util.HashMap;
 import java.util.Iterator;
 import java.util.Map;
 
-import wicket.util.parse.metapattern.parsers.VariableAssignmentParser;
-import wicket.util.string.IStringIterator;
-import wicket.util.string.StringList;
 import wicket.util.string.StringValue;
 import wicket.util.string.StringValueConversionException;
 import wicket.util.time.Duration;
@@ -114,28 +111,41 @@ public class ValueMap extends HashMap<String, Object> implements IValueMap
 	 */
 	public ValueMap(final String keyValuePairs, final String delimiter)
 	{
-		// Get list of strings separated by the delimiter
-		final StringList pairs = StringList.tokenize(keyValuePairs, delimiter);
-
-		// Go through each string in the list
-		for (IStringIterator iterator = pairs.iterator(); iterator.hasNext();)
+		int start = 0;
+		int equalsIndex = keyValuePairs.indexOf('=');
+		int delimiterIndex = keyValuePairs.indexOf(delimiter,equalsIndex);
+		if(delimiterIndex == -1) delimiterIndex = keyValuePairs.length();
+		while(equalsIndex != -1)
 		{
-			// Get the next key value pair
-			final String pair = iterator.next();
-
-			// Parse using metapattern parser for variable assignments
-			final VariableAssignmentParser parser = new VariableAssignmentParser(pair);
-
-			// Does it parse?
-			if (parser.matches())
+			if(delimiterIndex < keyValuePairs.length())
 			{
-				// Succeeded. Put key and value into map
-				put(parser.getKey(), parser.getValue());
+				int equalsIndex2 = keyValuePairs.indexOf('=', delimiterIndex+1);
+				if(equalsIndex2 != -1)
+				{
+					int delimiterIndex2 = keyValuePairs.lastIndexOf(delimiter, equalsIndex2);
+					delimiterIndex = delimiterIndex2;
+				}
+				else
+				{
+					delimiterIndex = keyValuePairs.length();
+				}
+			}
+			String key = keyValuePairs.substring(start,equalsIndex);
+			String value = keyValuePairs.substring(equalsIndex+1, delimiterIndex);
+			put(key,value);
+			if(delimiterIndex < keyValuePairs.length())
+			{
+				start = delimiterIndex+1;
+				equalsIndex = keyValuePairs.indexOf('=',start);
+				if(equalsIndex != -1)
+				{
+					delimiterIndex = keyValuePairs.indexOf(delimiter,equalsIndex);
+					if(delimiterIndex == -1) delimiterIndex = keyValuePairs.length();
+				}
 			}
 			else
 			{
-				throw new IllegalArgumentException("Invalid key value list: '" + keyValuePairs
-						+ "'");
+				equalsIndex = -1;
 			}
 		}
 	}
---
+++ wicket/src/test/wicket/util/value/ValueMapTest.java
@@ -0,0 +1,103 @@
+/*
+ * $Id: org.eclipse.jdt.ui.prefs 5004 2006-03-17 20:47:08 -0800 (Fri, 17 Mar 2006) eelco12 $
+ * $Revision: 5004 $
+ * $Date: 2006-03-17 20:47:08 -0800 (Fri, 17 Mar 2006) $
+ * 
+ * ==============================================================================
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package wicket.util.value;
+
+import junit.framework.TestCase;
+
+/**
+ * @author jcompagner
+ */
+public class ValueMapTest extends TestCase
+{
+	/**
+	 * @throws Exception
+	 */
+	public void testStringParseConstructorSimple() throws Exception
+	{
+		ValueMap vm = new ValueMap("param=value");
+		assertEquals(1, vm.size());
+		assertEquals("value", vm.get("param"));
+		
+
+		vm = new ValueMap("param1=value1,param2=value2");
+		assertEquals(2, vm.size());
+		assertEquals("value1", vm.get("param1"));
+		assertEquals("value2", vm.get("param2"));
+
+		vm = new ValueMap("param1=value1;param2=value2", ";");
+		assertEquals(2, vm.size());
+		assertEquals("value1", vm.get("param1"));
+		assertEquals("value2", vm.get("param2"));
+	}
+
+	/**
+	 * @throws Exception
+	 */
+	public void testStringParseConstructorSpecialChars() throws Exception
+	{
+		ValueMap vm = new ValueMap("param1=val>ue1;param2=value2", ";");
+		assertEquals(2, vm.size());
+		assertEquals("val>ue1", vm.get("param1"));
+		assertEquals("value2", vm.get("param2"));
+
+		vm = new ValueMap("param1=val:ue1;param2=value2", ";");
+		assertEquals(2, vm.size());
+		assertEquals("val:ue1", vm.get("param1"));
+		assertEquals("value2", vm.get("param2"));
+
+		vm = new ValueMap("param1=val?ue1;param2=val<>ue2", ";");
+		assertEquals(2, vm.size());
+		assertEquals("val?ue1", vm.get("param1"));
+		assertEquals("val<>ue2", vm.get("param2"));
+
+	}
+
+	/**
+	 * @throws Exception
+	 */
+	public void testStringParseConstructorDelimitersAndEqualsSign() throws Exception
+	{
+		ValueMap vm = new ValueMap("param1=val=ue1;param2=value2", ";");
+		assertEquals(2, vm.size());
+		assertEquals("val=ue1", vm.get("param1"));
+		assertEquals("value2", vm.get("param2"));
+
+		vm = new ValueMap("param1=value1;param2=val=ue2", ";");
+		assertEquals(2, vm.size());
+		assertEquals("value1", vm.get("param1"));
+		assertEquals("val=ue2", vm.get("param2"));
+		
+		vm = new ValueMap("param1=val;ue1;param2=value2", ";");
+		assertEquals(2, vm.size());
+		assertEquals("val;ue1", vm.get("param1"));
+		assertEquals("value2", vm.get("param2"));
+
+		vm = new ValueMap("param1=value1;param2=val;ue2", ";");
+		assertEquals(2, vm.size());
+		assertEquals("value1", vm.get("param1"));
+		assertEquals("val;ue2", vm.get("param2"));
+
+		vm = new ValueMap("param1=va=l;ue1;param2=val;ue2;param3=val=ue3", ";");
+		assertEquals(3, vm.size());
+		assertEquals("va=l;ue1", vm.get("param1"));
+		assertEquals("val;ue2", vm.get("param2"));
+		assertEquals("val=ue3", vm.get("param3"));
+		
+	}
+}
