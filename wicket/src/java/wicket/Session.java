31eff92cfa4e7fea55e9bf85144bb4fdc6e015f6
Igor Vaynberg <ivaynberg@apache.org>
2006-09-12 07:07:47 +0000

    fix the previous fix to eliminate possible npe
    git-svn-id: https://svn.apache.org/repos/asf/incubator/wicket/trunk@462170 13f79535-47bb-0310-9956-ffa450edef68

--- wicket/src/java/wicket/Session.java
+++ wicket/src/java/wicket/Session.java
@@ -452,7 +452,7 @@ public abstract class Session implements Serializable, IConverterLocator
 		}
 
 		// Get page map by name, creating the default page map automatically
-		PageMap pageMap = pageMapForName(pageMapName, pageMapName.equals(PageMap.DEFAULT_NAME));
+		PageMap pageMap = pageMapForName(pageMapName, Objects.equal(PageMap.DEFAULT_NAME,pageMapName));
 		if (pageMap != null)
 		{
 			long startTime = System.currentTimeMillis();
