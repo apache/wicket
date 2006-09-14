e5f29032367ab45519d3f2660b6585f3387d068e
Igor Vaynberg <ivaynberg@apache.org>
2006-09-14 07:36:28 +0000

    reimplemented to make mucho cleaner
    git-svn-id: https://svn.apache.org/repos/asf/incubator/wicket/trunk@462217 13f79535-47bb-0310-9956-ffa450edef68

--- wicket-extensions/src/java/wicket/extensions/model/AbstractCheckBoxModel.java
+++ wicket-extensions/src/java/wicket/extensions/model/AbstractCheckBoxModel.java
@@ -16,69 +16,64 @@
  */
 package wicket.extensions.model;
 
-import wicket.Component;
-import wicket.model.AbstractDetachableAssignmentAwareModel;
+import wicket.model.IModel;
 
 /**
  * Model adapter that makes working with models for checkboxes easier.
  * 
  * @author Igor Vaynberg (ivaynberg)
  */
-public abstract class AbstractCheckBoxModel extends AbstractDetachableAssignmentAwareModel
+public abstract class AbstractCheckBoxModel implements IModel<Boolean>
 {
 	private static final long serialVersionUID = 1L;
 
 	/**
-	 * Returns model's value
-	 * 
-	 * @param component
 	 * @return true to indicate the checkbox should be selected, false otherwise
 	 */
-	public abstract boolean isSelected(Component component);
+	public abstract boolean isSelected();
 
 	/**
-	 * Callback for setting the model's value to true or false
+	 * Called when checkbox has been selected
 	 * 
-	 * @param component
-	 * @param sel
-	 *            true if the checkbox is selected, false otherwise
 	 */
-	public abstract void setSelected(Component component, boolean sel);
+	public abstract void select();
 
 	/**
-	 * @see wicket.model.AbstractDetachableAssignmentAwareModel#onAttach()
+	 * Called when checkbox is unselected
+	 * 
 	 */
-	@Override
-	protected void onAttach()
+	public abstract void unselect();
+
+	/**
+	 * @see wicket.model.IModel#getNestedModel()
+	 */
+	public IModel getNestedModel()
 	{
+		return null;
 	}
 
 	/**
-	 * @see wicket.model.AbstractDetachableAssignmentAwareModel#onDetach()
+	 * 
+	 * @see wicket.model.IModel#getObject()
 	 */
-	@Override
-	protected void onDetach()
+	public final Boolean getObject()
 	{
+		return Boolean.valueOf(isSelected());
 	}
 
 	/**
-	 * @see wicket.model.AbstractDetachableAssignmentAwareModel#onGetObject(wicket.Component)
+	 * @see wicket.model.IModel#setObject(java.lang.Object)
 	 */
-	@Override
-	protected Object onGetObject(Component component)
+	public final void setObject(Boolean object)
 	{
-		return isSelected(component) ? Boolean.TRUE : Boolean.FALSE;
-	}
-
-	/**
-	 * @see wicket.model.AbstractDetachableAssignmentAwareModel#onSetObject(wicket.Component,
-	 *      java.lang.Object)
-	 */
-	@Override
-	protected void onSetObject(Component component, Object object)
-	{
-		boolean sel = Boolean.TRUE.equals(object);
-		setSelected(component, sel);
+		if (Boolean.TRUE.equals(object))
+		{
+			select();
+		}
+		else
+		{
+			unselect();
+		}
 	}
 
 }
