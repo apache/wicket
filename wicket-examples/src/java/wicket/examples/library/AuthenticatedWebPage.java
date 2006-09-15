1ba0c4125988c46adc3a10d8f6b779b68b2aaa84
Juegen Donnerstag <jdonnerstag@apache.org>
2006-09-15 19:24:32 +0000

    change IAlternateParentProvider to allow for more complex use cases
    git-svn-id: https://svn.apache.org/repos/asf/incubator/wicket/trunk@462259 13f79535-47bb-0310-9956-ffa450edef68

--- wicket-examples/src/java/wicket/examples/library/AuthenticatedWebPage.java
+++ wicket-examples/src/java/wicket/examples/library/AuthenticatedWebPage.java
@@ -68,9 +68,9 @@ public class AuthenticatedWebPage extends WicketExamplePage implements IAlternat
 	}
 
 	/**
-	 * @see wicket.markup.IAlternateParentProvider#getAlternateParent()
+	 * @see wicket.markup.IAlternateParentProvider#getAlternateParent(Class childClass, String childId)
 	 */
-	public MarkupContainer getAlternateParent()
+	public MarkupContainer getAlternateParent(final Class childClass, final String childId)
 	{
 		return (this.border == null ? this : this.border);
 	}
