3fb89d316db43816174570aec85cd4c7d3443ee7
Eelco Hillenius <ehillenius@apache.org>
2006-09-19 18:12:11 +0000

    deprecated responseType and added fileName
    git-svn-id: https://svn.apache.org/repos/asf/incubator/wicket/branches/wicket-1.x@462313 13f79535-47bb-0310-9956-ffa450edef68

--- wicket/src/java/wicket/request/target/resource/ResourceStreamRequestTarget.java
+++ wicket/src/java/wicket/request/target/resource/ResourceStreamRequestTarget.java
@@ -27,6 +27,7 @@ import wicket.IRequestTarget;
 import wicket.RequestCycle;
 import wicket.Response;
 import wicket.WicketRuntimeException;
+import wicket.protocol.http.WebResponse;
 import wicket.util.io.Streams;
 import wicket.util.resource.IResourceStream;
 
@@ -39,12 +40,33 @@ public class ResourceStreamRequestTarget implements IRequestTarget
 {
 	/** Logger */
 	private static final Log log = LogFactory.getLog(ResourceStreamRequestTarget.class);
-	
+
+	/**
+	 * Optional filename, used to set the content disposition header. Only
+	 * meaningful when using with web requests.
+	 */
+	private String fileName;
+
 	/** the resource stream for the response. */
 	private final IResourceStream resourceStream;
 
-	/** the response type, eg 'text/html' . */
+	/**
+	 * the response type, eg 'text/html'.
+	 * 
+	 * @deprecated Removed in 2.0
+	 */
 	private final String responseType;
+
+	/**
+	 * Construct.
+	 * 
+	 * @param resourceStream
+	 *            the resource stream for the response
+	 */
+	public ResourceStreamRequestTarget(IResourceStream resourceStream)
+	{
+		this(resourceStream, null);
+	}
 
 	/**
 	 * Construct.
@@ -53,6 +75,8 @@ public class ResourceStreamRequestTarget implements IRequestTarget
 	 *            the resource stream for the response
 	 * @param responseType
 	 *            the response type, eg 'text/html'
+	 * @deprecated Will be removed in 2.0. Response type can be determined from
+	 *             resource stream
 	 */
 	public ResourceStreamRequestTarget(IResourceStream resourceStream, String responseType)
 	{
@@ -61,17 +85,83 @@ public class ResourceStreamRequestTarget implements IRequestTarget
 			throw new IllegalArgumentException("Argument resourceStream must be not null");
 		}
 
-		if (responseType == null)
-		{
-			throw new IllegalArgumentException("Argument responseType must be not null");
-		}
-
 		this.resourceStream = resourceStream;
 		this.responseType = responseType;
 	}
 
 	/**
-	 * Does nothing at all.
+	 * @see wicket.IRequestTarget#detach(wicket.RequestCycle)
+	 */
+	public void detach(RequestCycle requestCycle)
+	{
+	}
+
+	/**
+	 * @see java.lang.Object#equals(java.lang.Object)
+	 */
+	public boolean equals(Object obj)
+	{
+		if (obj instanceof ResourceStreamRequestTarget)
+		{
+			ResourceStreamRequestTarget that = (ResourceStreamRequestTarget)obj;
+			return resourceStream.equals(that.resourceStream)
+					&& ((fileName != null) ? fileName.equals(this.fileName) : true);
+		}
+		return false;
+	}
+
+	/**
+	 * @return Optional filename, used to set the content disposition header.
+	 *         Only meaningful when using with web requests.
+	 */
+	public String getFileName()
+	{
+		return fileName;
+	}
+
+	/**
+	 * @see wicket.IRequestTarget#getLock(RequestCycle)
+	 */
+	public Object getLock(RequestCycle requestCycle)
+	{
+		return null;
+	}
+
+	/**
+	 * Gets the resource stream for the response.
+	 * 
+	 * @return the resource stream for the response
+	 */
+	public final IResourceStream getResourceStream()
+	{
+		return resourceStream;
+	}
+
+	/**
+	 * Gets the response type, eg 'text/html'.
+	 * 
+	 * @return the response type, eg 'text/html'
+	 * @deprecated Will be removed in 2.0. Response type can be determined by
+	 *             looking at the resource stream
+	 */
+	public final String getResponseType()
+	{
+		return responseType;
+	}
+
+	/**
+	 * @see java.lang.Object#hashCode()
+	 */
+	public int hashCode()
+	{
+		int result = "ResourceStreamRequestTarget".hashCode();
+		result += resourceStream.hashCode();
+		result += (fileName != null) ? fileName.hashCode() : 0;
+		return 17 * result;
+	}
+
+	/**
+	 * Responds by sending the contents of the resource stream.
 	 * 
 	 * @see wicket.IRequestTarget#respond(wicket.RequestCycle)
 	 */
@@ -134,7 +224,27 @@ public class ResourceStreamRequestTarget implements IRequestTarget
 	}
 
 	/**
-	 * Configures the response, default by setting the content type and length.
+	 * @param fileName
+	 *            Optional filename, used to set the content disposition header.
+	 *            Only meaningful when using with web requests.
+	 */
+	public void setFileName(String fileName)
+	{
+		this.fileName = fileName;
+	}
+
+	/**
+	 * @see java.lang.Object#toString()
+	 */
+	public String toString()
+	{
+		return "[ResourceStreamRequestTarget[resourceStream=" + resourceStream + ",fileName="
+				+ fileName + "]";
+	}
+
+	/**
+	 * Configures the response, default by setting the content type and length
+	 * and content disposition (in case the fileName property was set).
 	 * 
 	 * @param response
 	 *            the response
@@ -144,77 +254,21 @@ public class ResourceStreamRequestTarget implements IRequestTarget
 	protected void configure(final Response response, final IResourceStream resourceStream)
 	{
 		// Configure response with content type of resource
+		String responseType = resourceStream.getContentType();
+		if (responseType == null)
+		{
+			responseType = this.responseType;
+		}
 		response.setContentType(responseType + ";charset=" + response.getCharacterEncoding());
+
 		// and the content length
 		response.setContentLength((int)resourceStream.length());
-	}
 
-	/**
-	 * @see wicket.IRequestTarget#detach(wicket.RequestCycle)
-	 */
-	public void detach(RequestCycle requestCycle)
-	{
-	}
-
-	/**
-	 * @see wicket.IRequestTarget#getLock(RequestCycle)
-	 */
-	public Object getLock(RequestCycle requestCycle)
-	{
-		return null;
-	}
-
-	/**
-	 * Gets the resource stream for the response.
-	 * 
-	 * @return the resource stream for the response
-	 */
-	public final IResourceStream getResourceStream()
-	{
-		return resourceStream;
-	}
-
-	/**
-	 * Gets the response type, eg 'text/html'.
-	 * 
-	 * @return the response type, eg 'text/html'
-	 */
-	public final String getResponseType()
-	{
-		return responseType;
-	}
-
-	/**
-	 * @see java.lang.Object#equals(java.lang.Object)
-	 */
-	public boolean equals(Object obj)
-	{
-		if (obj instanceof ResourceStreamRequestTarget)
+		// and content disposition if any
+		String file = getFileName();
+		if (file != null && (response instanceof WebResponse))
 		{
-			ResourceStreamRequestTarget that = (ResourceStreamRequestTarget)obj;
-			return resourceStream.equals(that.resourceStream)
-					&& responseType.equals(that.responseType);
+			((WebResponse)response).setAttachmentHeader(file);
 		}
-		return false;
-	}
-
-	/**
-	 * @see java.lang.Object#hashCode()
-	 */
-	public int hashCode()
-	{
-		int result = "ResourceStreamRequestTarget".hashCode();
-		result += resourceStream.hashCode();
-		result += responseType.hashCode();
-		return 17 * result;
-	}
-
-	/**
-	 * @see java.lang.Object#toString()
-	 */
-	public String toString()
-	{
-		return "[ResourceStreamRequestTarget@" + hashCode() + " resourceStream=" + resourceStream
-				+ ",responseType=" + responseType + "]";
 	}
 }
