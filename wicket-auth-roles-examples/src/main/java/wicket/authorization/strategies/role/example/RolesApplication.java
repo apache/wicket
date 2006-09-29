32554b2e72b666eb601f4a423cc18aac9a653d9f
Eelco Hillenius <ehillenius@apache.org>
2006-09-29 18:19:43 +0000

    removed unneeded code
    git-svn-id: https://svn.apache.org/repos/asf/incubator/wicket/trunk@462411 13f79535-47bb-0310-9956-ffa450edef68

--- wicket-auth-roles-examples/src/main/java/wicket/authorization/strategies/role/example/RolesApplication.java
+++ wicket-auth-roles-examples/src/main/java/wicket/authorization/strategies/role/example/RolesApplication.java
@@ -70,7 +70,6 @@ public class RolesApplication extends WebApplication implements ISessionFactory
 	@Override
 	protected void init()
 	{
-		setSessionFactory(this);
 		getSecuritySettings().setAuthorizationStrategy(
 				new RoleAuthorizationStrategy(new UserRolesAuthorizer()));
 		MetaDataRoleAuthorizationStrategy.authorize(AdminBookmarkablePage.class, "ADMIN");
