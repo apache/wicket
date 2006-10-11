76f51e88a63df030184a5ea038995ddd7ea0273b
Gwyn Richard Evans <gwynevans@apache.org>
2006-10-11 15:20:34 +0000

    Modified SignInPanel to allow optional modification of the post-SignIn processing, e.g. add a possible redirect to a ChangePassword page, etc.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/wicket/trunk@462829 13f79535-47bb-0310-9956-ffa450edef68

--- wicket-auth-roles/src/main/java/wicket/authentication/panel/SignInPanel.java
+++ wicket-auth-roles/src/main/java/wicket/authentication/panel/SignInPanel.java
@@ -108,20 +108,11 @@ public class SignInPanel extends Panel
 		{
 			if (signIn(getUsername(), getPassword()))
 			{
-				// If login has been called because the user was not yet
-				// logged in, than continue to the original destination,
-				// otherwise to the Home page
-				if (!continueToOriginalDestination())
-				{
-					setResponsePage(getApplication().getSessionSettings().getPageFactory().newPage(
-							getApplication().getHomePage(), (PageParameters)null));
-				}
+			    onSignInSucceeded();
 			}
 			else
 			{
-				// Try the component based localizer first. If not found try the
-				// application localizer. Else use the default
-				error(getLocalizer().getString("signInFailed", this, "Sign in failed"));
+			    onSignInFailed();
 			}
 		}
 	}
@@ -235,4 +226,24 @@ public class SignInPanel extends Panel
 	{
 		return AuthenticatedWebSession.get().signIn(username, password);
 	}
+
+	protected void onSignInFailed()
+	{
+		// Try the component based localizer first. If not found try the
+		// application localizer. Else use the default
+		error(getLocalizer().getString("signInFailed", this, "Sign in failed"));
+	}
+
+	protected void onSignInSucceeded()
+	{
+		// If login has been called because the user was not yet
+		// logged in, than continue to the original destination,
+		// otherwise to the Home page
+		if (!continueToOriginalDestination())
+		{
+			setResponsePage(getApplication().getSessionSettings().getPageFactory().newPage(
+					getApplication().getHomePage(), (PageParameters)null));
+		}
+	}
+
 }
