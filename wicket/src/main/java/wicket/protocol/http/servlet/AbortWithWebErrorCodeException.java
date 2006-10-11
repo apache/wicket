eab58ee854892af4bb57d5549e1bc21e4c42c6b8
Eelco Hillenius <ehillenius@apache.org>
2006-10-11 23:36:22 +0000

    abort exception with error code and optional message
    git-svn-id: https://svn.apache.org/repos/asf/incubator/wicket/branches/wicket-1.x@463067 13f79535-47bb-0310-9956-ffa450edef68

---
+++ wicket/src/main/java/wicket/protocol/http/servlet/AbortWithWebErrorCodeException.java
@@ -0,0 +1,64 @@
+/*
+ * $Id: RestartResponseAtInterceptPageException.java,v 1.10 2006/02/13 00:16:32
+ * jonathanlocke Exp $ $Revision: 1.15 $ $Date: 2006/02/13 09:27:16 $
+ * 
+ * ==============================================================================
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package wicket.protocol.http.servlet;
+
+import wicket.AbortException;
+import wicket.RequestCycle;
+import wicket.protocol.http.request.WebErrorCodeResponseTarget;
+
+/**
+ * Causes Wicket to abort processing and set the specified HTTP error code, with
+ * the provided message if provided.
+ * 
+ * @author Eelco Hillenius
+ * 
+ * @see AbortException
+ * @see WebErrorCodeResponseTarget
+ */
+public final class AbortWithWebErrorCodeException extends AbortException
+{
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * Construct.
+	 * 
+	 * @param errorCode
+	 *            the servlet error code; use one of the
+	 *            {@link javax.servlet.http.HttpServletResponse} constants
+	 * @see javax.servlet.http.HttpServletResponse
+	 */
+	public AbortWithWebErrorCodeException(int errorCode)
+	{
+		this(errorCode, null);
+	}
+
+	/**
+	 * Construct.
+	 * 
+	 * @param errorCode
+	 *            the servlet error code; use one of the
+	 *            {@link javax.servlet.http.HttpServletResponse} constants
+	 * @param message
+	 *            the optional message to send to the client
+	 * @see javax.servlet.http.HttpServletResponse
+	 */
+	public AbortWithWebErrorCodeException(int errorCode, String message)
+	{
+		RequestCycle.get().setRequestTarget(new WebErrorCodeResponseTarget(errorCode, message));
+	}
+}
