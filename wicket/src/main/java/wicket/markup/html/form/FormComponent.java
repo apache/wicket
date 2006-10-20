7942cb2d4e12a937496f6d5d77125562f6b25803
Eelco Hillenius <ehillenius@apache.org>
2006-10-20 12:09:18 +0000

    made tweaking the tag for disabled components possible
    git-svn-id: https://svn.apache.org/repos/asf/incubator/wicket/trunk@466084 13f79535-47bb-0310-9956-ffa450edef68

--- wicket/src/main/java/wicket/markup/html/form/FormComponent.java
+++ wicket/src/main/java/wicket/markup/html/form/FormComponent.java
@@ -851,6 +851,20 @@ public abstract class FormComponent<T> extends WebMarkupContainer<T>
 	}
 
 	/**
+	 * Called by {@link #onComponentTag(ComponentTag)} when the component is
+	 * disabled. By default, this method will add a disabled="disabled"
+	 * attribute to the tag. Components may override this method to tweak the
+	 * tag as they think is fit.
+	 * 
+	 * @param tag
+	 *            the tag that is being rendered
+	 */
+	protected void onDisabled(final ComponentTag tag)
+	{
+		onDisabled(tag);
+	}
+
+	/**
 	 * Handle invalidation
 	 */
 	protected void onInvalid()
