fdf2a785dced0a6145ed4870e62ec634b94e1949
Eelco Hillenius <ehillenius@apache.org>
2006-10-23 22:30:59 +0000

    only set an explicit name when no other name was set. Solves some problems with frames
    git-svn-id: https://svn.apache.org/repos/asf/incubator/wicket/trunk@467154 13f79535-47bb-0310-9956-ffa450edef68

--- wicket/src/main/java/wicket/markup/html/WebPage.java
+++ wicket/src/main/java/wicket/markup/html/WebPage.java
@@ -434,9 +434,9 @@ public class WebPage<T> extends Page<T> implements INewBrowserWindowListener
 			{
 				// this is the first access to the pagemap, set window.name
 				JavascriptUtils.writeOpenTag(response);
-				response.write("window.name=\"");
+				response.write("if (window.name=='') { window.name=\"");
 				response.write(name);
-				response.write("\";");
+				response.write("\"; }");
 				JavascriptUtils.writeCloseTag(response);
 			}
 			else
