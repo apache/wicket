6b02eaeeefcaf9b1aa510425cf2c97884c009567
Eelco Hillenius <ehillenius@apache.org>
2006-10-30 17:39:38 +0000

    location.href -> window.location.href
    git-svn-id: https://svn.apache.org/repos/asf/incubator/wicket/branches/wicket-1.x@469206 13f79535-47bb-0310-9956-ffa450edef68

--- wicket/src/main/java/wicket/markup/html/form/RadioChoice.java
+++ wicket/src/main/java/wicket/markup/html/form/RadioChoice.java
@@ -449,7 +449,7 @@ public class RadioChoice extends AbstractSingleSelectChoice implements IOnChange
 					{
 						// NOTE: do not encode the url as that would give
 						// invalid JavaScript
-						buffer.append(" onclick=\"location.href='").append(url).append("&" + getInputName())
+						buffer.append(" onclick=\"window.location.href='").append(url).append("&" + getInputName())
 								.append("=").append(id).append("';\"");
 					}
 				}
