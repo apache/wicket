9637b657b89d6f2b198efceb4a0ae31827425fb6
Frank Bille Jensen <frankbille@apache.org>
2006-11-12 11:02:16 +0000

    WICKET-52 (Re)placed the correct ASL header in all .java files.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/wicket/trunk@473919 13f79535-47bb-0310-9956-ffa450edef68

--- wicket-extensions/src/main/java/wicket/extensions/markup/html/tree/Tree.java
+++ wicket-extensions/src/main/java/wicket/extensions/markup/html/tree/Tree.java
@@ -1,19 +1,18 @@
 /*
- * $Id: Tree.java 6483 2006-07-17 21:54:09Z jdonnerstag $ $Revision: 6483 $
- * $Date: 2006-07-17 23:54:09 +0200 (po, 17 VII 2006) $
- * 
- * ==============================================================================
- * Licensed under the Apache License, Version 2.0 (the "License"); you may not
- * use this file except in compliance with the License. You may obtain a copy of
- * the License at
- * 
- * http://www.apache.org/licenses/LICENSE-2.0
- * 
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
- * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
- * License for the specific language governing permissions and limitations under
- * the License.
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 package wicket.extensions.markup.html.tree;
 
--- wicket-extensions/src/test/java/wicket/extensions/markup/html/form/validation/RfcCompliantEmailValidatorTest.java
+++ wicket-extensions/src/test/java/wicket/extensions/markup/html/form/validation/RfcCompliantEmailValidatorTest.java
@@ -1,20 +1,18 @@
 /*
- * $Id: org.eclipse.jdt.ui.prefs 5004 2006-03-17 20:47:08 -0800 (Fri, 17 Mar
- * 2006) eelco12 $ $Revision: 5004 $ $Date: 2006-03-17 20:47:08 -0800 (Fri, 17
- * Mar 2006) $
- * 
- * ==============================================================================
- * Licensed under the Apache License, Version 2.0 (the "License"); you may not
- * use this file except in compliance with the License. You may obtain a copy of
- * the License at
- * 
- * http://www.apache.org/licenses/LICENSE-2.0
- * 
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
- * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
- * License for the specific language governing permissions and limitations under
- * the License.
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 package wicket.extensions.markup.html.form.validation;
 
@@ -42,7 +40,7 @@ public class RfcCompliantEmailValidatorTest extends TestCase
 		String[] validEmails = new String[] { "bill.gates@gmail.com",
 				"firstname.middlename@lastname.dk", "buy@something.nu", "user@post.inet.tele.dk",
 				"read@my.info", "my @email.com", "my@ email.com", "\"John Doe\"@email.com",
-				"no@domain", "german@mädchen.de", "another.german@ëmäïl.com" };
+				"no@domain", "german@mï¿½dchen.de", "another.german@ï¿½mï¿½ï¿½l.com" };
 		for (int i = 0; i < validEmails.length; i++)
 		{
 			String emailAddress = validEmails[i];
