430c12ae705b698caecd8f9cf3b49b4739c85cb6
Eelco Hillenius <ehillenius@apache.org>
2006-11-19 06:19:51 +0000

    WICKET-74
    git-svn-id: https://svn.apache.org/repos/asf/incubator/wicket/branches/wicket-1.x@476725 13f79535-47bb-0310-9956-ffa450edef68

--- wicket-auth-roles/src/main/java/wicket/authentication/AuthenticatedWebSession.java
+++ wicket-auth-roles/src/main/java/wicket/authentication/AuthenticatedWebSession.java
@@ -90,4 +90,12 @@ public abstract class AuthenticatedWebSession extends WebSession
 	{
 		return signedIn = authenticate(username, password);
 	}
+
+	/**
+	 * Sign the user out.
+	 */
+	public void signOut()
+	{
+		signedIn = false;
+	}
 }
