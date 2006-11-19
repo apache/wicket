a69e87406d47e49f31054313a54cd019f7bff436
Frank Bille Jensen <frankbille@apache.org>
2006-11-19 23:09:18 +0000

    WICKET-52 (Re)placed the correct ASL header in all .java files which were our own code. Thirdparty code is listed in ApacheLicenseHeaderTest and should be dealt with (if needed).
    git-svn-id: https://svn.apache.org/repos/asf/incubator/wicket/branches/wicket-1.x@476970 13f79535-47bb-0310-9956-ffa450edef68

--- wicket/src/test/java/wicket/markup/html/panel/InlinePanel_4.java
+++ wicket/src/test/java/wicket/markup/html/panel/InlinePanel_4.java
@@ -1,14 +1,12 @@
 /*
- * $Id: SimplePanel_1.java 3618 2006-01-04 09:28:14Z ivaynberg $
- * $Revision$
- * $Date: 2006-01-04 10:28:14 +0100 (Mi, 04 Jan 2006) $
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
  *
- * ====================================================================
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *  http://www.apache.org/licenses/LICENSE-2.0
+ *      http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
