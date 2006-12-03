3c7587594eb7f8e18af6e3ddd5d0d69be04f00d6
Johan Compagner <jcompagner@apache.org>
2006-12-03 00:41:17 +0000

    get the last modified date from the jar file if it is a JarUrlConnections. This to prevent file open issues.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/wicket/trunk@481677 13f79535-47bb-0310-9956-ffa450edef68

--- wicket/src/main/java/wicket/util/resource/UrlResourceStream.java
+++ wicket/src/main/java/wicket/util/resource/UrlResourceStream.java
@@ -20,6 +20,7 @@ import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.HttpURLConnection;
+import java.net.JarURLConnection;
 import java.net.URI;
 import java.net.URL;
 import java.net.URLConnection;
