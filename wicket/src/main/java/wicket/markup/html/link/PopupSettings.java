e5a7a3173d570b78c07ef52de5f5cbc94151019c
Eelco Hillenius <ehillenius@apache.org>
2006-12-11 07:20:05 +0000

    WICKET-159
    git-svn-id: https://svn.apache.org/repos/asf/incubator/wicket/trunk@485537 13f79535-47bb-0310-9956-ffa450edef68

--- wicket/src/main/java/wicket/markup/html/link/PopupSettings.java
+++ wicket/src/main/java/wicket/markup/html/link/PopupSettings.java
@@ -183,8 +183,8 @@ public class PopupSettings implements Serializable
 			windowTitle = windowTitle.replace(':', '_');
 		}
 
-		StringBuffer script = new StringBuffer("window.open(" + target + ", '").append(windowTitle)
-				.append("', '");
+		StringBuffer script = new StringBuffer("var w = window.open(" + target + ", '").append(
+				windowTitle).append("', '");
 
 		script.append("scrollbars=").append(flagToString(SCROLLBARS));
 		script.append(",location=").append(flagToString(LOCATION_BAR));
@@ -213,7 +213,7 @@ public class PopupSettings implements Serializable
 			script.append(",top=").append(top);
 		}
 
-		script.append("'); ").append(" return false;");
+		script.append("'); if(w.blur) w.focus();").append(" return false;");
 
 		return script.toString();
 	}
