d642c2f37f8d248fa0fd4153f85c905281d8c2f2
Frank Bille Jensen <frankbille@apache.org>
2006-12-21 18:49:19 +0000

    WICKET-52, WICKET-53: The unit test for detecting missing headers ignored any directory named "target". Therefore wicket.request.target wasn't checked.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/wicket/branches/wicket-1.x@489436 13f79535-47bb-0310-9956-ffa450edef68

--- wicket/src/main/java/wicket/request/target/IEventProcessor.java
+++ wicket/src/main/java/wicket/request/target/IEventProcessor.java
@@ -1,19 +1,18 @@
 /*
- * $Id: IListenerInterfaceRequestTarget.java,v 1.2 2005/11/30 21:43:01 joco01
- * Exp $ $Revision$ $Date$
- * 
- * ==============================================================================
- * Licensed under the Apache License, Version 2.0 (the "License"); you may not
- * use this file except in compliance with the License. You may obtain a copy of
- * the License at
- * 
- * http://www.apache.org/licenses/LICENSE-2.0
- * 
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
- * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
- * License for the specific language governing permissions and limitations under
- * the License.
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 package wicket.request.target;
 
