8439a3066cc45ef4ec9eca6af38c20a34698cd31
Eelco Hillenius <ehillenius@apache.org>
2007-01-05 05:20:02 +0000

    WICKET-81
    git-svn-id: https://svn.apache.org/repos/asf/incubator/wicket/branches/wicket-1.x@492908 13f79535-47bb-0310-9956-ffa450edef68

--- wicket/src/main/java/wicket/Component.java
+++ wicket/src/main/java/wicket/Component.java
@@ -43,8 +43,6 @@ import wicket.model.CompoundPropertyModel;
 import wicket.model.ICompoundModel;
 import wicket.model.IModel;
 import wicket.model.IModelComparator;
-import wicket.settings.IApplicationSettings;
-import wicket.settings.Settings;
 import wicket.util.convert.IConverter;
 import wicket.util.lang.Classes;
 import wicket.util.lang.Objects;
@@ -747,42 +745,6 @@ public abstract class Component implements Serializable
 	public final Application getApplication()
 	{
 		return Application.get();
-	}
-
-	/**
-	 * 
-	 * Deprecated. Use getApplication().getXXXSettings() instead
-	 * 
-	 * Gets the application pages from the application that this component
-	 * belongs to.
-	 * 
-	 * @return The application pages
-	 * @see IApplicationSettings
-	 * 
-	 * @deprecated
-	 */
-	// TODO Post 1.2: Remove this method
-	public final IApplicationSettings getApplicationPages()
-	{
-		return getApplication().getSettings();
-	}
-
-	/**
-	 * This method has been deprecated in favor of
-	 * Application.get().getXXXSettings()
-	 * 
-	 * Gets the application settings from the application that this component
-	 * belongs to.
-	 * 
-	 * @return The application settings from the application that this component
-	 *         belongs to
-	 * @see Settings
-	 * @deprecated will be removed after 1.2
-	 */
-	// TODO Post 1.2: Remove this method
-	public final Settings getApplicationSettings()
-	{
-		return getApplication().getSettings();
 	}
 
 	/**
