4e48cc1881beb2e47b276214b9571aef7b58a13a
Eelco Hillenius <ehillenius@apache.org>
2007-01-07 20:49:48 +0000

    move onAttach code to constructor
    git-svn-id: https://svn.apache.org/repos/asf/incubator/wicket/trunk@493832 13f79535-47bb-0310-9956-ffa450edef68

--- wicket-extensions/src/main/java/wicket/extensions/wizard/NextButton.java
+++ wicket-extensions/src/main/java/wicket/extensions/wizard/NextButton.java
@@ -1,18 +1,18 @@
 /*
  * Licensed to the Apache Software Foundation (ASF) under one or more
- * contributor license agreements.  See the NOTICE file distributed with
- * this work for additional information regarding copyright ownership.
- * The ASF licenses this file to You under the Apache License, Version 2.0
- * (the "License"); you may not use this file except in compliance with
- * the License.  You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
+ * contributor license agreements. See the NOTICE file distributed with this
+ * work for additional information regarding copyright ownership. The ASF
+ * licenses this file to You under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
  */
 package wicket.extensions.wizard;
 
@@ -42,6 +42,7 @@ public final class NextButton extends WizardButton
 	public NextButton(MarkupContainer parent, final String id, IWizard wizard)
 	{
 		super(parent, id, wizard, "wicket.extensions.wizard.next");
+		getForm().setDefaultButton(this);
 	}
 
 	/**
@@ -77,17 +78,5 @@ public final class NextButton extends WizardButton
 			error(getLocalizer().getString(
 					"wicket.extensions.wizard.NextButton.step.did.not.complete", this));
 		}
-	}
-
-	/**
-	 * @see wicket.Component#onAttach()
-	 */
-	@Override
-	protected final void onAttach()
-	{
-		super.onAttach();
-		// TODO after the constructor change we can do this in the constructor
-		// of either this class or the button form
-		getForm().setDefaultButton(this);
 	}
 }
