1696930d12d99d3b1df5b32890c17358677f8d26
Igor Vaynberg <ivaynberg@apache.org>
2007-01-31 21:08:08 +0000

    WICKET-28
    git-svn-id: https://svn.apache.org/repos/asf/incubator/wicket/branches/wicket-1.x@501997 13f79535-47bb-0310-9956-ffa450edef68

--- wicket-spring-annot/src/main/java/wicket/spring/injection/annot/AnnotProxyFieldValueFactory.java
+++ wicket-spring-annot/src/main/java/wicket/spring/injection/annot/AnnotProxyFieldValueFactory.java
@@ -25,11 +25,12 @@ import wicket.spring.ISpringContextLocator;
 import wicket.spring.SpringBeanLocator;
 
 /**
- * {@link IFieldValueFactory} that uses {@link LazyInitProxyFactory} to create proxies for Spring
- * dependencies based on the {@link SpringBean} annotation applied to a field.
- * This class is usually used by the {@link AnnotSpringInjector} to inject
- * objects with lazy init proxies. However, this class can be used on its own to
- * create proxies for any field decorated with a {@link SpringBean} annotation.
+ * {@link IFieldValueFactory} that uses {@link LazyInitProxyFactory} to create
+ * proxies for Spring dependencies based on the {@link SpringBean} annotation
+ * applied to a field. This class is usually used by the
+ * {@link AnnotSpringInjector} to inject objects with lazy init proxies.
+ * However, this class can be used on its own to create proxies for any field
+ * decorated with a {@link SpringBean} annotation.
  * <p>
  * Example:
  * 
@@ -39,9 +40,9 @@ import wicket.spring.SpringBeanLocator;
  * IDependency dependency = (IDependency) factory.getFieldValue(field, obj);
  * </pre>
  * 
- * In the example above the <code>dependency</code> object returned is a lazy init proxy
- * that will look up the actual IDependency bean from spring context upon first
- * access to one of the methods.
+ * In the example above the <code>dependency</code> object returned is a lazy
+ * init proxy that will look up the actual IDependency bean from spring context
+ * upon first access to one of the methods.
  * <p>
  * This class will also cache any produced proxies so that the same proxy is
  * always returned for the same spring dependency. This helps cut down on
@@ -55,21 +56,22 @@ import wicket.spring.SpringBeanLocator;
  * @author Igor Vaynberg (ivaynberg)
  * 
  */
-public class AnnotProxyFieldValueFactory implements IFieldValueFactory
-{
+public class AnnotProxyFieldValueFactory implements IFieldValueFactory {
 	private ISpringContextLocator contextLocator;
 
 	private final ConcurrentHashMap<SpringBeanLocator, Object> cache = new ConcurrentHashMap<SpringBeanLocator, Object>();
+
+	/** fail fast tag, see {@link #setFailFast(boolean)} */
+	private boolean failFast = true;
 
 	/**
 	 * @param contextLocator
 	 *            spring context locator
 	 */
-	public AnnotProxyFieldValueFactory(ISpringContextLocator contextLocator)
-	{
-		if (contextLocator == null)
-		{
-			throw new IllegalArgumentException("[contextLocator] argument cannot be null");
+	public AnnotProxyFieldValueFactory(ISpringContextLocator contextLocator) {
+		if (contextLocator == null) {
+			throw new IllegalArgumentException(
+					"[contextLocator] argument cannot be null");
 		}
 		this.contextLocator = contextLocator;
 	}
@@ -78,33 +80,31 @@ public class AnnotProxyFieldValueFactory implements IFieldValueFactory
 	 * @see wicket.injection.IFieldValueFactory#getFieldValue(java.lang.reflect.Field,
 	 *      java.lang.Object)
 	 */
-	public Object getFieldValue(Field field, Object fieldOwner)
-	{
+	public Object getFieldValue(Field field, Object fieldOwner) {
 
-		if (field.isAnnotationPresent(SpringBean.class))
-		{
+		if (field.isAnnotationPresent(SpringBean.class)) {
 			SpringBean annot = field.getAnnotation(SpringBean.class);
-			SpringBeanLocator locator = new SpringBeanLocator(annot.name(), field
-					.getType(), contextLocator);
+			SpringBeanLocator locator = new SpringBeanLocator(annot.name(),
+					field.getType(), contextLocator);
 
-			if (cache.containsKey(locator))
-			{
+			if (cache.containsKey(locator)) {
 				return cache.get(locator);
 			}
 
 			// fail early - see if the locator can locate the spring bean
-			testLocator(locator, fieldOwner, field);
-			
-			Object proxy = LazyInitProxyFactory.createProxy(field.getType(), locator);
+			if (failFast) {
+				testLocator(locator, fieldOwner, field);
+			}
+
+			Object proxy = LazyInitProxyFactory.createProxy(field.getType(),
+					locator);
 			cache.put(locator, proxy);
 			return proxy;
-		}
-		else
-		{
+		} else {
 			return null;
 		}
 	}
-	
+
 	/**
 	 * Tests if the locator can retrieve the bean it is responsible for.
 	 * 
@@ -112,34 +112,36 @@ public class AnnotProxyFieldValueFactory implements IFieldValueFactory
 	 * @param fieldOwner
 	 * @param field
 	 */
-	private void testLocator(SpringBeanLocator locator, Object fieldOwner, Field field)
-	{
-		try
-		{
+	private void testLocator(SpringBeanLocator locator, Object fieldOwner,
+			Field field) {
+		try {
 			locator.locateProxyTarget();
-		}
-		catch (Throwable e)
-		{
+		} catch (Throwable e) {
 			String errorMessage = "Could not locate spring bean of class [["
 					+ locator.getBeanType().getName() + "]] ";
-			if (locator.getBeanName() != null && locator.getBeanName().length() > 0)
-			{
+			if (locator.getBeanName() != null
+					&& locator.getBeanName().length() > 0) {
 				errorMessage += "and id [[" + locator.getBeanName() + "]] ";
 			}
 			errorMessage += "needed in class [["
-					+ fieldOwner.getClass().getName() + "]] field [[" + field.getName()
-					+ "]]";
+					+ fieldOwner.getClass().getName() + "]] field [["
+					+ field.getName() + "]]";
 			throw new RuntimeException(errorMessage, e);
 		}
 	}
 
-
 	/**
 	 * @see wicket.injection.IFieldValueFactory#supportsField(java.lang.reflect.Field)
 	 */
-	public boolean supportsField(Field field)
-	{
+	public boolean supportsField(Field field) {
 		return field.isAnnotationPresent(SpringBean.class);
 	}
 
+	/**
+	 * @param failFast
+	 *            true if the locator fails if a bean can't be located
+	 */
+	public void setFailFast(boolean failFast) {
+		this.failFast = failFast;
+	}
 }
