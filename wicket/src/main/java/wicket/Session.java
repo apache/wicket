da78b07782c6133f7461fd630783bd9f5fb16d16
Alastair Maw <almaw@apache.org>
2007-02-02 20:00:35 +0000

    Always update the session if we bind() it - this makes stateless form submits to work if you session.bind() during processing.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/wicket/trunk@502710 13f79535-47bb-0310-9956-ffa450edef68

--- wicket/src/main/java/wicket/Session.java
+++ wicket/src/main/java/wicket/Session.java
@@ -332,6 +332,7 @@ public abstract class Session implements Serializable, IConverterLocator
 			// bind it
 			store.bind(request, this);
 		}
+		RequestCycle.get().setUpdateSession(true);
 	}
 
 	/**
