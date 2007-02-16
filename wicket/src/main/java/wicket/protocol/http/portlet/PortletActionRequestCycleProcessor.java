02ff1c1ed60f95fe4faf63f9fe0ed659b680d4df
Eelco Hillenius <ehillenius@apache.org>
2007-02-16 20:33:32 +0000

    reordered and got rid of unused imports
    git-svn-id: https://svn.apache.org/repos/asf/incubator/wicket/branches/wicket-1.x@508564 13f79535-47bb-0310-9956-ffa450edef68

--- wicket/src/main/java/wicket/protocol/http/portlet/PortletActionRequestCycleProcessor.java
+++ wicket/src/main/java/wicket/protocol/http/portlet/PortletActionRequestCycleProcessor.java
@@ -16,40 +16,7 @@
  */
 package wicket.protocol.http.portlet;
 
-import javax.servlet.http.HttpServletResponse;
-
-import wicket.AccessStackPageMap;
-import wicket.Application;
-import wicket.Component;
-import wicket.IPageFactory;
-import wicket.IPageMap;
-import wicket.IRedirectListener;
-import wicket.IRequestTarget;
-import wicket.Page;
-import wicket.PageParameters;
 import wicket.RequestCycle;
-import wicket.RequestListenerInterface;
-import wicket.RestartResponseAtInterceptPageException;
-import wicket.Session;
-import wicket.WicketRuntimeException;
-import wicket.AccessStackPageMap.Access;
-import wicket.authorization.AuthorizationException;
-import wicket.authorization.UnauthorizedActionException;
-import wicket.markup.MarkupException;
-import wicket.protocol.http.portlet.pages.ExceptionErrorPortletPage;
-import wicket.protocol.http.request.WebErrorCodeResponseTarget;
-import wicket.request.AbstractRequestCycleProcessor;
-import wicket.request.IRequestCodingStrategy;
-import wicket.request.RequestParameters;
-import wicket.request.target.basic.EmptyRequestTarget;
-import wicket.request.target.component.BookmarkablePageRequestTarget;
-import wicket.request.target.component.ExpiredPageClassRequestTarget;
-import wicket.request.target.component.IPageRequestTarget;
-import wicket.request.target.component.PageRequestTarget;
-import wicket.request.target.component.listener.RedirectPageRequestTarget;
-import wicket.request.target.resource.SharedResourceRequestTarget;
-import wicket.settings.IExceptionSettings;
-import wicket.util.string.Strings;
 
 /**
  * A RequestCycleProcessor for portlet action requests. The page is not rendered
