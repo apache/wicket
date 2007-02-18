a0a69065ce5c73fed8fd82ccacff15263765eb49
Eelco Hillenius <ehillenius@apache.org>
2007-02-18 23:06:24 +0000

    aligned with 1.3 version
    git-svn-id: https://svn.apache.org/repos/asf/incubator/wicket/trunk@509025 13f79535-47bb-0310-9956-ffa450edef68

--- wicket-threadtest/src/main/java/wicket/threadtest/App1Test1.java
+++ wicket-threadtest/src/main/java/wicket/threadtest/App1Test1.java
@@ -23,15 +23,17 @@ public class App1Test1 {
 	public static void main(String[] args) throws Exception {
 
 		List<String> gets = Arrays.asList(new String[] {
-				"/app1?wicket:bookmarkablePage=one:wicket.threadtest.apps.app1.Home",
-				"/app1?wicket:bookmarkablePage=two:wicket.threadtest.apps.app1.Home",
-				"/app1?wicket:interface=two:${iteration}:link::ILinkListener",
-				"/app1?wicket:interface=one:${iteration}:link::ILinkListener",
-				"/app1?wicket:interface=two:${iteration}:link::ILinkListener" });
+				"/app1/?wicket:bookmarkablePage=:wicket.threadtest.apps.app1.Home",
+				"/app1/?wicket:interface=:${iteration}:link::ILinkListener:",
+				"/app1/?wicket:interface=:${iteration}:link:1:ILinkListener:",
+				"/app1/?wicket:interface=:${iteration}:link:2:ILinkListener:" });
 
-		SimpleGetCommand getCmd = new SimpleGetCommand(gets, 1);
-		//getCmd.setPrintResponse(true);
-		Tester tester = new Tester(getCmd, 50, true);
+		// you can turn this on if you e.g. want to attach to a profiler
+		// Thread.sleep(5000);
+
+		SimpleGetCommand getCmd = new SimpleGetCommand(gets, 100);
+		// getCmd.setPrintResponse(true);
+		Tester tester = new Tester(getCmd, 10, true);
 		tester.run();
 	}
 }
