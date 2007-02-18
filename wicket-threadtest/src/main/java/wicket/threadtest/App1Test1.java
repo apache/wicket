8c18eab5d818b47775b0dc8451e2a73a10647c0d
Eelco Hillenius <ehillenius@apache.org>
2007-02-18 00:57:34 +0000

    fixed test: as there is a dataview on the page that refreshes every time, each hit will increase the version number. This fix increases the version for the link gets.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/wicket/branches/wicket-1.x@508844 13f79535-47bb-0310-9956-ffa450edef68

--- wicket-threadtest/src/main/java/wicket/threadtest/App1Test1.java
+++ wicket-threadtest/src/main/java/wicket/threadtest/App1Test1.java
@@ -23,11 +23,10 @@ public class App1Test1 {
 	public static void main(String[] args) throws Exception {
 
 		List<String> gets = Arrays.asList(new String[] {
-				"/app1/?wicket:bookmarkablePage=one:wicket.threadtest.apps.app1.Home",
-				"/app1/?wicket:bookmarkablePage=two:wicket.threadtest.apps.app1.Home",
-				"/app1/?wicket:interface=two:${iteration}:link::ILinkListener:",
-				"/app1/?wicket:interface=one:${iteration}:link::ILinkListener:",
-				"/app1/?wicket:interface=two:${iteration}:link::ILinkListener:" });
+				"/app1/?wicket:bookmarkablePage=:wicket.threadtest.apps.app1.Home",
+				"/app1/?wicket:interface=:${iteration}:link::ILinkListener:",
+				"/app1/?wicket:interface=:${iteration}:link:1:ILinkListener:",
+				"/app1/?wicket:interface=:${iteration}:link:2:ILinkListener:" });
 
 		// you can turn this on if you e.g. want to attach to a profiler
 		// Thread.sleep(5000);
