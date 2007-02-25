96acd3c3cdb1fc763449a24bec3fef0a68b29cb0
Eelco Hillenius <ehillenius@apache.org>
2007-02-25 23:43:24 +0000

    implemented strategy and set it on the Objects class when loaded.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/wicket/branches/wicket-1.x@511662 13f79535-47bb-0310-9956-ffa450edef68

--- wicket-objectssizeof-agent/src/main/java/wicket/util/instrument/ObjectSizeOfAgent.java
+++ wicket-objectssizeof-agent/src/main/java/wicket/util/instrument/ObjectSizeOfAgent.java
@@ -17,125 +17,31 @@
 package wicket.util.instrument;
 
 import java.lang.instrument.Instrumentation;
-import java.lang.reflect.Array;
-import java.lang.reflect.Field;
-import java.lang.reflect.Modifier;
-import java.util.IdentityHashMap;
-import java.util.Map;
-import java.util.Stack;
+
+import wicket.util.lang.Objects;
 
 /** Instrumentation agent used */
 public class ObjectSizeOfAgent {
 
-	private static Instrumentation inst;
-
 	/** initializes agent when it is attached to an already running JVM. */
-	public static void agentmain(String agentArgs, Instrumentation inst) {
-		ObjectSizeOfAgent.inst = inst;
-	}
+	public static void agentmain(String agentArgs,
+			Instrumentation instrumentation) {
 
-	/**
-	 * Calculates full size of object iterating over its hierarchy graph.
-	 * 
-	 * @param obj
-	 *            object to calculate size of
-	 * @return object size
-	 */
-	public static long fullSizeOf(Object obj) {
-		Map<Object, Object> visited = new IdentityHashMap<Object, Object>();
-		Stack<Object> stack = new Stack<Object>();
-
-		long result = internalSizeOf(obj, stack, visited);
-		while (!stack.isEmpty()) {
-			result += internalSizeOf(stack.pop(), stack, visited);
-		}
-		visited.clear();
-		return result;
+		System.err.println("******* ENTER ***********");
+		InstrumentationObjectSizeOfStrategy strategy = new InstrumentationObjectSizeOfStrategy(
+				instrumentation);
+		Objects.setObjectSizeOfStrategy(strategy);
+		System.err.println("******* EXIT ***********");
 	}
 
 	/**
 	 * initializes agent before the main function of the application is
 	 * executed.
 	 */
-	public static void premain(String agentArgs, Instrumentation inst) {
-		ObjectSizeOfAgent.inst = inst;
-		System.out.println("SizeOfAgent initialized");
-	}
+	public static void premain(String agentArgs, Instrumentation instrumentation) {
 
-	/**
-	 * Returns object size without member sub-objects.
-	 * 
-	 * @param o
-	 *            object to get size of
-	 * @return object size
-	 */
-	public static long sizeOf(Object o) {
-		if (inst == null) {
-			throw new IllegalStateException(
-					"Can not access instrumentation environment.\n"
-							+ "Please check if jar file containing SizeOfAgent class is \n"
-							+ "specified in the java's \"-javaagent\" command line argument.");
-		}
-		return inst.getObjectSize(o);
-	}
-
-	private static long internalSizeOf(Object obj, Stack<Object> stack,
-			Map<Object, Object> visited) {
-		if (skipObject(obj, visited)) {
-			return 0;
-		}
-		visited.put(obj, null);
-
-		long result = 0;
-		// get size of object + primitive variables + member pointers
-		result += ObjectSizeOfAgent.sizeOf(obj);
-
-		// process all array elements
-		Class clazz = obj.getClass();
-		if (clazz.isArray()) {
-			if (clazz.getName().length() != 2) {// skip primitive type array
-				int length = Array.getLength(obj);
-				for (int i = 0; i < length; i++) {
-					stack.add(Array.get(obj, i));
-				}
-			}
-			return result;
-		}
-
-		// process all fields of the object
-		while (clazz != null) {
-			Field[] fields = clazz.getDeclaredFields();
-			for (int i = 0; i < fields.length; i++) {
-				if (!Modifier.isStatic(fields[i].getModifiers())) {
-					if (fields[i].getType().isPrimitive()) {
-						continue; // skip primitive fields
-					} else {
-						fields[i].setAccessible(true);
-						try {
-							// objects to be estimated are put to stack
-							Object objectToAdd = fields[i].get(obj);
-							if (objectToAdd != null) {
-								stack.add(objectToAdd);
-							}
-						} catch (IllegalAccessException ex) {
-							assert false;
-						}
-					}
-				}
-			}
-			clazz = clazz.getSuperclass();
-		}
-		return result;
-	}
-
-	private static boolean skipObject(Object obj, Map<Object, Object> visited) {
-		if (obj instanceof String) {
-			// skip interned string
-			if (obj == ((String) obj).intern()) {
-				return true;
-			}
-		}
-		return (obj == null) // skip visited object
-				|| visited.containsKey(obj);
+		InstrumentationObjectSizeOfStrategy strategy = new InstrumentationObjectSizeOfStrategy(
+				instrumentation);
+		Objects.setObjectSizeOfStrategy(strategy);
 	}
 }
