b6ffcb97ae55689ca6574d33ce34989327a10d82
Eelco Hillenius <ehillenius@apache.org>
2007-02-26 01:30:43 +0000

    + javadoc
    git-svn-id: https://svn.apache.org/repos/asf/incubator/wicket/branches/wicket-1.x@511686 13f79535-47bb-0310-9956-ffa450edef68

--- wicket-objectssizeof-agent/src/main/java/wicket/util/instrument/ObjectSizeOfAgent.java
+++ wicket-objectssizeof-agent/src/main/java/wicket/util/instrument/ObjectSizeOfAgent.java
@@ -19,11 +19,31 @@ package wicket.util.instrument;
 import java.lang.instrument.Instrumentation;
 
 import wicket.util.lang.Objects;
+import wicket.util.lang.Objects.IObjectSizeOfStrategy;
 
-/** Instrumentation agent used */
+/**
+ * Instrumentation agent for calculating object sizes using Java's
+ * instrumentation API. To use it, have the jar somewhere we you can access it
+ * (just having this class on the classpath is not enough) and startup your
+ * application with a -javaagent argument like e.g:
+ * '-javaagent:/mydir/wicket-objectsizeof-agent-1.3-incubating-SNAPSHOT.jar'.
+ * When the application starts up, this agent will register an
+ * {@link IObjectSizeOfStrategy} at
+ * {@link Objects#setObjectSizeOfStrategy(IObjectSizeOfStrategy)}. Note that
+ * this is a static registration.
+ * 
+ * @author eelcohillenius
+ */
 public class ObjectSizeOfAgent {
 
-	/** initializes agent when it is attached to an already running JVM. */
+	/**
+	 * Initializes agent when it is attached to an already running JVM.
+	 * 
+	 * @param agentArgs
+	 *            Arguments passed in to the agent
+	 * @param instrumentation
+	 *            The instrumentation class
+	 */
 	public static void agentmain(String agentArgs,
 			Instrumentation instrumentation) {
 
@@ -33,8 +53,13 @@ public class ObjectSizeOfAgent {
 	}
 
 	/**
-	 * initializes agent before the main function of the application is
+	 * Initializes agent before the main function of the application is
 	 * executed.
+	 * 
+	 * @param agentArgs
+	 *            Arguments passed in to the agent
+	 * @param instrumentation
+	 *            The instrumentation class
 	 */
 	public static void premain(String agentArgs, Instrumentation instrumentation) {
 
