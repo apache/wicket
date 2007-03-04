9f12b511fe99fa165ec54fdc59b0f3291c4381f5
Frank Bille Jensen <frankbille@apache.org>
2007-03-04 15:27:59 +0000

    Added missing license headers. Added attributions to the NOTICE. I'm not sure about DOJO and JettyHelper classes. Are they under an ASL compatible license?
    git-svn-id: https://svn.apache.org/repos/asf/incubator/wicket/branches/wicket-1.x@514405 13f79535-47bb-0310-9956-ffa450edef68

--- wicket-examples/src/main/java/wicket/examples/linkomatic/Home.java
+++ wicket-examples/src/main/java/wicket/examples/linkomatic/Home.java
@@ -1,19 +1,18 @@
 /*
- * $Id$ $Revision$ $Date:
- * 2006-03-08 21:05:26 +0100 (Mi, 08 Mrz 2006) $
- * 
- * ==============================================================================
- * Licensed under the Apache License, Version 2.0 (the "License"); you may not
- * use this file except in compliance with the License. You may obtain a copy of
- * the License at
- * 
- * http://www.apache.org/licenses/LICENSE-2.0
- * 
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
- * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
- * License for the specific language governing permissions and limitations under
- * the License.
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 package wicket.examples.linkomatic;
 
@@ -85,7 +84,7 @@ public class Home extends WicketExamplePage
 		// Link to Page2 is automaticLink, so no code
 		// Link to Page3 is an external link which takes a parameter
 		add(new BookmarkablePageLink("page3Link", Page3.class).setParameter("bookmarkparameter",
-				"3++2 & 5 € >< space + á"));
+				"3++2 & 5 ï¿½ >< space + ï¿½"));
 
 		// Link to BookDetails page
 		add(new PageLink("bookDetailsLink", new IPageLink()
