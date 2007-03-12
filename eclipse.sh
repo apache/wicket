8610f4ffb47b3ea930fa7a9a8564d8ac627a98ff
Martijn Dashorst <dashorst@apache.org>
2007-03-12 21:57:10 +0000

    Fixed Eelco's complaining about .classpath-e files
    git-svn-id: https://svn.apache.org/repos/asf/incubator/wicket/branches/wicket-1.x@517417 13f79535-47bb-0310-9956-ffa450edef68

--- eclipse.sh
+++ eclipse.sh
@@ -1,3 +1,3 @@
 #/bin/sh
 mvn eclipse:eclipse
-find jdk-1.4 -name ".classpath" | xargs sed -i -e "s/org.eclipse.jdt.launching.JRE_CONTAINER/org.eclipse.jdt.launching.JRE_CONTAINER\/org.eclipse.jdt.internal.debug.ui.launcher.StandardVMType\/J2SE-1.4/g"
+find jdk-1.4 -name ".classpath" | xargs sed -i "" -e "s/org.eclipse.jdt.launching.JRE_CONTAINER/org.eclipse.jdt.launching.JRE_CONTAINER\/org.eclipse.jdt.internal.debug.ui.launcher.StandardVMType\/J2SE-1.4/g" 
