251bb895d5d798b5ebb016d601603dc88e715b82
Eelco Hillenius <ehillenius@apache.org>
2007-04-02 17:57:33 +0000

    fix for stateless example
    git-svn-id: https://svn.apache.org/repos/asf/incubator/wicket/branches/wicket-1.x@524857 13f79535-47bb-0310-9956-ffa450edef68

--- jdk-1.5/wicket-examples/src/main/java/wicket/examples/stateless/StatelessPage.java
+++ jdk-1.5/wicket-examples/src/main/java/wicket/examples/stateless/StatelessPage.java
@@ -1,18 +1,18 @@
 /*
  * Licensed to the Apache Software Foundation (ASF) under one or more
- * contributor license agreements.  See the NOTICE file distributed with
- * this work for additional information regarding copyright ownership.
- * The ASF licenses this file to You under the Apache License, Version 2.0
- * (the "License"); you may not use this file except in compliance with
- * the License.  You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
+ * contributor license agreements. See the NOTICE file distributed with this
+ * work for additional information regarding copyright ownership. The ASF
+ * licenses this file to You under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
  */
 package wicket.examples.stateless;
 
@@ -21,6 +21,7 @@ import wicket.markup.html.basic.Label;
 import wicket.markup.html.form.StatelessForm;
 import wicket.markup.html.form.TextField;
 import wicket.markup.html.link.BookmarkablePageLink;
+import wicket.markup.html.panel.FeedbackPanel;
 import wicket.model.Model;
 
 /**
@@ -40,19 +41,20 @@ public class StatelessPage extends WebPage
 		setStatelessHint(true);
 		add(new Label("message", new SessionModel()));
 		add(new BookmarkablePageLink("indexLink", Index.class));
-		
-		StatelessForm statelessForm = new StatelessForm("statelessform"){
+
+		StatelessForm statelessForm = new StatelessForm("statelessform")
+		{
 			/**
 			 * @see wicket.markup.html.form.Form#onSubmit()
 			 */
 			protected void onSubmit()
 			{
-				info("Onsubmit of stateless page pressed, textfield updated: " + tf.getModelObject());
-				tf.setModelObject(tf.getModelObject()+ "_" +tf.getModelObject());
+				info("Submitted text: " + tf.getModelObject());
 			}
 		};
 		tf = new TextField("textfield", new Model());
 		add(statelessForm);
 		statelessForm.add(tf);
+		add(new FeedbackPanel("feedback"));
 	}
 }
