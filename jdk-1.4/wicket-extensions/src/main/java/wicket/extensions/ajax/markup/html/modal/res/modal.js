ca778ea6b4615dc49618d4e4f6204b68505dedcb
Matej Knopp <knopp@apache.org>
2007-04-10 08:45:37 +0000

    WICKET-448
    git-svn-id: https://svn.apache.org/repos/asf/incubator/wicket/branches/wicket-1.x@527060 13f79535-47bb-0310-9956-ffa450edef68

--- jdk-1.4/wicket-extensions/src/main/java/wicket/extensions/ajax/markup/html/modal/res/modal.js
+++ jdk-1.4/wicket-extensions/src/main/java/wicket/extensions/ajax/markup/html/modal/res/modal.js
@@ -49,146 +49,6 @@ if (typeof(Object.extend) == "undefined") {
 }
 
 /**
- * Flexible dragging support.
- */
-Wicket.Drag = {
-
-	/**
-	 * If no event is given as argument (IE), window.event is returned. 
-	 */
-	fixEvent: function(e) {
-		if (typeof e == 'undefined') 
-			e = window.event;
-		return e;		
-	},
-	
-	/**
-	 * Initializes the dragging on the specified element.
-	 * Element's onmousedown will be replaced by generated handler.
-	 *
-	 * @param {Element} element - element clicking on which the drag should begin 
-	 * @param {Function} onDragBegin - handler called at the begin on dragging - passed element as first parameter
-	 * @param {Function} onDragEnd - handler called at the end of dragging - passed element as first parameter
-	 * @param {Function} onDrag - handler called during dragging - passed element and mouse deltas	 
-	 */
-	init: function(element, onDragBegin, onDragEnd, onDrag) {		
-		
-		if (typeof(onDragBegin) == "undefined")
-			onDragBegin = Wicket.emptyFunction;
-		if (typeof(onDragEnd) == "undefined")
-			onDragEnd = Wicket.emptyFunction;
-		if (typeof(onDrag) == "undefined")
-			onDrag = Wicket.emptyFunction;
-		
-		// set the mousedown handler 
-		element.onmousedown = function(e) {			
-			
-			e = Wicket.Drag.fixEvent(e);
-	
-			// HACK - for safari stopPropagation doesn't work well because
-			// it also prevents scrollbars and form components getting the
-			// event. Therefore for safari the 'ignore' flag is set on event. 
-			if (typeof(e.ignore) == "undefined") {
-				
-				Wicket.Window.stopEvent(e);
-	
-				onDragBegin(element);
-			
-				element.onDrag = onDrag;
-				element.onDragEnd = onDragEnd;
-				
-				element.lastMouseX = e.clientX;
-				element.lastMouseY = e.clientY;
-				
-				element.old_onmousemove = document.onmousemove;
-				element.old_onmouseup = document.onmouseup;
-				element.old_onselectstart = document.onselectstart;			
-				
-				document.onselectstart = function() { return false; }
-				document.onmousemove = Wicket.Drag.mouseMove;
-				document.onmouseup = Wicket.Drag.mouseUp;
-							
-				Wicket.Drag.current = element;
-							
-				return false;
-			} 			
-		};		
-	},
-	
-	/**
-	 * Deinitializes the dragging support on given element. 
-	 */
-	clean: function(element) {
-		element.onmousedown = null;
-	},
-
-	/**
-	 * Called when mouse is moved. This method fires the onDrag event
-	 * with element instance, deltaX and deltaY (the distance
-	 * between this call and the previous one).
-	 
-	 * The onDrag handler can optionally return an array of two integers 
-	 * - the delta correction. This is used, for example, if there is
-	 * element being resized and the size limit has been reached (but the
-	 * mouse can still move).
-	 * 
-	 * @param {Event} e
-	 */	
-	mouseMove: function(e) {
-		e = Wicket.Drag.fixEvent(e);
-		var o = Wicket.Drag.current;
-
-		// this happens sometimes in Safari 
-		if (e.clientX < 0 || e.clientY < 0) {
-			return;
-		}
-
-		if (o != null) {		
-			var deltaX = e.clientX - o.lastMouseX;
-			var deltaY = e.clientY - o.lastMouseY;
-				
-			var res = o.onDrag(o, deltaX, deltaY);
-			
-			if (res == null)
-				res = [0, 0];
-			
-			o.lastMouseX = e.clientX + res[0];
-			o.lastMouseY = e.clientY + res[1];
-		}
-		
-		return false;
-	},
-
-	/**
-	 * Called when the mouse button is released.
-	 * Cleans all temporary variables and callback methods.
-	 * 
-	 * @param {Event} e
-	 */	
-	mouseUp: function(e) {		
-		e = Wicket.Drag.fixEvent(e);
-		var o = Wicket.Drag.current;
-		
-		o.onDragEnd(o);		
-		
-		o.onDrag = null;
-		o.onDragEnd = null;
-		o.lastMouseX = null;
-		o.lastMouseY = null;
-		
-		document.onmousemove = o.old_onmousemove;
-		document.onmouseup = o.old_onmouseup;		
-		document.onselectstart = o.old_onselectstart;
-		
-		o.old_mousemove = null;		
-		o.old_mouseup = null;
-		o.old_onselectstart = null;
-		
-		Wicket.Drag.current = null;
-	}
-};
-
-/**
  * Supporting code for getting mouse move and mouse up events from iframes.
  * The problem when dragging a div with an iframe is that when the mouse cursor
  * gets over an iframe, all mouse events are received by the iframe's document. (IE and FF)
@@ -1164,18 +1024,6 @@ Wicket.Window.prototype = {
 Wicket.Window.idCounter = 0;
 
 /**
- * Prevent event from bubbling up in the element hierarchy.
- */
-Wicket.Window.stopEvent = function(e) {
-	if (typeof(e) == "undefined")
-		e = window.event;
-		
-	e.cancelBubble = true;
-	if (e.stopPropagation)
-		e.stopPropagation();
-}
-
-/**
  * Returns the modal window markup with specified element identifiers.
  */
 Wicket.Window.getMarkup = function(idWindow, idClassElement, idCaption, idContent, idTop, idTopLeft, idTopRight, idLeft, idRight, idBottomLeft, idBottomRight, idBottom, idCaptionText, isFrame) {
@@ -1199,7 +1047,7 @@ Wicket.Window.getMarkup = function(idWindow, idClassElement, idCaption, idConten
 				"<div class=\"w_left\" id='"+idLeft+"'>"+
 					"<div class=\"w_right_1\">"+
 						"<div class=\"w_right\" id='"+idRight+"'>"+
-							"<div class=\"w_content_1\" onmousedown=\"if (Wicket.Browser.isSafari()) { event.ignore = true; }  else { Wicket.Window.stopEvent(event); } \">"+																			
+							"<div class=\"w_content_1\" onmousedown=\"if (Wicket.Browser.isSafari()) { event.ignore = true; }  else { Wicket.stopEvent(event); } \">"+																			
 								"<div class=\"w_caption\"  id=\""+idCaption+"\">"+
 									"<a class=\"w_close\" href=\"#\"></a>"+									
 									"<span id=\""+idCaptionText+"\" class=\"w_captionText\"></span>"+
@@ -1509,7 +1357,7 @@ Wicket.Window.Mask.prototype = {
 	 */
 	disableTabs: function () {		
 		this.tabbableTags = new Array("A","BUTTON","TEXTAREA","INPUT","IFRAME");
-		if (Wicket.Browser.isIE()) {
+		if (false && Wicket.Browser.isIE()) {
 			var win = Wicket.Window.current;			
 			var i = 0;			
 			this.tabIndexes = new Array();
