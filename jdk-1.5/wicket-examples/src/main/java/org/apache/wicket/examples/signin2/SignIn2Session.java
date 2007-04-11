0e191429d2d75e09f4c22ae31ddaae5569132e35
Eelco Hillenius <ehillenius@apache.org>
2007-04-11 02:10:58 +0000

    fixed compile errors
    git-svn-id: https://svn.apache.org/repos/asf/incubator/wicket/branches/wicket-1.x@527372 13f79535-47bb-0310-9956-ffa450edef68

--- jdk-1.5/wicket-examples/src/main/java/org/apache/wicket/examples/signin2/SignIn2Session.java
+++ jdk-1.5/wicket-examples/src/main/java/org/apache/wicket/examples/signin2/SignIn2Session.java
@@ -1,22 +1,23 @@
 /*
  * Licensed to the Apache Software Foundation (ASF) under one or more
- * contributor license agreements.  See the NOTICE file distributed with
- * this work for additional information regarding copyright ownership.
- * The ASF licenses this file to You under the Apache License, Version 2.0
- * (the "License"); you may not use this file except in compliance with
- * the License.  You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
+ * contributor license agreements. See the NOTICE file distributed with this
+ * work for additional information regarding copyright ownership. The ASF
+ * licenses this file to You under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
  */
 package org.apache.wicket.examples.signin2;
 
 import org.apache.wicket.Request;
+import org.apache.wicket.Response;
 import org.apache.wicket.protocol.http.WebApplication;
 import org.apache.wicket.protocol.http.WebSession;
 
@@ -37,10 +38,11 @@ public final class SignIn2Session extends WebSession
 	 *            The application
 	 * @param request
 	 *            The current request object
+	 * @param response
 	 */
-	protected SignIn2Session(final WebApplication application, Request request)
+	protected SignIn2Session(final WebApplication application, Request request, Response response)
 	{
-		super(application, request);
+		super(application, request, response);
 	}
 
 	/**
