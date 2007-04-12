f6b8bdee302d0dc17923479efcb07de969bc5fae
Martijn Dashorst <dashorst@apache.org>
2007-04-12 22:13:51 +0000

    Fixed path stuff between linux and darwin
    git-svn-id: https://svn.apache.org/repos/asf/incubator/wicket/branches/wicket-1.x@528253 13f79535-47bb-0310-9956-ffa450edef68

--- eclipse.sh
+++ eclipse.sh
@@ -1,3 +1,11 @@
 #/bin/sh
 mvn eclipse:eclipse
-find jdk-1.4 -name .classpath -exec sed -i -e 's/org.eclipse.jdt.launching.JRE_CONTAINER/org.eclipse.jdt.launching.JRE_CONTAINER\/org.eclipse.jdt.internal.debug.ui.launcher.StandardVMType\/J2SE-1.4/g' {} \;
+
+case `uname` in
+Linux)
+	find jdk-1.4 -name .classpath -exec sed -i -e "s/org.eclipse.jdt.launching.JRE_CONTAINER/org.eclipse.jdt.launching.JRE_CONTAINER\/org.eclipse.jdt.internal.debug.ui.launcher.StandardVMType\/J2SE-1.4/g" {} \;
+	;;
+Darwin)
+	find jdk-1.4 -name .classpath -exec sed -i "" -e "s/org.eclipse.jdt.launching.JRE_CONTAINER/org.eclipse.jdt.launching.JRE_CONTAINER\/org.eclipse.jdt.internal.debug.ui.launcher.StandardVMType\/J2SE-1.4/g" {} \;
+	;;
+esac
