74ddb9f57170a8cb285b27c7dad992b9a70a86ab
Jean-Baptiste Quenot <jbq@apache.org>
2007-04-16 07:30:57 +0000

    Use sed -i "" for all OS except Linux (I'm using FreeBSD, not Darwin)
    git-svn-id: https://svn.apache.org/repos/asf/incubator/wicket/branches/wicket-1.x@529151 13f79535-47bb-0310-9956-ffa450edef68

--- eclipse.sh
+++ eclipse.sh
@@ -5,7 +5,7 @@ case `uname` in
 Linux)
 	find jdk-1.4 -name .classpath -exec sed -i -e "s/org.eclipse.jdt.launching.JRE_CONTAINER/org.eclipse.jdt.launching.JRE_CONTAINER\/org.eclipse.jdt.internal.debug.ui.launcher.StandardVMType\/J2SE-1.4/g" {} \;
 	;;
-Darwin)
+*)
 	find jdk-1.4 -name .classpath -exec sed -i "" -e "s/org.eclipse.jdt.launching.JRE_CONTAINER/org.eclipse.jdt.launching.JRE_CONTAINER\/org.eclipse.jdt.internal.debug.ui.launcher.StandardVMType\/J2SE-1.4/g" {} \;
 	;;
 esac
