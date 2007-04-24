2d8131689068b3139d02db462ff2e1d2cb5a2871
Martijn Dashorst <dashorst@apache.org>
2007-04-24 15:56:37 +0000

    Fixed grammar error in modal.js
    git-svn-id: https://svn.apache.org/repos/asf/incubator/wicket/branches/wicket-1.2.x@531991 13f79535-47bb-0310-9956-ffa450edef68

--- wicket-extensions/src/main/java/wicket/extensions/ajax/markup/html/modal/res/modal.js
+++ wicket-extensions/src/main/java/wicket/extensions/ajax/markup/html/modal/res/modal.js
@@ -779,7 +779,7 @@ Wicket.Window.prototype = {
 		if (Wicket.Window.unloadConfirmation == true) {
 			// new beforeunload handler - ask user before reloading window
 			window.onbeforeunload = function() {			
-				return "Reloading this page will cause the modal window disappear.";
+				return "Reloading this page will cause the modal window to disappear.";
 			}				
 		}
 
