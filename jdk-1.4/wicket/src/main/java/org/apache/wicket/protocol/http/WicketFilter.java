e715450cc01804d2b06aec0f1bbdf3ea89114274
Matej Knopp <knopp@apache.org>
2007-05-25 18:39:07 +0000

    Removing the system.out.println line
    git-svn-id: https://svn.apache.org/repos/asf/incubator/wicket/trunk@541745 13f79535-47bb-0310-9956-ffa450edef68

--- jdk-1.4/wicket/src/main/java/org/apache/wicket/protocol/http/WicketFilter.java
+++ jdk-1.4/wicket/src/main/java/org/apache/wicket/protocol/http/WicketFilter.java
@@ -214,7 +214,6 @@ public class WicketFilter implements Filter
 					// Try to see if there is a redirect stored
 					ISessionStore sessionStore = webApplication.getSessionStore();
 					String sessionId = sessionStore.getSessionId(request, false);
-					System.out.println("THOOF-WICKET: Redirect to buffer for session id " + sessionId);
 					if (sessionId != null)
 					{
 						BufferedHttpServletResponse bufferedResponse = webApplication
