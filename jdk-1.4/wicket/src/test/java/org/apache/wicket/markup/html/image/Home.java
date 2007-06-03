f804efa10c4019b3df2e7a7b78ba9b8c66af6495
Juegen Donnerstag <jdonnerstag@apache.org>
2007-06-03 16:18:27 +0000

    Added junit test for localized images
    git-svn-id: https://svn.apache.org/repos/asf/incubator/wicket/trunk@543933 13f79535-47bb-0310-9956-ffa450edef68

---
+++ jdk-1.4/wicket/src/test/java/org/apache/wicket/markup/html/image/Home.java
@@ -0,0 +1,103 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.wicket.markup.html.image;
+
+import java.util.Locale;
+
+import org.apache.wicket.PageParameters;
+import org.apache.wicket.markup.html.WebPage;
+import org.apache.wicket.markup.html.link.Link;
+
+
+/**
+ * Demonstrates localization.
+ * 
+ * @author Jonathan Locke
+ * @author Eelco Hillenius
+ */
+public final class Home extends WebPage
+{
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * Constructor
+	 * 
+	 * @param parameters
+	 *            Page parameters (ignored since this is the home page)
+	 */
+	public Home(final PageParameters parameters)
+	{
+		add(new Image("beer"));
+
+		// Add a couple of links to be able to play around with the session
+		// locale
+		add(new Link("goCanadian")
+		{
+			private static final long serialVersionUID = 1L;
+
+			public void onClick()
+			{
+				getSession().setLocale(Locale.CANADA);
+			}
+		});
+		add(new Link("goUS")
+		{
+			private static final long serialVersionUID = 1L;
+
+			public void onClick()
+			{
+				getSession().setLocale(Locale.US);
+			}
+		});
+		add(new Link("goDutch")
+		{
+			private static final long serialVersionUID = 1L;
+
+			public void onClick()
+			{
+				getSession().setLocale(new Locale("nl", "NL"));
+			}
+		});
+		add(new Link("goGerman")
+		{
+			private static final long serialVersionUID = 1L;
+
+			public void onClick()
+			{
+				getSession().setLocale(new Locale("de", "DE"));
+			}
+		});
+		add(new Link("goChinese")
+		{
+			private static final long serialVersionUID = 1L;
+
+			public void onClick()
+			{
+				getSession().setLocale(new Locale("zh", "CN"));
+			}
+		});
+		add(new Link("goDanish")
+		{
+			private static final long serialVersionUID = 1L;
+
+			public void onClick()
+			{
+				getSession().setLocale(new Locale("da", "DK"));
+			}
+		});
+	}
+}
---
+++ jdk-1.4/wicket/src/test/java/org/apache/wicket/markup/html/image/ImageTest.java
@@ -0,0 +1,67 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.wicket.markup.html.image;
+
+import org.apache.wicket.WicketTestCase;
+
+/**
+ * @author Juergen Donnerstag
+ */
+public class ImageTest extends WicketTestCase
+{
+	/**
+	 * Construct.
+	 * @param name
+	 */
+	public ImageTest(String name)
+	{
+		super(name);
+	}
+
+	/**
+	 * 
+	 * @throws Exception
+	 */
+	public void test_1() throws Exception
+	{
+		tester.startPage(Home.class);
+		
+		tester.clickLink("goCanadian");
+		tester.startPage(Home.class);
+		tester.assertContains("src=\"resources/org.apache.wicket.markup.html.image.Home/Beer_en_CA.gif\"");
+		
+		tester.clickLink("goChinese");
+		tester.startPage(Home.class);
+		tester.assertContains("src=\"resources/org.apache.wicket.markup.html.image.Home/Beer_zh_CN.gif\"");
+		
+		tester.clickLink("goDanish");
+		tester.startPage(Home.class);
+		tester.assertContains("src=\"resources/org.apache.wicket.markup.html.image.Home/Beer_da_DK.gif\"");
+		
+		tester.clickLink("goDutch");
+		tester.startPage(Home.class);
+		tester.assertContains("src=\"resources/org.apache.wicket.markup.html.image.Home/Beer_nl_NL.gif\"");
+		
+		tester.clickLink("goGerman");
+		tester.startPage(Home.class);
+		tester.assertContains("src=\"resources/org.apache.wicket.markup.html.image.Home/Beer_de_DE.gif\"");
+		
+		tester.clickLink("goUS");
+		tester.startPage(Home.class);
+		tester.assertContains("src=\"resources/org.apache.wicket.markup.html.image.Home/Beer_en_US.gif\"");
+	}
+}
