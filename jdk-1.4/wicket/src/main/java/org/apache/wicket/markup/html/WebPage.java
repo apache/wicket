2a790c3f7079250c7ab97c5f28887aa5b9addc4c
Eelco Hillenius <ehillenius@apache.org>
2007-06-04 16:29:15 +0000

    sorted members
    git-svn-id: https://svn.apache.org/repos/asf/incubator/wicket/trunk@544178 13f79535-47bb-0310-9956-ffa450edef68

--- jdk-1.4/wicket/src/main/java/org/apache/wicket/markup/html/WebPage.java
+++ jdk-1.4/wicket/src/main/java/org/apache/wicket/markup/html/WebPage.java
@@ -70,247 +70,6 @@ import org.slf4j.LoggerFactory;
  */
 public class WebPage extends Page implements INewBrowserWindowListener
 {
-	private static final long serialVersionUID = 1L;
-
-	/** log. */
-	private static final Logger _log = LoggerFactory.getLogger(WebPage.class);
-
-	/** The resource references used for new window/tab support */
-	private static ResourceReference cookiesResource = new ResourceReference(WebPage.class,
-			"cookies.js");
-
-	/**
-	 * The url compressor that will compress the urls by collapsing the
-	 * component path and listener interface
-	 */
-	private UrlCompressor compressor;
-
-	/**
-	 * Boolean flag that represents whether or not we have already added a
-	 * {@link HtmlBodyContainer} to this page or not
-	 */
-	private boolean bodyContainerAdded = false;
-
-	/**
-	 * Constructor. Having this constructor public means that your page is
-	 * 'bookmarkable' and hence can be called/ created from anywhere.
-	 */
-	protected WebPage()
-	{
-		commonInit();
-	}
-
-	/**
-	 * @see Page#Page(IModel)
-	 */
-	protected WebPage(final IModel model)
-	{
-		super(model);
-		commonInit();
-	}
-
-	/**
-	 * @see Page#Page(PageMap)
-	 */
-	protected WebPage(final IPageMap pageMap)
-	{
-		super(pageMap);
-		commonInit();
-	}
-
-	/**
-	 * @see Page#Page(PageMap, IModel)
-	 */
-	protected WebPage(final IPageMap pageMap, final IModel model)
-	{
-		super(pageMap, model);
-		commonInit();
-	}
-
-	/**
-	 * Constructor which receives wrapped query string parameters for a request.
-	 * Having this constructor public means that your page is 'bookmarkable' and
-	 * hence can be called/ created from anywhere. For bookmarkable pages (as
-	 * opposed to when you construct page instances yourself, this constructor
-	 * will be used in preference to a no-arg constructor, if both exist. Note
-	 * that nothing is done with the page parameters argument. This constructor
-	 * is provided so that tools such as IDEs will include it their list of
-	 * suggested constructors for derived classes.
-	 * 
-	 * @param parameters
-	 *            Wrapped query string parameters.
-	 */
-	protected WebPage(final PageParameters parameters)
-	{
-		this((IModel)null);
-	}
-
-	/**
-	 * Gets the markup type for a WebPage, which is "html" by default. Support
-	 * for pages in another markup language, such as VXML, would require the
-	 * creation of a different Page subclass in an appropriate package under
-	 * org.apache.wicket.markup. To support VXML (voice markup), one might
-	 * create the package org.apache.wicket.markup.vxml and a subclass of Page
-	 * called VoicePage.
-	 * 
-	 * @return Markup type for HTML
-	 */
-	public String getMarkupType()
-	{
-		return "html";
-	}
-
-	/**
-	 * @see org.apache.wicket.Page#configureResponse()
-	 */
-	protected void configureResponse()
-	{
-		super.configureResponse();
-
-		final WebResponse response = getWebRequestCycle().getWebResponse();
-		response.setHeader("Pragma", "no-cache");
-		response.setHeader("Cache-Control", "no-cache, max-age=0, must-revalidate"); // no-store
-	}
-
-	/**
-	 * @return The WebRequestCycle for this WebPage.
-	 */
-	protected final WebRequestCycle getWebRequestCycle()
-	{
-		return (WebRequestCycle)getRequestCycle();
-	}
-
-	/**
-	 * Creates and returns a bookmarkable link to this application's home page.
-	 * 
-	 * @param id
-	 *            Name of link
-	 * @return Link to home page for this application
-	 */
-	protected final BookmarkablePageLink homePageLink(final String id)
-	{
-		return new BookmarkablePageLink(id, getApplication().getHomePage());
-	}
-
-	/**
-	 * Common code executed by constructors.
-	 */
-	private void commonInit()
-	{
-		// if automatic multi window support is on, add a page checker instance
-		if (getApplication().getPageSettings().getAutomaticMultiWindowSupport())
-		{
-			add(new PageMapChecker(this));
-		}
-	}
-
-	/**
-	 * This method is called when the compressing coding and response stategies
-	 * are configured in your Application object like this:
-	 * 
-	 * <pre>
-	 * protected IRequestCycleProcessor newRequestCycleProcessor()
-	 * {
-	 * 	return new UrlCompressingWebRequestProcessor();
-	 * }
-	 * </pre>
-	 * 
-	 * @return The URLCompressor for this webpage.
-	 * 
-	 * @since 1.2
-	 * 
-	 * @see UrlCompressingWebRequestProcessor
-	 * @see UrlCompressor
-	 */
-	public final UrlCompressor getUrlCompressor()
-	{
-		if (compressor == null)
-		{
-			compressor = new UrlCompressor();
-		}
-		return compressor;
-	}
-
-	protected void onBeforeRender()
-	{
-		super.onBeforeRender();
-
-		if (!bodyContainerAdded)
-		{
-			// Add a Body container if the associated markup contains a <body>
-			// tag get markup stream gracefully
-			MarkupStream markupStream = getAssociatedMarkupStream(false);
-			if (markupStream != null)
-			{
-				// The default <body> container. It can be accessed, replaced
-				// and attribute modifiers can be attached. <body> tags without
-				// wicket:id get automatically a wicket:id="body" assigned.
-				// find the body tag
-				while (markupStream.hasMore())
-				{
-					final MarkupElement element = markupStream.next();
-					if (element instanceof ComponentTag)
-					{
-						final ComponentTag tag = (ComponentTag)element;
-						if (tag.isOpen() && TagUtils.isBodyTag(tag))
-						{
-							// Add a default container if the tag has the
-							// default name
-							if (HtmlBodyContainer.BODY_ID.equals(tag.getId()))
-							{
-								add(new HtmlBodyContainer(tag.getId()));
-							}
-							bodyContainerAdded = true;
-							break;
-						}
-					}
-				}
-			}
-		}
-
-	}
-
-
-	/**
-	 * 
-	 * @see org.apache.wicket.Component#onDetach()
-	 */
-	protected void onDetach()
-	{
-		// This code can not go into HtmlHeaderContainer as
-		// header.onEndRequest() is executed inside an iterator
-		// and you can only call container.remove() which
-		// is != iter.remove(). And the iterator is not available
-		// inside onEndRequest(). Obviously WebPage.onEndRequest()
-		// is invoked outside the iterator loop.
-		final Component header = get(HtmlHeaderSectionHandler.HEADER_ID);
-		if (header != null)
-		{
-			this.remove(header);
-		}
-		super.onDetach();
-	}
-
-	/**
-	 * @see org.apache.wicket.markup.html.INewBrowserWindowListener#onNewBrowserWindow()
-	 */
-	public void onNewBrowserWindow()
-	{
-		// if the browser reports a history of 0 then make a new webpage
-		WebPage clonedPage = this;
-		try
-		{
-			clonedPage = (WebPage)Objects.cloneObject(this);
-		}
-		catch (Exception e)
-		{
-			_log.error("Page " + clonedPage + " couldn't be cloned to move to another pagemap", e);
-		}
-		final IPageMap map = getSession().createAutoPageMap();
-		clonedPage.moveToPageMap(map);
-		setResponsePage(clonedPage);
-	}
-
 	/**
 	 * Tries to determine whether this page was opened in a new window or tab.
 	 * If it is (and this checker were able to recognize that), a new page map
@@ -409,5 +168,246 @@ public class WebPage extends Page implements INewBrowserWindowListener
 				JavascriptUtils.writeCloseTag(response);
 			}
 		}
+	}
+
+	/** log. */
+	private static final Logger _log = LoggerFactory.getLogger(WebPage.class);
+
+	/** The resource references used for new window/tab support */
+	private static ResourceReference cookiesResource = new ResourceReference(WebPage.class,
+			"cookies.js");
+
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * Boolean flag that represents whether or not we have already added a
+	 * {@link HtmlBodyContainer} to this page or not
+	 */
+	private boolean bodyContainerAdded = false;
+
+	/**
+	 * The url compressor that will compress the urls by collapsing the
+	 * component path and listener interface
+	 */
+	private UrlCompressor compressor;
+
+	/**
+	 * Constructor. Having this constructor public means that your page is
+	 * 'bookmarkable' and hence can be called/ created from anywhere.
+	 */
+	protected WebPage()
+	{
+		commonInit();
+	}
+
+	/**
+	 * @see Page#Page(IModel)
+	 */
+	protected WebPage(final IModel model)
+	{
+		super(model);
+		commonInit();
+	}
+
+	/**
+	 * @see Page#Page(PageMap)
+	 */
+	protected WebPage(final IPageMap pageMap)
+	{
+		super(pageMap);
+		commonInit();
+	}
+
+	/**
+	 * @see Page#Page(PageMap, IModel)
+	 */
+	protected WebPage(final IPageMap pageMap, final IModel model)
+	{
+		super(pageMap, model);
+		commonInit();
+	}
+
+	/**
+	 * Constructor which receives wrapped query string parameters for a request.
+	 * Having this constructor public means that your page is 'bookmarkable' and
+	 * hence can be called/ created from anywhere. For bookmarkable pages (as
+	 * opposed to when you construct page instances yourself, this constructor
+	 * will be used in preference to a no-arg constructor, if both exist. Note
+	 * that nothing is done with the page parameters argument. This constructor
+	 * is provided so that tools such as IDEs will include it their list of
+	 * suggested constructors for derived classes.
+	 * 
+	 * @param parameters
+	 *            Wrapped query string parameters.
+	 */
+	protected WebPage(final PageParameters parameters)
+	{
+		this((IModel)null);
+	}
+
+	/**
+	 * Gets the markup type for a WebPage, which is "html" by default. Support
+	 * for pages in another markup language, such as VXML, would require the
+	 * creation of a different Page subclass in an appropriate package under
+	 * org.apache.wicket.markup. To support VXML (voice markup), one might
+	 * create the package org.apache.wicket.markup.vxml and a subclass of Page
+	 * called VoicePage.
+	 * 
+	 * @return Markup type for HTML
+	 */
+	public String getMarkupType()
+	{
+		return "html";
+	}
+
+	/**
+	 * This method is called when the compressing coding and response stategies
+	 * are configured in your Application object like this:
+	 * 
+	 * <pre>
+	 * protected IRequestCycleProcessor newRequestCycleProcessor()
+	 * {
+	 * 	return new UrlCompressingWebRequestProcessor();
+	 * }
+	 * </pre>
+	 * 
+	 * @return The URLCompressor for this webpage.
+	 * 
+	 * @since 1.2
+	 * 
+	 * @see UrlCompressingWebRequestProcessor
+	 * @see UrlCompressor
+	 */
+	public final UrlCompressor getUrlCompressor()
+	{
+		if (compressor == null)
+		{
+			compressor = new UrlCompressor();
+		}
+		return compressor;
+	}
+
+	/**
+	 * @see org.apache.wicket.markup.html.INewBrowserWindowListener#onNewBrowserWindow()
+	 */
+	public void onNewBrowserWindow()
+	{
+		// if the browser reports a history of 0 then make a new webpage
+		WebPage clonedPage = this;
+		try
+		{
+			clonedPage = (WebPage)Objects.cloneObject(this);
+		}
+		catch (Exception e)
+		{
+			_log.error("Page " + clonedPage + " couldn't be cloned to move to another pagemap", e);
+		}
+		final IPageMap map = getSession().createAutoPageMap();
+		clonedPage.moveToPageMap(map);
+		setResponsePage(clonedPage);
+	}
+
+	/**
+	 * Common code executed by constructors.
+	 */
+	private void commonInit()
+	{
+		// if automatic multi window support is on, add a page checker instance
+		if (getApplication().getPageSettings().getAutomaticMultiWindowSupport())
+		{
+			add(new PageMapChecker(this));
+		}
+	}
+
+	/**
+	 * @see org.apache.wicket.Page#configureResponse()
+	 */
+	protected void configureResponse()
+	{
+		super.configureResponse();
+
+		final WebResponse response = getWebRequestCycle().getWebResponse();
+		response.setHeader("Pragma", "no-cache");
+		response.setHeader("Cache-Control", "no-cache, max-age=0, must-revalidate"); // no-store
+	}
+
+	/**
+	 * @return The WebRequestCycle for this WebPage.
+	 */
+	protected final WebRequestCycle getWebRequestCycle()
+	{
+		return (WebRequestCycle)getRequestCycle();
+	}
+
+
+	/**
+	 * Creates and returns a bookmarkable link to this application's home page.
+	 * 
+	 * @param id
+	 *            Name of link
+	 * @return Link to home page for this application
+	 */
+	protected final BookmarkablePageLink homePageLink(final String id)
+	{
+		return new BookmarkablePageLink(id, getApplication().getHomePage());
+	}
+
+	protected void onBeforeRender()
+	{
+		super.onBeforeRender();
+
+		if (!bodyContainerAdded)
+		{
+			// Add a Body container if the associated markup contains a <body>
+			// tag get markup stream gracefully
+			MarkupStream markupStream = getAssociatedMarkupStream(false);
+			if (markupStream != null)
+			{
+				// The default <body> container. It can be accessed, replaced
+				// and attribute modifiers can be attached. <body> tags without
+				// wicket:id get automatically a wicket:id="body" assigned.
+				// find the body tag
+				while (markupStream.hasMore())
+				{
+					final MarkupElement element = markupStream.next();
+					if (element instanceof ComponentTag)
+					{
+						final ComponentTag tag = (ComponentTag)element;
+						if (tag.isOpen() && TagUtils.isBodyTag(tag))
+						{
+							// Add a default container if the tag has the
+							// default name
+							if (HtmlBodyContainer.BODY_ID.equals(tag.getId()))
+							{
+								add(new HtmlBodyContainer(tag.getId()));
+							}
+							bodyContainerAdded = true;
+							break;
+						}
+					}
+				}
+			}
+		}
+
+	}
+
+	/**
+	 * 
+	 * @see org.apache.wicket.Component#onDetach()
+	 */
+	protected void onDetach()
+	{
+		// This code can not go into HtmlHeaderContainer as
+		// header.onEndRequest() is executed inside an iterator
+		// and you can only call container.remove() which
+		// is != iter.remove(). And the iterator is not available
+		// inside onEndRequest(). Obviously WebPage.onEndRequest()
+		// is invoked outside the iterator loop.
+		final Component header = get(HtmlHeaderSectionHandler.HEADER_ID);
+		if (header != null)
+		{
+			this.remove(header);
+		}
+		super.onDetach();
 	}
 }
