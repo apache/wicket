d94bf6ae021d2827e6d5da59ba6406ad69b54bfe
Alastair Maw <almaw@apache.org>
2007-06-18 19:01:54 +0000

    Add bug number.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/wicket/trunk@548445 13f79535-47bb-0310-9956-ffa450edef68

--- jdk-1.4/wicket/src/test/java/org/apache/wicket/util/lang/PropertyResolverTest.java
+++ jdk-1.4/wicket/src/test/java/org/apache/wicket/util/lang/PropertyResolverTest.java
@@ -430,6 +430,7 @@ public class PropertyResolverTest extends TestCase
 		Address address = new Address();
 		
 		// FIXME: We shouldn't need to run this first in order for the getName() stuff to work.
+		// See WICKET-668 for details.
 		PropertyResolver.setValue("number", address, new Integer(1), CONVERTER);
 		
 		Method method = PropertyResolver.getPropertySetter("number", address);
