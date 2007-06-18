0f1b6e58b2eb094539223868d9a4e234a167f1ea
Johan Compagner <jcompagner@apache.org>
2007-06-18 12:01:12 +0000

    extracted the form out of the page so that it is easier reusable (form ajax if needed or there own info page)
    git-svn-id: https://svn.apache.org/repos/asf/incubator/wicket/trunk@548335 13f79535-47bb-0310-9956-ffa450edef68

--- jdk-1.4/wicket/src/main/java/org/apache/wicket/markup/html/pages/BrowserInfoPage.java
+++ jdk-1.4/wicket/src/main/java/org/apache/wicket/markup/html/pages/BrowserInfoPage.java
@@ -19,16 +19,12 @@ package org.apache.wicket.markup.html.pages;
 import java.io.Serializable;
 
 import org.apache.wicket.AttributeModifier;
-import org.apache.wicket.IClusterable;
 import org.apache.wicket.PageParameters;
 import org.apache.wicket.RequestCycle;
 import org.apache.wicket.Response;
 import org.apache.wicket.markup.html.WebComponent;
 import org.apache.wicket.markup.html.WebMarkupContainer;
 import org.apache.wicket.markup.html.WebPage;
-import org.apache.wicket.markup.html.form.Form;
-import org.apache.wicket.markup.html.form.TextField;
-import org.apache.wicket.model.CompoundPropertyModel;
 import org.apache.wicket.model.Model;
 import org.apache.wicket.protocol.http.ClientProperties;
 import org.apache.wicket.protocol.http.WebRequestCycle;
@@ -62,428 +58,6 @@ import org.slf4j.LoggerFactory;
  */
 public class BrowserInfoPage extends WebPage
 {
-	/**
-	 * Holds properties of the client.
-	 */
-	public static class ClientPropertiesBean implements IClusterable
-	{
-		private static final long serialVersionUID = 1L;
-
-		private String navigatorAppCodeName;
-		private String navigatorAppName;
-		private String navigatorAppVersion;
-		private Boolean navigatorCookieEnabled = Boolean.FALSE;
-		private Boolean navigatorJavaEnabled = Boolean.FALSE;
-		private String navigatorLanguage;
-		private String navigatorPlatform;
-		private String navigatorUserAgent;
-		private String screenColorDepth;
-		private String screenHeight;
-		private String screenWidth;
-		private String utcOffset;
-		private String browserWidth;
-		private String browserHeight;
-
-		/**
-		 * Gets browserHeight.
-		 * 
-		 * @return browserHeight
-		 */
-		public String getBrowserHeight()
-		{
-			return browserHeight;
-		}
-
-		/**
-		 * Gets browserWidth.
-		 * 
-		 * @return browserWidth
-		 */
-		public String getBrowserWidth()
-		{
-			return browserWidth;
-		}
-
-		/**
-		 * Gets navigatorAppCodeName.
-		 * 
-		 * @return navigatorAppCodeName
-		 */
-		public String getNavigatorAppCodeName()
-		{
-			return navigatorAppCodeName;
-		}
-
-		/**
-		 * Gets navigatorAppName.
-		 * 
-		 * @return navigatorAppName
-		 */
-		public String getNavigatorAppName()
-		{
-			return navigatorAppName;
-		}
-
-		/**
-		 * Gets navigatorAppVersion.
-		 * 
-		 * @return navigatorAppVersion
-		 */
-		public String getNavigatorAppVersion()
-		{
-			return navigatorAppVersion;
-		}
-
-		/**
-		 * Gets navigatorCookieEnabled.
-		 * 
-		 * @return navigatorCookieEnabled
-		 */
-		public Boolean getNavigatorCookieEnabled()
-		{
-			return navigatorCookieEnabled;
-		}
-
-		/**
-		 * Gets navigatorJavaEnabled.
-		 * 
-		 * @return navigatorJavaEnabled
-		 */
-		public Boolean getNavigatorJavaEnabled()
-		{
-			return navigatorJavaEnabled;
-		}
-
-		/**
-		 * Gets navigatorLanguage.
-		 * 
-		 * @return navigatorLanguage
-		 */
-		public String getNavigatorLanguage()
-		{
-			return navigatorLanguage;
-		}
-
-		/**
-		 * Gets navigatorPlatform.
-		 * 
-		 * @return navigatorPlatform
-		 */
-		public String getNavigatorPlatform()
-		{
-			return navigatorPlatform;
-		}
-
-		/**
-		 * Gets navigatorUserAgent.
-		 * 
-		 * @return navigatorUserAgent
-		 */
-		public String getNavigatorUserAgent()
-		{
-			return navigatorUserAgent;
-		}
-
-		/**
-		 * Gets screenColorDepth.
-		 * 
-		 * @return screenColorDepth
-		 */
-		public String getScreenColorDepth()
-		{
-			return screenColorDepth;
-		}
-
-		/**
-		 * Gets screenHeight.
-		 * 
-		 * @return screenHeight
-		 */
-		public String getScreenHeight()
-		{
-			return screenHeight;
-		}
-
-		/**
-		 * Gets screenWidth.
-		 * 
-		 * @return screenWidth
-		 */
-		public String getScreenWidth()
-		{
-			return screenWidth;
-		}
-
-		/**
-		 * Gets utcOffset.
-		 * 
-		 * @return utcOffset
-		 */
-		public String getUtcOffset()
-		{
-			return utcOffset;
-		}
-
-		/**
-		 * Merge this with the given properties object.
-		 * 
-		 * @param properties
-		 *            the properties object to merge with
-		 */
-		public void merge(ClientProperties properties)
-		{
-			properties.setNavigatorAppName(navigatorAppName);
-			properties.setNavigatorAppVersion(navigatorAppVersion);
-			properties.setNavigatorAppCodeName(navigatorAppCodeName);
-			properties.setCookiesEnabled((navigatorCookieEnabled != null) ? navigatorCookieEnabled
-					.booleanValue() : false);
-			properties.setJavaEnabled((navigatorJavaEnabled != null) ? navigatorJavaEnabled
-					.booleanValue() : false);
-			properties.setNavigatorLanguage(navigatorLanguage);
-			properties.setNavigatorPlatform(navigatorPlatform);
-			properties.setNavigatorUserAgent(navigatorUserAgent);
-			properties.setScreenWidth(getInt(screenWidth));
-			properties.setScreenHeight(getInt(screenHeight));
-			properties.setBrowserWidth(getInt(browserWidth));
-			properties.setBrowserHeight(getInt(browserHeight));
-			properties.setScreenColorDepth(getInt(screenColorDepth));
-			properties.setUtcOffset(utcOffset);
-		}
-
-		/**
-		 * Sets browserHeight.
-		 * 
-		 * @param browserHeight
-		 *            browserHeight
-		 */
-		public void setBrowserHeight(String browserHeight)
-		{
-			this.browserHeight = browserHeight;
-		}
-
-		/**
-		 * Sets browserWidth.
-		 * 
-		 * @param browserWidth
-		 *            browserWidth
-		 */
-		public void setBrowserWidth(String browserWidth)
-		{
-			this.browserWidth = browserWidth;
-		}
-
-		/**
-		 * Sets navigatorAppCodeName.
-		 * 
-		 * @param navigatorAppCodeName
-		 *            navigatorAppCodeName
-		 */
-		public void setNavigatorAppCodeName(String navigatorAppCodeName)
-		{
-			this.navigatorAppCodeName = navigatorAppCodeName;
-		}
-
-		/**
-		 * Sets navigatorAppName.
-		 * 
-		 * @param navigatorAppName
-		 *            navigatorAppName
-		 */
-		public void setNavigatorAppName(String navigatorAppName)
-		{
-			this.navigatorAppName = navigatorAppName;
-		}
-
-		/**
-		 * Sets navigatorAppVersion.
-		 * 
-		 * @param navigatorAppVersion
-		 *            navigatorAppVersion
-		 */
-		public void setNavigatorAppVersion(String navigatorAppVersion)
-		{
-			this.navigatorAppVersion = navigatorAppVersion;
-		}
-
-		/**
-		 * Sets navigatorCookieEnabled.
-		 * 
-		 * @param navigatorCookieEnabled
-		 *            navigatorCookieEnabled
-		 */
-		public void setNavigatorCookieEnabled(Boolean navigatorCookieEnabled)
-		{
-			this.navigatorCookieEnabled = navigatorCookieEnabled;
-		}
-
-		/**
-		 * Sets navigatorJavaEnabled.
-		 * 
-		 * @param navigatorJavaEnabled
-		 *            navigatorJavaEnabled
-		 */
-		public void setNavigatorJavaEnabled(Boolean navigatorJavaEnabled)
-		{
-			this.navigatorJavaEnabled = navigatorJavaEnabled;
-		}
-
-		/**
-		 * Sets navigatorLanguage.
-		 * 
-		 * @param navigatorLanguage
-		 *            navigatorLanguage
-		 */
-		public void setNavigatorLanguage(String navigatorLanguage)
-		{
-			this.navigatorLanguage = navigatorLanguage;
-		}
-
-		/**
-		 * Sets navigatorPlatform.
-		 * 
-		 * @param navigatorPlatform
-		 *            navigatorPlatform
-		 */
-		public void setNavigatorPlatform(String navigatorPlatform)
-		{
-			this.navigatorPlatform = navigatorPlatform;
-		}
-
-		/**
-		 * Sets navigatorUserAgent.
-		 * 
-		 * @param navigatorUserAgent
-		 *            navigatorUserAgent
-		 */
-		public void setNavigatorUserAgent(String navigatorUserAgent)
-		{
-			this.navigatorUserAgent = navigatorUserAgent;
-		}
-
-		/**
-		 * Sets screenColorDepth.
-		 * 
-		 * @param screenColorDepth
-		 *            screenColorDepth
-		 */
-		public void setScreenColorDepth(String screenColorDepth)
-		{
-			this.screenColorDepth = screenColorDepth;
-		}
-
-		/**
-		 * Sets screenHeight.
-		 * 
-		 * @param screenHeight
-		 *            screenHeight
-		 */
-		public void setScreenHeight(String screenHeight)
-		{
-			this.screenHeight = screenHeight;
-		}
-
-		/**
-		 * Sets screenWidth.
-		 * 
-		 * @param screenWidth
-		 *            screenWidth
-		 */
-		public void setScreenWidth(String screenWidth)
-		{
-			this.screenWidth = screenWidth;
-		}
-
-		/**
-		 * Sets utcOffset.
-		 * 
-		 * @param utcOffset
-		 *            utcOffset
-		 */
-		public void setUtcOffset(String utcOffset)
-		{
-			this.utcOffset = utcOffset;
-		}
-
-		private int getInt(String value)
-		{
-			int intValue = -1;
-			try
-			{
-				intValue = Integer.parseInt(value);
-			}
-			catch (NumberFormatException e)
-			{
-				// Do nothing
-			}
-			return intValue;
-		}
-	}
-
-	/**
-	 * Form for posting JavaScript properties.
-	 */
-	private final class PostBackForm extends Form
-	{
-		private static final long serialVersionUID = 1L;
-
-		/**
-		 * Construct.
-		 * 
-		 * @param id
-		 *            component id
-		 */
-		public PostBackForm(String id)
-		{
-			super(id, new CompoundPropertyModel(new ClientPropertiesBean()));
-
-			add(new TextField("navigatorAppName"));
-			add(new TextField("navigatorAppVersion"));
-			add(new TextField("navigatorAppCodeName"));
-			add(new TextField("navigatorCookieEnabled"));
-			add(new TextField("navigatorJavaEnabled"));
-			add(new TextField("navigatorLanguage"));
-			add(new TextField("navigatorPlatform"));
-			add(new TextField("navigatorUserAgent"));
-			add(new TextField("screenWidth"));
-			add(new TextField("screenHeight"));
-			add(new TextField("screenColorDepth"));
-			add(new TextField("utcOffset"));
-			add(new TextField("browserWidth"));
-			add(new TextField("browserHeight"));
-		}
-
-		/**
-		 * @see org.apache.wicket.markup.html.form.Form#onSubmit()
-		 */
-		protected void onSubmit()
-		{
-			ClientPropertiesBean propertiesBean = (ClientPropertiesBean)getModelObject();
-
-			WebRequestCycle requestCycle = (WebRequestCycle)getRequestCycle();
-			WebSession session = (WebSession)getSession();
-			ClientInfo clientInfo = session.getClientInfo();
-
-			if (clientInfo == null)
-			{
-				clientInfo = new WebClientInfo(requestCycle);
-				getSession().setClientInfo(clientInfo);
-			}
-
-			if (clientInfo instanceof WebClientInfo)
-			{
-				WebClientInfo info = (WebClientInfo)clientInfo;
-				ClientProperties properties = info.getProperties();
-				propertiesBean.merge(properties);
-			}
-			else
-			{
-				warnNotUsingWebClientInfo(clientInfo);
-			}
-
-			continueToPrevious();
-		}
-	}
-
 	/** log. */
 	private static final Logger log = LoggerFactory.getLogger(BrowserInfoPage.class);
 
@@ -571,21 +145,18 @@ public class BrowserInfoPage extends WebPage
 		WebMarkupContainer link = new WebMarkupContainer("link");
 		link.add(new AttributeModifier("href", true, new Model((Serializable)url)));
 		add(link);
-		add(new PostBackForm("postback"));
-	}
+		add(new BrowserInfoForm("postback")
+		{
+			private static final long serialVersionUID = 1L;
 
-	/**
-	 * Log a warning that for in order to use this page, you should really be
-	 * using {@link WebClientInfo}.
-	 * 
-	 * @param clientInfo
-	 *            the actual client info object
-	 */
-	private void warnNotUsingWebClientInfo(ClientInfo clientInfo)
-	{
-		log.warn("using " + getClass().getName() + " makes no sense if you are not using "
-				+ WebClientInfo.class.getName() + " (you are using "
-				+ clientInfo.getClass().getName() + " instead)");
+			/**
+			 * @see org.apache.wicket.markup.html.pages.BrowserInfoForm#afterSubmit()
+			 */
+			protected void afterSubmit()
+			{
+				continueToPrevious();
+			}
+		});
 	}
 
 	/**
@@ -603,6 +174,20 @@ public class BrowserInfoPage extends WebPage
 		response.reset();
 		response.redirect(requestCycle.getRequest().getRelativePathPrefixToWicketHandler()
 				+ continueTo);
+	}
+	
+	/**
+	 * Log a warning that for in order to use this page, you should really be
+	 * using {@link WebClientInfo}.
+	 * 
+	 * @param clientInfo
+	 *            the actual client info object
+	 */
+	void warnNotUsingWebClientInfo(ClientInfo clientInfo)
+	{
+		log.warn("using " + getClass().getName() + " makes no sense if you are not using "
+				+ WebClientInfo.class.getName() + " (you are using "
+				+ clientInfo.getClass().getName() + " instead)");
 	}
 
 	/**
