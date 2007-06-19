8b06c650bf1d37349421e9bb99bf98cc6fa9568c
Alastair Maw <almaw@apache.org>
2007-06-19 12:03:48 +0000

    immediate -> defaultFormProcessing
    git-svn-id: https://svn.apache.org/repos/asf/incubator/wicket/trunk@548701 13f79535-47bb-0310-9956-ffa450edef68

--- jdk-1.5/wicket-examples/src/main/java/org/apache/wicket/examples/compref/ButtonPage.java
+++ jdk-1.5/wicket-examples/src/main/java/org/apache/wicket/examples/compref/ButtonPage.java
@@ -77,7 +77,7 @@ public class ButtonPage extends WicketExamplePage
 		String html = " <form wicket:id=\"form\">\n"
 				+ "  <input type=\"submit\" value=\"non wicket submit button\" />\n"
 				+ "  <input wicket:id=\"button1\" type=\"submit\" value=\"default wicket button\" />\n"
-				+ "  <input wicket:id=\"button2\" type=\"submit\" value=\"wicket button with immediate == true\" />\n"
+				+ "  <input wicket:id=\"button2\" type=\"submit\" value=\"wicket button with setDefaultFormProcessing(false)\" />\n"
 				+ " </form>";
 		String code = "&nbsp;&nbsp;&nbsp;&nbsp;// Add a form with an onSubmit implementation that sets a message\n"
 				+ "&nbsp;&nbsp;&nbsp;&nbsp;Form form = new Form(\"form\") {\n"
