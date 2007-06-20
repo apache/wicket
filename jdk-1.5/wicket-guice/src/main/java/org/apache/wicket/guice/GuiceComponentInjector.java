f37b6402a36f8f46eab750e923535a9f3e34f649
Alastair Maw <almaw@apache.org>
2007-06-20 11:46:19 +0000

    Improve javadoc.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/wicket/trunk@549075 13f79535-47bb-0310-9956-ffa450edef68

--- jdk-1.5/wicket-guice/src/main/java/org/apache/wicket/guice/GuiceComponentInjector.java
+++ jdk-1.5/wicket-guice/src/main/java/org/apache/wicket/guice/GuiceComponentInjector.java
@@ -25,22 +25,62 @@ import org.apache.wicket.application.IComponentInstantiationListener;
 import org.apache.wicket.proxy.LazyInitProxyFactory;
 
 import com.google.inject.Guice;
+import com.google.inject.ImplementedBy;
 import com.google.inject.Inject;
 import com.google.inject.Injector;
 import com.google.inject.Module;
 
 /**
  * Injects fields/members of components using Guice.
+ * <p>
+ * Add this to your application in its {@link Application#init()} method like
+ * so:
+ * <pre>
+ * addComponentInstantiationListener(new GuiceComponentInjector(this));
+ * </pre>
+ * <p>
+ * There are different constructors for this object depending on how you want to
+ * wire things. See the javadoc for the constructors for more information.
  * 
  * @author Alastair Maw
  */
 public class GuiceComponentInjector implements IComponentInstantiationListener
-{	
+{
+	/**
+	 * Creates a new Wicket GuiceComponentInjector instance.
+	 * <p>
+	 * Internally this will create a new Guice {@link Injector} instance, with
+	 * no {@link Module} instances. This is only useful if your beans have
+	 * appropriate {@link ImplementedBy} annotations on them so that they can be
+	 * automatically picked up with no extra configuration code.
+	 * 
+	 * @param app
+	 */
+	public GuiceComponentInjector(Application app)
+	{
+		this(app, new Module[0]);
+	}
+	
+	/**
+	 * Creates a new Wicket GuiceComponentInjector instance, using the supplied
+	 * Guice {@link Module} instances to create a new Guice {@link Injector}
+	 * instance internally.
+	 * 
+	 * @param app
+	 * @param modules
+	 */
 	public GuiceComponentInjector(Application app, Module ... modules)
 	{
 		this(app, Guice.createInjector(modules));
 	}
 
+	/**
+	 * Creates a new Wicket GuiceComponentInjector instance, using the provided
+	 * Guice {@link Injector} instance.
+	 * 
+	 * @param app
+	 * @param modules
+	 */
 	public GuiceComponentInjector(Application app, Injector injector)
 	{
 		app.setMetaData(GuiceInjectorHolder.INJECTOR_KEY, new GuiceInjectorHolder(injector));
