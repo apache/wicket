ea5de7fd4130cc5f5d38df7bab4b18d307caa47e
Igor Vaynberg <ivaynberg@apache.org>
2007-06-26 02:40:46 +0000

    initial checkin of quickstart archetype
    git-svn-id: https://svn.apache.org/repos/asf/incubator/wicket/trunk@550663 13f79535-47bb-0310-9956-ffa450edef68

---
+++ archetypes/quickstart/src/main/resources/archetype-resources/src/main/java/HomePage.java
@@ -0,0 +1,29 @@
+package ${groupId};
+
+import org.apache.wicket.PageParameters;
+import org.apache.wicket.markup.html.basic.Label;
+import org.apache.wicket.markup.html.WebPage;
+
+/**
+ * Homepage
+ */
+public class HomePage extends WebPage {
+
+	private static final long serialVersionUID = 1L;
+
+	// TODO Add any page properties or variables here
+
+    /**
+	 * Constructor that is invoked when page is invoked without a session.
+	 * 
+	 * @param parameters
+	 *            Page parameters
+	 */
+    public HomePage(final PageParameters parameters) {
+
+        // Add the simplest type of label
+        add(new Label("message", "If you see this message wicket is properly configured and running"));
+
+        // TODO Add your page's components here
+    }
+}
---
+++ archetypes/quickstart/target/classes/archetype-resources/src/main/java/WicketApplication.java
@@ -0,0 +1,27 @@
+package ${groupId};
+
+import org.apache.wicket.protocol.http.WebApplication;
+
+/**
+ * Application object for your web application. If you want to run this application without deploying, run the Start class.
+ * 
+ * @see wicket.myproject.Start#main(String[])
+ */
+public class WicketApplication extends WebApplication
+{    
+    /**
+     * Constructor
+     */
+	public WicketApplication()
+	{
+	}
+	
+	/**
+	 * @see wicket.Application#getHomePage()
+	 */
+	public Class getHomePage()
+	{
+		return HomePage.class;
+	}
+
+}
