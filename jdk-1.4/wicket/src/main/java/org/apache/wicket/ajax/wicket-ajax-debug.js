ddabd6f600751d422d7f10ee6abba04db1568913
Matej Knopp <knopp@apache.org>
2007-06-28 13:08:09 +0000

    Support for logging other things than just strings
    git-svn-id: https://svn.apache.org/repos/asf/incubator/wicket/trunk@551546 13f79535-47bb-0310-9956-ffa450edef68

--- jdk-1.4/wicket/src/main/java/org/apache/wicket/ajax/wicket-ajax-debug.js
+++ jdk-1.4/wicket/src/main/java/org/apache/wicket/ajax/wicket-ajax-debug.js
@@ -1,4 +1,4 @@
-/*
+info/*
  * Licensed to the Apache Software Foundation (ASF) under one or more
  * contributor license agreements.  See the NOTICE file distributed with
  * this work for additional information regarding copyright ownership.
@@ -45,7 +45,8 @@ var WicketAjaxDebug = {
 		WicketAjaxDebug.init();
         var d = wicketGet(WicketAjaxDebug.debugWindowLogId);
         var c = document.createElement("div");
-		
+
+		msg = "" + msg;		
         msg = msg.replace(/</g, "&lt;");
         msg = msg.replace(/>/g, "&gt;");
         msg = msg.replace(/\n/g, "<br/>");
