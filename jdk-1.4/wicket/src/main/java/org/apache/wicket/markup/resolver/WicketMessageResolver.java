746808482a4d833aac1c004990a9caf0f5272e9b
Alastair Maw <almaw@apache.org>
2007-07-24 17:04:58 +0000

    Let us override wicket:message values to empty strings.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/wicket/trunk@559109 13f79535-47bb-0310-9956-ffa450edef68

--- jdk-1.4/wicket/src/main/java/org/apache/wicket/markup/resolver/WicketMessageResolver.java
+++ jdk-1.4/wicket/src/main/java/org/apache/wicket/markup/resolver/WicketMessageResolver.java
@@ -82,14 +82,21 @@ public class WicketMessageResolver implements IComponentResolver
 							"Wrong format of <wicket:message key='xxx'>: attribute 'key' is missing");
 				}
 
+				// If the key can't be resolved and the default is null, an
+				// exception will be thrown. Instead, we default to a unique
+				// string and check against this later. Don't just use an empty
+				// string here, as people might want to override wicket:messages
+				// to empty strings.
+				final String defaultValue = "DEFAULT_WICKET_MESSAGE_RESOLVER_VALUE";
+				
 				final String value = container.getApplication().getResourceSettings()
-						.getLocalizer().getString(messageKey, container, "");
+						.getLocalizer().getString(messageKey, container, defaultValue);
 
 				final String id = "_message_" + container.getPage().getAutoIndex();
 				Component component = null;
-				if ((value != null) && (value.trim().length() > 0))
+				if (value != null && !defaultValue.equals(value))
 				{
-					component = new MyLabel(id, value);
+					component = new MyLabel(id, value.trim());
 				}
 				else
 				{
