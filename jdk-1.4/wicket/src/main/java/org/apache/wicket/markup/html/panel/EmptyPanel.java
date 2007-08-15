bc4adbfdd7219845c1e5c184dcd066341696eebb
Eelco Hillenius <ehillenius@apache.org>
2007-08-15 17:57:33 +0000

    javadoc fix: using emptypanel is not the same at all as using webmarkupcontainer
    git-svn-id: https://svn.apache.org/repos/asf/wicket/trunk@566281 13f79535-47bb-0310-9956-ffa450edef68

--- jdk-1.4/wicket/src/main/java/org/apache/wicket/markup/html/panel/EmptyPanel.java
+++ jdk-1.4/wicket/src/main/java/org/apache/wicket/markup/html/panel/EmptyPanel.java
@@ -18,10 +18,10 @@ package org.apache.wicket.markup.html.panel;
 
 import org.apache.wicket.markup.html.WebMarkupContainer;
 
-
 /**
- * Panel that does absolutely nothing. Can be used to display an empty block.
- * Same as using {@link WebMarkupContainer}.
+ * Panel that does absolutely nothing and that can be used to display an empty
+ * block. If you need to conditionally display markup, use a
+ * {@link WebMarkupContainer} directly.
  */
 public final class EmptyPanel extends Panel
 {
