93f3b798fb8871d17c3ea6ee65a17176638b3124
Frank Bille Jensen <frankbille@apache.org>
2007-08-21 17:42:29 +0000

    Release year 2007
    git-svn-id: https://svn.apache.org/repos/asf/wicket/trunk@568207 13f79535-47bb-0310-9956-ffa450edef68

--- release.sh
+++ release.sh
@@ -31,7 +31,7 @@ stty $stty_orig
 NOTICE=NOTICE
 > $NOTICE 
 echo "Apache Wicket" >> $NOTICE
-echo "Copyright 2006 The Apache Software Foundation" >> $NOTICE
+echo "Copyright 2007 The Apache Software Foundation" >> $NOTICE
 echo "" >> $NOTICE
 echo "This product includes software developed at" >> $NOTICE
 echo "The Apache Software Foundation (http://www.apache.org/)." >> $NOTICE
