5da90bf4496d9d24bc1b38d14ec9d85fcae2df33
Eelco Hillenius <ehillenius@apache.org>
2007-08-26 23:19:07 +0000

    markup loading test (currently fails on variation)
    git-svn-id: https://svn.apache.org/repos/asf/wicket/trunk@569917 13f79535-47bb-0310-9956-ffa450edef68

---
+++ jdk-1.4/wicket/src/test/java/org/apache/wicket/PageMarkupLoadingTest.java
@@ -0,0 +1,85 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.wicket;
+
+import java.util.Locale;
+
+import org.apache.wicket.markup.html.WebPage;
+import org.apache.wicket.protocol.http.WebRequestCycle;
+
+/**
+ * Tests markup loading.
+ */
+public class PageMarkupLoadingTest extends WicketTestCase
+{
+	/**
+	 * Test default locale loaded page.
+	 * 
+	 * @throws Exception
+	 */
+	public void testDefault() throws Exception
+	{
+		tester.setupRequestAndResponse();
+		WebRequestCycle cycle = tester.createRequestCycle();
+		cycle.getSession().setLocale(Locale.ENGLISH);
+		tester.startPage(Page1.class);
+		tester.assertRenderedPage(Page1.class);
+		tester.assertResultPage(getClass(), "PageMarkupLoadingTest$Page1_expected.html");
+	}
+
+	/**
+	 * Test Dutch locale loaded page.
+	 * 
+	 * @throws Exception
+	 */
+	public void testDutch() throws Exception
+	{
+		tester.setupRequestAndResponse();
+		WebRequestCycle cycle = tester.createRequestCycle();
+		cycle.getSession().setLocale(new Locale("nl"));
+		tester.startPage(Page1.class);
+		tester.assertRenderedPage(Page1.class);
+		tester.assertResultPage(getClass(), "PageMarkupLoadingTest$Page1_nl_expected.html");
+	}
+
+	/**
+	 * Test Dutch/ my style locale loaded page.
+	 * 
+	 * @throws Exception
+	 */
+	public void testDutchMyStyle() throws Exception
+	{
+		tester.setupRequestAndResponse();
+		WebRequestCycle cycle = tester.createRequestCycle();
+		cycle.getSession().setLocale(new Locale("nl"));
+		cycle.getSession().setStyle("mystyle");
+		tester.startPage(Page1.class);
+		tester.assertRenderedPage(Page1.class);
+		tester.assertResultPage(getClass(), "PageMarkupLoadingTest$Page1_mystyle_nl_expected.html");
+	}
+
+	/** simple test page */
+	public static class Page1 extends WebPage
+	{
+		private static final long serialVersionUID = 1L;
+
+		/** Construct. */
+		public Page1()
+		{
+		}
+	}
+}
