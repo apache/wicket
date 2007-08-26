0d02713923611ab78e6ce7248953f06a2bdabe42
Matej Knopp <knopp@apache.org>
2007-08-26 19:26:40 +0000

    Show the date picker without iframe
    git-svn-id: https://svn.apache.org/repos/asf/wicket/trunk@569868 13f79535-47bb-0310-9956-ffa450edef68

--- jdk-1.5/wicket-examples/src/main/java/org/apache/wicket/examples/ajax/builtin/modal/ModalPanel1.java
+++ jdk-1.5/wicket-examples/src/main/java/org/apache/wicket/examples/ajax/builtin/modal/ModalPanel1.java
@@ -16,6 +16,8 @@
  */
 package org.apache.wicket.examples.ajax.builtin.modal;
 
+import java.util.Map;
+
 import org.apache.wicket.extensions.yui.calendar.DateTimeField;
 import org.apache.wicket.markup.html.panel.Panel;
 
@@ -30,7 +32,19 @@ public class ModalPanel1 extends Panel {
 	public ModalPanel1(String id) {
 		super(id);
 		
-		add(new DateTimeField("dateTimeField"));
+		add(new DateTimeField("dateTimeField") {
+			/**
+			 * @see org.apache.wicket.extensions.yui.calendar.DateTimeField#configure(java.util.Map)
+			 */
+			@SuppressWarnings("unchecked")
+			@Override
+			protected void configure(Map widgetProperties)
+			{
+				super.configure(widgetProperties);
+				// IE 6 breaks layout with iframe - is that a YUI bug?
+				widgetProperties.put("iframe", false);
+			}
+		});
 	}
 
 }
