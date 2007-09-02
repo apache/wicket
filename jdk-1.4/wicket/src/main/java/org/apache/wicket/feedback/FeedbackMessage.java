1a0eb129d13d299615ddf2d802209ebe82538e32
Matej Knopp <knopp@apache.org>
2007-09-02 00:25:35 +0000

    quick fix
    git-svn-id: https://svn.apache.org/repos/asf/wicket/trunk@571909 13f79535-47bb-0310-9956-ffa450edef68

--- jdk-1.4/wicket/src/main/java/org/apache/wicket/feedback/FeedbackMessage.java
+++ jdk-1.4/wicket/src/main/java/org/apache/wicket/feedback/FeedbackMessage.java
@@ -130,7 +130,7 @@ public class FeedbackMessage implements IClusterable
 	 */
 	public String getLevelAsString()
 	{
-		return levelStrings[getLevel()];
+		return levelStrings[getLevel() / 100];
 	}
 
 	/**
