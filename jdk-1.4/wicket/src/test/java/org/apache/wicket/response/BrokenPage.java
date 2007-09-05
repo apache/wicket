6a68774a671152a1b521940a72cb98e25dae3cae
Jean-Baptiste Quenot <jbq@apache.org>
2007-09-05 15:14:26 +0000

    Move "response" package to the proper place
    git-svn-id: https://svn.apache.org/repos/asf/wicket/trunk@572966 13f79535-47bb-0310-9956-ffa450edef68

--- jdk-1.4/wicket/src/test/java/org/apache/wicket/response/BrokenPage.java
+++ jdk-1.4/wicket/src/test/java/org/apache/wicket/response/BrokenPage.java
@@ -14,7 +14,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package response;
+package org.apache.wicket.response;
 
 import org.apache.wicket.markup.html.WebPage;
 
--- jdk-1.4/wicket/src/test/java/org/apache/wicket/response/StringResponseTest.java
+++ jdk-1.4/wicket/src/test/java/org/apache/wicket/response/StringResponseTest.java
@@ -14,7 +14,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package response;
+package org.apache.wicket.response;
 
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
