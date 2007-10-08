3c639d1975bed29d1827402d61ff5cb3cf6246b0
Alastair Maw <almaw@apache.org>
2007-10-08 17:11:18 +0000

    Timeouts for debugging
    git-svn-id: https://svn.apache.org/repos/asf/wicket/trunk@582889 13f79535-47bb-0310-9956-ffa450edef68

--- archetypes/quickstart/src/main/resources/archetype-resources/src/test/java/Start.java
+++ archetypes/quickstart/src/main/resources/archetype-resources/src/test/java/Start.java
@@ -10,6 +10,9 @@ public class Start {
 	public static void main(String[] args) throws Exception {
 		Server server = new Server();
 		SocketConnector connector = new SocketConnector();
+		// Set some timeout options to make debugging easier.
+		connector.setMaxIdleTime(1000 * 60 * 60);
+		connector.setSoLingerTime(-1);
 		connector.setPort(8080);
 		server.setConnectors(new Connector[] { connector });
 
