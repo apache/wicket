4a7ae24120406e7f82faf608075eba01f0a6579f
Eelco Hillenius <ehillenius@apache.org>
2007-10-20 06:22:47 +0000

    added test for resource model that also tests that the model gets wrapped when you pass it into a component's constructor
    git-svn-id: https://svn.apache.org/repos/asf/wicket/trunk@586672 13f79535-47bb-0310-9956-ffa450edef68

---
+++ jdk-1.4/wicket/src/test/java/org/apache/wicket/model/ResourceModelTest.java
@@ -0,0 +1,70 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.wicket.model;
+
+import org.apache.wicket.WicketTestCase;
+import org.apache.wicket.markup.html.WebPage;
+import org.apache.wicket.markup.html.basic.Label;
+
+/**
+ * Tests {@link ResourceModel}.
+ */
+public class ResourceModelTest extends WicketTestCase
+{
+	/**
+	 * Test page.
+	 */
+	public static class TestPage extends WebPage
+	{
+		private static final long serialVersionUID = 1L;
+
+		/**
+		 * Construct.
+		 */
+		public TestPage()
+		{
+			add(new Label("testlabel", new ResourceModel("test.label")));
+		}
+	}
+
+	/**
+	 * Construct.
+	 */
+	public ResourceModelTest()
+	{
+	}
+
+	/**
+	 * Construct.
+	 * 
+	 * @param name
+	 */
+	public ResourceModelTest(String name)
+	{
+		super(name);
+	}
+
+	/**
+	 * Tests a simple {@link ResourceModel} on a test page.
+	 * 
+	 * @throws Exception
+	 */
+	public void testResourceModel() throws Exception
+	{
+		executeTest(TestPage.class, "ResourceModelTest$TestPage_expected.html");
+	}
+}
