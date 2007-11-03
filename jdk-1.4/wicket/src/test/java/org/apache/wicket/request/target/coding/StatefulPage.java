022765dd5f337118f9b7ae0a48b4d6338d2b957e
Johan Compagner <jcompagner@apache.org>
2007-11-03 01:14:00 +0000

    large test for all the url coding strategies. With stateless forms in stateless pages and in statefull pages
    git-svn-id: https://svn.apache.org/repos/asf/wicket/trunk@591530 13f79535-47bb-0310-9956-ffa450edef68

---
+++ jdk-1.4/wicket/src/test/java/org/apache/wicket/request/target/coding/StatefulPage.java
@@ -0,0 +1,102 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.wicket.request.target.coding;
+
+import org.apache.wicket.markup.html.WebPage;
+import org.apache.wicket.markup.html.autolink.Index;
+import org.apache.wicket.markup.html.basic.Label;
+import org.apache.wicket.markup.html.form.StatelessForm;
+import org.apache.wicket.markup.html.form.TextField;
+import org.apache.wicket.markup.html.link.BookmarkablePageLink;
+import org.apache.wicket.markup.html.link.Link;
+import org.apache.wicket.markup.html.panel.FeedbackPanel;
+import org.apache.wicket.model.Model;
+import org.apache.wicket.model.PropertyModel;
+
+/**
+ * Another page of the stateless example.
+ * 
+ * @author Eelco Hillenius
+ */
+public class StatefulPage extends WebPage
+{
+	private static final long serialVersionUID = 1L;
+
+	/** click count for Link. */
+	private int linkClickCount = 0;
+
+	/**
+	 * Construct.
+	 */
+	public StatefulPage()
+	{
+		add(new BookmarkablePageLink("indexLink", Index.class));
+
+		// Action link counts link clicks
+		final Link actionLink = new Link("actionLink")
+		{
+			private static final long serialVersionUID = 1L;
+
+			public void onClick()
+			{
+				linkClickCount++;
+			}
+		};
+		add(actionLink);
+		actionLink.add(new Label("linkClickCount", new PropertyModel(this, "linkClickCount")));
+
+		final TextField field = new TextField("textfield", new Model());
+
+		StatelessForm statelessForm = new StatelessForm("statelessform")
+		{
+			private static final long serialVersionUID = 1L;
+
+			/**
+			 * @see org.apache.wicket.markup.html.form.Form#onSubmit()
+			 */
+			protected void onSubmit()
+			{
+				info("Submitted text: " + field.getModelObject() + ", link click count: " +
+					linkClickCount);
+			}
+		};
+		statelessForm.add(field);
+		add(statelessForm);
+		add(new FeedbackPanel("feedback"));
+	}
+
+	/**
+	 * Gets linkClickCount.
+	 * 
+	 * @return linkClickCount
+	 */
+	public int getLinkClickCount()
+	{
+		return linkClickCount;
+	}
+
+	/**
+	 * Sets linkClickCount.
+	 * 
+	 * @param linkClickCount
+	 *            linkClickCount
+	 */
+	public void setLinkClickCount(int linkClickCount)
+	{
+		this.linkClickCount = linkClickCount;
+	}
+}
---
+++ jdk-1.4/wicket/src/test/java/org/apache/wicket/request/target/coding/StatelessPage.java
@@ -0,0 +1,62 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.wicket.request.target.coding;
+
+import org.apache.wicket.markup.html.WebPage;
+import org.apache.wicket.markup.html.autolink.Index;
+import org.apache.wicket.markup.html.form.StatelessForm;
+import org.apache.wicket.markup.html.form.TextField;
+import org.apache.wicket.markup.html.link.BookmarkablePageLink;
+import org.apache.wicket.markup.html.panel.FeedbackPanel;
+import org.apache.wicket.model.Model;
+
+/**
+ * Another page of the stateless example.
+ * 
+ * @author Eelco Hillenius
+ */
+public class StatelessPage extends WebPage
+{
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * Constructor
+	 */
+	public StatelessPage()
+	{
+		setStatelessHint(true);
+		add(new BookmarkablePageLink("indexLink", Index.class));
+		final TextField field = new TextField("textfield", new Model());
+		field.setRequired(true);
+
+		StatelessForm statelessForm = new StatelessForm("statelessform")
+		{
+			private static final long serialVersionUID = 1L;
+
+			/**
+			 * @see org.apache.wicket.markup.html.form.Form#onSubmit()
+			 */
+			protected void onSubmit()
+			{
+				info("Submitted text: " + field.getModelObject());
+			}
+		};
+		statelessForm.add(field);
+		add(statelessForm);
+		add(new FeedbackPanel("feedback"));
+	}
+}
