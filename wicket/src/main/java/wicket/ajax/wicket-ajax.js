1ea1399e3a366de472664d5cd6402c8af13f46dc
Johan Compagner <jcompagner@apache.org>
2007-11-14 13:02:05 +0000

    search input field support (safari) WICKET-1145
    git-svn-id: https://svn.apache.org/repos/asf/wicket/branches/wicket-1.2.x@594861 13f79535-47bb-0310-9956-ffa450edef68

--- wicket/src/main/java/wicket/ajax/wicket-ajax.js
+++ wicket/src/main/java/wicket/ajax/wicket-ajax.js
@@ -273,7 +273,7 @@ Wicket.Form.serializeInput = function(input) {
     var type = input.type.toLowerCase();
     if ((type == "checkbox" || type == "radio") && input.checked) {
         return Wicket.Form.encode(input.name) + "=" + Wicket.Form.encode(input.value) + "&";
-    } else if (type == "text" || type == "password" || type == "hidden" || type == "textarea") {
+    } else if (type == "text" || type == "password" || type == "hidden" || type == "textarea" || type == "search") {
 		return Wicket.Form.encode(input.name) + "=" + Wicket.Form.encode(input.value) + "&";
 	} else {
 		return "";
