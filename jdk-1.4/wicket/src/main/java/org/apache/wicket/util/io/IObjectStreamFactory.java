149514c774cf9182e1aab2e1d74b938b71c84af9
Johan Compagner <jcompagner@apache.org>
2007-11-20 11:11:32 +0000

    Default ObjectInputStream should override resolveClass so that the IClassResolver is used when the class is not found
    git-svn-id: https://svn.apache.org/repos/asf/wicket/trunk@596619 13f79535-47bb-0310-9956-ffa450edef68

--- jdk-1.4/wicket/src/main/java/org/apache/wicket/util/io/IObjectStreamFactory.java
+++ jdk-1.4/wicket/src/main/java/org/apache/wicket/util/io/IObjectStreamFactory.java
@@ -21,9 +21,16 @@ import java.io.InputStream;
 import java.io.NotSerializableException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
+import java.io.ObjectStreamClass;
 import java.io.OutputStream;
 
+import org.apache.wicket.Application;
+import org.apache.wicket.WicketRuntimeException;
+import org.apache.wicket.application.IClassResolver;
+import org.apache.wicket.settings.IApplicationSettings;
 import org.apache.wicket.util.lang.Objects;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 
 /**
@@ -42,12 +49,57 @@ public interface IObjectStreamFactory
 	 */
 	public static final class DefaultObjectStreamFactory implements IObjectStreamFactory
 	{
+		private static final Logger log = LoggerFactory.getLogger(DefaultObjectStreamFactory.class);
+
 		/**
 		 * @see org.apache.wicket.util.io.IObjectStreamFactory#newObjectInputStream(java.io.InputStream)
 		 */
 		public ObjectInputStream newObjectInputStream(InputStream in) throws IOException
 		{
-			return new ObjectInputStream(in);
+			return new ObjectInputStream(in)
+			{
+				// This override is required to resolve classes inside in different bundle, i.e.
+				// The classes can be resolved by OSGI classresolver implementation
+				protected Class resolveClass(ObjectStreamClass desc) throws IOException,
+					ClassNotFoundException
+				{
+					String className = desc.getName();
+
+					try
+					{
+						return super.resolveClass(desc);
+					}
+					catch (ClassNotFoundException ex1)
+					{
+						// ignore this exception.
+						log.debug("Class not found by the object outputstream itself, trying the IClassResolver");
+					}
+
+
+					Class candidate = null;
+					try
+					{
+						// Can the application always be taken??
+						Application application = Application.get();
+						IApplicationSettings applicationSettings = application.getApplicationSettings();
+						IClassResolver classResolver = applicationSettings.getClassResolver();
+
+						candidate = classResolver.resolveClass(className);
+						if (candidate == null)
+						{
+							candidate = super.resolveClass(desc);
+						}
+					}
+					catch (WicketRuntimeException ex)
+					{
+						if (ex.getCause() instanceof ClassNotFoundException)
+						{
+							throw (ClassNotFoundException)ex.getCause();
+						}
+					}
+					return candidate;
+				}
+			};
 		}
 
 		/**
