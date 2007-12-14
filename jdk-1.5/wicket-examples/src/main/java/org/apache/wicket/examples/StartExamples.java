3cb6d804483d0b216f884e12f5fffb92ea8f80c1
Eelco Hillenius <ehillenius@apache.org>
2007-12-14 06:00:49 +0000

    made code shorter (jff)
    git-svn-id: https://svn.apache.org/repos/asf/wicket/trunk@604111 13f79535-47bb-0310-9956-ffa450edef68

--- jdk-1.5/wicket-examples/src/main/java/org/apache/wicket/examples/StartExamples.java
+++ jdk-1.5/wicket-examples/src/main/java/org/apache/wicket/examples/StartExamples.java
@@ -22,9 +22,7 @@ import javax.management.MBeanServer;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
-import org.mortbay.jetty.Connector;
 import org.mortbay.jetty.Server;
-import org.mortbay.jetty.nio.SelectChannelConnector;
 import org.mortbay.jetty.webapp.WebAppContext;
 import org.mortbay.management.MBeanContainer;
 
@@ -46,15 +44,8 @@ public class StartExamples
 	 */
 	public static void main(String[] args)
 	{
-		Server server = new Server();
-		SelectChannelConnector connector = new SelectChannelConnector();
-		connector.setPort(8080);
-		server.setConnectors(new Connector[] { connector });
-
-		WebAppContext web = new WebAppContext();
-		web.setContextPath("/wicket-examples");
-		web.setWar("src/main/webapp");
-		server.addHandler(web);
+		Server server = new Server(8080);
+		WebAppContext web = new WebAppContext(server, "src/main/webapp", "/wicket-examples");
 
 		MBeanServer mBeanServer = ManagementFactory.getPlatformMBeanServer();
 		MBeanContainer mBeanContainer = new MBeanContainer(mBeanServer);
