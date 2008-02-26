c8ea6ee7360a1ca389401643b650d07a007d75fe
Gerolf Seitz <gseitz@apache.org>
2008-02-26 23:33:49 +0000

    corrected wrong key in javadoc
    git-svn-id: https://svn.apache.org/repos/asf/wicket/trunk@631425 13f79535-47bb-0310-9956-ffa450edef68

--- jdk-1.4/wicket/src/main/java/org/apache/wicket/markup/html/form/validation/EqualPasswordInputValidator.java
+++ jdk-1.4/wicket/src/main/java/org/apache/wicket/markup/html/form/validation/EqualPasswordInputValidator.java
@@ -20,27 +20,27 @@ import org.apache.wicket.markup.html.form.FormComponent;
 
 /**
  * Validates that the input of two form components is identical. Errors are reported on the second
- * form component with key 'EqualInputValidator' and the variables:
+ * form component with key 'EqualPasswordInputValidator' and the variables:
  * <ul>
  * <li>${input(n)}: the user's input</li>
  * <li>${name}: the name of the component</li>
  * <li>${label(n)}: the label of the component - either comes from FormComponent.labelModel or
  * resource key [form-id].[form-component-id] in that order</li>
  * </ul>
- * 
+ *
  * @author Igor Vaynberg (ivaynberg)
  */
 public class EqualPasswordInputValidator extends EqualInputValidator
 {
 	/**
-	 * 
+	 *
 	 */
 	private static final long serialVersionUID = 1L;
 
 
 	/**
 	 * Construct.
-	 * 
+	 *
 	 * @param formComponent1
 	 * @param formComponent2
 	 */
