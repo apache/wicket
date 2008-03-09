273020be82926d5658b631d6c71699166a9bb1df
Igor Vaynberg <ivaynberg@apache.org>
2008-03-09 07:58:02 +0000

    wicket-546: submitlink.oncomponenttag() doesnt call super
    git-svn-id: https://svn.apache.org/repos/asf/wicket/branches/wicket-1.2.x@635186 13f79535-47bb-0310-9956-ffa450edef68

--- wicket/src/main/java/wicket/markup/html/form/SubmitLink.java
+++ wicket/src/main/java/wicket/markup/html/form/SubmitLink.java
@@ -176,6 +176,7 @@ public class SubmitLink extends Button implements ILinkListener
 	 */
 	protected void onComponentTag(ComponentTag tag)
 	{
+		super.onComponentTag(tag);
 		if (getOutputMarkupId())
 		{
 			tag.put("id", getMarkupId());
