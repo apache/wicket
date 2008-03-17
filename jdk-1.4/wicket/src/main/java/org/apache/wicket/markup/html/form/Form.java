7614520e94eb49cb2e5c2c560d055b18c8785301
Igor Vaynberg <ivaynberg@apache.org>
2008-03-17 19:00:35 +0000

    WICKET-1430: Form processing throws NPE if button.onsubmit() moves the button or its parents out of the component hierarchy
    git-svn-id: https://svn.apache.org/repos/asf/wicket/trunk@638021 13f79535-47bb-0310-9956-ffa450edef68

--- jdk-1.4/wicket/src/main/java/org/apache/wicket/markup/html/form/Form.java
+++ jdk-1.4/wicket/src/main/java/org/apache/wicket/markup/html/form/Form.java
@@ -1319,9 +1319,9 @@ public class Form extends WebMarkupContainer implements IFormSubmitListener
 		Form formToProcess = this;
 		if (submittingComponent != null)
 		{
-			submittingComponent.onSubmit();
 			// use the form which the submittingComponent has submitted for further processing
 			formToProcess = submittingComponent.getForm();
+			submittingComponent.onSubmit();
 		}
 
 		// Model was successfully updated with valid data
@@ -1368,13 +1368,12 @@ public class Form extends WebMarkupContainer implements IFormSubmitListener
 
 
 	/**
-	 * Gets the HTTP submit method that will appear in form markup. If no method is specified in 
-	 * the template, "post" is the default. Note that the markup-declared HTTP method may not
-	 * correspond to the one actually used to submit the form; in an Ajax submit, for example, 
-	 * JavaScript event handlers may submit the form with a "get" even when the form method is 
-	 * declared as "post." Therefore this method should not be considered a guarantee of the 
-	 * HTTP method used, but a value for the markup only.
-	 * Override if you have a requirement to alter this behavior.
+	 * Gets the HTTP submit method that will appear in form markup. If no method is specified in the
+	 * template, "post" is the default. Note that the markup-declared HTTP method may not correspond
+	 * to the one actually used to submit the form; in an Ajax submit, for example, JavaScript event
+	 * handlers may submit the form with a "get" even when the form method is declared as "post."
+	 * Therefore this method should not be considered a guarantee of the HTTP method used, but a
+	 * value for the markup only. Override if you have a requirement to alter this behavior.
 	 * 
 	 * @return the submit method specified in markup.
 	 */
