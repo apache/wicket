96bdd81def0921654b4a8089c481103e673839dd
Frank Bille Jensen <frankbille@apache.org>
2008-04-10 08:47:27 +0000

    All projects are now jdk1.5
    git-svn-id: https://svn.apache.org/repos/asf/wicket/trunk@646687 13f79535-47bb-0310-9956-ffa450edef68

--- eclipse.sh
+++ eclipse.sh
@@ -3,11 +3,9 @@ mvn eclipse:eclipse -DdownloadSources=true
 
 case `uname` in
 Linux)
-	find jdk-1.4 -name .classpath -exec sed -i -e "s/org.eclipse.jdt.launching.JRE_CONTAINER/org.eclipse.jdt.launching.JRE_CONTAINER\/org.eclipse.jdt.internal.debug.ui.launcher.StandardVMType\/J2SE-1.4/g" {} \;
-	find jdk-1.5 -name .classpath -exec sed -i -e "s/org.eclipse.jdt.launching.JRE_CONTAINER/org.eclipse.jdt.launching.JRE_CONTAINER\/org.eclipse.jdt.internal.debug.ui.launcher.StandardVMType\/J2SE-1.5/g" {} \;
+	find . -name .classpath -exec sed -i -e "s/org.eclipse.jdt.launching.JRE_CONTAINER/org.eclipse.jdt.launching.JRE_CONTAINER\/org.eclipse.jdt.internal.debug.ui.launcher.StandardVMType\/J2SE-1.5/g" {} \;
 	;;
 *)
-	find jdk-1.4 -name .classpath -exec sed -i "" -e "s/org.eclipse.jdt.launching.JRE_CONTAINER/org.eclipse.jdt.launching.JRE_CONTAINER\/org.eclipse.jdt.internal.debug.ui.launcher.StandardVMType\/J2SE-1.4/g" {} \;
-	find jdk-1.5 -name .classpath -exec sed -i "" -e "s/org.eclipse.jdt.launching.JRE_CONTAINER/org.eclipse.jdt.launching.JRE_CONTAINER\/org.eclipse.jdt.internal.debug.ui.launcher.StandardVMType\/J2SE-1.5/g" {} \;
+	find . -name .classpath -exec sed -i "" -e "s/org.eclipse.jdt.launching.JRE_CONTAINER/org.eclipse.jdt.launching.JRE_CONTAINER\/org.eclipse.jdt.internal.debug.ui.launcher.StandardVMType\/J2SE-1.5/g" {} \;
 	;;
 esac
