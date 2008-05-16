c6e167bd8eefc12e834db61dd15ed87b3bd3152c
Gerolf Seitz <gseitz@apache.org>
2008-05-16 17:32:23 +0000

    added missing parts ("::") of the url
    git-svn-id: https://svn.apache.org/repos/asf/wicket/trunk@657142 13f79535-47bb-0310-9956-ffa450edef68

--- testing/wicket-threadtest/src/main/java/org/apache/wicket/threadtest/App1Test3.java
+++ testing/wicket-threadtest/src/main/java/org/apache/wicket/threadtest/App1Test3.java
@@ -41,12 +41,11 @@ public class App1Test3
 	{
 
 		List<String> gets = new ArrayList<String>();
-		gets
-				.add("/app1?wicket:bookmarkablePage=one:org.apache.wicket.threadtest.apps.app1.ResourceTestPage");
+		gets.add("/app1?wicket:bookmarkablePage=one:org.apache.wicket.threadtest.apps.app1.ResourceTestPage");
 		for (int i = 0; i < ResourceTestPage.IMAGES_PER_PAGE; i++)
 		{
-			gets.add("/app1?wicket:interface=one:${iteration}:listView:" + i
-					+ ":image::IResourceListener");
+			gets.add("/app1?wicket:interface=one:${iteration}:listView:" + i +
+				":image::IResourceListener::");
 		}
 
 		SimpleGetCommand getCmd = new SimpleGetCommand(gets, 5);
