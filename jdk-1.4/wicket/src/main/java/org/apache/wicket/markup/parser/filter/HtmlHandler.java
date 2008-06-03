699186e19de8500179bc7ef81c34c7df1ac4af53
Igor Vaynberg <ivaynberg@apache.org>
2008-06-03 17:08:06 +0000

    WICKET-1682
    git-svn-id: https://svn.apache.org/repos/asf/wicket/branches/wicket-1.3.x@662842 13f79535-47bb-0310-9956-ffa450edef68

--- jdk-1.4/wicket/src/main/java/org/apache/wicket/markup/parser/filter/HtmlHandler.java
+++ jdk-1.4/wicket/src/main/java/org/apache/wicket/markup/parser/filter/HtmlHandler.java
@@ -91,7 +91,7 @@ public final class HtmlHandler extends AbstractMarkupFilter
 				else
 				{
 					throw new ParseException("Tag " + top + " at " + top.getPos() +
-							" did not have a close tag", top.getPos());
+						" did not have a close tag", top.getPos());
 				}
 			}
 
@@ -142,8 +142,8 @@ public final class HtmlHandler extends AbstractMarkupFilter
 					if (mismatch)
 					{
 						throw new ParseException("Tag " + top.toUserDebugString() +
-								" has a mismatched close tag at " + tag.toUserDebugString(), top
-								.getPos());
+							" has a mismatched close tag at " + tag.toUserDebugString(),
+							top.getPos());
 					}
 				}
 
@@ -153,7 +153,7 @@ public final class HtmlHandler extends AbstractMarkupFilter
 			else
 			{
 				throw new ParseException("Tag " + tag.toUserDebugString() +
-						" does not have a matching open tag", tag.getPos());
+					" does not have a matching open tag", tag.getPos());
 			}
 		}
 		else if (tag.isOpenClose())
@@ -174,6 +174,6 @@ public final class HtmlHandler extends AbstractMarkupFilter
 	 */
 	public static boolean requiresCloseTag(final String name)
 	{
-		return doesNotRequireCloseTag.get(name) == null;
+		return doesNotRequireCloseTag.get(name.toLowerCase()) == null;
 	}
 }
