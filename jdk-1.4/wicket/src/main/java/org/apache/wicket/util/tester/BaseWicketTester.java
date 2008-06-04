81cd6e3acdca82239da036b2e3e02863ace4635a
Frank Bille Jensen <frankbille@apache.org>
2008-06-04 07:26:17 +0000

    WICKET-1473: BaseWicketTester.isComponentOnAjaxResponse() test is too strong
    git-svn-id: https://svn.apache.org/repos/asf/wicket/branches/wicket-1.3.x@663016 13f79535-47bb-0310-9956-ffa450edef68

--- jdk-1.4/wicket/src/main/java/org/apache/wicket/util/tester/BaseWicketTester.java
+++ jdk-1.4/wicket/src/main/java/org/apache/wicket/util/tester/BaseWicketTester.java
@@ -1035,7 +1035,7 @@ public class BaseWicketTester extends MockWebApplication
 
 		// Look for that the component is on the response, using the markup id
 		boolean isComponentInAjaxResponse = ajaxResponse.matches("(?s).*<component id=\"" +
-			markupId + "\" ?>.*");
+			markupId + "\"[^>]*?>.*");
 		failMessage = "Component wasn't found in the AJAX response";
 		return isTrue(failMessage, isComponentInAjaxResponse);
 	}
--- jdk-1.4/wicket/src/test/java/org/apache/wicket/util/tester/WicketTesterTest.java
+++ jdk-1.4/wicket/src/test/java/org/apache/wicket/util/tester/WicketTesterTest.java
@@ -33,6 +33,7 @@ import org.apache.wicket.markup.html.basic.Label;
 import org.apache.wicket.markup.html.form.Button;
 import org.apache.wicket.markup.html.form.TextField;
 import org.apache.wicket.markup.html.link.Link;
+import org.apache.wicket.model.IModel;
 import org.apache.wicket.request.target.coding.IRequestTargetUrlCodingStrategy;
 import org.apache.wicket.util.tester.MockPageParameterPage.MockInnerClassPage;
 import org.apache.wicket.util.tester.MockPageWithFormAndAjaxFormSubmitBehavior.Pojo;
@@ -336,11 +337,80 @@ public class WicketTesterTest extends TestCase
 		// The link must be a Link :)
 		tester.assertComponent(MockPageWithLink.LINK_ID, Link.class);
 
-		// Get the new link component
-		Component component = tester.getComponentFromLastRenderedPage(MockPageWithLink.LINK_ID);
+		// This must not fail
+		tester.assertComponentOnAjaxResponse(MockPageWithLink.LINK_ID);
+
+		tester.dumpPage();
+	}
+
+	/**
+	 * Test that testing if a component is on the ajax response can handle if the response is
+	 * encoded.
+	 */
+	public void testAssertComponentOnAjaxResponse_encoding()
+	{
+		final IModel labelModel = new IModel()
+		{
+			private static final long serialVersionUID = 1L;
+
+			private String value;
+
+			public Object getObject()
+			{
+				return value;
+			}
+
+			public void setObject(Object object)
+			{
+				value = (String)object;
+			}
+
+			public void detach()
+			{
+			}
+		};
+
+		labelModel.setObject("Label 1");
+		final Label label = new Label(MockPageWithLinkAndLabel.LABEL_ID, labelModel);
+		label.setOutputMarkupId(true);
+
+		final Page page = new MockPageWithLinkAndLabel();
+		AjaxLink ajaxLink = new AjaxLink(MockPageWithLinkAndLabel.LINK_ID)
+		{
+			private static final long serialVersionUID = 1L;
+
+			public void onClick(AjaxRequestTarget target)
+			{
+				labelModel.setObject("Label which needs encoding: [] ][");
+				target.addComponent(label);
+			}
+		};
+		ajaxLink.setOutputMarkupId(true);
+
+		page.add(ajaxLink);
+		ajaxLink.add(label);
+
+		tester.startPage(new ITestPageSource()
+		{
+			private static final long serialVersionUID = 1L;
+
+			public Page getTestPage()
+			{
+				return page;
+			}
+		});
+
+
+		// Click the link
+		tester.clickLink(MockPageWithLinkAndLabel.LINK_ID);
+
+		tester.assertComponent(MockPageWithLinkAndLabel.LABEL_PATH, Label.class);
+
+		tester.dumpPage();
 
 		// This must not fail
-		tester.assertComponentOnAjaxResponse(component);
+		tester.assertComponentOnAjaxResponse(MockPageWithLinkAndLabel.LABEL_PATH);
+
 	}
 
 	/**
