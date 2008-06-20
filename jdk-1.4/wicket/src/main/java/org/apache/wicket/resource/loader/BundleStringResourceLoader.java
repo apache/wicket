6d507b1d1c94b1564ff3ebb89d9c25f13fdf7010
Igor Vaynberg <ivaynberg@apache.org>
2008-06-20 16:05:11 +0000

    WICKET-1711
    git-svn-id: https://svn.apache.org/repos/asf/wicket/branches/wicket-1.3.x@669963 13f79535-47bb-0310-9956-ffa450edef68

--- jdk-1.4/wicket/src/main/java/org/apache/wicket/resource/loader/BundleStringResourceLoader.java
+++ jdk-1.4/wicket/src/main/java/org/apache/wicket/resource/loader/BundleStringResourceLoader.java
@@ -68,6 +68,10 @@ public class BundleStringResourceLoader implements IStringResourceLoader
 		if (locale == null)
 		{
 			locale = Session.get().getLocale();
+			if (locale == null)
+			{
+				locale = Locale.getDefault();
+			}
 		}
 		try
 		{
