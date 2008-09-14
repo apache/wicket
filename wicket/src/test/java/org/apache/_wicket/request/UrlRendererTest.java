09b58135978c5df4ec932c1c72fafaf5b4cd6abf
Matej Knopp <knopp@apache.org>
2008-09-14 00:03:04 +0000

    git-svn-id: https://svn.apache.org/repos/asf/wicket/sandbox/knopp/experimental@695090 13f79535-47bb-0310-9956-ffa450edef68

--- wicket/src/test/java/org/apache/_wicket/request/UrlRendererTest.java
+++ wicket/src/test/java/org/apache/_wicket/request/UrlRendererTest.java
@@ -14,7 +14,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.apache.wicket.requestng;
+package org.apache._wicket.request;
 
 import org.apache._wicket.request.Url;
 import org.apache._wicket.request.UrlRenderer;
