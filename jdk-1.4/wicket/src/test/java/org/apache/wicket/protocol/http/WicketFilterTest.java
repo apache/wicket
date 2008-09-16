655ca6fd47455807c5d15a9408f21e60c97388c4
Martijn Dashorst <dashorst@apache.org>
2008-09-16 19:35:46 +0000

    Serial id's are mandatory in our code conventions
    git-svn-id: https://svn.apache.org/repos/asf/wicket/branches/wicket-1.3.x@696021 13f79535-47bb-0310-9956-ffa450edef68

--- jdk-1.4/wicket/src/test/java/org/apache/wicket/protocol/http/WicketFilterTest.java
+++ jdk-1.4/wicket/src/test/java/org/apache/wicket/protocol/http/WicketFilterTest.java
@@ -30,6 +30,7 @@ import java.util.HashMap;
 import java.util.Locale;
 import java.util.Map;
 import java.util.TimeZone;
+
 import javax.servlet.FilterChain;
 import javax.servlet.FilterConfig;
 import javax.servlet.ServletContext;
@@ -39,6 +40,7 @@ import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletResponse;
 
 import junit.framework.TestCase;
+
 import org.apache.wicket.Application;
 import org.apache.wicket.markup.html.image.resource.DynamicImageResource;
 import org.apache.wicket.util.tester.WicketTester.DummyWebApplication;
@@ -46,8 +48,9 @@ import org.apache.wicket.util.tester.WicketTester.DummyWebApplication;
 public class WicketFilterTest extends TestCase
 {
 	private static WebApplication application;
-	private DateFormat fullDateFormat = DateFormat.getDateInstance(DateFormat.FULL);
-	private DateFormat headerDateFormat = new SimpleDateFormat("EEE, d MMM yyyy HH:mm:ss z", Locale.UK);
+	private final DateFormat fullDateFormat = DateFormat.getDateInstance(DateFormat.FULL);
+	private final DateFormat headerDateFormat = new SimpleDateFormat("EEE, d MMM yyyy HH:mm:ss z",
+		Locale.UK);
 
 	protected void tearDown() throws Exception
 	{
@@ -68,7 +71,8 @@ public class WicketFilterTest extends TestCase
 		assertEquals("filtertest/", filterPath);
 	}
 
-	public void testNotModifiedResponseIncludesExpiresHeader() throws IOException, ServletException, ParseException
+	public void testNotModifiedResponseIncludesExpiresHeader() throws IOException,
+		ServletException, ParseException
 	{
 		application = new DummyWebApplication();
 		WicketFilter filter = new WicketFilter();
@@ -76,6 +80,8 @@ public class WicketFilterTest extends TestCase
 		Application.set(application);
 		DynamicImageResource resource = new DynamicImageResource()
 		{
+			private static final long serialVersionUID = 1L;
+
 			protected byte[] getImageData()
 			{
 				throw new UnsupportedOperationException("Not implemented");
@@ -84,12 +90,14 @@ public class WicketFilterTest extends TestCase
 		resource.setCacheable(true);
 		application.getSharedResources().add("foo.gif", resource);
 		MockHttpServletRequest request = new MockHttpServletRequest(application, null, null);
-		request.setURL(request.getContextPath() +  "/app/" + "resources/" + Application.class.getName() + "/foo.gif");
+		request.setURL(request.getContextPath() + "/app/" + "resources/" +
+			Application.class.getName() + "/foo.gif");
 		setIfModifiedSinceToNextWeek(request);
 		MockHttpServletResponse response = new MockHttpServletResponse(request);
 		filter.doFilter(request, response, new FilterChain()
 		{
-			public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse) throws IOException, ServletException
+			public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse)
+				throws IOException, ServletException
 			{
 			}
 		});
@@ -98,7 +106,8 @@ public class WicketFilterTest extends TestCase
 		assertNotNull("Expires header must be set on not modified response", responseExpiresHeader);
 
 		Date responseExpires = headerDateFormat.parse(responseExpiresHeader);
-		assertTrue("Expected later than current date but was " + responseExpires, responseExpires.after(new Date()));
+		assertTrue("Expected later than current date but was " + responseExpires,
+			responseExpires.after(new Date()));
 	}
 
 	private void setIfModifiedSinceToNextWeek(MockHttpServletRequest request)
@@ -143,12 +152,15 @@ public class WicketFilterTest extends TestCase
 
 	private static class FilterTestingConfig implements FilterConfig
 	{
-		private Map initParameters = new HashMap();
+		private final Map initParameters = new HashMap();
 
-		public FilterTestingConfig() {
-			initParameters.put(WicketFilter.APP_FACT_PARAM, FilterTestingApplicationFactory.class.getName());
+		public FilterTestingConfig()
+		{
+			initParameters.put(WicketFilter.APP_FACT_PARAM,
+				FilterTestingApplicationFactory.class.getName());
 			initParameters.put(WicketFilter.FILTER_MAPPING_PARAM, "/app/*");
-			initParameters.put(ContextParamWebApplicationFactory.APP_CLASS_PARAM, DummyWebApplication.class.getName());
+			initParameters.put(ContextParamWebApplicationFactory.APP_CLASS_PARAM,
+				DummyWebApplication.class.getName());
 		}
 
 		public String getFilterName()
@@ -163,7 +175,7 @@ public class WicketFilterTest extends TestCase
 
 		public String getInitParameter(String s)
 		{
-			return (String) initParameters.get(s);
+			return (String)initParameters.get(s);
 		}
 
 		public Enumeration getInitParameterNames()
