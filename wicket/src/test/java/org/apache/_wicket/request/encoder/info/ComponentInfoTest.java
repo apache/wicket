d7670c5c9a4b38df573b1f35f054f8a7345a7892
Matej Knopp <knopp@apache.org>
2008-09-18 11:12:43 +0000

    git-svn-id: https://svn.apache.org/repos/asf/wicket/sandbox/knopp/experimental@696632 13f79535-47bb-0310-9956-ffa450edef68

--- wicket/src/test/java/org/apache/_wicket/request/encoder/info/ComponentInfoTest.java
+++ wicket/src/test/java/org/apache/_wicket/request/encoder/info/ComponentInfoTest.java
@@ -14,7 +14,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.apache._wicket.request.encoder;
+package org.apache._wicket.request.encoder.info;
 
 import org.apache._wicket.request.encoder.info.ComponentInfo;
 
