e55ccb52385717a1f04e3cd39cfdea789ef981d2
Igor Vaynberg <ivaynberg@apache.org>
2008-09-26 20:03:50 +0000

    missing class
    git-svn-id: https://svn.apache.org/repos/asf/wicket/trunk@699473 13f79535-47bb-0310-9956-ffa450edef68

---
+++ wicket/src/main/java/org/apache/wicket/util/string/UrlUtils.java
@@ -0,0 +1,74 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.wicket.util.string;
+
+import org.apache.wicket.Request;
+
+/**
+ * Various url utilities
+ * 
+ * @author igor.vaynberg
+ */
+public class UrlUtils
+{
+	/**
+	 * Constructor
+	 */
+	private UrlUtils()
+	{
+
+	}
+
+	/**
+	 * Checks if the url is relative or absolute
+	 * 
+	 * @param url
+	 * @return <code>true</code> if url is relative, <code>false</code> otherwise
+	 */
+	public static boolean isRelative(String url)
+	{
+		if ((url != null) && (url.startsWith("/") == false) && (url.indexOf("://") < 0) &&
+			!(url.startsWith("#")))
+		{
+			return true;
+		}
+		else
+		{
+			return false;
+		}
+	}
+
+	/**
+	 * Rewrites a relative url to be context relative, leaves absolute urls same.
+	 * 
+	 * @param url
+	 * @param request
+	 * @return rewritten url
+	 */
+	public static String rewriteToContextRelative(String url, Request request)
+	{
+		if (isRelative(url))
+		{
+			final String prefix = request.getRelativePathPrefixToContextRoot();
+			return prefix + url;
+		}
+		else
+		{
+			return url;
+		}
+	}
+}
