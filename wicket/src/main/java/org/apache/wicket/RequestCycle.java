5627a1f334c42e18e26c49f9c2d48c0d9a447996
Johan Compagner <jcompagner@apache.org>
2008-10-05 19:35:23 +0000

    dont alter the pageparameters of a Page instance WICKET-1740
    git-svn-id: https://svn.apache.org/repos/asf/wicket/trunk@701870 13f79535-47bb-0310-9956-ffa450edef68

--- wicket/src/main/java/org/apache/wicket/RequestCycle.java
+++ wicket/src/main/java/org/apache/wicket/RequestCycle.java
@@ -850,6 +850,10 @@ public abstract class RequestCycle
 			{
 				pageParameters = new PageParameters();
 			}
+			else
+			{
+				pageParameters = (PageParameters)pageParameters.clone();
+			}
 
 			if (params != null)
 			{
