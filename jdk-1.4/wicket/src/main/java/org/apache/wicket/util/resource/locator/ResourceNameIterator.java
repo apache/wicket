752712edf5398a74c25a0e1f19c09f71e8cc9621
Igor Vaynberg <ivaynberg@apache.org>
2008-10-13 22:22:06 +0000

    reapplying WICKET-1868 to wicket-1.3.x since it doesnt seem to break this branch
    git-svn-id: https://svn.apache.org/repos/asf/wicket/branches/wicket-1.3.x@704273 13f79535-47bb-0310-9956-ffa450edef68

--- jdk-1.4/wicket/src/main/java/org/apache/wicket/util/resource/locator/ResourceNameIterator.java
+++ jdk-1.4/wicket/src/main/java/org/apache/wicket/util/resource/locator/ResourceNameIterator.java
@@ -16,8 +16,12 @@
  */
 package org.apache.wicket.util.resource.locator;
 
+import java.util.Arrays;
+import java.util.HashSet;
 import java.util.Iterator;
 import java.util.Locale;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import org.apache.wicket.WicketRuntimeException;
 import org.apache.wicket.util.string.Strings;
@@ -50,6 +54,8 @@ import org.apache.wicket.util.string.Strings;
  */
 public class ResourceNameIterator implements Iterator
 {
+	private static final Pattern LOCALE_PATTERN = Pattern.compile("_[a-zA-Z]{2}($|(?=_))");
+
 	// The locale to search for the resource file
 	private final Locale locale;
 
@@ -64,6 +70,10 @@ public class ResourceNameIterator implements Iterator
 	// The latest exact Locale used
 	private Locale currentLocale;
 
+	private final HashSet isoCountries = new HashSet(Arrays.asList(Locale.getISOCountries()));
+
+	private final HashSet isoLanguages = new HashSet(Arrays.asList(Locale.getISOLanguages()));
+
 	/**
 	 * Construct.
 	 * 
@@ -77,7 +87,7 @@ public class ResourceNameIterator implements Iterator
 	 *            the filname's extensions (comma separated)
 	 */
 	public ResourceNameIterator(String path, final String style, final Locale locale,
-			final String extensions)
+		final String extensions)
 	{
 		this.locale = locale;
 		if (extensions == null)
@@ -90,7 +100,63 @@ public class ResourceNameIterator implements Iterator
 			this.extensions = extensions;
 		}
 
-		this.styleIterator = new StyleAndVariationResourceNameIterator(path, style, null);
+		Matcher matcher = LOCALE_PATTERN.matcher(path);
+		if (matcher.find())
+		{
+			String language = null;
+			String country = null;
+			String variant = null;
+			int firstValidLocalePatternFragment = -1;
+			do
+			{
+				String s = matcher.group().substring(1, 3);
+				if (Character.isLowerCase(s.charAt(0)))
+				{
+					if (isoLanguages.contains(s))
+					{
+						language = s;
+						firstValidLocalePatternFragment = matcher.start();
+						break;
+					}
+				}
+			}
+			while (matcher.find());
+
+			// did we find a language?
+			if (language != null)
+			{
+				// check for country
+				if (matcher.find())
+				{
+					do
+					{
+						String s = matcher.group().substring(1, 3);
+						if (Character.isUpperCase(s.charAt(0)))
+						{
+							if (isoCountries.contains(s))
+							{
+								country = s;
+								break;
+							}
+						}
+					}
+					while (matcher.find());
+				}
+				if (country != null)
+				{
+					// country found... just get the rest of the string for any variant
+					if (matcher.find())
+					{
+						variant = path.substring(matcher.start());
+					}
+				}
+				path = path.substring(0, firstValidLocalePatternFragment);
+				localeIterator = new LocaleResourceNameIterator(path, new Locale(language,
+					country != null ? country : "", variant != null ? variant : ""));
+			} // else skip the whole thing... probably user specific underscores used
+		}
+
+		styleIterator = new StyleAndVariationResourceNameIterator(path, style, null);
 	}
 
 	/**
@@ -100,7 +166,7 @@ public class ResourceNameIterator implements Iterator
 	 */
 	public final Locale getLocale()
 	{
-		return this.currentLocale;
+		return currentLocale;
 	}
 
 	/**
@@ -109,9 +175,9 @@ public class ResourceNameIterator implements Iterator
 	public boolean hasNext()
 	{
 		// Most inner loop. Loop through all extensions provided
-		if (this.extenstionsIterator != null)
+		if (extenstionsIterator != null)
 		{
-			if (this.extenstionsIterator.hasNext() == true)
+			if (extenstionsIterator.hasNext() == true)
 			{
 				return true;
 			}
@@ -123,38 +189,36 @@ public class ResourceNameIterator implements Iterator
 		}
 
 		// 2nd inner loop: Loop through all Locale combinations
-		if (this.localeIterator != null)
+		if (localeIterator != null)
 		{
-			while (this.localeIterator.hasNext())
+			while (localeIterator.hasNext())
 			{
 				// Get the next Locale from the iterator and start the next
 				// inner iterator over again.
-				String newPath = (String)this.localeIterator.next();
-				this.currentLocale = this.localeIterator.getLocale();
-				this.extenstionsIterator = new ExtensionResourceNameIterator(newPath,
-						this.extensions);
-				if (this.extenstionsIterator.hasNext() == true)
+				String newPath = (String)localeIterator.next();
+				currentLocale = localeIterator.getLocale();
+				extenstionsIterator = new ExtensionResourceNameIterator(newPath, extensions);
+				if (extenstionsIterator.hasNext() == true)
 				{
 					return true;
 				}
 			}
-			this.localeIterator = null;
+			localeIterator = null;
 		}
 
 		// Most outer loop: Loop through all combinations of styles and
 		// variations
-		while (this.styleIterator.hasNext())
+		while (styleIterator.hasNext())
 		{
-			String newPath = (String)this.styleIterator.next();
+			String newPath = (String)styleIterator.next();
 
-			this.localeIterator = new LocaleResourceNameIterator(newPath, this.locale);
-			while (this.localeIterator.hasNext())
+			localeIterator = new LocaleResourceNameIterator(newPath, locale);
+			while (localeIterator.hasNext())
 			{
-				newPath = (String)this.localeIterator.next();
-				this.currentLocale = this.localeIterator.getLocale();
-				this.extenstionsIterator = new ExtensionResourceNameIterator(newPath,
-						this.extensions);
-				if (this.extenstionsIterator.hasNext() == true)
+				newPath = (String)localeIterator.next();
+				currentLocale = localeIterator.getLocale();
+				extenstionsIterator = new ExtensionResourceNameIterator(newPath, extensions);
+				if (extenstionsIterator.hasNext() == true)
 				{
 					return true;
 				}
@@ -175,7 +239,7 @@ public class ResourceNameIterator implements Iterator
 			return extenstionsIterator.next();
 		}
 		throw new WicketRuntimeException(
-				"Illegal call of next(). Iterator not properly initialized");
+			"Illegal call of next(). Iterator not properly initialized");
 	}
 
 	/**
