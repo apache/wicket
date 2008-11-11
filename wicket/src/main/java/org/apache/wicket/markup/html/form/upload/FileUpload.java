59b1b3a29959d17629b0e0caed6e0e5ec44e49a2
Igor Vaynberg <ivaynberg@apache.org>
2008-11-11 23:09:58 +0000

    WICKET-1930
    git-svn-id: https://svn.apache.org/repos/asf/wicket/trunk@713218 13f79535-47bb-0310-9956-ffa450edef68

--- wicket/src/main/java/org/apache/wicket/markup/html/form/upload/FileUpload.java
+++ wicket/src/main/java/org/apache/wicket/markup/html/form/upload/FileUpload.java
@@ -186,7 +186,8 @@ public class FileUpload implements IClusterable
 	 */
 	public final File writeToTempFile() throws IOException
 	{
-		File temp = File.createTempFile(Session.get().getId(), item.getFieldName());
+		File temp = File.createTempFile(Session.get().getId(),
+			Files.cleanupFilename(item.getFieldName()));
 		writeTo(temp);
 		return temp;
 	}
