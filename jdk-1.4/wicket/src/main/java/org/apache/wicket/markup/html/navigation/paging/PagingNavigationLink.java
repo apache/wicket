22896d37867f774ffd8cbf3935157ed6ec94946a
Igor Vaynberg <ivaynberg@apache.org>
2008-11-27 18:49:56 +0000

    backport refactor of page number culling into an overridable method
    git-svn-id: https://svn.apache.org/repos/asf/wicket/branches/wicket-1.3.x@721259 13f79535-47bb-0310-9956-ffa450edef68

--- jdk-1.4/wicket/src/main/java/org/apache/wicket/markup/html/navigation/paging/PagingNavigationLink.java
+++ jdk-1.4/wicket/src/main/java/org/apache/wicket/markup/html/navigation/paging/PagingNavigationLink.java
@@ -70,6 +70,18 @@ public class PagingNavigationLink extends Link
 	 */
 	public final int getPageNumber()
 	{
+		return cullPageNumber(pageNumber);
+	}
+
+	/**
+	 * Allows the link to cull the page number to the valid range before it is retrieved from the
+	 * link
+	 * 
+	 * @param pageNumber
+	 * @return culled page number
+	 */
+	protected int cullPageNumber(int pageNumber)
+	{
 		int idx = pageNumber;
 		if (idx < 0)
 		{
