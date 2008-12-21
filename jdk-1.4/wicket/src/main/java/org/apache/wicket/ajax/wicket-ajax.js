d28c5a25cecfe4d6c7e2550a46eb63bc793ee7dc
Johan Compagner <jcompagner@apache.org>
2008-12-21 18:22:58 +0000

    remove the prefix/suffix from the js text to eval
    git-svn-id: https://svn.apache.org/repos/asf/wicket/branches/wicket-1.3.x@728467 13f79535-47bb-0310-9956-ffa450edef68

--- jdk-1.4/wicket/src/main/java/org/apache/wicket/ajax/wicket-ajax.js
+++ jdk-1.4/wicket/src/main/java/org/apache/wicket/ajax/wicket-ajax.js
@@ -1439,6 +1439,9 @@ Wicket.Head.Contributor.prototype = {
 			} else {
 				// serialize the element content to string
 				var text = Wicket.DOM.serializeNodeChildren(node);
+				// get rid of prefix and suffix, they are not eval-d correctly
+				text = text.replace(/^<!--\/\*--><!\[CDATA\[\/\*><!--\*\//, "");
+				text = text.replace(/\/\*-->]]>\*\/$/, "");
 				
 				var id = node.getAttribute("id");
 				
