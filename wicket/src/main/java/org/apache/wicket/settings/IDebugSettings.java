5bc31596eafba17c33ed801b7bfc1341c1862262
Juegen Donnerstag <jdonnerstag@apache.org>
2009-01-17 13:41:20 +0000

    Updated javadoc (wicket-2025)
    git-svn-id: https://svn.apache.org/repos/asf/wicket/trunk@735290 13f79535-47bb-0310-9956-ffa450edef68

--- wicket/src/main/java/org/apache/wicket/settings/IDebugSettings.java
+++ wicket/src/main/java/org/apache/wicket/settings/IDebugSettings.java
@@ -19,13 +19,12 @@ package org.apache.wicket.settings;
 /**
  * Settings interface for various debug settings
  * <p>
- * <i>componentUseCheck </i> (defaults to true) - Causes the framework to do a check after rendering
- * each page to ensure that each component was used in rendering the markup. If components are found
- * that are not referenced in the markup, an appropriate error will be displayed
- * <i>serializeSessionAttributes</i> (defaults to true in development mode) - Causes the framework
- * to serialize any attribute put into session - this helps find Not Serializable errors early
- * <p>
- * 
+ * <i>componentUseCheck </i> (defaults to true in development mode) - causes the framework to do a
+ * check after rendering each page to ensure that each component was used in rendering the markup.
+ * If components are found that are not referenced in the markup, an appropriate error will be
+ * displayed <i>serializeSessionAttributes</i> (defaults to true in development mode) - Causes the
+ * framework to serialize any attribute put into session - this helps find Not Serializable errors
+ * early
  * 
  * @author Igor Vaynberg (ivaynberg)
  */
