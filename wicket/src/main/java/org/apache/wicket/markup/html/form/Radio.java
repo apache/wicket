f830467e3b8e615e1335a77f94c46676773bce33
Igor Vaynberg <ivaynberg@apache.org>
2009-04-07 17:17:05 +0000

    fix markup id from going crazy on repeating renders
    git-svn-id: https://svn.apache.org/repos/asf/wicket/trunk@762860 13f79535-47bb-0310-9956-ffa450edef68

--- wicket/src/main/java/org/apache/wicket/markup/html/form/Radio.java
+++ wicket/src/main/java/org/apache/wicket/markup/html/form/Radio.java
@@ -128,7 +128,11 @@ public class Radio<T> extends LabeledWebMarkupContainer
 	{
 		// prefix markup id of this radio with its group's id
 		// this will make it easier to identify all radios that belong to a specific group
-		setMarkupId(getGroup().getMarkupId() + "-" + getMarkupId());
+		final String prefix = getGroup().getMarkupId() + "-";
+		if (!getMarkupId().startsWith(prefix))
+		{
+			setMarkupId(prefix + getMarkupId());
+		}
 		super.onBeforeRender();
 	}
 
