d78ba4c4889e785a72396dc3ebb4cbdf6d5ed9b7
Jeremy Ryan Thomerson <jrthomerson@apache.org>
2009-04-13 23:27:38 +0000

    remove the InspectorBug from examples since it is now part of DebugBar and it causes an exception to be thrown if dev utils are not enabled.
    git-svn-id: https://svn.apache.org/repos/asf/wicket/trunk@764647 13f79535-47bb-0310-9956-ffa450edef68

--- wicket-examples/src/main/java/org/apache/wicket/examples/WicketExampleHeader.java
+++ wicket-examples/src/main/java/org/apache/wicket/examples/WicketExampleHeader.java
@@ -19,7 +19,6 @@ package org.apache.wicket.examples;
 import org.apache.wicket.PageMap;
 import org.apache.wicket.RequestContext;
 import org.apache.wicket.devutils.debugbar.DebugBar;
-import org.apache.wicket.devutils.inspector.InspectorBug;
 import org.apache.wicket.examples.source.SourcesPage;
 import org.apache.wicket.markup.html.WebMarkupContainer;
 import org.apache.wicket.markup.html.WebPage;
@@ -60,7 +59,6 @@ public final class WicketExampleHeader extends Panel
 			}
 		};
 		add(hideInPortlet);
-		hideInPortlet.add(new InspectorBug("inspector", page));
 		hideInPortlet.add(new DebugBar("debug"));
 		add(new Label("exampleTitle", exampleTitle));
 		BookmarkablePageLink<Void> link = new BookmarkablePageLink<Void>("sources",
