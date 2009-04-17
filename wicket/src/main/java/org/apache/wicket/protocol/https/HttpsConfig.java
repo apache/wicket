904395f1c89465ea51e853f328dadc12c5aabbfb
Igor Vaynberg <ivaynberg@apache.org>
2009-04-17 17:37:36 +0000

    https/http switching support, WICKET-2229 Issue: WICKET-2229
    git-svn-id: https://svn.apache.org/repos/asf/wicket/trunk@766095 13f79535-47bb-0310-9956-ffa450edef68

---
+++ wicket/src/main/java/org/apache/wicket/protocol/https/HttpsConfig.java
@@ -0,0 +1,87 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.wicket.protocol.https;
+
+/**
+ * Configuration for http-https switching
+ * 
+ * @see HttpsRequestCycleProcessor
+ */
+public class HttpsConfig
+{
+	private int httpPort = 80;
+	private int httpsPort = 443;
+
+	/**
+	 * Constructor
+	 */
+	public HttpsConfig()
+	{
+
+	}
+
+	/**
+	 * Constructor
+	 * 
+	 * @param httpPort
+	 *            http port
+	 * @param httpsPort
+	 *            https port
+	 */
+	public HttpsConfig(int httpPort, int httpsPort)
+	{
+		this.httpPort = httpPort;
+		this.httpsPort = httpsPort;
+	}
+
+
+	/**
+	 * Sets http port
+	 * 
+	 * @param httpPort
+	 */
+	public void setHttpPort(int httpPort)
+	{
+		this.httpPort = httpPort;
+	}
+
+	/**
+	 * Sets https port
+	 * 
+	 * @param httpsPort
+	 */
+	public void setHttpsPort(int httpsPort)
+	{
+		this.httpsPort = httpsPort;
+	}
+
+	/**
+	 * @return http port
+	 */
+	public int getHttpPort()
+	{
+		return httpPort;
+	}
+
+	/**
+	 * @return https port
+	 */
+	public int getHttpsPort()
+	{
+		return httpsPort;
+	}
+}
