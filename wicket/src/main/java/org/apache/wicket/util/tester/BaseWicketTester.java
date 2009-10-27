5290203b8fc726bb930ac2412979dca71f7c4320
Igor Vaynberg <ivaynberg@apache.org>
2009-10-27 22:17:45 +0000

    WICKET-2545 Issue: WICKET-2545
    git-svn-id: https://svn.apache.org/repos/asf/wicket/branches/wicket-1.4.x@830369 13f79535-47bb-0310-9956-ffa450edef68

--- wicket/src/main/java/org/apache/wicket/util/tester/BaseWicketTester.java
+++ wicket/src/main/java/org/apache/wicket/util/tester/BaseWicketTester.java
@@ -571,7 +571,7 @@ public class BaseWicketTester extends MockWebApplication
 		Component component = getComponentFromLastRenderedPage(path);
 		if (component == null)
 		{
-			Result.fail("Component not found: " + path);
+			return Result.fail("Component not found: " + path);
 		}
 		return isTrue("component '" + Classes.simpleName(component.getClass()) + "' is not type:" +
 			Classes.simpleName(expectedComponentClass),
