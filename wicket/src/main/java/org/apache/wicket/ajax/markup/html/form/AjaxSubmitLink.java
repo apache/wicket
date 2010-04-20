faf0b0169158d6a32b6b269fd21a877b03d0dbaf
Jeremy Ryan Thomerson <jrthomerson@apache.org>
2010-04-20 20:38:47 +0000

    fixes WICKET-2837
    git-svn-id: https://svn.apache.org/repos/asf/wicket/branches/wicket-1.4.x@936053 13f79535-47bb-0310-9956-ffa450edef68

--- wicket/src/main/java/org/apache/wicket/ajax/markup/html/form/AjaxSubmitLink.java
+++ wicket/src/main/java/org/apache/wicket/ajax/markup/html/form/AjaxSubmitLink.java
@@ -28,34 +28,6 @@ import org.apache.wicket.util.string.AppendingStringBuffer;
  * A link that submits a form via ajax. Since this link takes the form as a constructor argument it
  * does not need to be inside form's component hierarchy.
  * 
- * <p/>
- * It works by splitting the javascript/Ajax calls from the normal non-ajax requests by generating:
- * 
- * <pre>
- * &lt;a href=&quot;normal action url&quot; onclick=&quot;ajax javascript script; return
- * false;&quot;&gt;link&lt;/a&gt;
- * </pre>
- * 
- * If/when javascript is turned off in the browser, or it doesn't support javascript, then the
- * browser will not respond to the onclick event, using the href directly. Wicket will then use a
- * normal request target, and call the serverside onClick with a null {@link AjaxRequestTarget}.
- * 
- * If javascript is enabled, Wicket will send an ajax request, and process it serverside with an
- * {@link AjaxRequestTarget} that is supplied to the server-side onClick method. The "return false"
- * in the &lt;a href&gt; onclick handler ensures the browser doesn't perform the normal request too.
- * 
- * The latter is nicely illustrated with this:
- * 
- * <pre>
- * &lt;a href=&quot;javascript:alert('href event handler');&quot;
- * onclick=&quot;alert('onclick event handler');&quot;&gt;clicking me gives two
- * alerts&lt;/a&gt;
- * 
- * &lt;a href=&quot;javascript:alert('href event handler');&quot;
- * onclick=&quot;alert('onclick event handler');return false;&quot;&gt;clicking me
- * gives only one alert&lt;/a&gt;
- * </pre>
- * 
  * @since 1.2
  * 
  * @author Igor Vaynberg (ivaynberg)
