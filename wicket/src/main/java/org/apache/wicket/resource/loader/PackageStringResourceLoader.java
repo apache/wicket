4a7abd6909087771c08cbe304f8663a0721e9664
Igor Vaynberg <ivaynberg@apache.org>
2010-05-18 21:49:50 +0000

    WICKET-2856: PackageStringResourceLoader.loadStringResource() causes NullPointerException when used in a Class within the root package (i.e. it has no package declaration) Issue: WICKET-2856
    git-svn-id: https://svn.apache.org/repos/asf/wicket/branches/wicket-1.4.x@945909 13f79535-47bb-0310-9956-ffa450edef68

--- wicket/src/main/java/org/apache/wicket/resource/loader/PackageStringResourceLoader.java
+++ wicket/src/main/java/org/apache/wicket/resource/loader/PackageStringResourceLoader.java
@@ -78,13 +78,18 @@ public class PackageStringResourceLoader extends ComponentStringResourceLoader
 
 		while (true)
 		{
-			String packageName = clazz.getPackage().getName();
+			Package pkg = clazz.getPackage();
+			String packageName = (pkg == null) ? "" : pkg.getName();
 			packageName = packageName.replace('.', '/');
 
-			while (packageName.length() > 0)
+			do
 			{
 				// Create the base path
-				String path = packageName + "/" + filename;
+				String path = filename;
+				if (packageName.length() > 0)
+				{
+					path = packageName + "/" + path;
+				}
 
 				// Iterator over all the combinations
 				ResourceNameIterator iter = new ResourceNameIterator(path, style, locale, null);
@@ -112,6 +117,7 @@ public class PackageStringResourceLoader extends ComponentStringResourceLoader
 				// Didn't find the key yet, continue searching if possible
 				packageName = Strings.beforeLast(packageName, '/');
 			}
+			while (packageName.length() > 0);
 
 			// Move to the next superclass
 			clazz = clazz.getSuperclass();
