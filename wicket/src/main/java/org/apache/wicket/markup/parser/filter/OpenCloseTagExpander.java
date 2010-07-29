3ae5b8688c88c05b8e0990db1b55d07b0ddfa5dc
Igor Vaynberg <ivaynberg@apache.org>
2010-07-29 15:31:07 +0000

    tweak to tag expander
    git-svn-id: https://svn.apache.org/repos/asf/wicket/trunk@980473 13f79535-47bb-0310-9956-ffa450edef68

--- wicket/src/main/java/org/apache/wicket/markup/parser/filter/OpenCloseTagExpander.java
+++ wicket/src/main/java/org/apache/wicket/markup/parser/filter/OpenCloseTagExpander.java
@@ -41,7 +41,11 @@ public class OpenCloseTagExpander extends AbstractMarkupFilter
 			"sub", "sup", "abbr", "acronym", "cite", "code", "del", "dfn", "em", "ins", "kbd",
 			"samp", "var", "label", "textarea", "tr", "td", "th", "caption", "thead", "tbody",
 			"tfoot", "dl", "dt", "dd", "li", "ol", "ul", "h1", "h2", "h3", "h4", "h5", "h6", "pre",
-			"title", "div" });
+			"title", "div"
+
+			// tags from pre 1.5 days, shouldnt really be here but make this release more backwards
+			// compatible
+			, "span", "p", "strong", "b", "e", "select" });
 
 	private ComponentTag next = null;
 
