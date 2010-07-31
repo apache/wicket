df11c279e1cc57bbbeae44fcb976c6791bd49b99
Juegen Donnerstag <jdonnerstag@apache.org>
2010-07-31 09:28:38 +0000

    update to the test
    git-svn-id: https://svn.apache.org/repos/asf/wicket/trunk@981024 13f79535-47bb-0310-9956-ffa450edef68

--- wicket-util/src/test/java/org/apache/wicket/util/string/StringsTest.java
+++ wicket-util/src/test/java/org/apache/wicket/util/string/StringsTest.java
@@ -17,11 +17,11 @@ public class StringsTest extends TestCase
 		assertEquals(url, Strings.stripJSessionId(url));
 		assertEquals(url + "/", Strings.stripJSessionId(url + "/"));
 		assertEquals(url + "?param", Strings.stripJSessionId(url + "?param"));
-		assertEquals(url + "?param=a:b", Strings.stripJSessionId(url + "?param=a:b"));
+		assertEquals(url + "?param=a;b", Strings.stripJSessionId(url + "?param=a;b"));
 		assertEquals(url + "/?param", Strings.stripJSessionId(url + "/?param"));
 		assertEquals(url, Strings.stripJSessionId(url + ";jsessionid=12345"));
 		assertEquals(url + "?param", Strings.stripJSessionId(url + ";jsessionid=12345?param"));
-		assertEquals(url + "?param=a:b", Strings.stripJSessionId(url +
-			";jsessionid=12345?param=a:b"));
+		assertEquals(url + "?param=a;b", Strings.stripJSessionId(url +
+			";jsessionid=12345?param=a;b"));
 	}
 }
