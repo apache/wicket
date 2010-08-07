38132b72d823badde05be16be628c298356241c6
Igor Vaynberg <ivaynberg@apache.org>
2010-08-07 00:50:58 +0000

    WICKET-2969
    git-svn-id: https://svn.apache.org/repos/asf/wicket/trunk@983156 13f79535-47bb-0310-9956-ffa450edef68

--- wicket/src/test/java/org/apache/wicket/ComponentInitializationTest.java
+++ wicket/src/test/java/org/apache/wicket/ComponentInitializationTest.java
@@ -117,7 +117,7 @@ public class ComponentInitializationTest extends WicketTestCase
 		boolean illegalState = false;
 		try
 		{
-			page.add(new PossibleDevelopedComponent("addedComponent"));
+			page.add(new InvalidComponent("addedComponent"));
 		}
 		catch (IllegalStateException e)
 		{
@@ -182,29 +182,19 @@ public class ComponentInitializationTest extends WicketTestCase
 
 	}
 
-	private static class PossibleDevelopedComponent extends WebComponent
+	private static class InvalidComponent extends WebComponent
 	{
 		private final boolean initialized = false;
 
-		public PossibleDevelopedComponent(String id)
+		public InvalidComponent(String id)
 		{
 			super(id);
 		}
 
 		@Override
-		protected void onBeforeRender()
-		{
-			super.onBeforeRender();
-			if (!initialized)
-			{
-				onInitialize();
-			}
-		}
-
-		@Override
 		protected void onInitialize()
 		{
-			// possible already implemented method by some user
+			// missing super call
 		}
 	}
 }
