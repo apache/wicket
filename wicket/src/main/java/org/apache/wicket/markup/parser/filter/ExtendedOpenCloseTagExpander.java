6798beda487af1c2d138d521d753ea1556512755
Igor Vaynberg <ivaynberg@apache.org>
2010-08-25 00:36:16 +0000

    Issue: WICKET-2765
    git-svn-id: https://svn.apache.org/repos/asf/wicket/branches/wicket-1.4.x@988773 13f79535-47bb-0310-9956-ffa450edef68

--- wicket/src/main/java/org/apache/wicket/markup/parser/filter/ExtendedOpenCloseTagExpander.java
+++ wicket/src/main/java/org/apache/wicket/markup/parser/filter/ExtendedOpenCloseTagExpander.java
@@ -55,7 +55,7 @@ public class ExtendedOpenCloseTagExpander extends OpenCloseTagExpander
 			"sub", "sup", "abbr", "acronym", "cite", "code", "del", "dfn", "em", "ins", "kbd",
 			"samp", "var", "label", "textarea", "tr", "td", "th", "caption", "thead", "tbody",
 			"tfoot", "dl", "dt", "dd", "li", "ol", "ul", "h1", "h2", "h3", "h4", "h5", "h6", "pre",
-			"title" });
+			"title", "col" });
 
 	/**
 	 * @see org.apache.wicket.markup.parser.filter.OpenCloseTagExpander#contains(java.lang.String)
