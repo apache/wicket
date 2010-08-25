46c271484ab665278786f4692a38a1abe46236b3
Igor Vaynberg <ivaynberg@apache.org>
2010-08-25 19:21:06 +0000

    fixed minor bug
    git-svn-id: https://svn.apache.org/repos/asf/wicket/trunk@989311 13f79535-47bb-0310-9956-ffa450edef68

--- wicket/src/main/java/org/apache/wicket/markup/html/form/Form.java
+++ wicket/src/main/java/org/apache/wicket/markup/html/form/Form.java
@@ -1633,7 +1633,8 @@ public class Form<T> extends WebMarkupContainer implements IFormSubmitListener, 
 			if (defaultSubmittingComponent instanceof Component)
 			{
 				final Component submittingComponent = (Component)defaultSubmittingComponent;
-				if (submittingComponent.isVisibleInHierarchy() && submittingComponent.isEnabled())
+				if (submittingComponent.isVisibleInHierarchy() &&
+					submittingComponent.isEnabledInHierarchy())
 				{
 					appendDefaultButtonField(markupStream, openTag);
 				}
