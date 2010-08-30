56dceac350b8d4c37f9b5ae245d684d74b0b7bfc
Igor Vaynberg <ivaynberg@apache.org>
2010-08-30 23:51:44 +0000

    WICKET-2850
    git-svn-id: https://svn.apache.org/repos/asf/wicket/branches/wicket-1.4.x@991034 13f79535-47bb-0310-9956-ffa450edef68

--- wicket-datetime/src/main/java/org/apache/wicket/extensions/yui/calendar/DatePicker.java
+++ wicket-datetime/src/main/java/org/apache/wicket/extensions/yui/calendar/DatePicker.java
@@ -329,10 +329,13 @@ public class DatePicker extends AbstractBehavior implements IHeaderContributor
 		// ajax requests to not render the yui calendar multiple times
 		if (AjaxRequestTarget.get() != null)
 		{
-			final String javascript = "var e = Wicket.$('" + getEscapedComponentMarkupId() + "Dp" +
+			final String escapedComponentMarkupId = getEscapedComponentMarkupId();
+			final String javascript = "var e = Wicket.$('" + escapedComponentMarkupId + "Dp" +
 					"'); if (e != null && typeof(e.parentNode) != 'undefined' && " +
 					"typeof(e.parentNode.parentNode != 'undefined')) " +
-					"e.parentNode.parentNode.removeChild(e.parentNode);";
+					"e.parentNode.parentNode.removeChild(e.parentNode);" + "YAHOO.wicket." +
+					escapedComponentMarkupId + "DpJs.destroy(); delete YAHOO.wicket." +
+					escapedComponentMarkupId + "DpJs;";
 
 			response.renderJavascript(javascript, null);
 		}
