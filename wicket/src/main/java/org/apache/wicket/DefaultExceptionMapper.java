3bdeda42c05c49eb6f7012484e25c07efe8e8d8a
Peter Ertl <pete@apache.org>
2010-09-20 20:19:35 +0000

    don't make things overcomplicated / bloated: in case of regular requests, error pages are rendered under the current url. in case of ajax, a redirect to the error page url will happen. that's it!
    git-svn-id: https://svn.apache.org/repos/asf/wicket/trunk@999100 13f79535-47bb-0310-9956-ffa450edef68

--- wicket/src/main/java/org/apache/wicket/DefaultExceptionMapper.java
+++ wicket/src/main/java/org/apache/wicket/DefaultExceptionMapper.java
@@ -43,34 +43,6 @@ public class DefaultExceptionMapper implements IExceptionMapper
 {
 	private static final Logger logger = LoggerFactory.getLogger(DefaultExceptionMapper.class);
 
-	// default policy is to not change the URL in the address bar of the browser:
-	// - the url syntax eventually gives the user some indication of the error
-	// - the user can hit refresh in the browser to retry loading the page
-	private RenderPageRequestHandler.RedirectPolicy redirectPolicy = RenderPageRequestHandler.RedirectPolicy.NEVER_REDIRECT;
-
-	/**
-	 * get the redirect policy in case of error (controls if the URL changes in case of displaying
-	 * an error)
-	 * 
-	 * @return redirect policy
-	 */
-	public RenderPageRequestHandler.RedirectPolicy getRedirectPolicy()
-	{
-		return redirectPolicy;
-	}
-
-	/**
-	 * set the redirect policy in case of error (you can control if the URL changes in case of
-	 * displaying an error)
-	 * 
-	 * @param redirectPolicy
-	 *            redirection policy
-	 */
-	public void setRedirectPolicy(RenderPageRequestHandler.RedirectPolicy redirectPolicy)
-	{
-		this.redirectPolicy = redirectPolicy;
-	}
-
 	public IRequestHandler map(Exception e)
 	{
 		try
@@ -80,11 +52,9 @@ public class DefaultExceptionMapper implements IExceptionMapper
 		catch (RuntimeException e2)
 		{
 			// hmmm, we were already handling an exception! give up
-			logger.error("unexpected exception when handling another exception: " + e.getMessage(),
-				e);
+			logger.error("unexpected exception when handling another exception: " + e.getMessage(), e);
 			return new ErrorCodeResponseHandler(500);
 		}
-
 	}
 
 	private IRequestHandler internalMap(Exception e)
@@ -141,7 +111,7 @@ public class DefaultExceptionMapper implements IExceptionMapper
 		if (requestCycle == null)
 			throw new IllegalStateException("there is no current request cycle attached to this thread");
 
-		RenderPageRequestHandler.RedirectPolicy redirect = redirectPolicy;
+		RenderPageRequestHandler.RedirectPolicy redirect = RenderPageRequestHandler.RedirectPolicy.NEVER_REDIRECT;
 
 		// in case of ajax we must redirect to show the error page
 		if (requestCycle.getRequest() instanceof WebRequest)
