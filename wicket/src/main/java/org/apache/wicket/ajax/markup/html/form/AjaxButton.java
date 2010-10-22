908c3e373723203ed5dfc7262390b3245644119a
Jeremy Ryan Thomerson <jrthomerson@apache.org>
2010-10-22 06:10:31 +0000

    fix lying javadoc on AjaxButton
    git-svn-id: https://svn.apache.org/repos/asf/wicket/branches/wicket-1.4.x@1026220 13f79535-47bb-0310-9956-ffa450edef68

--- wicket/src/main/java/org/apache/wicket/ajax/markup/html/form/AjaxButton.java
+++ wicket/src/main/java/org/apache/wicket/ajax/markup/html/form/AjaxButton.java
@@ -26,8 +26,7 @@ import org.apache.wicket.util.string.AppendingStringBuffer;
 import org.apache.wicket.util.string.Strings;
 
 /**
- * A button that submits the form via ajax. Since this button takes the form as a constructor
- * argument it does not need to be added to it unlike the {@link Button} component.
+ * A button that submits the form via ajax.
  * 
  * @since 1.3
  * 
