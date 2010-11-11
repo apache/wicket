b25bf8f5069bcc7322baec60bda806e76268b3de
Jeremy Ryan Thomerson <jrthomerson@apache.org>
2010-11-11 02:10:55 +0000

    add several implementations of IHeaderResponse that can be used with IHeaderResponseDecorator to build up various kinds of decoration for your header response.  Most of these are the core of the functionality, meant to be built on and extended as needed for your use cases
    git-svn-id: https://svn.apache.org/repos/asf/wicket/branches/wicket-1.4.x@1033800 13f79535-47bb-0310-9956-ffa450edef68

---
+++ wicket/src/main/java/org/apache/wicket/resource/filtering/AbstractHeaderResponseFilter.java
@@ -0,0 +1,64 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.wicket.resource.filtering;
+
+import org.apache.wicket.ResourceReference;
+import org.apache.wicket.resource.filtering.HeaderResponseContainerFilteringHeaderResponse.IHeaderResponseFilter;
+
+/**
+ * A default implementation of IHeaderResponseFilter that returns true for everything. It is defined
+ * as abstract because you are not supposed to use it directly, but use it as a base and override
+ * any methods that you need to return something other than true from (whether that's always false
+ * or conditional logic).
+ * 
+ * @author Jeremy Thomerson
+ */
+public abstract class AbstractHeaderResponseFilter implements IHeaderResponseFilter
+{
+
+	private final String name;
+
+	/**
+	 * Create a response filter.
+	 * 
+	 * @param name
+	 */
+	public AbstractHeaderResponseFilter(String name)
+	{
+		this.name = name;
+	}
+
+	public String getName()
+	{
+		return name;
+	}
+
+	public boolean acceptReference(ResourceReference object)
+	{
+		return true;
+	}
+
+	public boolean acceptOtherJavascript()
+	{
+		return true;
+	}
+
+	public boolean acceptOtherCss()
+	{
+		return true;
+	}
+}
