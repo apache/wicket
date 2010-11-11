a665cd6e0684afdb03e962736ac38601fd2fda6e
Martin Tzvetanov Grigorov <mgrigorov@apache.org>
2010-11-11 19:44:27 +0000

    WICKET-3138 Wicket 1.5 and GAE
    Add IDataStore implementation that stores the pages in the HttpSession.
    With two default eviction strategies:
     - PageNumberEvictionStrategy - sets maximum number of pages in the session
     - MemorySizeEvictionStrategy - sets maximum memory size for the pages in the session.
    
    git-svn-id: https://svn.apache.org/repos/asf/wicket/trunk@1034078 13f79535-47bb-0310-9956-ffa450edef68

---
+++ wicket/src/main/java/org/apache/wicket/pageStore/memory/HttpSessionDataStore.java
@@ -0,0 +1,140 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.wicket.pageStore.memory;
+
+import javax.servlet.http.HttpSession;
+
+import org.apache.wicket.page.IPageManagerContext;
+import org.apache.wicket.pageStore.IDataStore;
+
+/**
+ * A {@link DataStore} which stores the pages in the {@link HttpSession}. Uses
+ * {@link DataStoreEvictionStrategy} to keep the memory footprint reasonable.
+ * 
+ * <p>
+ * Usage:
+ * 
+ * <pre>
+ * <!--@formatter:off-->
+ * MyApp#init()
+ * {
+ * 
+ * 	setPageManagerProvider(new IPageManagerProvider() 
+ * 	{
+ * 		public IPageManager get(IPageManagerContext pageManagerContext) 
+ * 		{ 
+ * 			IDataStore dataStore = new HttpSessionDataStore(pageManagerContext, new PageNumberEvictionStrategy(20)); 
+ * 			IPageStore pageStore = new DefaultPageStore(application.getName(), dataStore, getCacheSize()); 
+ * 			return new PersistentPageManager(application.getName(), pageStore, pageManagerContext);
+ * 		}
+ * 	}
+ * }
+ * <!--@formatter:on-->
+ * </pre>
+ */
+public class HttpSessionDataStore implements IDataStore
+{
+
+	/** the session attribute key. auto-prefixed with application.getSessionAttributePrefix() */
+	private static final String PAGE_TABLE_KEY = "page:store:memory";
+
+	private final IPageManagerContext pageManagerContext;
+
+	private final DataStoreEvictionStrategy evictionStrategy;
+
+	/**
+	 * Construct.
+	 * 
+	 * @param pageManagerContext
+	 * @param evictionStrategy
+	 */
+	public HttpSessionDataStore(IPageManagerContext pageManagerContext,
+		DataStoreEvictionStrategy evictionStrategy)
+	{
+		this.pageManagerContext = pageManagerContext;
+		this.evictionStrategy = evictionStrategy;
+	}
+
+	/**
+	 * @param sessionId
+	 *            Ignored. Only pages from the current http session can be read
+	 * @see org.apache.wicket.pageStore.IDataStore#getData(java.lang.String, int)
+	 */
+	public byte[] getData(String sessionId, int pageId)
+	{
+		PageTable pageTable = getPageTable(false);
+		byte[] pageAsBytes = null;
+		if (pageTable != null)
+		{
+			pageAsBytes = pageTable.getPage(pageId);
+		}
+		return pageAsBytes;
+	}
+
+	public void removeData(String sessionId, int pageId)
+	{
+		PageTable pageTable = getPageTable(false);
+		if (pageTable != null)
+		{
+			pageTable.removePage(pageId);
+		}
+	}
+
+	public void removeData(String sessionId)
+	{
+		PageTable pageTable = getPageTable(false);
+		if (pageTable != null)
+		{
+			pageTable.clear();
+		}
+	}
+
+	public void storeData(String sessionId, int pageId, byte[] pageAsBytes)
+	{
+		PageTable pageTable = getPageTable(true);
+		pageTable.storePage(pageId, pageAsBytes);
+
+		evictionStrategy.evict(pageTable);
+	}
+
+	public void destroy()
+	{
+		PageTable pageTable = getPageTable(false);
+		if (pageTable != null)
+		{
+			pageTable.clear();
+		}
+	}
+
+	public boolean isReplicated()
+	{
+		return true;
+	}
+
+	private PageTable getPageTable(boolean create)
+	{
+		PageTable pageTable = (PageTable)pageManagerContext.getSessionAttribute(PAGE_TABLE_KEY);
+		if (pageTable == null && create)
+		{
+			pageTable = new PageTable();
+			pageManagerContext.setSessionAttribute(PAGE_TABLE_KEY, pageTable);
+		}
+
+		return pageTable;
+	}
+
+}
---
+++ wicket/src/main/java/org/apache/wicket/pageStore/memory/PageTable.java
@@ -0,0 +1,123 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.wicket.pageStore.memory;
+
+import java.util.Queue;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentLinkedQueue;
+import java.util.concurrent.ConcurrentMap;
+
+import org.apache.wicket.IClusterable;
+
+/**
+ * A structure that holds page id => pageAsBytes.
+ * 
+ * <p>
+ * Additionally it has an index of the least recently used pages
+ */
+class PageTable implements IClusterable
+{
+	private static final long serialVersionUID = 1L;
+
+
+	/**
+	 * Holds the index of last/least recently used page ids. The most recently used page id is in
+	 * the tail, the least recently used is in the head.
+	 */
+	/*
+	 * Can be replaced later with PriorityQueue to deal with lightweight (Ajax) and heavyweight
+	 * pages
+	 */
+	private final Queue<Integer> index;
+
+	/**
+	 * The actual container for the pages.
+	 * 
+	 * <p>
+	 * page id => page as bytes
+	 */
+	private final ConcurrentMap<Integer, byte[]> pages;
+
+	public PageTable()
+	{
+		pages = new ConcurrentHashMap<Integer, byte[]>();
+		index = new ConcurrentLinkedQueue<Integer>();
+	}
+
+	void storePage(Integer pageId, byte[] pageAsBytes)
+	{
+		synchronized (index)
+		{
+			pages.put(pageId, pageAsBytes);
+
+			updateIndex(pageId);
+		}
+	}
+
+	byte[] getPage(final Integer pageId)
+	{
+		synchronized (index)
+		{
+			updateIndex(pageId);
+
+			return pages.get(pageId);
+		}
+	}
+
+	public byte[] removePage(Integer pageId)
+	{
+		synchronized (index)
+		{
+			index.remove(pageId);
+
+			return pages.remove(pageId);
+		}
+	}
+
+	public void clear()
+	{
+		synchronized (index)
+		{
+			index.clear();
+			pages.clear();
+		}
+	}
+
+	public int size()
+	{
+		return pages.size();
+	}
+
+	Integer getOldest()
+	{
+		return index.peek();
+	}
+
+	/**
+	 * Updates the index of last/least recently used pages by removing the page id from the index
+	 * (in case it is already in) and (re-)adding it at the head
+	 * 
+	 * @param pageId
+	 *            the id of a recently used page
+	 */
+	private void updateIndex(Integer pageId)
+	{
+		index.remove(pageId);
+		index.offer(pageId);
+	}
+
+}
