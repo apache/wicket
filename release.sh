a666e39bbf7714a47c722930a44ca9a0fb91630a
Jeremy Ryan Thomerson <jrthomerson@apache.org>
2010-11-17 03:53:39 +0000

    work on adding mvn license plugin to our build
    git-svn-id: https://svn.apache.org/repos/asf/wicket/branches/wicket-1.4.x@1035916 13f79535-47bb-0310-9956-ffa450edef68

--- release.sh
+++ release.sh
@@ -13,6 +13,7 @@
 #  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
+
 echo "Apache Wicket Release script"
 echo "----------------------------"
 echo "Building a release for Apache Wicket. We will need the passphrase for"
