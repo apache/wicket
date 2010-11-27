e9ce5d7e8ad291a94a9f15bdb143185d224bf790
Juegen Donnerstag <jdonnerstag@apache.org>
2010-11-27 21:17:17 +0000

    license header
    git-svn-id: https://svn.apache.org/repos/asf/wicket/trunk@1039764 13f79535-47bb-0310-9956-ffa450edef68

--- wicket-datetime/src/main/java/org/apache/wicket/extensions/yui/calendar/TimeField.java
+++ wicket-datetime/src/main/java/org/apache/wicket/extensions/yui/calendar/TimeField.java
@@ -13,8 +13,6 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- * 
- * This is a modified version of the wicket-datetime project's DateTimeField.
  */
 package org.apache.wicket.extensions.yui.calendar;
 
