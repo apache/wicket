d5e870804df7f2e945999cd0ad332c4e59008ed2
Martin Tzvetanov Grigorov <mgrigorov@apache.org>
2010-12-01 18:03:29 +0000

    Use in-memory page store for the multi-threaded tests
    git-svn-id: https://svn.apache.org/repos/asf/wicket/trunk@1041111 13f79535-47bb-0310-9956-ffa450edef68

--- testing/wicket-threadtest/src/main/java/org/apache/wicket/threadtest/apps/app2/TestApp2.java
+++ testing/wicket-threadtest/src/main/java/org/apache/wicket/threadtest/apps/app2/TestApp2.java
@@ -17,8 +17,17 @@
 package org.apache.wicket.threadtest.apps.app2;
 
 import org.apache.wicket.Application;
+import org.apache.wicket.DefaultPageManagerProvider;
 import org.apache.wicket.Page;
 import org.apache.wicket.markup.html.image.resource.DefaultButtonImageResource;
+import org.apache.wicket.page.IPageManager;
+import org.apache.wicket.page.IPageManagerContext;
+import org.apache.wicket.page.PersistentPageManager;
+import org.apache.wicket.pageStore.DefaultPageStore;
+import org.apache.wicket.pageStore.IDataStore;
+import org.apache.wicket.pageStore.IPageStore;
+import org.apache.wicket.pageStore.memory.HttpSessionDataStore;
+import org.apache.wicket.pageStore.memory.PageNumberEvictionStrategy;
 import org.apache.wicket.protocol.http.WebApplication;
 
 /**
@@ -49,12 +58,19 @@ public class TestApp2 extends WebApplication
 	protected void init()
 	{
 		getSharedResources().add("cancelButton", new DefaultButtonImageResource("Cancel"));
+
+		setPageManagerProvider(new DefaultPageManagerProvider(this)
+		{
+			@Override
+			public IPageManager get(IPageManagerContext pageManagerContext)
+			{
+				IDataStore dataStore = new HttpSessionDataStore(pageManagerContext,
+					new PageNumberEvictionStrategy(100));
+				IPageStore pageStore = new DefaultPageStore(getName(), dataStore, getCacheSize());
+				return new PersistentPageManager(getName(), pageStore, pageManagerContext);
+
+			}
+		});
 	}
 
-	// TODO ng change the store/pagemanager to a non-disk one
-// @Override
-// protected ISessionStore newSessionStore()
-// {
-// return super.newSessionStore();
-// }
 }
