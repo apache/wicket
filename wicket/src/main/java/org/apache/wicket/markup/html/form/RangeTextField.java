94c6bd9a3a12a4c3e6e1d218d5fe06f55316a39f
Martin Tzvetanov Grigorov <mgrigorov@apache.org>
2010-12-19 18:58:09 +0000

    WICKET-3241 Add support for the new HTML 5 input types
    Add an component for <input type="range"/> which is just the same as 'number'.
    
    git-svn-id: https://svn.apache.org/repos/asf/wicket/trunk@1050931 13f79535-47bb-0310-9956-ffa450edef68

---
+++ wicket/src/main/java/org/apache/wicket/markup/html/form/RangeTextField.java
@@ -0,0 +1,62 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.wicket.markup.html.form;
+
+import org.apache.wicket.model.IModel;
+
+/**
+ * A {@link TextField} for HTML5 &lt;input&gt; with type <em>range</em>.
+ * 
+ * <p>
+ * Automatically validates the input against the configured {@link #setMinimum(Double) min} and
+ * {@link #setMaximum(Double) max} attributes. If any of them is <code>null</code> then
+ * {@link Double#MIN_VALUE} and {@link Double#MAX_VALUE} are used respectfully.
+ */
+public class RangeTextField extends NumberTextField
+{
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * Construct.
+	 * 
+	 * @param id
+	 *            component id
+	 */
+	public RangeTextField(String id)
+	{
+		this(id, null);
+	}
+
+	/**
+	 * Construct.
+	 * 
+	 * @param id
+	 *            see Component
+	 * @param model
+	 *            the model
+	 */
+	public RangeTextField(String id, IModel<Double> model)
+	{
+		super(id, model);
+	}
+
+	@Override
+	protected String getInputType()
+	{
+		return "range";
+	}
+}
