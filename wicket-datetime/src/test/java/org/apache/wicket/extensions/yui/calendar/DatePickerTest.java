ce386f6859be46012d5e6a4aec6c243e8d51e54d
Juegen Donnerstag <jdonnerstag@apache.org>
2010-12-29 09:03:51 +0000

    some additional tests to figure out why it doesn't work on hudson
    git-svn-id: https://svn.apache.org/repos/asf/wicket/trunk@1053572 13f79535-47bb-0310-9956-ffa450edef68

--- wicket-datetime/src/test/java/org/apache/wicket/extensions/yui/calendar/DatePickerTest.java
+++ wicket-datetime/src/test/java/org/apache/wicket/extensions/yui/calendar/DatePickerTest.java
@@ -16,6 +16,8 @@
  */
 package org.apache.wicket.extensions.yui.calendar;
 
+import java.text.DateFormat;
+import java.text.ParseException;
 import java.util.Date;
 import java.util.GregorianCalendar;
 import java.util.Locale;
@@ -23,9 +25,11 @@ import java.util.TimeZone;
 
 import org.apache.wicket.Page;
 import org.apache.wicket.WicketTestCase;
+import org.apache.wicket.extensions.yui.calendar.DateTimeField.AM_PM;
 import org.apache.wicket.protocol.http.request.WebClientInfo;
 import org.apache.wicket.util.tester.DiffUtil;
 import org.apache.wicket.util.tester.FormTester;
+import org.joda.time.DateTimeFieldType;
 import org.joda.time.DateTimeZone;
 import org.joda.time.MutableDateTime;
 import org.slf4j.Logger;
@@ -122,6 +126,143 @@ public class DatePickerTest extends WicketTestCase
 		log.error("orig: " + date + "; date: " + page.date + "; dateTime: " + page.dateTime);
 		assertEquals(0, date.compareTo(page.dateTime));
 		assertEquals(0, date.compareTo(page.date));
+	}
+
+	/**
+	 * 
+	 * @throws ParseException
+	 */
+	public void testDates1() throws ParseException
+	{
+		TimeZone tzClient = TimeZone.getTimeZone("America/Los_Angeles");
+		TimeZone tzServer = TimeZone.getTimeZone("Europe/Berlin");
+
+		TimeZone.setDefault(tzServer);
+		Locale.setDefault(Locale.GERMAN);
+
+		Date orig = convertDate("06.11.2010", null, null, null, false, tzClient);
+
+		MutableDateTime dt = new MutableDateTime(DateTimeZone.forTimeZone(tzClient));
+		dt.setDateTime(2010, 11, 06, 0, 0, 0, 0);
+		Date date = new Date(dt.getMillis());
+
+		log.error("actual: " + orig.getTime() + "; expected: " + date.getTime());
+		log.error("actual: " + orig + "; expected: " + date);
+		assertEquals(date.getTime(), orig.getTime());
+	}
+
+	/**
+	 * 
+	 * @throws ParseException
+	 */
+	public void testDates2() throws ParseException
+	{
+		TimeZone tzClient = TimeZone.getTimeZone("America/Los_Angeles");
+		TimeZone tzServer = TimeZone.getTimeZone("Europe/Berlin");
+
+		TimeZone.setDefault(tzServer);
+		Locale.setDefault(Locale.GERMAN);
+
+		Date orig = convertDate("06.11.2010", 0, 0, AM_PM.AM, false, tzClient);
+
+		MutableDateTime dt = new MutableDateTime(DateTimeZone.forTimeZone(tzClient));
+		dt.setDateTime(2010, 11, 06, 0, 0, 0, 0);
+		Date date = new Date(dt.getMillis());
+
+		log.error("actual: " + orig.getTime() + "; expected: " + date.getTime());
+		log.error("actual: " + orig + "; expected: " + date);
+		assertEquals(date.getTime(), orig.getTime());
+	}
+
+	/**
+	 * 
+	 * @throws ParseException
+	 */
+	public void testDates3() throws ParseException
+	{
+		TimeZone tzClient = TimeZone.getTimeZone("America/Los_Angeles");
+		TimeZone tzServer = TimeZone.getTimeZone("Europe/Berlin");
+
+		TimeZone.setDefault(tzServer);
+		Locale.setDefault(Locale.GERMAN);
+
+		Date orig = convertDate("06.11.2010", 12, 0, null, false, tzClient);
+
+		MutableDateTime dt = new MutableDateTime(DateTimeZone.forTimeZone(tzClient));
+		dt.setDateTime(2010, 11, 06, 12, 0, 0, 0);
+		Date date = new Date(dt.getMillis());
+
+		log.error("actual: " + orig.getTime() + "; expected: " + date.getTime());
+		log.error("actual: " + orig + "; expected: " + date);
+		assertEquals(date.getTime(), orig.getTime());
+	}
+
+	/**
+	 * Simulate what DateTimeField does
+	 * 
+	 * @param dateStr
+	 * @param hours
+	 * @param minutes
+	 * @param amOrPm
+	 * @param use12HourFormat
+	 * @param tzClient
+	 * @return Date
+	 * @throws ParseException
+	 */
+	public Date convertDate(final String dateStr, final Integer hours, final Integer minutes,
+		final AM_PM amOrPm, final boolean use12HourFormat, final TimeZone tzClient)
+		throws ParseException
+	{
+		Date dateFieldInput = (dateStr != null ? DateFormat.getDateInstance().parse(dateStr) : null);
+
+		// Default to today, if date entry was invisible
+		final MutableDateTime date;
+		if (dateFieldInput != null)
+		{
+			date = new MutableDateTime(dateFieldInput);
+		}
+		else
+		{
+			// Current date
+			date = new MutableDateTime();
+		}
+
+		// always set secs to 0
+		date.setSecondOfMinute(0);
+
+		// The AM/PM field
+		if (use12HourFormat)
+		{
+			date.set(DateTimeFieldType.halfdayOfDay(), amOrPm == AM_PM.PM ? 1 : 0);
+		}
+
+		// The hours
+		if (hours == null)
+		{
+			date.setHourOfDay(0);
+		}
+		else
+		{
+			date.set(DateTimeFieldType.hourOfDay(), hours % (use12HourFormat ? 12 : 24));
+		}
+
+		// The minutes
+		if (minutes == null)
+		{
+			date.setMinuteOfHour(0);
+		}
+		else
+		{
+			date.setMinuteOfHour(minutes);
+		}
+
+		// Use the client timezone to properly calculate the millisecs
+		if (tzClient != null)
+		{
+			date.setZoneRetainFields(DateTimeZone.forTimeZone(tzClient));
+		}
+
+		return new Date(date.getMillis());
 	}
 
 	/**
