e9aea54db9e469fd8273e3dac6bd08a924b7c56d
Martijn Dashorst <dashorst@apache.org>
2011-01-17 11:25:02 +0000

    WICKET-3332: new design for quickstart home page
    git-svn-id: https://svn.apache.org/repos/asf/wicket/trunk@1059880 13f79535-47bb-0310-9956-ffa450edef68

--- archetypes/quickstart/src/main/resources/archetype-resources/src/main/java/HomePage.java
+++ archetypes/quickstart/src/main/resources/archetype-resources/src/main/java/HomePage.java
@@ -4,26 +4,11 @@ import org.apache.wicket.request.mapper.parameter.PageParameters;
 import org.apache.wicket.markup.html.basic.Label;
 import org.apache.wicket.markup.html.WebPage;
 
-/**
- * Homepage
- */
 public class HomePage extends WebPage {
-
 	private static final long serialVersionUID = 1L;
 
-	// TODO Add any page properties or variables here
-
-    /**
-	 * Constructor that is invoked when page is invoked without a session.
-	 * 
-	 * @param parameters
-	 *            Page parameters
-	 */
     public HomePage(final PageParameters parameters) {
-
-        // Add the simplest type of label
-        add(new Label("message", "If you see this message wicket is properly configured and running"));
-
+		add(new Label("version", getApplication().getFrameworkSettings().getVersion()));
         // TODO Add your page's components here
     }
 }
