3fa97daf016232ab1c67dd463afa8367db5af92d
Martijn Dashorst <dashorst@apache.org>
2011-01-18 07:06:32 +0000

    Fixes WICKET-3344
    git-svn-id: https://svn.apache.org/repos/asf/wicket/trunk@1060215 13f79535-47bb-0310-9956-ffa450edef68

--- wicket-examples/src/test/java/org/apache/wicket/examples/JettyTestCaseDecorator.java
+++ wicket-examples/src/test/java/org/apache/wicket/examples/JettyTestCaseDecorator.java
@@ -19,10 +19,10 @@ package org.apache.wicket.examples;
 import junit.extensions.TestSetup;
 import junit.framework.Test;
 
-import org.mortbay.jetty.Connector;
-import org.mortbay.jetty.Server;
-import org.mortbay.jetty.nio.SelectChannelConnector;
-import org.mortbay.jetty.webapp.WebAppContext;
+import org.eclipse.jetty.server.Connector;
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.server.nio.SelectChannelConnector;
+import org.eclipse.jetty.webapp.WebAppContext;
 
 /**
  * Test decorator that starts a jetty instance
@@ -73,7 +73,7 @@ public class JettyTestCaseDecorator extends TestSetup
 		{
 			web.setWar(webappLocation);
 		}
-		server.addHandler(web);
+		server.setHandler(web);
 
 		server.start();
 
