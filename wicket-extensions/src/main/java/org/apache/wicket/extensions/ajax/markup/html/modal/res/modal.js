2eaeb7d3bf02b80d4eb1470d7437e65a6a9098ae
Martin Tzvetanov Grigorov <mgrigorov@apache.org>
2011-02-14 18:58:07 +0000

    WICKET-3403 Cannot convert 'this.content' to object in Opera
    First set the onload handler and then update the iframe's location.
    Additionally bind onload to this so it is possible to get a reference to this.content (the iframe).
    
    
    
    git-svn-id: https://svn.apache.org/repos/asf/wicket/branches/wicket-1.4.x@1070605 13f79535-47bb-0310-9956-ffa450edef68

--- wicket-extensions/src/main/java/org/apache/wicket/extensions/ajax/markup/html/modal/res/modal.js
+++ wicket-extensions/src/main/java/org/apache/wicket/extensions/ajax/markup/html/modal/res/modal.js
@@ -617,6 +617,15 @@ Wicket.Window.prototype = {
 	load: function() {
 		if (this.settings.title == null)
 			this.update = window.setInterval(this.updateTitle.bind(this), 100);
+
+		// opera seems to have problem accessing contentWindow here
+		if (Wicket.Browser.isOpera()) {
+			this.content.onload = function() {
+				this.content.contentWindow.name = this.settings.iframeName;
+			}.bind(this);
+		} else {
+			this.content.contentWindow.name = this.settings.iframeName;
+		}
 		
 		try
 		{
@@ -626,15 +635,6 @@ Wicket.Window.prototype = {
 		{
 			this.content.src = this.settings.src;
 		}		
-	
-		// opera seems to have problem accessing contentWindow here
-		if (Wicket.Browser.isOpera()) {
-			this.content.onload = function() {
-				this.content.contentWindow.name = this.settings.iframeName;
-			}
-		} else {
-			this.content.contentWindow.name = this.settings.iframeName;
-		}
 	},
 	
 	/**
