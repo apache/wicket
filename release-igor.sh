68541e535375973cb13264900f92b15fc5c883b6
Igor Vaynberg <ivaynberg@apache.org>
2011-02-21 03:53:47 +0000

    tweaked release script to use mvn:versions
    git-svn-id: https://svn.apache.org/repos/asf/wicket/trunk@1072837 13f79535-47bb-0310-9956-ffa450edef68

--- release-igor.sh
+++ release-igor.sh
@@ -34,6 +34,8 @@ read passphrase
 stty $stty_orig
 
 echo "modifying poms with the new version: $version"
+mvn versions:set -DnewVersion=$version
+mvn versions:commit
 find . -name "pom.xml" | xargs sed -i -e "s/1.5-SNAPSHOT/$version/g"
 find . -name "pom.xml" | xargs sed -i -e "s/wicket\/trunk/wicket\/releases\/$version/g"
 
