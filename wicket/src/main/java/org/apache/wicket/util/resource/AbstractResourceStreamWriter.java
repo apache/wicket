c715a94b1d3859b873732b8ed53d2660a39131d1
Martin Tzvetanov Grigorov <mgrigorov@apache.org>
2011-04-04 12:07:16 +0000

    WICKET-3585 Let AbstractResourceStreamWriter implement empty getContentType()
    git-svn-id: https://svn.apache.org/repos/asf/wicket/branches/wicket-1.4.x@1088577 13f79535-47bb-0310-9956-ffa450edef68

--- wicket/src/main/java/org/apache/wicket/util/resource/AbstractResourceStreamWriter.java
+++ wicket/src/main/java/org/apache/wicket/util/resource/AbstractResourceStreamWriter.java
@@ -93,4 +93,9 @@ public abstract class AbstractResourceStreamWriter implements IResourceStreamWri
 	public final void close() throws IOException
 	{
 	}
+
+	public String getContentType()
+	{
+		return null;
+	}
 }
