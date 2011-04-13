01ab361c1461c93bf5151ea617cccd11f71b661b
Peter Ertl <pete@apache.org>
2011-04-13 21:49:24 +0000

    not quite sure about the last change so rolling it back
    git-svn-id: https://svn.apache.org/repos/asf/wicket/trunk@1091929 13f79535-47bb-0310-9956-ffa450edef68

--- wicket-datetime/src/main/java/org/apache/wicket/extensions/yui/calendar/DateTimeField.java
+++ wicket-datetime/src/main/java/org/apache/wicket/extensions/yui/calendar/DateTimeField.java
@@ -347,10 +347,11 @@ public class DateTimeField extends FormComponentPanel<Date>
 			}
 
 			// Get year, month and day ignoring any timezone of the Date object
-			final LocalDate localDate = new LocalDate(dateFieldInput);
-			int year = localDate.getYear();
-			int month = localDate.getMonthOfYear();
-			int day = localDate.getDayOfMonth();
+			Calendar cal = Calendar.getInstance();
+			cal.setTime(dateFieldInput);
+			int year = cal.get(Calendar.YEAR);
+			int month = cal.get(Calendar.MONTH) + 1;
+			int day = cal.get(Calendar.DAY_OF_MONTH);
 			int hours = (hoursInput == null ? 0 : hoursInput % 24);
 			int minutes = (minutesInput == null ? 0 : minutesInput);
 
