7c364566e96dd8bc534ab1c6f2a9b14bcf6de69d
Martin Tzvetanov Grigorov <mgrigorov@apache.org>
2011-04-21 14:07:32 +0000

    WICKET-3598 DatePicker issues with locale medium date format
    Use the locale when creating the DateFormat.
    
    
    
    git-svn-id: https://svn.apache.org/repos/asf/wicket/trunk@1095724 13f79535-47bb-0310-9956-ffa450edef68

--- wicket-datetime/src/main/java/org/apache/wicket/datetime/StyleDateConverter.java
+++ wicket-datetime/src/main/java/org/apache/wicket/datetime/StyleDateConverter.java
@@ -16,13 +16,13 @@
  */
 package org.apache.wicket.datetime;
 
-import java.util.Locale;
-
 import org.apache.wicket.datetime.markup.html.form.DateTextField;
 import org.joda.time.DateTime;
 import org.joda.time.DateTimeZone;
 import org.joda.time.format.DateTimeFormat;
 import org.joda.time.format.DateTimeFormatter;
+
+import java.util.Locale;
 
 
 /**
@@ -52,15 +52,13 @@ public class StyleDateConverter extends DateConverter
 
 	/**
 	 * Construct. The dateStyle 'S-' (which is the same as {@link DateTimeFormat#shortDate()}) will
-	 * be used for constructing the date format for the current locale.
-	 * </p>
-	 * When applyTimeZoneDifference is true, the current time is applied on the parsed date, and the
-	 * date will be corrected for the time zone difference between the server and the client. For
+	 * be used for constructing the date format for the current locale. </p> When
+	 * applyTimeZoneDifference is true, the current time is applied on the parsed date, and the date
+	 * will be corrected for the time zone difference between the server and the client. For
 	 * instance, if I'm in Seattle and the server I'm working on is in Amsterdam, the server is 9
 	 * hours ahead. So, if I'm inputting say 12/24 at a couple of hours before midnight, at the
 	 * server it is already 12/25. If this boolean is true, it will be transformed to 12/25, while
-	 * the client sees 12/24.
-	 * </p>
+	 * the client sees 12/24. </p>
 	 * 
 	 * @param applyTimeZoneDifference
 	 *            whether to apply the difference in time zones between client and server
@@ -72,15 +70,13 @@ public class StyleDateConverter extends DateConverter
 
 	/**
 	 * Construct. The provided pattern will be used as the base format (but they will be localized
-	 * for the current locale) and if null, {@link DateTimeFormat#shortDate()} will be used.
-	 * </p>
+	 * for the current locale) and if null, {@link DateTimeFormat#shortDate()} will be used. </p>
 	 * When applyTimeZoneDifference is true, the current time is applied on the parsed date, and the
 	 * date will be corrected for the time zone difference between the server and the client. For
 	 * instance, if I'm in Seattle and the server I'm working on is in Amsterdam, the server is 9
 	 * hours ahead. So, if I'm inputting say 12/24 at a couple of hours before midnight, at the
 	 * server it is already 12/25. If this boolean is true, it will be transformed to 12/25, while
-	 * the client sees 12/24.
-	 * </p>
+	 * the client sees 12/24. </p>
 	 * 
 	 * @param dateStyle
 	 *            Date style to use. The first character is the date style, and the second character
@@ -119,6 +115,8 @@ public class StyleDateConverter extends DateConverter
 	@Override
 	protected DateTimeFormatter getFormat(Locale locale)
 	{
-		return DateTimeFormat.forPattern(getDatePattern(locale)).withPivotYear(2000);
+		return DateTimeFormat.forPattern(getDatePattern(locale))
+			.withLocale(locale)
+			.withPivotYear(2000);
 	}
 }
