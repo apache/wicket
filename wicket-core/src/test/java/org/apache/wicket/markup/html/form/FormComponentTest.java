43fe81a2abf7418f9bac61eae92bb024ff77aee2
Igor Vaynberg <ivaynberg@apache.org>
2011-05-24 06:09:10 +0000

    Issue: WICKET-3738
    git-svn-id: https://svn.apache.org/repos/asf/wicket/trunk@1126886 13f79535-47bb-0310-9956-ffa450edef68

---
+++ wicket-core/src/test/java/org/apache/wicket/markup/html/form/FormComponentTest.java
@@ -0,0 +1,101 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.wicket.markup.html.form;
+
+import junit.framework.Assert;
+import junit.framework.TestCase;
+
+import org.apache.wicket.MarkupContainer;
+import org.apache.wicket.markup.IMarkupResourceStreamProvider;
+import org.apache.wicket.markup.html.WebPage;
+import org.apache.wicket.util.resource.IResourceStream;
+import org.apache.wicket.util.resource.StringResourceStream;
+import org.apache.wicket.util.tester.WicketTester;
+
+/**
+ * 
+ */
+public class FormComponentTest extends TestCase
+{
+	private WicketTester wicketTester;
+
+	@Override
+	protected void setUp() throws Exception
+	{
+		super.setUp();
+		wicketTester = new WicketTester();
+	}
+
+	public void testArrayType()
+	{
+		final FormComponent<?> fc = new TextField<String>("foo");
+		fc.setType(String[].class);
+		Assert.assertSame(String[].class, fc.getType());
+	}
+
+	public void testMultiDimentionalArrayType()
+	{
+		final FormComponent<?> fc = new TextField<String>("foo");
+		fc.setType(String[][][].class);
+		Assert.assertSame(String[][][].class, fc.getType());
+	}
+
+	public void testPrimitiveArrayType()
+	{
+		final FormComponent<?> fc = new TextField<String>("foo");
+		fc.setType(boolean[].class);
+		Assert.assertSame(boolean[].class, fc.getType());
+	}
+
+	public void testGetDefaultlabel()
+	{
+		wicketTester.startPage(TestPage1.class);
+		TestPage1 page = (TestPage1)wicketTester.getLastRenderedPage();
+		assertEquals("set", page.field1.getDefaultLabel());
+		assertEquals("field2", page.field2.getDefaultLabel());
+	}
+
+	@Override
+	protected void tearDown() throws Exception
+	{
+		wicketTester.destroy();
+		wicketTester = null;
+		super.tearDown();
+	}
+
+	public static class TestPage1 extends WebPage implements IMarkupResourceStreamProvider
+	{
+		public final TextField field1, field2;
+
+		public TestPage1()
+		{
+			Form form = new Form("form");
+			add(form);
+			form.add(field1 = new TextField("field1"));
+			form.add(field2 = new TextField("field2"));
+		}
+
+		public IResourceStream getMarkupResourceStream(MarkupContainer container,
+			Class<?> containerClass)
+		{
+			return new StringResourceStream(
+				"<html><body><form wicket:id='form'><input wicket:id='field1' type='text'/><input wicket:id='field2' type='text'/></form></body></html>");
+		}
+
+
+	}
+}
