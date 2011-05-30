0f396152a429cdab68c023eb68f54f269dc16737
Martin Tzvetanov Grigorov <mgrigorov@apache.org>
2011-05-30 08:08:20 +0000

    WICKET-3652 Provide a Typesafe Panel, that allows to use getDefaultModel and getDefaultModelObject without casting
    git-svn-id: https://svn.apache.org/repos/asf/wicket/trunk@1129038 13f79535-47bb-0310-9956-ffa450edef68

---
+++ wicket-core/src/main/java/org/apache/wicket/markup/html/GenericWebPage.java
@@ -0,0 +1,115 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.wicket.markup.html;
+
+import org.apache.wicket.Page;
+import org.apache.wicket.model.IModel;
+import org.apache.wicket.request.mapper.parameter.PageParameters;
+
+/**
+ * A {@link WebPage} with typesafe getters and setters for the model and its underlying object
+ * 
+ * @param <T>
+ *            the type of the page's model object
+ */
+public class GenericWebPage<T> extends WebPage
+{
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * Constructor. Having this constructor public means that your page is 'bookmarkable' and hence
+	 * can be called/ created from anywhere.
+	 */
+	protected GenericWebPage()
+	{
+		super();
+	}
+
+	/**
+	 * @param model
+	 *            the page's model
+	 * @see Page#Page(IModel)
+	 */
+	protected GenericWebPage(final IModel<T> model)
+	{
+		super(model);
+	}
+
+	/**
+	 * Constructor which receives wrapped query string parameters for a request. Having this
+	 * constructor public means that your page is 'bookmarkable' and hence can be called/ created
+	 * from anywhere. For bookmarkable pages (as opposed to when you construct page instances
+	 * yourself, this constructor will be used in preference to a no-arg constructor, if both exist.
+	 * Note that nothing is done with the page parameters argument. This constructor is provided so
+	 * that tools such as IDEs will include it their list of suggested constructors for derived
+	 * classes.
+	 * 
+	 * Please call this constructor if you want to remember the pageparameters
+	 * {@link #getPageParameters()}. So that they are reused for stateless links.
+	 * 
+	 * @param parameters
+	 *            Wrapped query string parameters.
+	 */
+	protected GenericWebPage(final PageParameters parameters)
+	{
+		super(parameters);
+	}
+
+	/**
+	 * Typesafe getter for the model's object
+	 * 
+	 * @return the model object
+	 */
+	@SuppressWarnings("unchecked")
+	public final T getModelObject()
+	{
+		return (T)getDefaultModelObject();
+	}
+
+	/**
+	 * Typesafe setter for the model object
+	 * 
+	 * @param modelObject
+	 *            the new model object
+	 */
+	public final void setModelObject(final T modelObject)
+	{
+		setDefaultModelObject(modelObject);
+	}
+
+	/**
+	 * Typesafe getter for the page's model
+	 * 
+	 * @return the model
+	 */
+	@SuppressWarnings("unchecked")
+	public final T getModel()
+	{
+		return (T)getDefaultModel();
+	}
+
+	/**
+	 * Typesafe setter for the model
+	 * 
+	 * @param model
+	 *            the new model
+	 */
+	public final void setModel(final IModel<T> model)
+	{
+		setDefaultModel(model);
+	}
+}
---
+++ wicket-core/src/main/java/org/apache/wicket/markup/html/panel/GenericPanel.java
@@ -0,0 +1,94 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.wicket.markup.html.panel;
+
+import org.apache.wicket.model.IModel;
+
+/**
+ * A {@link Panel} with typesafe getters and setters for the model and its underlying object
+ * 
+ * @param <T>
+ *            the type of the panel's model object
+ */
+public class GenericPanel<T> extends Panel
+{
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see org.apache.wicket.Component#Component(String)
+	 */
+	public GenericPanel(final String id)
+	{
+		this(id, null);
+	}
+
+	/**
+	 * @param id
+	 *            the component id
+	 * @param model
+	 *            the component model
+	 * @see org.apache.wicket.Component#Component(String, IModel)
+	 */
+	public GenericPanel(final String id, final IModel<T> model)
+	{
+		super(id, model);
+	}
+
+	/**
+	 * Typesafe getter for the model's object
+	 * 
+	 * @return the model object
+	 */
+	@SuppressWarnings("unchecked")
+	public final T getModelObject()
+	{
+		return (T)getDefaultModelObject();
+	}
+
+	/**
+	 * Typesafe setter for the model object
+	 * 
+	 * @param modelObject
+	 *            the new model object
+	 */
+	public final void setModelObject(final T modelObject)
+	{
+		setDefaultModelObject(modelObject);
+	}
+
+	/**
+	 * Typesafe getter for the page's model
+	 * 
+	 * @return the model
+	 */
+	@SuppressWarnings("unchecked")
+	public final T getModel()
+	{
+		return (T)getDefaultModel();
+	}
+
+	/**
+	 * Typesafe setter for the model
+	 * 
+	 * @param model
+	 *            the new model
+	 */
+	public final void setModel(final IModel<T> model)
+	{
+		setDefaultModel(model);
+	}
+}
