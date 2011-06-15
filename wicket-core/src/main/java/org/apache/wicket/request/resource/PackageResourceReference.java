2dbc1c58ea0f7fa8841e8624ec046aafe2e4af3c
Pedro Henrique Oliveira dos Santos <pedro@apache.org>
2011-06-15 13:22:56 +0000

    Reverting changes since variant of resources get ignored Issue: WICKET-3796
    git-svn-id: https://svn.apache.org/repos/asf/wicket/trunk@1136029 13f79535-47bb-0310-9956-ffa450edef68

--- wicket-core/src/main/java/org/apache/wicket/request/resource/PackageResourceReference.java
+++ wicket-core/src/main/java/org/apache/wicket/request/resource/PackageResourceReference.java
@@ -114,10 +114,8 @@ public class PackageResourceReference extends ResourceReference
 		String variation)
 	{
 		String absolutePath = Packages.absolutePath(getScope(), getName());
-		// strict only if an attribute was specified
-		boolean strict = getLocale() != null || getStyle() != null || getVariation() != null;
 		IResourceStream stream = locator.locate(getScope(), absolutePath, style, variation, locale,
-			null, strict);
+			null, true);
 
 		if (stream == null)
 			return null;
