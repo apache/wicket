bcff8daf491937396e581788e7da0b1263b4e49d
Peter Ertl <pete@apache.org>
2011-07-01 00:17:01 +0000

    adjust test name with tested class name
    git-svn-id: https://svn.apache.org/repos/asf/wicket/trunk@1141766 13f79535-47bb-0310-9956-ffa450edef68

--- wicket-request/src/test/java/org/apache/wicket/request/HttpHeaderCollectionTest.java
+++ wicket-request/src/test/java/org/apache/wicket/request/HttpHeaderCollectionTest.java
@@ -24,7 +24,7 @@ import org.junit.Test;
 
 import static org.junit.Assert.*;
 
-public class HeadersCollectionTest
+public class HttpHeaderCollectionTest
 {
 	@Test
 	public void testHeaderCollection()
