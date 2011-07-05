3feb0e3ad4aab3f12e0d0a291b3791c26cd2cb26
Pedro Henrique Oliveira dos Santos <pedro@apache.org>
2011-07-05 20:31:35 +0000

    - detach models recursively Issue: WICKET-3872
    git-svn-id: https://svn.apache.org/repos/asf/wicket/trunk@1143195 13f79535-47bb-0310-9956-ffa450edef68

--- wicket-core/src/main/java/org/apache/wicket/MarkupContainer.java
+++ wicket-core/src/main/java/org/apache/wicket/MarkupContainer.java
@@ -644,7 +644,7 @@ public abstract class MarkupContainer extends Component implements Iterable<Comp
 					// Do not call remove() because the state change would than be
 					// recorded twice.
 					child.internalOnRemove();
-					child.detachModel();
+					child.detach();
 					child.setParent(null);
 				}
 			}
