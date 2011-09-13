f3d7565cc1946a0ef4d257064daa1206f42bd242
Martin Tzvetanov Grigorov <mgrigorov@apache.org>
2011-09-13 09:09:56 +0000

    WICKET-4038 Don't create an empty PageParameters if the original is null.
    git-svn-id: https://svn.apache.org/repos/asf/wicket/trunk@1170096 13f79535-47bb-0310-9956-ffa450edef68

--- wicket-core/src/main/java/org/apache/wicket/request/mapper/AbstractBookmarkableMapper.java
+++ wicket-core/src/main/java/org/apache/wicket/request/mapper/AbstractBookmarkableMapper.java
@@ -88,13 +88,16 @@ public abstract class AbstractBookmarkableMapper extends AbstractComponentMapper
 		 */
 		private PageParameters cleanPageParameters(final PageParameters originalParameters)
 		{
-			PageParameters cleanParameters = new PageParameters(originalParameters);
+			PageParameters cleanParameters = null;
+			if (originalParameters != null)
+			{
+				cleanParameters = new PageParameters(originalParameters);
 
-			// WICKET-4038: Ajax related parameters are set by wicket-ajax.js when needed.
-			// They shouldn't be propagated to the next requests
-			cleanParameters.remove(WebRequest.PARAM_AJAX);
-			cleanParameters.remove(WebRequest.PARAM_AJAX_BASE_URL);
-
+				// WICKET-4038: Ajax related parameters are set by wicket-ajax.js when needed.
+				// They shouldn't be propagated to the next requests
+				cleanParameters.remove(WebRequest.PARAM_AJAX);
+				cleanParameters.remove(WebRequest.PARAM_AJAX_BASE_URL);
+			}
 			return cleanParameters;
 		}
 
