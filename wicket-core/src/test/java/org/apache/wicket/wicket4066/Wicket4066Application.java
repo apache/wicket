2401825e80d199f7a433df66d10ea6d2b7a1b38d
Martin Tzvetanov Grigorov <mgrigorov@apache.org>
2011-09-20 08:50:03 +0000

    WICKET-4066 RestartResponseAtInterceptPageException.InterceptData is never cleared
    Add licence headers
    
    
    
    git-svn-id: https://svn.apache.org/repos/asf/wicket/trunk@1173028 13f79535-47bb-0310-9956-ffa450edef68

--- wicket-core/src/test/java/org/apache/wicket/wicket4066/Wicket4066Application.java
+++ wicket-core/src/test/java/org/apache/wicket/wicket4066/Wicket4066Application.java
@@ -1,3 +1,19 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.apache.wicket.wicket4066;
 
 import org.apache.wicket.protocol.http.WebApplication;
@@ -5,35 +21,36 @@ import org.apache.wicket.request.Request;
 import org.apache.wicket.request.Response;
 
 /**
- * Application object for your web application. If you want to run this application without deploying, run the Start class.
+ * Application object for your web application. If you want to run this application without
+ * deploying, run the Start class.
  * 
  * @see com.mycompany.Start#main(String[])
  */
 public class Wicket4066Application extends WebApplication
-{       
-    /**
-     * @see org.apache.wicket.Application#getHomePage()
-     */
-    @Override
-    public Class<HomePage> getHomePage()
-    {
-        return HomePage.class;
-    }
+{
+	/**
+	 * @see org.apache.wicket.Application#getHomePage()
+	 */
+	@Override
+	public Class<HomePage> getHomePage()
+	{
+		return HomePage.class;
+	}
 
-    /**
-     * @see org.apache.wicket.Application#init()
-     */
-    @Override
-    public void init()
-    {
-        super.init();
+	/**
+	 * @see org.apache.wicket.Application#init()
+	 */
+	@Override
+	public void init()
+	{
+		super.init();
 
-        // add your configuration here
-    }
+		// add your configuration here
+	}
 
-    @Override
-    public MySession newSession(Request request, Response response)
-    {
-        return new MySession(request);
-    }
+	@Override
+	public MySession newSession(Request request, Response response)
+	{
+		return new MySession(request);
+	}
 }
