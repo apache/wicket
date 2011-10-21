816bd49b8adc56f41a2935faf26ac26ff9f2446b
Igor Vaynberg <ivaynberg@apache.org>
2011-10-21 00:37:55 +0000

    Issue: WICKET-4144
    git-svn-id: https://svn.apache.org/repos/asf/wicket/trunk@1187145 13f79535-47bb-0310-9956-ffa450edef68

--- wicket-extensions/src/main/java/org/apache/wicket/extensions/markup/html/repeater/data/table/filter/FilterForm.java
+++ wicket-extensions/src/main/java/org/apache/wicket/extensions/markup/html/repeater/data/table/filter/FilterForm.java
@@ -68,8 +68,8 @@ public class FilterForm<T> extends Form<T>
 		String id = Strings.escapeMarkup(getFocusTrackerFieldCssId()).toString();
 		String value = getRequest().getPostParameters().getParameterValue(id).toString("");
 		getResponse().write(
-			String.format("<input type=\"hidden\" name=\"%s\" id=\"%s\" value=\"%s\"/>", id, id,
-				value));
+			String.format("<div><input type=\"hidden\" name=\"%s\" id=\"%s\" value=\"%s\"/></div>",
+				id, id, value));
 	}
 
 	/**
