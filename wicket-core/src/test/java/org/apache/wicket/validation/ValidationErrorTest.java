7cbb9e352b62281e567f6e717733b55098fca7d2
Igor Vaynberg <ivaynberg@apache.org>
2011-11-16 23:06:31 +0000

    test case for validation errror Issue: WICKET-4234
    git-svn-id: https://svn.apache.org/repos/asf/wicket/trunk@1202923 13f79535-47bb-0310-9956-ffa450edef68

---
+++ wicket-core/src/test/java/org/apache/wicket/validation/ValidationErrorTest.java
@@ -0,0 +1,68 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.wicket.validation;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+/**
+ * {@link ValidationError} related tests
+ * 
+ * @author igor
+ */
+public class ValidationErrorTest
+{
+	/**
+	 * tests standard key values
+	 */
+	@Test
+	public void standardKeys()
+	{
+		ValidationError e = new ValidationError();
+		e.addKey(new TestValidator());
+		assertEquals("TestValidator", e.getKeys().get(0));
+
+		e = new ValidationError();
+		e.addKey(new TestValidator(), "foo");
+		assertEquals("TestValidator.foo", e.getKeys().get(0));
+
+		e = new ValidationError();
+		e.addKey(new TestValidator(), null);
+		assertEquals("TestValidator", e.getKeys().get(0));
+
+		e = new ValidationError();
+		e.addKey(new TestValidator(), " ");
+		assertEquals("TestValidator", e.getKeys().get(0));
+
+		e = new ValidationError();
+		e.addKey(new TestValidator(), " foo ");
+		assertEquals("TestValidator.foo", e.getKeys().get(0));
+
+
+	}
+
+	private static class TestValidator implements IValidator<String>
+	{
+
+		@Override
+		public void validate(IValidatable<String> validatable)
+		{
+		}
+
+	}
+}
