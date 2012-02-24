16eda14d31890ddc1bfa65b0285e9a2081ea3e40
Martin Tzvetanov Grigorov <mgrigorov@apache.org>
2012-02-24 10:09:07 +0100

    Javadoc fix

--- wicket-core/src/main/java/org/apache/wicket/util/lang/PropertyResolver.java
+++ wicket-core/src/main/java/org/apache/wicket/util/lang/PropertyResolver.java
@@ -1494,9 +1494,9 @@ public final class PropertyResolver
 
 	/**
 	 * An implementation of the class can be set on the
-	 * {@link PropertyResolver#setClassCacheCreator(Application, IClassCache)} method for a specific
-	 * application. This class cache can then be a special map with an eviction policy or do nothing
-	 * if nothing should be cached for the given class.
+	 * {@link PropertyResolver#setClassCache(org.apache.wicket.Application, org.apache.wicket.util.lang.PropertyResolver.IClassCache)}
+	 * method for a specific application. This class cache can then be a special map with
+	 * an eviction policy or do nothing if nothing should be cached for the given class.
 	 * 
 	 * For example if you have proxy classes that are constantly created you could opt for not
 	 * caching those at all or have a special Map implementation that will evict that class at a
