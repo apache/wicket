fad0de1ad00ea67cec44199cf25b026a2590a688
Martin Tzvetanov Grigorov <mgrigorov@apache.org>
2012-04-04 11:44:56 +0200

    WICKET-4486 Give StringValue toEnum methods
    Add automatic forward to #toEnum() in #to(Class) when the argument is an Enum

--- wicket-util/src/main/java/org/apache/wicket/util/string/StringValue.java
+++ wicket-util/src/main/java/org/apache/wicket/util/string/StringValue.java
@@ -311,6 +311,11 @@ public class StringValue implements IClusterable
 	 */
 	public final Object to(final Class<?> type) throws StringValueConversionException
 	{
+		if (type == null)
+		{
+			return null;
+		}
+
 		if (type == String.class)
 		{
 			return toString();
@@ -349,6 +354,11 @@ public class StringValue implements IClusterable
 		if (type == Duration.class)
 		{
 			return toDuration();
+		}
+
+		if (type.isEnum())
+		{
+			return (T) toEnum((Class) type);
 		}
 
 		throw new StringValueConversionException("Cannot convert '" + toString() + "'to type " +
