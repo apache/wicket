1fb1322ababb6a545114e3b2d7aa92b10b40ada8
Martin Tzvetanov Grigorov <mgrigorov@apache.org>
2012-06-20 16:34:24 +0300

    Move ApacheLicenceHeaderTest's in -core and -request to a package that doesn't match with the one in -util. No functional change.

--- wicket-core/src/test/java/org/apache/wicket/core/util/license/ApacheLicenceHeaderTest.java
+++ wicket-core/src/test/java/org/apache/wicket/core/util/license/ApacheLicenceHeaderTest.java
@@ -14,12 +14,14 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.apache.wicket.util.license;
+package org.apache.wicket.core.util.license;
 
+
+import org.apache.wicket.util.license.ApacheLicenseHeaderTestCase;
 
 /**
  * Test that the license headers are in place in this project. The tests are run from
- * {@link ApacheLicenseHeaderTestCase}, but you can add project specific tests here if needed.
+ * {@link org.apache.wicket.util.license.ApacheLicenseHeaderTestCase}, but you can add project specific tests here if needed.
  * 
  * @author Frank Bille Jensen (frankbille)
  */
