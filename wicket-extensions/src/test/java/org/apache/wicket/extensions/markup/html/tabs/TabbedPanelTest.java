0f9349b629017299b37f5f4841569f16dce732bc
svenmeier <svenmeier@apache.org>
2012-07-16 19:50:37 +0200

    WICKET-4593 bi-directional model second try, more tests for TabbedPanel

--- wicket-extensions/src/test/java/org/apache/wicket/extensions/markup/html/tabs/TabbedPanelTest.java
+++ wicket-extensions/src/test/java/org/apache/wicket/extensions/markup/html/tabs/TabbedPanelTest.java
@@ -32,15 +32,22 @@ import org.junit.Test;
  */
 public class TabbedPanelTest extends WicketTestCase
 {
+	/**
+	 */
 	public class TestPage extends WebPage
 	{
-		public TabbedPanel<ITab> tabbedPanel;
+		private static final long serialVersionUID = 1L;
 
+		protected TabbedPanel<ITab> tabbedPanel;
+
+		/**
+		 */
 		public TestPage()
 		{
 			List<ITab> defaultTabs = new ArrayList<ITab>();
 			defaultTabs.add(new AbstractTab(Model.of("default 1"))
 			{
+				private static final long serialVersionUID = 1L;
 
 				@Override
 				public WebMarkupContainer getPanel(String panelId)
@@ -50,6 +57,7 @@ public class TabbedPanelTest extends WicketTestCase
 			});
 			defaultTabs.add(new AbstractTab(Model.of("default 2"))
 			{
+				private static final long serialVersionUID = 1L;
 
 				@Override
 				public WebMarkupContainer getPanel(String panelId)
@@ -68,8 +76,16 @@ public class TabbedPanelTest extends WicketTestCase
 		return new TabbedPanel<ITab>("tabpanel", defaultTabs);
 	}
 
+	/**
+	 */
 	public static class TestPanel extends Panel
 	{
+		private static final long serialVersionUID = 1L;
+
+		/**
+		 * @param id
+		 * @param panelTestId
+		 */
 		public TestPanel(String id, String panelTestId)
 		{
 			super(id);
@@ -77,6 +93,11 @@ public class TabbedPanelTest extends WicketTestCase
 		}
 	}
 
+	/**
+	 * No tabs thus no tab component rendered.
+	 * 
+	 * @throws Exception
+	 */
 	@Test
 	public void renderNoTabs() throws Exception
 	{
@@ -84,15 +105,20 @@ public class TabbedPanelTest extends WicketTestCase
 		page.tabbedPanel.getTabs().clear();
 		tester.startPage(page);
 
-		tester.assertContainsNot("<span wicket:id=\"title\">default 1</span></a>");
+		tester.assertContainsNot("<span wicket:id=\"title\">default 1</span>");
 		tester.assertContainsNot("<span wicket:id=\"label\">default 1</span>");
-		tester.assertContainsNot("<span wicket:id=\"title\">default 2</span></a>");
+		tester.assertContainsNot("<span wicket:id=\"title\">default 2</span>");
 		tester.assertContainsNot("<span wicket:id=\"label\">default 2</span>");
 		tester.assertContains("<!-- no panel -->");
 
-		assertEquals(Integer.valueOf(0), page.tabbedPanel.getDefaultModelObject());
+		assertEquals(Integer.valueOf(-1), page.tabbedPanel.getDefaultModelObject());
 	}
 
+	/**
+	 * Switching between tabsS.
+	 * 
+	 * @throws Exception
+	 */
 	@Test
 	public void renderDefaultTabsOnly() throws Exception
 	{
@@ -109,12 +135,19 @@ public class TabbedPanelTest extends WicketTestCase
 		assertEquals(Integer.valueOf(1), page.tabbedPanel.getDefaultModelObject());
 	}
 
+	/**
+	 * Additional tabs are rendered.
+	 * 
+	 * @throws Exception
+	 */
 	@Test
 	public void renderAdditionalTabs() throws Exception
 	{
 		TestPage page = tester.startPage(new TestPage());
 		page.tabbedPanel.getTabs().add(new AbstractTab(Model.of("added 1"))
 		{
+			private static final long serialVersionUID = 1L;
+
 			@Override
 			public WebMarkupContainer getPanel(String panelId)
 			{
@@ -122,23 +155,167 @@ public class TabbedPanelTest extends WicketTestCase
 			}
 		});
 		// the additional tab isn't rendered yet
-		tester.assertContainsNot("<span wicket:id=\"title\">added 1</span></a>");
+		tester.assertContainsNot("<span wicket:id=\"title\">added 1</span>");
 		tester.assertContainsNot("<span wicket:id=\"label\">added 1</span>");
 
 		assertEquals(Integer.valueOf(0), page.tabbedPanel.getDefaultModelObject());
 
 		// now its title is visible, but the contents not
 		tester.clickLink("tabpanel:tabs-container:tabs:1:link");
-		tester.assertContains("<span wicket:id=\"title\">added 1</span></a>");
+		tester.assertContains("<span wicket:id=\"title\">added 1</span>");
 		tester.assertContainsNot("<span wicket:id=\"label\">added 1</span>");
 
 		assertEquals(Integer.valueOf(1), page.tabbedPanel.getDefaultModelObject());
 
 		// now the entire panel should be there
 		tester.clickLink("tabpanel:tabs-container:tabs:2:link");
-		tester.assertContains("<span wicket:id=\"title\">added 1</span></a>");
+		tester.assertContains("<span wicket:id=\"title\">added 1</span>");
 		tester.assertContains("<span wicket:id=\"label\">added 1</span>");
 
 		assertEquals(Integer.valueOf(2), page.tabbedPanel.getDefaultModelObject());
+	}
+
+	/**
+	 * Changing model switches tab.
+	 * 
+	 * @throws Exception
+	 */
+	@Test
+	public void renderModelChange() throws Exception
+	{
+		TestPage page = new TestPage();
+
+		tester.startPage(page);
+		tester.assertContains("<span wicket:id=\"label\">default 1</span>");
+
+		page.tabbedPanel.setDefaultModelObject(Integer.valueOf(1));
+		tester.startPage(page);
+
+		tester.assertContains("<span wicket:id=\"label\">default 2</span>");
+	}
+
+	/**
+	 * Tab's component is aquired once only.
+	 * 
+	 * @throws Exception
+	 */
+	@Test
+	public void tabComponentAquiredOnChangeOnly() throws Exception
+	{
+
+		final int[] count = new int[1];
+
+		TestPage page = new TestPage();
+		page.tabbedPanel.getTabs().clear();
+		page.tabbedPanel.getTabs().add(new AbstractTab(Model.of("added 1"))
+		{
+			private static final long serialVersionUID = 1L;
+
+			@Override
+			public WebMarkupContainer getPanel(String panelId)
+			{
+				count[0]++;
+
+				return new TestPanel(panelId, "added 1");
+			}
+		});
+
+		assertEquals(0, count[0]);
+		tester.startPage(page);
+		assertEquals(1, count[0]);
+		tester.startPage(page);
+		assertEquals(1, count[0]);
+
+		page.tabbedPanel.setSelectedTab(0);
+
+		assertEquals(2, count[0]);
+		tester.startPage(page);
+		assertEquals(2, count[0]);
+	}
+
+	/**
+	 * An invisible tab gets replaced by another one.
+	 * 
+	 * @throws Exception
+	 */
+	@Test
+	public void invisibleTabGetsReplaced() throws Exception
+	{
+		final boolean[] visible = { true, true };
+
+		TestPage page = new TestPage();
+		page.tabbedPanel.getTabs().clear();
+		page.tabbedPanel.getTabs().add(new AbstractTab(Model.of("added 1"))
+		{
+			private static final long serialVersionUID = 1L;
+
+			@Override
+			public WebMarkupContainer getPanel(String panelId)
+			{
+				return new TestPanel(panelId, "added 1");
+			}
+
+			@Override
+			public boolean isVisible()
+			{
+				return visible[0];
+			}
+		});
+		page.tabbedPanel.getTabs().add(new AbstractTab(Model.of("added 2"))
+		{
+			private static final long serialVersionUID = 1L;
+
+			@Override
+			public WebMarkupContainer getPanel(String panelId)
+			{
+				return new TestPanel(panelId, "added 2");
+			}
+
+			@Override
+			public boolean isVisible()
+			{
+				return visible[1];
+			}
+		});
+
+		page.tabbedPanel.setSelectedTab(1);
+
+		tester.startPage(page);
+
+		assertEquals(Integer.valueOf(1), page.tabbedPanel.getDefaultModelObject());
+		tester.assertContains("<span wicket:id=\"title\">added 1</span>");
+		tester.assertContains("<span wicket:id=\"title\">added 2</span>");
+		tester.assertContains("<span wicket:id=\"label\">added 2</span>");
+
+		visible[1] = false;
+
+		tester.startPage(page);
+
+		// now first tab is selected
+		assertEquals(Integer.valueOf(0), page.tabbedPanel.getDefaultModelObject());
+
+		tester.assertContains("<span wicket:id=\"title\">added 1</span>");
+		tester.assertContainsNot("<span wicket:id=\"title\">added 2</span>");
+		tester.assertContains("<span wicket:id=\"label\">added 1</span>");
+
+		visible[0] = false;
+
+		tester.startPage(page);
+
+		// first tab stays selected since no other is visible
+		assertEquals(Integer.valueOf(0), page.tabbedPanel.getDefaultModelObject());
+		tester.assertContainsNot("<span wicket:id=\"title\">added 1</span>");
+		tester.assertContainsNot("<span wicket:id=\"title\">added 2</span>");
+		tester.assertContains("<!-- no panel -->");
+
+		visible[1] = true;
+
+		tester.startPage(page);
+
+		// second selected again
+		assertEquals(Integer.valueOf(1), page.tabbedPanel.getDefaultModelObject());
+		tester.assertContainsNot("<span wicket:id=\"title\">added 1</span>");
+		tester.assertContains("<span wicket:id=\"title\">added 2</span>");
+		tester.assertContains("<span wicket:id=\"label\">added 2</span>");
 	}
 }
