f5f802c56388eef5a7336ce00961e0f895f418db
svenmeier <svenmeier@apache.org>
2012-08-05 23:16:17 +0200

    WICKET-4696 MIN_VALUE of Double and Float is not minimum number

--- wicket-util/src/test/java/org/apache/wicket/util/lang/NumbersTest.java
+++ wicket-util/src/test/java/org/apache/wicket/util/lang/NumbersTest.java
@@ -27,6 +27,9 @@ import org.junit.Test;
  */
 public class NumbersTest extends Assert
 {
+	/**
+	 * See WICKET-4696 for Float and Double minimum value
+	 */
 	@Test
 	public void getMinValue()
 	{
@@ -34,17 +37,17 @@ public class NumbersTest extends Assert
 		assertEquals(Integer.MIN_VALUE, Numbers.getMinValue(int.class));
 		assertEquals(Long.MIN_VALUE, Numbers.getMinValue(Long.class));
 		assertEquals(Long.MIN_VALUE, Numbers.getMinValue(long.class));
-		assertEquals(Float.MIN_VALUE, Numbers.getMinValue(Float.class));
-		assertEquals(Float.MIN_VALUE, Numbers.getMinValue(float.class));
-		assertEquals(Double.MIN_VALUE, Numbers.getMinValue(Double.class));
-		assertEquals(Double.MIN_VALUE, Numbers.getMinValue(double.class));
+		assertEquals(-Float.MAX_VALUE, Numbers.getMinValue(Float.class));
+		assertEquals(-Float.MAX_VALUE, Numbers.getMinValue(float.class));
+		assertEquals(-Double.MAX_VALUE, Numbers.getMinValue(Double.class));
+		assertEquals(-Double.MAX_VALUE, Numbers.getMinValue(double.class));
 		assertEquals(Byte.MIN_VALUE, Numbers.getMinValue(Byte.class));
 		assertEquals(Byte.MIN_VALUE, Numbers.getMinValue(byte.class));
 		assertEquals(Short.MIN_VALUE, Numbers.getMinValue(Short.class));
 		assertEquals(Short.MIN_VALUE, Numbers.getMinValue(short.class));
-		assertEquals(Double.MIN_VALUE, Numbers.getMinValue(BigDecimal.class));
-		assertEquals(Double.MIN_VALUE, Numbers.getMinValue(BigInteger.class));
-		assertEquals(Double.MIN_VALUE, Numbers.getMinValue(null));
+		assertEquals(-Double.MAX_VALUE, Numbers.getMinValue(BigDecimal.class));
+		assertEquals(-Double.MAX_VALUE, Numbers.getMinValue(BigInteger.class));
+		assertEquals(-Double.MAX_VALUE, Numbers.getMinValue(null));
 	}
 
 	@Test
