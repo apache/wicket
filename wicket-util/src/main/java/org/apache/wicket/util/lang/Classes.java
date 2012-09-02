128f720c2459b610508805939fd2fe964762eab5
Martin Tzvetanov Grigorov <mgrigorov@apache.org>
2012-09-02 16:59:36 +0200

    Remove duplicate method #getSimpleName(Class)

--- wicket-util/src/main/java/org/apache/wicket/util/lang/Classes.java
+++ wicket-util/src/main/java/org/apache/wicket/util/lang/Classes.java
@@ -51,6 +51,24 @@ public final class Classes
 	}
 
 	/**
+	 * Takes a Class and a relative path to a class and returns any class at that relative path. For
+	 * example, if the given Class was java.lang.System and the relative path was "../util/List",
+	 * then the java.util.List class would be returned.
+	 *
+	 * @param scope
+	 *            The package to start at
+	 * @param path
+	 *            The relative path to the class
+	 * @return The class
+	 * @throws ClassNotFoundException
+	 */
+	public static Class<?> relativeClass(final Class<?> scope, final String path)
+		throws ClassNotFoundException
+	{
+		return Class.forName(Packages.absolutePath(scope, path).replace('/', '.'));
+	}
+
+	/**
 	 * Gets the simple name (without the package) of the given class or null if the class is null.
 	 *
 	 * @param c
@@ -70,39 +88,6 @@ public final class Classes
 		}
 
 		return simpleName;
-	}
-
-	/**
-	 * Takes a Class and a relative path to a class and returns any class at that relative path. For
-	 * example, if the given Class was java.lang.System and the relative path was "../util/List",
-	 * then the java.util.List class would be returned.
-	 *
-	 * @param scope
-	 *            The package to start at
-	 * @param path
-	 *            The relative path to the class
-	 * @return The class
-	 * @throws ClassNotFoundException
-	 */
-	public static Class<?> relativeClass(final Class<?> scope, final String path)
-		throws ClassNotFoundException
-	{
-		return Class.forName(Packages.absolutePath(scope, path).replace('/', '.'));
-	}
-
-
-	/**
-	 * Gets the name of a given class without the prefixed package path
-	 *
-	 * @param <C>
-	 *
-	 * @param c
-	 *            The class
-	 * @return The class name
-	 */
-	public static <C> String simpleName(final Class<C> c)
-	{
-		return Strings.lastPathComponent(c.getName(), '.');
 	}
 
 	/**
