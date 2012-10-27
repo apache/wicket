d5720e633dd130735028c19fe5bcf92a1acc6a6e
svenmeier <svenmeier@apache.org>
2012-10-27 10:45:45 +0200

    replaced deprecated import, formatted

--- archetypes/quickstart/src/main/resources/archetype-resources/src/main/java/HomePage.java
+++ archetypes/quickstart/src/main/resources/archetype-resources/src/main/java/HomePage.java
@@ -7,12 +7,12 @@ import org.apache.wicket.markup.html.WebPage;
 public class HomePage extends WebPage {
 	private static final long serialVersionUID = 1L;
 
-    public HomePage(final PageParameters parameters) {
-	super(parameters);
+	public HomePage(final PageParameters parameters) {
+		super(parameters);
 
-	add(new Label("version", getApplication().getFrameworkSettings().getVersion()));
+		add(new Label("version", getApplication().getFrameworkSettings().getVersion()));
 
-        // TODO Add your page's components here
+		// TODO Add your page's components here
 
     }
 }
