4e308198c2c499d40120a5fc5ea72d5ffbfb5aef
svenmeier <svenmeier@apache.org>
2012-12-11 10:03:01 +0100

    Wicket 6 requires Java 6, added testcase

--- wicket-core/src/main/java/org/apache/wicket/resource/UtfPropertiesFilePropertiesLoader.java
+++ wicket-core/src/main/java/org/apache/wicket/resource/UtfPropertiesFilePropertiesLoader.java
@@ -20,17 +20,12 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.Reader;
-import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Method;
 
 import org.apache.wicket.util.value.ValueMap;
 
 /**
  * Load properties from properties file via a Reader, which allows to provide the charset and thus
  * the encoding can be different than ISO 8859-1.
- * 
- * The implementation depends on the Java 6 Properties implementation and is disable if the
- * reflection cannot find the method load(Reader).
  * 
  * @author Juergen Donnerstag
  */
@@ -39,8 +34,6 @@ public class UtfPropertiesFilePropertiesLoader implements IPropertiesLoader
 	private final String fileExtension;
 
 	private final String encoding;
-
-	private Method load;
 
 	/**
 	 * Construct.
@@ -52,15 +45,6 @@ public class UtfPropertiesFilePropertiesLoader implements IPropertiesLoader
 	{
 		this.fileExtension = fileExtension;
 		this.encoding = encoding;
-
-		try
-		{
-			load = java.util.Properties.class.getMethod("load", new Class[] { Reader.class });
-		}
-		catch (NoSuchMethodException ex)
-		{
-			load = null;
-		}
 	}
 
 	/**
@@ -78,30 +62,10 @@ public class UtfPropertiesFilePropertiesLoader implements IPropertiesLoader
 	@Override
 	public java.util.Properties loadJavaProperties(final InputStream in) throws IOException
 	{
-		if (load == null)
-		{
-			return null;
-		}
-
 		java.util.Properties properties = new java.util.Properties();
 		Reader reader = new InputStreamReader(in, encoding);
 
-		try
-		{
-			load.invoke(properties, reader);
-		}
-		catch (IllegalArgumentException ex)
-		{
-			properties = null;
-		}
-		catch (IllegalAccessException ex)
-		{
-			properties = null;
-		}
-		catch (InvocationTargetException ex)
-		{
-			properties = null;
-		}
+		properties.load(reader);
 
 		return properties;
 	}
