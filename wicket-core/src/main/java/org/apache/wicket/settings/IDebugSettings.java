cbbffe5a4142b0f830fd06ba076c3d2c9ee40761
Martin Tzvetanov Grigorov <mgrigorov@apache.org>
2013-02-26 17:36:39 +0200

    Remove obsolete javadoc

--- wicket-core/src/main/java/org/apache/wicket/settings/IDebugSettings.java
+++ wicket-core/src/main/java/org/apache/wicket/settings/IDebugSettings.java
@@ -22,9 +22,7 @@ package org.apache.wicket.settings;
  * <i>componentUseCheck </i> (defaults to true in development mode) - causes the framework to do a
  * check after rendering each page to ensure that each component was used in rendering the markup.
  * If components are found that are not referenced in the markup, an appropriate error will be
- * displayed <i>serializeSessionAttributes</i> (defaults to true in development mode) - Causes the
- * framework to serialize any attribute put into session - this helps find Not Serializable errors
- * early
+ * displayed
  * 
  * @author Igor Vaynberg (ivaynberg)
  */
