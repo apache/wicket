aa9578ee4e2a92588877032567e98e20923a2c5d
Martin Tzvetanov Grigorov <mgrigorov@apache.org>
2013-02-27 15:32:58 +0200

    Re-enable a disabled test. Use full page markup assertion instead of TagTester.

--- wicket-core/src/test/java/org/apache/wicket/ajax/AjaxBehaviorEnabledTest.java
+++ wicket-core/src/test/java/org/apache/wicket/ajax/AjaxBehaviorEnabledTest.java
@@ -28,7 +28,6 @@ import org.apache.wicket.request.Response;
 import org.apache.wicket.request.component.IRequestableComponent;
 import org.apache.wicket.util.tester.WicketTester;
 import org.junit.Before;
-import org.junit.Ignore;
 import org.junit.Test;
 
 /**
@@ -103,19 +102,9 @@ public class AjaxBehaviorEnabledTest extends WicketTestCase
 	 * @see <a href="https://issues.apache.org/jira/browse/WICKET-1575">1575</a>
 	 */
 	@Test
-	@Ignore
-	// TODO Wicket.next re-enable
-	public void disabledBehavior()
+	public void disabledBehavior() throws Exception
 	{
-		tester.startPage(AjaxBehaviorEnabledPage.class);
-		tester.assertRenderedPage(AjaxBehaviorEnabledPage.class);
-		System.out.println(tester.getLastResponseAsString());
-		tester.assertVisible("enabled");
-		tester.assertVisible("disabled");
-		assertTrue(tester.getTagByWicketId("enabled").hasAttribute("onclick"));
-		assertFalse("disabled behaviors should not generate onclick",
-			tester.getTagByWicketId("disabled").hasAttribute("onclick"));
-
+		executeTest(AjaxBehaviorEnabledPage.class, "AjaxBehaviorEnabled_expected.html");
 	}
 
 }
