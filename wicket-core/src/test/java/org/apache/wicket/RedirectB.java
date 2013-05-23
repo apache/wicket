8c9fc99a28210d2941f9c05d0810dacdc5f3e790
Martin Tzvetanov Grigorov <mgrigorov@apache.org>
2013-05-23 17:37:27 +0300

    Remove a space character. No functional changes

--- wicket-core/src/test/java/org/apache/wicket/RedirectB.java
+++ wicket-core/src/test/java/org/apache/wicket/RedirectB.java
@@ -34,7 +34,7 @@ public class RedirectB extends WebPage
 	 */
 	public RedirectB()
 	{
-		add(new Label("interceptContinuationURL", Model .of(InterceptData.get()
+		add(new Label("interceptContinuationURL", Model.of(InterceptData.get()
 			.getOriginalUrl()
 			.toString())));
 
