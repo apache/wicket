b2427ddb1dd7db1b5426a39a1b8ac97ed902c7d4
Martin Tzvetanov Grigorov <mgrigorov@apache.org>
2013-06-26 12:40:18 +0300

    Fix indenting - use tabs

--- wicket-core/src/main/java/org/apache/wicket/request/resource/PackageResourceReference.java
+++ wicket-core/src/main/java/org/apache/wicket/request/resource/PackageResourceReference.java
@@ -207,14 +207,14 @@ public class PackageResourceReference extends ResourceReference
 		if (idxOfExtension > -1)
 		{
 			String extension = name.substring(idxOfExtension);
-            final String baseName = name.substring(0, name.length() - extension.length() + 1);
-            if (!".min".equals(extension) && !baseName.endsWith(".min."))
-            {
-                minifiedName = baseName + "min" + extension;
-            } else
-            {
-                minifiedName = name;
-            }
+			final String baseName = name.substring(0, name.length() - extension.length() + 1);
+			if (!".min".equals(extension) && !baseName.endsWith(".min."))
+			{
+				minifiedName = baseName + "min" + extension;
+			} else
+			{
+				minifiedName = name;
+			}
 		} else
 		{
 			minifiedName = name + ".min";
