75812d6dd501f67ff9c94348165f97b7da6cbaeb
Emond Papegaaij <emond.papegaaij@topicus.nl>
2013-07-05 16:56:01 +0200

    Revert "WICKET-5226 CDI integration fails in Glassfish 4.0 with WELD-000070"
    This reverts commit 90eba1eef88cdf3a19b04ba3811643dd55c604cc.

--- wicket-cdi/src/main/java/org/apache/wicket/cdi/ComponentInjector.java
+++ wicket-cdi/src/main/java/org/apache/wicket/cdi/ComponentInjector.java
@@ -16,8 +16,12 @@
  */
 package org.apache.wicket.cdi;
 
+import java.lang.reflect.Modifier;
+
 import org.apache.wicket.Component;
 import org.apache.wicket.application.IComponentInstantiationListener;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * Injects components with CDI dependencies
@@ -27,6 +31,8 @@ import org.apache.wicket.application.IComponentInstantiationListener;
  */
 class ComponentInjector extends AbstractInjector implements IComponentInstantiationListener
 {
+	private static final Logger LOG = LoggerFactory.getLogger(ComponentInjector.class);
+
 	/**
 	 * Constructor
 	 * 
@@ -40,6 +46,16 @@ class ComponentInjector extends AbstractInjector implements IComponentInstantiat
 	@Override
 	public void onInstantiation(Component component)
 	{
-		inject(component);
+		Class<? extends Component> componentClass = component.getClass();
+
+		if (componentClass.isAnonymousClass() ||
+			(componentClass.isMemberClass() && Modifier.isStatic(componentClass.getModifiers()) == false))
+		{
+			LOG.debug("Skipping non-static inner class '{}' ", componentClass);
+		}
+		else
+		{
+			inject(component);
+		}
 	}
 }
