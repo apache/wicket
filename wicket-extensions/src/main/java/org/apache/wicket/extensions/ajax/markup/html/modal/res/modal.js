8fda26e515f83540ead654d997a5ffd8844ee839
Igor Vaynberg <igor.vaynberg@gmail.com>
2013-07-26 22:07:23 -0700

    Revert "WICKET-4948 fix vertical centering of modals in safari" safari seems to work fine now. WICKET-5224
    This reverts commit 3bd454b10271aeabdb6be7e2559eafce92e4ac1e.

--- wicket-extensions/src/main/java/org/apache/wicket/extensions/ajax/markup/html/modal/res/modal.js
+++ wicket-extensions/src/main/java/org/apache/wicket/extensions/ajax/markup/html/modal/res/modal.js
@@ -119,7 +119,7 @@
 
 					var dx = 0;
 					var dy = 0;
-					if (Wicket.Browser.isIE() || Wicket.Browser.isGecko() || Wicket.Browser.isSafari()) {
+					if (Wicket.Browser.isIE() || Wicket.Browser.isGecko) {
 						dx = Wicket.Window.getScrollX();
 						dy = Wicket.Window.getScrollY();
 					}
@@ -137,7 +137,7 @@
 
 					var dx = 0;
 					var dy = 0;
-					if (Wicket.Browser.isIE() || Wicket.Browser.isGecko() || Wicket.Browser.isSafari()) {
+					if (Wicket.Browser.isIE() || Wicket.Browser.isGecko()) {
 						dx = Wicket.Window.getScrollX();
 						dy = Wicket.Window.getScrollY();
 					}
@@ -483,7 +483,7 @@
 			var scTop = 0;
 			var scLeft = 0;
 
-			if (Wicket.Browser.isIE() || Wicket.Browser.isGecko() || Wicket.Browser.isSafari()) {
+			if (Wicket.Browser.isIE() || Wicket.Browser.isGecko()) {
 				scLeft = Wicket.Window.getScrollX();
 				scTop = Wicket.Window.getScrollY();
 			}
