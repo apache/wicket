4aa9ba8d9760401216ea37f8f5524d3d7eae62a7
svenmeier <sven@meiers.net>
2013-10-31 11:20:20 +0100

    Allow access to modifiable list of previous requests/responses

--- wicket-core/src/main/java/org/apache/wicket/util/tester/BaseWicketTester.java
+++ wicket-core/src/main/java/org/apache/wicket/util/tester/BaseWicketTester.java
@@ -26,7 +26,6 @@ import java.lang.reflect.Method;
 import java.nio.charset.Charset;
 import java.text.ParseException;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.Enumeration;
 import java.util.HashMap;
 import java.util.List;
@@ -964,7 +963,7 @@ public class BaseWicketTester
 	 */
 	public List<MockHttpServletRequest> getPreviousRequests()
 	{
-		return Collections.unmodifiableList(previousRequests);
+		return previousRequests;
 	}
 
 	/**
@@ -972,7 +971,7 @@ public class BaseWicketTester
 	 */
 	public List<MockHttpServletResponse> getPreviousResponses()
 	{
-		return Collections.unmodifiableList(previousResponses);
+		return previousResponses;
 	}
 
 	/**
