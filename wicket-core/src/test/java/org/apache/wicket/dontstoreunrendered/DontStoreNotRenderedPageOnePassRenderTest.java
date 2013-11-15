95e4778c9289c13cb75b1e6a8683f98671b2e272
Martin Tzvetanov Grigorov <mgrigorov@apache.org>
2013-11-15 11:40:37 +0200

    WICKET-5415 Do not initialize pages which were not rendered WICKET-5387 Page#onInitialize called after an exception in the constructor of Page
    Add specializations of the test with the three render strategies.

---
+++ wicket-core/src/test/java/org/apache/wicket/dontstoreunrendered/DontStoreNotRenderedPageOnePassRenderTest.java
@@ -0,0 +1,42 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.wicket.dontstoreunrendered;
+
+import org.apache.wicket.mock.MockApplication;
+import org.apache.wicket.protocol.http.WebApplication;
+import org.apache.wicket.settings.IRequestCycleSettings;
+
+/**
+ * https://issues.apache.org/jira/browse/WICKET-5415
+ */
+public class DontStoreNotRenderedPageOnePassRenderTest extends DontStoreNotRenderedPageTestCase
+{
+	@Override
+	protected WebApplication newApplication()
+	{
+		return new MockApplication()
+		{
+			@Override
+			protected void init()
+			{
+				super.init();
+
+				getRequestCycleSettings().setRenderStrategy(IRequestCycleSettings.RenderStrategy.ONE_PASS_RENDER);
+			}
+		};
+	}
+}
