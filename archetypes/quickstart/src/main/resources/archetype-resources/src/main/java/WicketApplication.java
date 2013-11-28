c8abde8be1c0c983a7ac837a1b057a476478b783
Martin Tzvetanov Grigorov <mgrigorov@apache.org>
2013-11-28 15:02:55 +0100

    Whitespace fixes

--- archetypes/quickstart/src/main/resources/archetype-resources/src/main/java/WicketApplication.java
+++ archetypes/quickstart/src/main/resources/archetype-resources/src/main/java/WicketApplication.java
@@ -4,12 +4,13 @@ import org.apache.wicket.markup.html.WebPage;
 import org.apache.wicket.protocol.http.WebApplication;
 
 /**
- * Application object for your web application. If you want to run this application without deploying, run the Start class.
+ * Application object for your web application.
+ * If you want to run this application without deploying, run the Start class.
  * 
  * @see ${package}.Start#main(String[])
  */
 public class WicketApplication extends WebApplication
-{    	
+{
 	/**
 	 * @see org.apache.wicket.Application#getHomePage()
 	 */
