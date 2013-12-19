fb45a781c24f17d7a44658061c4f17e95c4cd1ef
svenmeier <sven@meiers.net>
2013-12-19 15:11:13 +0100

    WICKET-5426 reset stateless flag on render, since stateful components might be added *and* removed

--- wicket-core/src/main/java/org/apache/wicket/Page.java
+++ wicket-core/src/main/java/org/apache/wicket/Page.java
@@ -801,11 +801,8 @@ public abstract class Page extends MarkupContainer implements IRedirectListener,
 		// Make sure it is really empty
 		renderedComponents = null;
 
-		// if the page is stateless, reset the flag so that it is tested again
-		if (Boolean.TRUE.equals(stateless))
-		{
-			stateless = null;
-		}
+		// rendering might remove or add stateful components, so clear flag to force reevaluation
+		stateless = null;
 
 		super.onBeforeRender();
 
@@ -1020,6 +1017,8 @@ public abstract class Page extends MarkupContainer implements IRedirectListener,
 		{
 			++renderCount;
 			render();
+
+			// stateless = null;
 		}
 		finally
 		{
