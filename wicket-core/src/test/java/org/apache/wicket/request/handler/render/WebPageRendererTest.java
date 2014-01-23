ecdfc124453ca614178d33d0c119d4e71872cfef
svenmeier <sven@meiers.net>
2014-01-23 14:30:01 +0100

    WICKET-5484 rendering and writing of a page should never happen on an Ajax request, even if other circumstances imply otherwise

--- wicket-core/src/test/java/org/apache/wicket/request/handler/render/WebPageRendererTest.java
+++ wicket-core/src/test/java/org/apache/wicket/request/handler/render/WebPageRendererTest.java
@@ -574,7 +574,7 @@ public class WebPageRendererTest
 		// if
 		// the policy is never to redirect
 		renderer.redirectPolicy = RedirectPolicy.NEVER_REDIRECT;
-		renderer.ajax = true;
+		renderer.ajax = false;
 		renderer.onePassRender = true;
 		renderer.redirectToRender = true;
 		renderer.shouldPreserveClientUrl = true;
@@ -635,7 +635,7 @@ public class WebPageRendererTest
 		renderer.shouldPreserveClientUrl = true;
 
 		renderer.redirectPolicy = RedirectPolicy.AUTO_REDIRECT;
-		renderer.ajax = true;
+		renderer.ajax = false;
 		renderer.onePassRender = false;
 		renderer.redirectToRender = false;
 		renderer.newPageInstance = true;
@@ -698,26 +698,26 @@ public class WebPageRendererTest
 						"    XXXXXXXXXXXX" +
 						"    X   XXXXXXXX" +
 						"    XXXXXXXXXXXX" +
-						"        XXXXXXXX" +
-						"    XXXXXXXXXXXX" +
-						"        XXXXXXXX" +
-						"    XXXXXXXXXXXX" +
+						"                " +
+						"                " +
+						"                " +
+						"                " +
 						"XXXXXXXXXXXXXXXX" +
 						"XXXXXXXXXXXXXXXX" +
 						"XXXXXXXXXXXXXXXX" +
 						"XXXXXXXXXXXXXXXX" +
-						"XXXXXXXXXXXXXXXX" +
-						"XXXXXXXXXXXXXXXX" +
-						"XXXXXXXXXXXXXXXX" +
-						"XXXXXXXXXXXXXXXX" +
+						"                " +
+						"                " +
+						"                " +
+						"                " +
 						"    X   XXXXXXXX" +
 						"    XXXXXXXXXXXX" +
 						"XXXXXXXXXXXXXXXX" +
 						"XXXXXXXXXXXXXXXX" +
-						"        XXXXXXXX" +
-						"    XXXXXXXXXXXX" +
-						"        XXXXXXXX" +
-						"    XXXXXXXXXXXX";
+						"                " +
+						"                " +
+						"                " +
+						"                ";
 
 		checkVariations(match,new ShouldRenderPageAndWriteResponseVariations());
 	}
@@ -955,5 +955,52 @@ public class WebPageRendererTest
 		verify(response, never()).write(any(byte[].class));
 		verify(response).sendRedirect(anyString());
 		Assert.assertTrue(stored.get());
+	}
+
+	/**
+	 * Tests that when {@link WebRequest#shouldPreserveClientUrl()} returns {@code true} and the
+	 * current request is Ajax then a redirect should be issued
+	 */
+	@Test
+	public void testShouldPreserveClientUrlAndAjaxRequest()
+	{
+		TestPageRenderer renderer = new TestPageRenderer(handler);
+		renderer.ajax = true;
+		renderer.newPageInstance = true;
+
+		when(urlRenderer.getBaseUrl()).thenReturn(Url.parse("base"));
+
+		when(requestCycle.mapUrlFor(eq(handler))).thenReturn(Url.parse("base/a"));
+
+		when(request.shouldPreserveClientUrl()).thenReturn(true);
+
+		renderer.respond(requestCycle);
+
+		verify(response).sendRedirect(anyString());
+		verify(response, never()).write(any(byte[].class));
+
+	}
+
+	/**
+	 * Tests that when {@link RedirectPolicy#NEVER_REDIRECT} is configured but the current request
+	 * is Ajax then a redirect should still be issued
+	 */
+	@Test
+	public void testNeverRedirectButAjaxRequest()
+	{
+		TestPageRenderer renderer = new TestPageRenderer(handler);
+		renderer.redirectPolicy = RedirectPolicy.NEVER_REDIRECT;
+		renderer.ajax = true;
+		renderer.newPageInstance = true;
+
+		when(urlRenderer.getBaseUrl()).thenReturn(Url.parse("base"));
+
+		when(requestCycle.mapUrlFor(eq(handler))).thenReturn(Url.parse("base/a"));
+
+		renderer.respond(requestCycle);
+
+		verify(response).sendRedirect(anyString());
+		verify(response, never()).write(any(byte[].class));
+
 	}
 }
