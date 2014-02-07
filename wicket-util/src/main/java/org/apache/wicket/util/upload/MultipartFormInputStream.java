7cd01f71cbf3688c22b095dfd9bab2fae5db3c39
Martin Tzvetanov Grigorov <mgrigorov@apache.org>
2014-02-07 15:15:21 +0100

    WICKET-5502 Patch FileUploadBase to fix CVE-2014-0050
    (cherry picked from commit b7fe180d850da71b0ac639ff741b0c590b4cd6eb)

--- wicket-util/src/main/java/org/apache/wicket/util/upload/MultipartFormInputStream.java
+++ wicket-util/src/main/java/org/apache/wicket/util/upload/MultipartFormInputStream.java
@@ -321,9 +321,15 @@ public class MultipartFormInputStream
 
 		// We prepend CR/LF to the boundary to chop trailng CR/LF from
 		// body-data tokens.
-		this.boundary = new byte[boundary.length + BOUNDARY_PREFIX.length];
 		boundaryLength = boundary.length + BOUNDARY_PREFIX.length;
+		if (bufSize < this.boundaryLength + 1)
+		{
+			throw new IllegalArgumentException(
+				"The buffer size specified for the MultipartFormInputStream is too small");
+		}
+		this.boundary = new byte[this.boundaryLength];
 		keepRegion = this.boundary.length;
+
 		System.arraycopy(BOUNDARY_PREFIX, 0, this.boundary, 0, BOUNDARY_PREFIX.length);
 		System.arraycopy(boundary, 0, this.boundary, BOUNDARY_PREFIX.length, boundary.length);
 
@@ -346,6 +352,7 @@ public class MultipartFormInputStream
 	 * 
 	 * @see #MultipartFormInputStream(InputStream, byte[], int,
 	 *      MultipartFormInputStream.ProgressNotifier)
+	 * @throws IllegalArgumentException If the buffer size is too small
 	 */
 	MultipartFormInputStream(final InputStream input, final byte[] boundary,
 		final ProgressNotifier pNotifier)
