669e595a24976c27ec01e3fa2c1fb858288f6c57
Martijn Dashorst <dashorst@apache.org>
2014-04-17 23:42:50 +0200

    Remove obsolete experimental modules
    As previously discussed on the dev@ list, these experimental
    modules are no longer needed and better served with
    alternatives, or with a refocused effort.
    
    Conflicts:
    	pom.xml
    	wicket-experimental/wicket-bootstrap/pom.xml
    	wicket-experimental/wicket-bootstrap/src/main/java/org/apache/wicket/bootstrap/Bootstrap.java
    	wicket-experimental/wicket-examples-parent/pom.xml
    	wicket-experimental/wicket-examples-parent/wicket-examples-jar/pom.xml
    	wicket-experimental/wicket-examples-parent/wicket-examples-jar/src/main/java/org/apache/wicket/examples/base/Examples.java
    	wicket-experimental/wicket-examples-parent/wicket-examples-jar/src/main/java/org/apache/wicket/examples/base/prettify/Prettify.java
    	wicket-experimental/wicket-examples-parent/wicket-examples-jar/src/test/java/org/apache/wicket/examples/base/markdown/MarkdownTest.java
    	wicket-experimental/wicket-examples-parent/wicket-examples-war/pom.xml

--- wicket-experimental/wicket-examples-parent/wicket-examples-jar/src/main/java/org/apache/wicket/examples/base/HomePage.java
+++
@@ -1,9 +0,0 @@
-package org.apache.wicket.examples.base;
-
-import org.apache.wicket.examples.base.annot.ExamplePage;
-
-@ExamplePage(title = "Home")
-public class HomePage extends AbstractBasePage
-{
-	private static final long serialVersionUID = 1L;
-}
--- wicket-experimental/wicket-examples-parent/wicket-examples-jar/src/main/java/org/apache/wicket/examples/components/Layout.md
+++
@@ -1,4 +0,0 @@
-<div class="page-header">
-	<h2>Layout <small>Panels, borders, fragments, tabs and markup inheritance</small></h2>
-</div>
-
