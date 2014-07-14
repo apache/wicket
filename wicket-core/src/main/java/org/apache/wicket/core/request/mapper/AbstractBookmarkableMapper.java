bea11510645192d94b6b2c226e220b72a1364d15
Martin Tzvetanov Grigorov <mgrigorov@apache.org>
2014-07-14 10:52:08 +0300

    remove obsolete comment about page maps

--- wicket-core/src/main/java/org/apache/wicket/core/request/mapper/AbstractBookmarkableMapper.java
+++ wicket-core/src/main/java/org/apache/wicket/core/request/mapper/AbstractBookmarkableMapper.java
@@ -300,7 +300,7 @@ public abstract class AbstractBookmarkableMapper extends AbstractComponentMapper
 
 			if (info == null)
 			{
-				// if there are is no page instance information (only page map name - optionally)
+				// if there are is no page instance information
 				// then this is a simple bookmarkable URL
 				return processBookmarkable(pageClass, pageParameters);
 			}
