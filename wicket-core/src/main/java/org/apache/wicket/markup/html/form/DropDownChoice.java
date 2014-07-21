f314fc4193628dc9e2bd9e77a0571e148c118115
Martin Tzvetanov Grigorov <mgrigorov@apache.org>
2014-07-21 17:23:20 +0300

    Optimize imports

--- wicket-core/src/main/java/org/apache/wicket/markup/html/form/DropDownChoice.java
+++ wicket-core/src/main/java/org/apache/wicket/markup/html/form/DropDownChoice.java
@@ -21,8 +21,6 @@ import java.util.List;
 import org.apache.wicket.ajax.form.AjaxFormComponentUpdatingBehavior;
 import org.apache.wicket.markup.ComponentTag;
 import org.apache.wicket.model.IModel;
-import org.apache.wicket.core.request.handler.ListenerInterfaceRequestHandler;
-import org.apache.wicket.core.request.handler.PageAndComponentProvider;
 import org.apache.wicket.request.mapper.parameter.PageParameters;
 
 
