54337592c72be89d38c0f8984ecb2d083e91895b
Martin Tzvetanov Grigorov <mgrigorov@apache.org>
2014-08-12 17:08:30 +0200

    WICKET-5452 Make Wicket-Atmosphere testable - AtmosphereTester
    Quick and dirty implementation of WicketAtmosphereTester
    
    (cherry picked from commit 2ab1d798e3348ba7037ab461e1be6df55cd68457)

---
+++ wicket-experimental/wicket-atmosphere/src/main/java/org/apache/wicket/atmosphere/tester/TesterEventBus.java
@@ -0,0 +1,66 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.wicket.atmosphere.tester;
+
+import org.apache.wicket.application.IComponentOnBeforeRenderListener;
+import org.apache.wicket.atmosphere.EventBus;
+import org.apache.wicket.protocol.http.WebApplication;
+import org.atmosphere.cpr.AtmosphereConfig;
+import org.atmosphere.cpr.AtmosphereFramework;
+import org.atmosphere.cpr.BroadcasterLifeCyclePolicy;
+import org.atmosphere.cpr.DefaultBroadcasterFactory;
+import org.atmosphere.util.SimpleBroadcaster;
+
+/**
+ *
+ */
+public class TesterEventBus extends EventBus
+{
+	AtmosphereFramework framework = new AtmosphereFramework();
+	AtmosphereConfig config = new AtmosphereConfig(framework);
+	IComponentOnBeforeRenderListener eventSubscriptionCollector;
+
+	public TesterEventBus(WebApplication application)
+	{
+		super(application, new SimpleBroadcaster());
+
+		framework.setBroadcasterFactory(new TesterBroadcasterFactory(config));
+
+		getBroadcaster().initialize("wicket-atmopshere-tester", config);
+	}
+
+	@Override
+	protected IComponentOnBeforeRenderListener createEventSubscriptionCollector()
+	{
+		eventSubscriptionCollector = super.createEventSubscriptionCollector();
+		return eventSubscriptionCollector;
+	}
+
+	@Override
+	public SimpleBroadcaster getBroadcaster()
+	{
+		return (SimpleBroadcaster) super.getBroadcaster();
+	}
+
+	private static class TesterBroadcasterFactory extends DefaultBroadcasterFactory
+	{
+		protected TesterBroadcasterFactory(AtmosphereConfig c)
+		{
+			super(SimpleBroadcaster.class, BroadcasterLifeCyclePolicy.ATMOSPHERE_RESOURCE_POLICY.NEVER.name(), c);
+		}
+	}
+}
