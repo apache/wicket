1962d273fe3e4a0764bbdd37679e9a627b830bc9
Martin Tzvetanov Grigorov <mgrigorov@apache.org>
2014-08-12 15:14:37 +0200

    No need to cache the response object. It is passed to the callback.

--- wicket-experimental/wicket-atmosphere/src/main/java/org/apache/wicket/atmosphere/jquery.wicketatmosphere.js
+++ wicket-experimental/wicket-atmosphere/src/main/java/org/apache/wicket/atmosphere/jquery.wicketatmosphere.js
@@ -20,7 +20,6 @@
 
 	jQuery.fn.wicketAtmosphere = function(params) {
 		var callbackAdded = false;
-		var response;
 
 		// jquery.atmosphere.response
 		function callback(response) {
@@ -37,7 +36,6 @@
 		var connectedEndpoint = $.atmosphere.subscribe(params.url,
 				!callbackAdded ? callback : null, $.atmosphere.request = params);
 		callbackAdded = true;
-		response = $.atmosphere.response;
 
 		$(window).bind("beforeunload", function() {
 			callbackAdded = false;
