50cb47f8ddadcdf9743c8994bbd739edf95eda44
Martin Tzvetanov Grigorov <mgrigorov@apache.org>
2014-08-13 10:32:46 +0200

    Rename WicketAtmosphereTester to AtmosphereTester
    Start the page after configuring AtmosphereTester/TesterEventBus so the method collector is called for all components in the page
    Expose TesterBroadcasterFactory as BroadcasterFactory#getDefault()

--- wicket-experimental/wicket-atmosphere/src/test/java/org/apache/wicket/atmosphere/AtmosphereTest.java
+++ wicket-experimental/wicket-atmosphere/src/test/java/org/apache/wicket/atmosphere/AtmosphereTest.java
@@ -19,7 +19,7 @@ package org.apache.wicket.atmosphere;
 import java.util.Date;
 
 import org.apache.wicket.ajax.AjaxRequestTarget;
-import org.apache.wicket.atmosphere.tester.WicketAtmosphereTester;
+import org.apache.wicket.atmosphere.tester.AtmosphereTester;
 import org.apache.wicket.request.mapper.parameter.PageParameters;
 import org.apache.wicket.util.tester.WicketTester;
 import org.junit.Assert;
@@ -31,7 +31,7 @@ import org.junit.Test;
 public class AtmosphereTest extends Assert
 {
 	@Test
-	public void aa()
+	public void atmospherePush()
 	{
 		WicketTester tester = new WicketTester();
 		HomePage page = new HomePage(new PageParameters())
@@ -54,9 +54,7 @@ public class AtmosphereTest extends Assert
 			}
 		};
 
-		tester.startPage(page);
-
-		WicketAtmosphereTester waTester = new WicketAtmosphereTester(tester, page);
+		AtmosphereTester waTester = new AtmosphereTester(tester, page);
 
 		Date payload = new Date();
 		waTester.post(payload);
