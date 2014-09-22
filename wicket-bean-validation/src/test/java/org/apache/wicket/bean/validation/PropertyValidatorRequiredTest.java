49aaea8313fb2079e90579e434e6239e21ee652b
Martin Tzvetanov Grigorov <mgrigorov@apache.org>
2014-09-22 22:51:20 +0200

    Java 6 no-diamonds

--- wicket-bean-validation/src/test/java/org/apache/wicket/bean/validation/PropertyValidatorRequiredTest.java
+++ wicket-bean-validation/src/test/java/org/apache/wicket/bean/validation/PropertyValidatorRequiredTest.java
@@ -163,7 +163,7 @@ public class PropertyValidatorRequiredTest
 				"bean.propertyOneTwo")).add(new PropertyValidator<String>(GroupThree.class));
 
 			input21 = new TextField<String>("input21", new PropertyModel<String>(this,
-				"bean.subBeanList[0].property")).add(new PropertyValidator<>());
+				"bean.subBeanList[0].property")).add(new PropertyValidator<String>());
 
 			form.add(input1, input2, input3, input4, input5, input6, input7, input8, input9,
 				input10, input11, input12, input13, input14, input15, input16, input17, input18,
