5837817c94c7bb707f187526fb4e4e084f10d8f3
Martin Tzvetanov Grigorov <mgrigorov@apache.org>
2014-09-30 14:55:04 +0200

    WICKET-5711 OnChangeAjaxBehavior should listen for both 'inputchange' and 'change' events for TextField and TextArea
    (cherry picked from commit 6501ab7396e888b0eccd70b0b502334a69c823a8)
    
    Conflicts:
    	wicket-core/src/main/java/org/apache/wicket/ajax/form/OnChangeAjaxBehavior.java
    	wicket-core/src/test/java/org/apache/wicket/ajax/form/OnChangeAjaxBehaviorTestPage_expected.html

--- wicket-core/src/main/java/org/apache/wicket/ajax/form/OnChangeAjaxBehavior.java
+++ wicket-core/src/main/java/org/apache/wicket/ajax/form/OnChangeAjaxBehavior.java
@@ -47,7 +47,8 @@ public abstract class OnChangeAjaxBehavior extends AjaxFormComponentUpdatingBeha
 	 * for text input form component depending on the browser.
 	 * 'change' is used as a fallback for all other form component types.
 	 */
-	public static final String EVENT_INPUTCHANGE = "inputchange";
+	public static final String EVENT_NAME = "inputchange change";
+
 	public static final String EVENT_CHANGE = "change";
 
 	/**
@@ -55,23 +56,19 @@ public abstract class OnChangeAjaxBehavior extends AjaxFormComponentUpdatingBeha
 	 */
 	public OnChangeAjaxBehavior()
 	{
-		super(EVENT_INPUTCHANGE + " " + EVENT_CHANGE);
+		super(EVENT_NAME);
 	}
 
 	@Override
 	protected void updateAjaxAttributes(AjaxRequestAttributes attributes) 
 	{
 		super.updateAjaxAttributes(attributes);
-		
+
 		Component component = getComponent();
-		
-		//textfiels and textareas will trigger this behavior with event 'inputchange'
-		//while all the other components will use 'change'
-		if (component instanceof TextField || component instanceof TextArea) 
-		{
-			attributes.setEventNames(EVENT_INPUTCHANGE);
-		} 
-		else 
+
+		// textfiels and textareas will trigger this behavior with either 'inputchange' or 'change' events
+		// all the other components will use just 'change'
+		if (!(component instanceof TextField || component instanceof TextArea))
 		{
 			attributes.setEventNames(EVENT_CHANGE);
 		}
