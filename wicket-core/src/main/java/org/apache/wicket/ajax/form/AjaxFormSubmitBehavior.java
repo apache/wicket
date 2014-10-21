9716f2a7e799133fdf3f7927d0093e6dfe16f529
Andrea Del Bene <an.delbene@gmail.com>
2014-10-21 11:35:35 +0200

    WICKET-5197 Rename AjaxRequestAttributes#allowDefault to #preventDefault
    (AJAX Guestbook example was broken)

--- wicket-core/src/main/java/org/apache/wicket/ajax/form/AjaxFormSubmitBehavior.java
+++ wicket-core/src/main/java/org/apache/wicket/ajax/form/AjaxFormSubmitBehavior.java
@@ -148,6 +148,8 @@ public abstract class AjaxFormSubmitBehavior extends AjaxEventBehavior
 			String submittingComponentName = ((IFormSubmittingComponent)getComponent()).getInputName();
 			attributes.setSubmittingComponentName(submittingComponentName);
 		}
+		
+		attributes.setPreventDefault(true);
 	}
 
 	/**
