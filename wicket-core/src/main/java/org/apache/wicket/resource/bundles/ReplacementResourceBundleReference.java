06027af8e11e7a4c5bde50d7d174b523539c9d73
Andrea Del Bene <adelbene@apache.org>
2014-11-08 16:27:19 +0100

    WICKET-5752 ReplacementResourceBundleReference should return the dependencies for the replacing resource reference

--- wicket-core/src/main/java/org/apache/wicket/resource/bundles/ReplacementResourceBundleReference.java
+++ wicket-core/src/main/java/org/apache/wicket/resource/bundles/ReplacementResourceBundleReference.java
@@ -16,6 +16,9 @@
  */
 package org.apache.wicket.resource.bundles;
 
+import java.util.List;
+
+import org.apache.wicket.markup.head.HeaderItem;
 import org.apache.wicket.request.resource.ResourceReference;
 
 /**
@@ -38,5 +41,16 @@ public class ReplacementResourceBundleReference extends ResourceBundleReference
 	public ReplacementResourceBundleReference(ResourceReference bundleRef)
 	{
 		super(bundleRef);
+	}
+	
+	/**
+	 * Returns the dependencies of the replacing resource reference.
+	 * 
+	 * @return the dependencies of the replacing resource reference
+	 */
+	@Override
+	public List<HeaderItem> getDependencies() 
+	{
+		return getBundleReference().getDependencies();
 	}
 }
