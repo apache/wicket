2200ad261d11d0354bd66c89d3665fce651f816e
Martin Tzvetanov Grigorov <mgrigorov@apache.org>
2014-11-20 10:25:04 +0200

    WICKET-5770 PageParametersEncoder should not decode parameters with no name

--- wicket-request/src/main/java/org/apache/wicket/request/mapper/parameter/PageParametersEncoder.java
+++ wicket-request/src/main/java/org/apache/wicket/request/mapper/parameter/PageParametersEncoder.java
@@ -18,6 +18,7 @@ package org.apache.wicket.request.mapper.parameter;
 
 import org.apache.wicket.request.Url;
 import org.apache.wicket.request.Url.QueryParameter;
+import org.apache.wicket.util.string.Strings;
 
 /**
  * Simple encoder with direct indexed/named parameters mapping.
@@ -50,7 +51,11 @@ public class PageParametersEncoder implements IPageParametersEncoder
 
 		for (QueryParameter p : url.getQueryParameters())
 		{
-			parameters.add(p.getName(), p.getValue());
+			String parameterName = p.getName();
+			if (Strings.isEmpty(parameterName) == false)
+			{
+				parameters.add(parameterName, p.getValue());
+			}
 		}
 
 		return parameters.isEmpty() ? null : parameters;
