29bb9a4d8f47f5155dcfa6a00c79b12bc3dcc284
klopfdreh <klopfdreh@tobiass-mbp>
2015-01-22 22:52:18 +0100

    WICKET-5819 Media tags - audio / video implementation

---
+++ wicket-core/src/main/java/org/apache/wicket/markup/html/media/MediaStreamingResourceReference.java
@@ -0,0 +1,306 @@
+package org.apache.wicket.markup.html.media;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import java.util.Locale;
+
+import org.apache.wicket.Application;
+import org.apache.wicket.WicketRuntimeException;
+import org.apache.wicket.core.util.resource.PackageResourceStream;
+import org.apache.wicket.protocol.http.servlet.ResponseIOException;
+import org.apache.wicket.request.Request;
+import org.apache.wicket.request.Response;
+import org.apache.wicket.request.cycle.RequestCycle;
+import org.apache.wicket.request.http.WebRequest;
+import org.apache.wicket.request.http.WebResponse;
+import org.apache.wicket.request.resource.AbstractResource;
+import org.apache.wicket.request.resource.ContentDisposition;
+import org.apache.wicket.request.resource.IResource;
+import org.apache.wicket.request.resource.ResourceReference;
+
+/**
+ * The media streaming resource reference is used to provided streamed data based on bytes requested
+ * by the client for video and audio files
+ * 
+ * @author Tobias Soloschenko
+ */
+public class MediaStreamingResourceReference extends ResourceReference
+{
+
+	private static final long serialVersionUID = 1L;
+
+	private Integer buffer;
+
+	public MediaStreamingResourceReference(Class<?> scope, String name, Locale locale,
+		String style, String variation)
+	{
+		super(scope, name, locale, style, variation);
+	}
+
+	public MediaStreamingResourceReference(Class<?> scope, String name)
+	{
+		super(scope, name, RequestCycle.get().getRequest().getLocale(), null, null);
+	}
+
+	public MediaStreamingResourceReference(Key key)
+	{
+		super(key);
+	}
+
+	public MediaStreamingResourceReference(String name)
+	{
+		super(name);
+	}
+
+	@Override
+	public IResource getResource()
+	{
+		AbstractResource mediaStreamingResource = new AbstractResource()
+		{
+			private static final long serialVersionUID = 1L;
+			private Long startbyte;
+			private Long endbyte;
+			private PackageResourceStream packageResourceStream;
+
+			@Override
+			protected ResourceResponse newResourceResponse(Attributes attributes)
+			{
+				try
+				{
+					Request request = attributes.getRequest();
+					Response response = attributes.getResponse();
+					if (request instanceof WebRequest && response instanceof WebResponse)
+					{
+						WebRequest webRequest = (WebRequest)request;
+						WebResponse webResponse = (WebResponse)response;
+
+						this.packageResourceStream = new PackageResourceStream(
+							MediaStreamingResourceReference.this.getScope(),
+							MediaStreamingResourceReference.this.getName(),
+							MediaStreamingResourceReference.this.getLocale(),
+							MediaStreamingResourceReference.this.getStyle(),
+							MediaStreamingResourceReference.this.getVariation());
+						long length = this.packageResourceStream.length().bytes();
+
+						ResourceResponse resourceResponse = new ResourceResponse();
+						resourceResponse.setContentType(this.packageResourceStream.getContentType());
+						resourceResponse.setFileName(MediaStreamingResourceReference.this.getName());
+						resourceResponse.setContentDisposition(ContentDisposition.ATTACHMENT);
+						resourceResponse.setLastModified(this.packageResourceStream.lastModifiedTime());
+
+						// We accept ranges, so that the player can
+						// load and play content from a specific byte position
+						webResponse.setHeader("Accept-Range", "bytes");
+
+						// Calculating the response code and the byte range to be played
+						String rangeHeader = webRequest.getHeader("range");
+						if (rangeHeader == null || "".equals(rangeHeader))
+						{
+							resourceResponse.setStatusCode(200);
+							resourceResponse.setContentLength(length);
+						}
+						else
+						{
+							rangeHeader = rangeHeader.replaceAll(" ", "");
+							// If the range header is filled 206 for
+							// partial content has to be returned
+							resourceResponse.setStatusCode(206);
+
+							// And now the calculation of the range to be read
+							// and to be given as response within the Content-Range header
+							String range = rangeHeader.substring(rangeHeader.indexOf('=') + 1,
+								rangeHeader.length());
+							String[] rangeParts = range.split("-");
+							if (rangeParts[0].equals("0"))
+							{
+								webResponse.setHeader("Content-Range", "bytes 0-" + (length - 1) +
+									"/" + length);
+								resourceResponse.setContentLength(length);
+							}
+							else
+							{
+								this.startbyte = Long.parseLong(rangeParts[0]);
+								if (rangeParts.length == 2)
+								{
+									this.endbyte = Long.parseLong(rangeParts[1]);
+								}
+								else
+								{
+									this.endbyte = length - 1;
+								}
+								webResponse.setHeader("Content-Range", "bytes " + this.startbyte +
+									"-" + this.endbyte + "/" + length);
+								resourceResponse.setContentLength((this.endbyte - this.startbyte) + 1);
+							}
+						}
+
+						resourceResponse.setWriteCallback(new WriteCallback()
+						{
+							@Override
+							public void writeData(Attributes attributes) throws IOException
+							{
+								try
+								{
+									InputStream inputStream = packageResourceStream.getInputStream();
+									OutputStream outputStream = attributes.getResponse()
+										.getOutputStream();
+									byte[] buffer = new byte[MediaStreamingResourceReference.this.getBuffer()];
+
+									if (startbyte != null || endbyte != null)
+									{
+										// skipping the first bytes which are
+										// not requested by the client
+										inputStream.skip(startbyte);
+
+										long totalBytes = 0;
+										int actualReadBytes = 0;
+
+										while ((actualReadBytes = inputStream.read(buffer)) != -1)
+										{
+											totalBytes = totalBytes + buffer.length;
+											long lowerBuffer = endbyte - totalBytes;
+											if (lowerBuffer <= 0)
+											{
+												buffer = (byte[])resizeArray(buffer,
+													actualReadBytes);
+												outputStream.write(buffer);
+												break;
+											}
+											else
+											{
+												outputStream.write(buffer);
+											}
+										}
+									}
+									else
+									{
+										while (inputStream.read(buffer) != -1)
+										{
+											outputStream.write(buffer);
+										}
+									}
+								}
+								catch (ResponseIOException e)
+								{
+									// the client has closed the connection and
+									// doesn't read the stream further on
+									// (in tomcats
+									// org.apache.catalina.connector.ClientAbortException)
+									// we ignore this case
+								}
+								catch (Exception e)
+								{
+									StringWriter stringWriter = printStack(e);
+									throw new WicketRuntimeException(stringWriter.toString());
+								}
+							}
+						});
+
+						return resourceResponse;
+					}
+					else
+					{
+						throw new IllegalStateException(
+							"Either the request is no web request or the response is no web response");
+					}
+				}
+				catch (Exception e)
+				{
+					StringWriter stringWriter = this.printStack(e);
+					throw new WicketRuntimeException(stringWriter.toString());
+				}
+				finally
+				{
+					if (this.packageResourceStream != null)
+					{
+						try
+						{
+							this.packageResourceStream.close();
+						}
+						catch (IOException e)
+						{
+							StringWriter stringWriter = this.printStack(e);
+							throw new WicketRuntimeException(stringWriter.toString());
+						}
+					}
+				}
+			}
+
+			/**
+			 * Prints the stack trace to a print writer
+			 * 
+			 * @param exception
+			 *            the exception
+			 * @return the string writer containing the stack trace
+			 */
+			private StringWriter printStack(Exception exception)
+			{
+				StringWriter stringWriter = new StringWriter();
+				PrintWriter printWriter = new PrintWriter(stringWriter);
+				exception.printStackTrace(printWriter);
+				return stringWriter;
+			}
+		};
+		return mediaStreamingResource;
+
+	}
+
+	/**
+	 * Sets the buffer size used to send the data to the client
+	 * 
+	 * @return the buffer size used to send the data to the client
+	 */
+	public Integer getBuffer()
+	{
+		return this.buffer != null ? this.buffer : 4048;
+	}
+
+	/**
+	 * Sets the buffer size used to send the data to the client
+	 * 
+	 * @param buffer
+	 *            the buffer size used to send the data to the client
+	 */
+	public void setBuffer(Integer buffer)
+	{
+		this.buffer = buffer;
+	}
+
+	/**
+	 * Reallocates an array with a new size, and copies the contents of the old array to the new
+	 * array.
+	 * 
+	 * @param oldArray
+	 *            the old array, to be reallocated.
+	 * @param newSize
+	 *            the new array size.
+	 * @return A new array with the same contents.
+	 */
+	@SuppressWarnings("rawtypes")
+	private static Object resizeArray(Object oldArray, int newSize)
+	{
+		int oldSize = java.lang.reflect.Array.getLength(oldArray);
+		Class elementType = oldArray.getClass().getComponentType();
+		Object newArray = java.lang.reflect.Array.newInstance(elementType, newSize);
+		int preserveLength = Math.min(oldSize, newSize);
+		if (preserveLength > 0)
+		{
+			System.arraycopy(oldArray, 0, newArray, 0, preserveLength);
+		}
+		return newArray;
+	}
+
+	/**
+	 * Gets the type of the media this resource reference belongs to
+	 * 
+	 * @return the type of this media
+	 */
+	public String getType()
+	{
+		final String resourceName = MediaStreamingResourceReference.this.getName();
+		return Application.get().getMimeType(resourceName);
+	}
+}
---
+++ wicket-core/src/main/java/org/apache/wicket/markup/html/media/Track.java
@@ -0,0 +1,274 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.wicket.markup.html.media;
+
+import java.util.Locale;
+
+import org.apache.wicket.markup.ComponentTag;
+import org.apache.wicket.markup.html.WebMarkupContainer;
+import org.apache.wicket.model.IModel;
+import org.apache.wicket.request.cycle.RequestCycle;
+import org.apache.wicket.request.mapper.parameter.PageParameters;
+import org.apache.wicket.request.resource.ResourceReference;
+
+/**
+ * The track tag is used to provide subtitles, captions, descriptions, chapters, metadata to a video
+ * media component
+ * 
+ * @author Tobias Soloschenko
+ * 
+ */
+public class Track extends WebMarkupContainer
+{
+
+	private static final long serialVersionUID = 1L;
+
+	private Kind kind;
+
+	private String label;
+
+	private Boolean defaultTrack;
+
+	private Locale srclang;
+
+	private ResourceReference resourceReference;
+
+	private String url;
+
+	private PageParameters pageParameters;
+
+	public Track(String id)
+	{
+		super(id);
+	}
+
+	public Track(String id, IModel<?> model)
+	{
+		super(id, model);
+	}
+
+	public Track(String id, ResourceReference resourceReference)
+	{
+		this(id);
+		this.resourceReference = resourceReference;
+	}
+
+	public Track(String id, IModel<?> model, ResourceReference resourceReference)
+	{
+		this(id, model);
+		this.resourceReference = resourceReference;
+	}
+
+	public Track(String id, ResourceReference resourceReference, PageParameters pageParameters)
+	{
+		this(id);
+		this.resourceReference = resourceReference;
+		this.pageParameters = pageParameters;
+	}
+
+	public Track(String id, IModel<?> model, ResourceReference resourceReference,
+		PageParameters pageParameters)
+	{
+		this(id, model);
+		this.resourceReference = resourceReference;
+		this.pageParameters = pageParameters;
+	}
+
+	public Track(String id, String url)
+	{
+		this(id);
+		this.url = url;
+	}
+
+	public Track(String id, IModel<?> model, String url)
+	{
+		this(id, model);
+		this.url = url;
+	}
+
+	public Track(String id, String url, PageParameters pageParameters)
+	{
+		this(id);
+		this.url = url;
+		this.pageParameters = pageParameters;
+	}
+
+	public Track(String id, IModel<?> model, String url, PageParameters pageParameters)
+	{
+		this(id, model);
+		this.url = url;
+		this.pageParameters = pageParameters;
+	}
+
+	@Override
+	protected void onComponentTag(ComponentTag tag)
+	{
+		this.checkComponentTag(tag, "track");
+		super.onComponentTag(tag);
+
+		if (this.resourceReference != null)
+		{
+			tag.put("src", RequestCycle.get().urlFor(this.resourceReference, this.pageParameters));
+		}
+
+		if (this.url != null)
+		{
+			tag.put("src", this.url);
+		}
+
+		if (this.kind != null)
+		{
+			tag.put("kind", this.kind.name());
+		}
+
+		if (this.label != null)
+		{
+			tag.put("label", this.label);
+		}
+
+		if (this.defaultTrack != null && this.defaultTrack)
+		{
+			tag.put("default", "default");
+		}
+
+		// if the srclang field is set use this, else if the
+		// resource reference provides a locale use the language
+		// of the resource reference
+		if (this.srclang != null)
+		{
+			tag.put("srclang", this.srclang.getLanguage());
+		}
+		else if (this.resourceReference != null && this.resourceReference.getLocale() != null)
+		{
+			tag.put("srclang", this.resourceReference.getLocale().getLanguage());
+		}
+	}
+
+	/**
+	 * Gets the kind of the track belongs to the media component
+	 * 
+	 * @see {@link #setKind(Kind)}
+	 * 
+	 * @return the kind
+	 */
+	public Kind getKind()
+	{
+		return this.kind;
+	}
+
+	/**
+	 * Sets the kind of the track belongs to the media component<br>
+	 * <br>
+	 * <b>subtitles</b>: Transcription or translation of the dialogue, suitable for when the sound
+	 * is available but not understood (e.g. because the user does not understand the language of
+	 * the media resource's soundtrack). Displayed over the video.<br>
+	 * <br>
+	 * <b>captions</b>: Transcription or translation of the dialogue, sound effects, relevant
+	 * musical cues, and other relevant audio information, suitable for when the soundtrack is
+	 * unavailable (e.g. because it is muted or because the user is deaf). Displayed over the video;
+	 * labeled as appropriate for the hard-of-hearing.<br>
+	 * <br>
+	 * <b>descriptions</b>: Textual descriptions of the video component of the media resource,
+	 * intended for audio synthesis when the visual component is unavailable (e.g. because the user
+	 * is interacting with the application without a screen while driving, or because the user is
+	 * blind). Synthesized as separate audio track.<br>
+	 * <br>
+	 * <b>chapters</b>: Chapter titles, intended to be used for navigating the media resource.
+	 * Displayed as an interactive list in the user agent's interface.<br>
+	 * <br>
+	 * <b>metadata</b>: Tracks intended for use from script. Not displayed by the user agent.<br>
+	 * <br>
+	 * 
+	 * @param the
+	 *            kind
+	 */
+	public void setKind(Kind kind)
+	{
+		this.kind = kind;
+	}
+
+	/**
+	 * The label for this track
+	 * 
+	 * @return the label
+	 */
+	public String getLabel()
+	{
+		return this.label;
+	}
+
+	/**
+	 * Sets the label for this track
+	 * 
+	 * @param label
+	 *            the label to be set
+	 */
+	public void setLabel(String label)
+	{
+		this.label = label;
+	}
+
+	/**
+	 * If the track is the default track
+	 * 
+	 * @return if the track is the default track
+	 */
+	public Boolean getDefaultTrack()
+	{
+		return this.defaultTrack != null ? this.defaultTrack : false;
+	}
+
+	/**
+	 * Sets if this track is the default track
+	 * 
+	 * @param defaultTrack
+	 *            if the track is the default track
+	 */
+	public void setDefaultTrack(Boolean defaultTrack)
+	{
+		this.defaultTrack = defaultTrack;
+	}
+
+	/**
+	 * Gets the src lang
+	 * 
+	 * @return the src lang
+	 */
+	public Locale getSrclang()
+	{
+		return this.srclang;
+	}
+
+	/**
+	 * Sets the src lang
+	 * 
+	 * @param srclang
+	 *            the src lang to set
+	 */
+	public void setSrclang(Locale srclang)
+	{
+		this.srclang = srclang;
+	}
+
+	/**
+	 * To be used for the kind attribute
+	 */
+	public enum Kind
+	{
+		subtitles, captions, descriptions, chapters, metadata
+	}
+}
