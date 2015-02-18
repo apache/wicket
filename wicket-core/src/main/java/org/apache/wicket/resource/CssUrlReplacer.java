8c9fd4efa2abdd774fd0e396131b4237a3bc528f
klopfdreh <klopfdreh@192.168.2.110>
2015-02-18 16:33:21 +0100

    WICKET-5827 - CssUrlReplacer supports base64 encoded images

--- wicket-core/src/main/java/org/apache/wicket/resource/CssUrlReplacer.java
+++ wicket-core/src/main/java/org/apache/wicket/resource/CssUrlReplacer.java
@@ -16,13 +16,20 @@
  */
 package org.apache.wicket.resource;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+import org.apache.wicket.WicketRuntimeException;
 import org.apache.wicket.css.ICssCompressor;
 import org.apache.wicket.request.Url;
 import org.apache.wicket.request.cycle.RequestCycle;
 import org.apache.wicket.request.resource.PackageResourceReference;
+import org.apache.wicket.util.crypt.Base64;
+import org.apache.wicket.util.io.IOUtils;
+import org.apache.wicket.util.resource.IResourceStream;
+import org.apache.wicket.util.resource.ResourceStreamNotFoundException;
 
 /**
  * This compressor is used to replace url within css files with resources that belongs to their
@@ -44,6 +51,12 @@ public class CssUrlReplacer implements IScopeAwareTextResourceProcessor, ICssCom
 	private static final Pattern URL_PATTERN = Pattern.compile("url\\(['|\"]*(.*?)['|\"]*\\)");
 
 	/**
+	 * Used to be append to CSS URLs (background-image: url('Beer.gif?embedBase64');). The
+	 * CssUrlReplacer embeds the base64 content instead of using an URL.
+	 */
+	public static final String EMBED_BASE64 = "embedBase64";
+
+	/**
 	 * Replaces the URLs of CSS resources with Wicket representatives.
 	 */
 	@Override
@@ -58,6 +71,8 @@ public class CssUrlReplacer implements IScopeAwareTextResourceProcessor, ICssCom
 		{
 			Url imageCandidateUrl = Url.parse(matcher.group(1));
 			CharSequence processedUrl;
+			boolean embedded = false;
+
 			if (imageCandidateUrl.isFull())
 			{
 				processedUrl = imageCandidateUrl.toString(Url.StringMode.FULL);
@@ -71,15 +86,66 @@ public class CssUrlReplacer implements IScopeAwareTextResourceProcessor, ICssCom
 				// relativize against the url for the containing CSS file
 				Url cssUrlCopy = new Url(cssUrl);
 				cssUrlCopy.resolveRelative(imageCandidateUrl);
-				PackageResourceReference imageReference = new PackageResourceReference(scope,
-					cssUrlCopy.toString());
-				processedUrl = cycle.urlFor(imageReference, null);
+
+				// if the image should be processed as URL or base64 embedded
+				if (cssUrlCopy.getQueryString() != null &&
+					cssUrlCopy.getQueryString().contains(EMBED_BASE64))
+				{
+					embedded = true;
+					PackageResourceReference imageReference = new PackageResourceReference(scope,
+						cssUrlCopy.toString().replace("?" + EMBED_BASE64, ""));
+					try
+					{
+						processedUrl = createBase64EncodedImage(imageReference);
+					}
+					catch (Exception e)
+					{
+						throw new WicketRuntimeException(
+							"Error while embedding an image into the css: " + imageReference, e);
+					}
+				}
+				else
+				{
+					PackageResourceReference imageReference = new PackageResourceReference(scope,
+						cssUrlCopy.toString());
+					processedUrl = cycle.urlFor(imageReference, null);
+				}
 
 			}
-			matcher.appendReplacement(output, "url('" + processedUrl + "')");
+			matcher.appendReplacement(output, embedded ? "url(" + processedUrl + ")" : "url('" +
+				processedUrl + "')");
 		}
 		matcher.appendTail(output);
 		return output.toString();
+	}
+
+	/**
+	 * Creates a base64 encoded image string based on the given image reference
+	 * 
+	 * @param imageReference
+	 *            the image reference to create the base64 encoded image string of
+	 * @return the base64 encoded image string
+	 * @throws ResourceStreamNotFoundException
+	 *             if the resource couldn't be found
+	 * @throws IOException
+	 *             if the stream couldn't be read
+	 */
+	private CharSequence createBase64EncodedImage(PackageResourceReference imageReference)
+		throws ResourceStreamNotFoundException, IOException
+	{
+		IResourceStream resourceStream = imageReference.getResource().getResourceStream();
+		InputStream inputStream = resourceStream.getInputStream();
+		try
+		{
+			byte[] bytes = IOUtils.toByteArray(inputStream);
+			String base64EncodedImage = Base64.encodeBase64String(bytes);
+			return "data:" + resourceStream.getContentType() + ";base64," +
+				base64EncodedImage.replaceAll("\\s", "");
+		}
+		finally
+		{
+			IOUtils.closeQuietly(inputStream);
+		}
 	}
 
 	@Override
