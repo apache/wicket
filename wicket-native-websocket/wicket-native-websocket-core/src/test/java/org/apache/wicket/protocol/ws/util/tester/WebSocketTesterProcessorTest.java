c9d1b22a82948b7ef4aa180d51474c25f2cf7d78
admin <gergely.nagy@mail.rakuten.com>
2015-03-20 11:00:27 +0900

    WebSocketTesterProcessorTest fixed and cleaned up

--- wicket-native-websocket/wicket-native-websocket-core/src/test/java/org/apache/wicket/protocol/ws/util/tester/WebSocketTesterProcessorTest.java
+++ wicket-native-websocket/wicket-native-websocket-core/src/test/java/org/apache/wicket/protocol/ws/util/tester/WebSocketTesterProcessorTest.java
@@ -25,7 +25,6 @@ import org.apache.wicket.mock.MockApplication;
 import org.apache.wicket.protocol.http.WebApplication;
 import org.apache.wicket.protocol.http.mock.MockHttpServletRequest;
 import org.apache.wicket.protocol.ws.WebSocketSettings;
-import org.apache.wicket.protocol.ws.api.ConnectionRejectedException;
 import org.apache.wicket.request.http.WebRequest;
 import org.apache.wicket.util.tester.WicketTester;
 import org.junit.After;
@@ -74,17 +73,13 @@ public class WebSocketTesterProcessorTest extends Assert {
     @After
     public void after() {
         tester.destroy();
+        TestWebSocketResource.ON_ABORT_CALLED.set(false);
     }
 
     @Test
     public void onConnectNoOrigin() throws Exception {
         // Given header 'Origin' is missing
-        WebSocketSettings webSocketSettings = WebSocketSettings.Holder.get(application);
-        webSocketSettings.setHijackingProtectionEnabled(true);
-        webSocketSettings.setAllowedDomains(Arrays.asList(new String[] { "http://www.example.com" }));
-        MockHttpServletRequest request = tester.getRequest();
-        request.addParameter("resourceName", TestWebSocketResource.TEXT);
-        request.addParameter(WebRequest.PARAM_AJAX_BASE_URL, ".");
+        configureRequest(true, new String[] { "http://www.example.com" }, new String[] {});
 
         // When we open a connection
         TestWebSocketProcessor processor = new TestProcessor(tester.getRequest(), tester.getApplication());
@@ -95,53 +90,10 @@ public class WebSocketTesterProcessorTest extends Assert {
     }
 
     @Ignore
-    @Test(expected = ConnectionRejectedException.class)
+    @Test
     public void onConnectMultipleOrigins() throws Exception {
         // Given the request contains multiple header 'Origin's
-        WebSocketSettings webSocketSettings = WebSocketSettings.Holder.get(application);
-        webSocketSettings.setHijackingProtectionEnabled(true);
-        webSocketSettings.setAllowedDomains(Arrays.asList(new String[] { "http://www.example.com" }));
-        MockHttpServletRequest request = tester.getRequest();
-        request.addHeader("origin", "http://www.example.com");
-        request.addHeader("origin", "http://ww2.example.com");
-        request.addParameter("resourceName", TestWebSocketResource.TEXT);
-        request.addParameter(WebRequest.PARAM_AJAX_BASE_URL, ".");
-
-        // When we open a connection
-        TestWebSocketProcessor processor = new TestProcessor(tester.getRequest(), tester.getApplication());
-        processor.onOpen(new Object());
-
-        // Then it fails
-    }
-
-    @Test
-    public void onConnectMatchingOrigin() throws Exception {
-        // Given header 'Origin' matches the host origin
-        WebSocketSettings webSocketSettings = WebSocketSettings.Holder.get(application);
-        webSocketSettings.setHijackingProtectionEnabled(true);
-        webSocketSettings.setAllowedDomains(Arrays.asList(new String[] { "http://www.example.com" }));
-        MockHttpServletRequest request = tester.getRequest();
-        request.addHeader("origin", "http://www.example.com");
-        request.addParameter("resourceName", TestWebSocketResource.TEXT);
-        request.addParameter(WebRequest.PARAM_AJAX_BASE_URL, ".");
-
-        // When we open a connection
-        TestWebSocketProcessor processor = new TestProcessor(tester.getRequest(), tester.getApplication());
-        processor.onOpen(new Object());
-
-        // Then it succeeds
-    }
-
-    @Test
-    public void onConnectMismatchingOrigin() throws Exception {
-        // Given header 'Origin' does not match the host origin
-        WebSocketSettings webSocketSettings = WebSocketSettings.Holder.get(application);
-        webSocketSettings.setHijackingProtectionEnabled(true);
-        webSocketSettings.setAllowedDomains(Arrays.asList(new String[] { "http://www.example.com" }));
-        MockHttpServletRequest request = tester.getRequest();
-        request.addHeader("origin", "http://ww2.example.com");
-        request.addParameter("resourceName", TestWebSocketResource.TEXT);
-        request.addParameter(WebRequest.PARAM_AJAX_BASE_URL, ".");
+        configureRequest(true, new String[] { "http://www.example.com" }, new String[] { "http://www.example.com", "http://ww2.example.com" });
 
         // When we open a connection
         TestWebSocketProcessor processor = new TestProcessor(tester.getRequest(), tester.getApplication());
@@ -150,4 +102,43 @@ public class WebSocketTesterProcessorTest extends Assert {
         // Then it fails
         assertEquals(true, TestWebSocketResource.ON_ABORT_CALLED.get());
     }
+
+    @Test
+    public void onConnectMatchingOrigin() throws Exception {
+        // Given header 'Origin' matches the host origin
+        configureRequest(true, new String[] { "http://www.example.com" }, new String[] { "http://www.example.com" });
+
+        // When we open a connection
+        TestWebSocketProcessor processor = new TestProcessor(tester.getRequest(), tester.getApplication());
+        processor.onOpen(new Object());
+
+        // Then it succeeds
+        assertEquals(false, TestWebSocketResource.ON_ABORT_CALLED.get());
+    }
+
+    @Test
+    public void onConnectMismatchingOrigin() throws Exception {
+        // Given header 'Origin' does not match the host origin
+        configureRequest(true, new String[] { "http://www.example.com" }, new String[] { "http://ww2.example.com" });
+
+        // When we open a connection
+        TestWebSocketProcessor processor = new TestProcessor(tester.getRequest(), tester.getApplication());
+        processor.onOpen(new Object());
+
+        // Then it fails
+        assertEquals(true, TestWebSocketResource.ON_ABORT_CALLED.get());
+    }
+
+    protected void configureRequest(boolean protectionNeeded, String[] allowedDomains, String[] origins) {
+        WebSocketSettings webSocketSettings = WebSocketSettings.Holder.get(application);
+        webSocketSettings.setHijackingProtectionEnabled(protectionNeeded);
+        webSocketSettings.setAllowedDomains(Arrays.asList(allowedDomains));
+        MockHttpServletRequest request = tester.getRequest();
+        for (String origin : origins) {
+            request.addHeader("Origin", origin);
+        }
+        request.addParameter("resourceName", TestWebSocketResource.TEXT);
+        request.addParameter(WebRequest.PARAM_AJAX_BASE_URL, ".");
+    }
+
 }
