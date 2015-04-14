273814a957d2a7c742ff2ced3de4372f9a955eb7
Alexander Morozov <amorozov@frte.ru>
2015-04-14 13:39:31 +0600

    WICKET-5881 FormComponent#updateCollectionModel must check convertedInput for null before wrapping into ArrayListFormComponent#updateCollectionModel must check convertedInput for null before wrapping into ArrayList

--- wicket-core/src/main/java/org/apache/wicket/markup/html/form/FormComponent.java
+++ wicket-core/src/main/java/org/apache/wicket/markup/html/form/FormComponent.java
@@ -1616,6 +1616,9 @@ public abstract class FormComponent<T> extends LabeledWebMarkupContainer impleme
 	public static <S> void updateCollectionModel(FormComponent<Collection<S>> formComponent)
 	{
 		Collection<S> convertedInput = formComponent.getConvertedInput();
+		if (convertedInput == null) {
+			convertedInput = Collections.emptyList();
+		}
 
 		Collection<S> collection = formComponent.getModelObject();
 		if (collection == null)
@@ -1632,10 +1635,7 @@ public abstract class FormComponent<T> extends LabeledWebMarkupContainer impleme
 			try
 			{
 				collection.clear();
-				if (convertedInput != null)
-				{
-					collection.addAll(convertedInput);
-				}
+				collection.addAll(convertedInput);
 				modified = true;
 			}
 			catch (UnsupportedOperationException unmodifiable)
@@ -1645,7 +1645,6 @@ public abstract class FormComponent<T> extends LabeledWebMarkupContainer impleme
 					logger.debug("An error occurred while trying to modify the collection attached to "
 							+ formComponent, unmodifiable);
 				}
-
 				collection = new ArrayList<S>(convertedInput);
 			}
 			
