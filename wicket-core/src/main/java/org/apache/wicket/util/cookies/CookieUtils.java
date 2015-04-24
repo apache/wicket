bffd084c96f7a93fa391251a2fc59445a5424cf6
Martin Tzvetanov Grigorov <mgrigorov@apache.org>
2015-04-24 15:22:04 +0300

    WICKET-5893 CookieUtils should use the original response when saving a cookie
    (cherry picked from commit e3808b8df3625b0134f0ca8bef6e45f9ccf27995)

--- wicket-core/src/main/java/org/apache/wicket/util/cookies/CookieUtils.java
+++ wicket-core/src/main/java/org/apache/wicket/util/cookies/CookieUtils.java
@@ -359,7 +359,7 @@ public class CookieUtils
 	 */
 	private WebResponse getWebResponse()
 	{
-		return (WebResponse)RequestCycle.get().getResponse();
+		return (WebResponse)RequestCycle.get().getOriginalResponse();
 	}
 
 	/**
