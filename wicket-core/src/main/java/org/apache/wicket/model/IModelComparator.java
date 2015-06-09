0dab604341abc7e48fd34f4c4c2bc29c188d4054
Martin Tzvetanov Grigorov <mgrigorov@apache.org>
2015-06-09 14:38:30 +0300

    WICKET-5921 Provide a default implementation of IModelComparator that always returns false

--- wicket-core/src/main/java/org/apache/wicket/model/IModelComparator.java
+++ wicket-core/src/main/java/org/apache/wicket/model/IModelComparator.java
@@ -34,6 +34,18 @@ import org.apache.wicket.util.io.IClusterable;
 public interface IModelComparator extends IClusterable
 {
 	/**
+	 * A model comparator that always returns false
+	 */
+	IModelComparator ALWAYS_FALSE = new IModelComparator()
+	{
+		@Override
+		public boolean compare(Component component, Object newObject)
+		{
+			return false;
+		}
+	};
+
+	/**
 	 * @param component
 	 *            The component which received the new object
 	 * @param newObject
