e0983f1c1544e0a08932835e7b201a596f37630f
Sven Meier <svenmeier@apache.org>
2015-06-23 14:05:43 +0200

    WICKET-5929 IPartialPageRequestHandler for updating of components

--- wicket-core/src/main/java/org/apache/wicket/ajax/AjaxRequestTarget.java
+++ wicket-core/src/main/java/org/apache/wicket/ajax/AjaxRequestTarget.java
@@ -16,22 +16,19 @@
  */
 package org.apache.wicket.ajax;
 
-import java.util.Collection;
 import java.util.Map;
 
 import org.apache.wicket.Component;
-import org.apache.wicket.MarkupContainer;
 import org.apache.wicket.Page;
 import org.apache.wicket.ajax.attributes.AjaxRequestAttributes;
-import org.apache.wicket.core.request.handler.IPageRequestHandler;
-import org.apache.wicket.markup.head.IHeaderResponse;
+import org.apache.wicket.core.request.handler.IPartialPageRequestHandler;
 import org.apache.wicket.request.ILoggableRequestHandler;
 
 /**
  *
  * @since 6.0
  */
-public interface AjaxRequestTarget extends IPageRequestHandler, ILoggableRequestHandler
+public interface AjaxRequestTarget extends IPartialPageRequestHandler, ILoggableRequestHandler
 {
 	/**
 	 * An {@link AjaxRequestTarget} listener that can be used to respond to various target-related
@@ -135,43 +132,6 @@ public interface AjaxRequestTarget extends IPageRequestHandler, ILoggableRequest
 		void onTargetRespond(AjaxRequestTarget target);
 	}
 
-
-	/**
-	 * Adds a component to the list of components to be rendered
-	 *
-	 * @param markupId
-	 *            id of client-side dom element that will be updated
-	 * @param component
-	 *            component to be rendered
-	 * @throws IllegalArgumentException
-	 *             if the component is a {@link org.apache.wicket.Page} or an {@link org.apache.wicket.markup.repeater.AbstractRepeater}
-	 * @throws IllegalStateException
-	 *             if the components are currently being rendered, or have already been rendered
-	 */
-	void add(final Component component, final String markupId);
-
-	/**
-	 * Adds components to the list of components to be rendered.
-	 *
-	 * @param components
-	 *            components to be rendered
-	 */
-	void add(Component... components);
-
-
-	/**
-	 * Visits all children of the specified parent container and adds them to the target if they are
-	 * of same type as <code>childCriteria</code>
-	 *
-	 * @param parent
-	 *            Must not be null.
-	 * @param childCriteria
-	 *            Must not be null. If you want to traverse all components use ` Component.class as
-	 *            the value for this argument.
-	 */
-	void addChildren(MarkupContainer parent, Class<?> childCriteria);
-
-
 	/**
 	 * Adds a listener to this target
 	 *
@@ -183,36 +143,6 @@ public interface AjaxRequestTarget extends IPageRequestHandler, ILoggableRequest
 	void addListener(AjaxRequestTarget.IListener listener);
 
 	/**
-	 * Adds javascript that will be evaluated on the client side after components are replaced
-	 *
-	 * <p>If the javascript needs to do something asynchronously (i.e. needs to use window.setTimeout(), for example
-	 * to do animations) then the following special syntax may be used: <code>someFunctionName|myJsLogic(someFunctionName);</code>.
-	 * Wicket will transform it to: <code>function(someFunctionName){myJsLogic(someFunctionName);}</code> and your code
-	 * is responsible to execute <em>someFunctionName()</em> when the asynchronous task is finished. Once <em>someFunctionName</em>
-	 * is executed the next appended script will be executed. <strong>Important</strong>: it is highly recommended to
-	 * execute your code in try/finally to make sure <em>someFunctionName</em> is executed even an error happens in
-	 * your code, otherwise all following scripts wont be executed.</p>
-	 *
-	 * @param javascript
-	 */
-	void appendJavaScript(CharSequence javascript);
-
-	/**
-	 * Adds javascript that will be evaluated on the client side before components are replaced.
-	 *
-	 * <p>If the javascript needs to do something asynchronously (i.e. needs to use window.setTimeout(), for example
-	 * to do animations) then the following special syntax may be used: <code>someFunctionName|myJsLogic(someFunctionName);</code>.
-	 * Wicket will transform it to: <code>function(someFunctionName){myJsLogic(someFunctionName);}</code> and your code
-	 * is responsible to execute <em>someFunctionName()</em> when the asynchronous task is finished. Once <em>someFunctionName</em>
-	 * is executed the next prepended script will be executed. <strong>Important</strong>: it is highly recommended to
-	 * execute your code in try/finally to make sure <em>someFunctionName</em> is executed even an error happens in
-	 * your code, otherwise all following scripts and component replacements wont be made.</p>
-	 *
-	 * @param javascript
-	 */
-	void prependJavaScript(CharSequence javascript);
-
-	/**
 	 * Register the given respond listener. The listener's
 	 * {@link org.apache.wicket.ajax.AjaxRequestTarget.ITargetRespondListener#onTargetRespond} method will be invoked when
 	 * the {@link AjaxRequestTarget} starts to respond.
@@ -220,41 +150,6 @@ public interface AjaxRequestTarget extends IPageRequestHandler, ILoggableRequest
 	 * @param listener
 	 */
 	void registerRespondListener(ITargetRespondListener listener);
-
-	/**
-	 * Returns an unmodifiable collection of all components added to this target
-	 *
-	 * @return unmodifiable collection of all components added to this target
-	 */
-	Collection<? extends Component> getComponents();
-
-	/**
-	 * Sets the focus in the browser to the given component. The markup id must be set. If the
-	 * component is null the focus will not be set to any component.
-	 *
-	 * @param component
-	 *            The component to get the focus or null.
-	 */
-	void focusComponent(Component component);
-
-	/**
-	 * Returns the header response associated with current AjaxRequestTarget.
-	 *
-	 * Beware that only renderOnDomReadyJavaScript and renderOnLoadJavaScript can be called outside
-	 * the renderHeader(IHeaderResponse response) method. Calls to other render** methods will
-	 * result in the call failing with a debug-level log statement to help you see why it failed.
-	 *
-	 * @return header response
-	 */
-	IHeaderResponse getHeaderResponse();
-
-	/**
-	 * Returns the HTML id of the last focused element.
-	 *
-	 * @return markup id of last focused element, <code>null</code> if none
-	 */
-	String getLastFocusedElementId();
-
 
 	/**
 	 * Returns the page. Be aware that the page can be instantiated if this wasn't the case already.
