bbe9170a8de717804aa2418d30f9424f05608b2f
Martin Tzvetanov Grigorov <mgrigorov@apache.org>
2015-06-30 15:02:13 +0300

    WICKET-5939 AjaxEventBehavior should preserve the event name as provided

--- wicket-core/src/main/java/org/apache/wicket/ajax/AjaxEventBehavior.java
+++ wicket-core/src/main/java/org/apache/wicket/ajax/AjaxEventBehavior.java
@@ -118,8 +118,7 @@ public abstract class AjaxEventBehavior extends AbstractDefaultAjaxBehavior
 	 */
 	public String getEvent()
 	{
-		String events = event.toLowerCase();
-		String[] splitEvents = events.split("\\s+");
+		String[] splitEvents = event.split("\\s+");
 		List<String> cleanedEvents = new ArrayList<String>(splitEvents.length);
 		for (String evt : splitEvents)
 		{
