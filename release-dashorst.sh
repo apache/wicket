cc6540441db12415d54c725769813248f6948098
Martijn Dashorst <dashorst@apache.org>
2015-07-10 09:18:39 +0200

    Don't debug

--- release-dashorst.sh
+++ release-dashorst.sh
@@ -14,8 +14,8 @@
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
 
-set -e
-set -x
+# set -e
+# set -x
 
 function fail {
     >&2 echo "\033[31m
