c34f2f9dd2838d075968280b042b8826e325f012
Sven Meier <svenmeier@apache.org>
2015-08-04 21:28:34 +0200

    WICKET-5960 drop headerResponse and renderedComponentsPerScope in #onAfterRender(), so a second rendering re-renders the header

--- wicket-core/src/test/java/org/apache/wicket/request/cycle/RerenderPageTest.java
+++ wicket-core/src/test/java/org/apache/wicket/request/cycle/RerenderPageTest.java
@@ -1,10 +1,25 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.apache.wicket.request.cycle;
 
+import org.apache.wicket.WicketTestCase;
 import org.apache.wicket.core.request.mapper.MountedMapper;
 import org.apache.wicket.request.cycle.RerenderPage.Supplier;
 import org.apache.wicket.request.mapper.parameter.PageParameters;
-import org.apache.wicket.util.tester.WicketTestCase;
-import org.junit.Ignore;
 import org.junit.Test;
 
 /**
@@ -20,7 +35,6 @@ public class RerenderPageTest extends WicketTestCase
 	 * because it was already rendered in the first try.
 	 */
 	@Test
-	@Ignore("Committed reproduction case, but added disabled to not fail the build")
 	public void wicket5960()
 	{
 		// mount the page so we have URL segments
