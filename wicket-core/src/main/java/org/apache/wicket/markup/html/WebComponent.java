b99ac75dfead729d23be6513c50a5cf65f9547d9
Martin Tzvetanov Grigorov <mgrigorov@apache.org>
2015-10-11 19:56:58 +0200

    Add convenience method to WebMarkupContainer for getting the WebApplication
    Add the same methods to WebComponent

--- wicket-core/src/main/java/org/apache/wicket/markup/html/WebComponent.java
+++ wicket-core/src/main/java/org/apache/wicket/markup/html/WebComponent.java
@@ -18,6 +18,10 @@ package org.apache.wicket.markup.html;
 
 import org.apache.wicket.Component;
 import org.apache.wicket.model.IModel;
+import org.apache.wicket.protocol.http.WebApplication;
+import org.apache.wicket.protocol.http.WebSession;
+import org.apache.wicket.request.http.WebRequest;
+import org.apache.wicket.request.http.WebResponse;
 
 /**
  * Base class for simple HTML components which do not hold nested components. If you need to support
@@ -54,5 +58,56 @@ public class WebComponent extends Component
 	protected void onRender()
 	{
 		internalRenderComponent();
+	}
+
+
+	/**
+	 * A convenience method to return the WebPage. Same as getPage().
+	 *
+	 * @return WebPage
+	 */
+	public final WebPage getWebPage()
+	{
+		return (WebPage)getPage();
+	}
+
+	/**
+	 * A convenience method to return the current WebRequest. Same as {@link org.apache.wicket.Component#getRequest()}.
+	 *
+	 * @return the current WebRequest
+	 */
+	public final WebRequest getWebRequest()
+	{
+		return (WebRequest)getRequest();
+	}
+
+	/**
+	 * A convenience method to return the current WebResponse. Same as {@link org.apache.wicket.Component#getResponse()}.
+	 *
+	 * @return the current WebResponse
+	 */
+	public final WebResponse getWebResponse()
+	{
+		return (WebResponse)getResponse();
+	}
+
+	/**
+	 * A convenience method to return the WebSession. Same as {@link org.apache.wicket.Component#getSession()} .
+	 *
+	 * @return the current WebSession
+	 */
+	public final WebSession getWebSession()
+	{
+		return WebSession.get();
+	}
+
+	/**
+	 * A convenience method to return the WebApplication. Same as {@link WebApplication#get()}.
+	 *
+	 * @return the current WebApplication
+	 */
+	public final WebApplication getWebApplication()
+	{
+		return WebApplication.get();
 	}
 }
