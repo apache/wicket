f0364dad0db9d1d3b49891ca9a49662a4e69e378
Andrea Del Bene <adelbene@apache.org>
2015-10-26 13:00:53 +0100

    improved WICKET-5988 relaying on component resolvers

--- wicket-core/src/main/java/org/apache/wicket/markup/html/border/Border.java
+++ wicket-core/src/main/java/org/apache/wicket/markup/html/border/Border.java
@@ -166,7 +166,7 @@ public abstract class Border extends WebMarkupContainer implements IComponentRes
 		super(id, model);
 
 		body = new BorderBodyContainer(id + "_" + BODY);
-		addToBorder(body);
+		//addToBorder(body);
 	}
 
 	/**
@@ -205,7 +205,17 @@ public abstract class Border extends WebMarkupContainer implements IComponentRes
 	@Override
 	public Border add(final Component... children)
 	{
-		getBodyContainer().add(children);
+		for (Component component : children)
+		{
+			if (component.equals(body))
+			{
+				addToBorder(component);
+			}
+			else 
+			{
+				getBodyContainer().add(component);				
+			}
+		}
 		return this;
 	}
 
@@ -631,17 +641,4 @@ public abstract class Border extends WebMarkupContainer implements IComponentRes
 		return borderMarkup != null ? borderMarkup : markup;
 	}
 	
-	@Override
-	protected void onBeforeRender()
-	{
-		super.onBeforeRender();
-		/**
-		 * https://issues.apache.org/jira/browse/WICKET-5981
-		 * dequeue border to adjust children hierarchy.
-		 */
-		if (!hasBeenRendered())
-		{
-			dequeue();
-		}
-	}
 }
