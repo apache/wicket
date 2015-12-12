a7a04e4b17b8ca8075637879c77b173df98cacb4
Martin Tzvetanov Grigorov <mgrigorov@apache.org>
2015-12-12 23:09:15 +0100

    WICKET-6006 ModalWindow.closeCurrent() causes 414 status error
    Do not submit the form elements inside the ModalWindow when closing it.

--- wicket-extensions/src/main/java/org/apache/wicket/extensions/ajax/markup/html/modal/ModalWindow.java
+++ wicket-extensions/src/main/java/org/apache/wicket/extensions/ajax/markup/html/modal/ModalWindow.java
@@ -21,6 +21,8 @@ import org.apache.wicket.Page;
 import org.apache.wicket.WicketRuntimeException;
 import org.apache.wicket.ajax.AbstractDefaultAjaxBehavior;
 import org.apache.wicket.ajax.AjaxRequestTarget;
+import org.apache.wicket.ajax.attributes.AjaxCallListener;
+import org.apache.wicket.ajax.attributes.AjaxRequestAttributes;
 import org.apache.wicket.core.request.handler.PageProvider;
 import org.apache.wicket.core.request.handler.RenderPageRequestHandler;
 import org.apache.wicket.markup.ComponentTag;
@@ -973,6 +975,16 @@ public class ModalWindow extends Panel
 		{
 			return super.getCallbackScript();
 		}
+
+		@Override
+		protected void updateAjaxAttributes(AjaxRequestAttributes attributes)
+		{
+			super.updateAjaxAttributes(attributes);
+			AjaxCallListener listener = new AjaxCallListener();
+			// disable all form elements inside the modal window to avoid serializing them in the url
+			listener.onBefore("$('#'+attrs.c+' :input').prop('disabled', true);");
+			attributes.getAjaxCallListeners().add(listener);
+		}
 	}
 
 	/**
@@ -1003,6 +1015,16 @@ public class ModalWindow extends Panel
 		public final CharSequence getCallbackScript()
 		{
 			return super.getCallbackScript();
+		}
+
+		@Override
+		protected void updateAjaxAttributes(AjaxRequestAttributes attributes)
+		{
+			super.updateAjaxAttributes(attributes);
+			AjaxCallListener listener = new AjaxCallListener();
+			// disable all form elements inside the modal window to avoid serializing them in the url
+			listener.onBefore("$('#'+attrs.c+' :input').prop('disabled', true);");
+			attributes.getAjaxCallListeners().add(listener);
 		}
 	}
 
