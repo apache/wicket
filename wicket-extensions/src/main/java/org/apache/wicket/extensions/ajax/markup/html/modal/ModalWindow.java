1daec2ef2b6b9ae5ff4f60d42ed5d119cbaaf4a0
Martin Tzvetanov Grigorov <mgrigorov@apache.org>
2015-12-24 14:05:39 +0100

    WICKET-6050 Wicket Ajax (Wicket.From.serializeElement) causes 400 bad request WICKET-5948 wicket-ajax.js probably doesn't traverse the children of <div> or <span> WICKET-6006 ModalWindow.closeCurrent() causes 414 status error
    Introduce AjaxRequestAttributes#serializeRecursively with default value 'false'.
    This is a flag indicating whether to collect (submit) the name/value pairs for all HTML form elements children of the HTML element with the JavaScript listener

--- wicket-extensions/src/main/java/org/apache/wicket/extensions/ajax/markup/html/modal/ModalWindow.java
+++ wicket-extensions/src/main/java/org/apache/wicket/extensions/ajax/markup/html/modal/ModalWindow.java
@@ -22,8 +22,6 @@ import org.apache.wicket.WicketRuntimeException;
 import org.apache.wicket.ajax.AbstractDefaultAjaxBehavior;
 import org.apache.wicket.ajax.AjaxRequestTarget;
 import org.apache.wicket.core.request.handler.IPartialPageRequestHandler;
-import org.apache.wicket.ajax.attributes.AjaxCallListener;
-import org.apache.wicket.ajax.attributes.AjaxRequestAttributes;
 import org.apache.wicket.core.request.handler.PageProvider;
 import org.apache.wicket.core.request.handler.RenderPageRequestHandler;
 import org.apache.wicket.markup.ComponentTag;
@@ -970,16 +968,6 @@ public class ModalWindow extends Panel
 				windowClosedCallback.onClose(target);
 			}
 		}
-
-		@Override
-		protected void updateAjaxAttributes(AjaxRequestAttributes attributes)
-		{
-			super.updateAjaxAttributes(attributes);
-			AjaxCallListener listener = new AjaxCallListener();
-			// disable all form elements inside the modal window to avoid serializing them in the url
-			listener.onBefore("$('#'+attrs.c+' :input').prop('disabled', true);");
-			attributes.getAjaxCallListeners().add(listener);
-		}
 	}
 
 	/**
@@ -1001,16 +989,6 @@ public class ModalWindow extends Panel
 			{
 				close(target);
 			}
-		}
-
-		@Override
-		protected void updateAjaxAttributes(AjaxRequestAttributes attributes)
-		{
-			super.updateAjaxAttributes(attributes);
-			AjaxCallListener listener = new AjaxCallListener();
-			// disable all form elements inside the modal window to avoid serializing them in the url
-			listener.onBefore("$('#'+attrs.c+' :input').prop('disabled', true);");
-			attributes.getAjaxCallListeners().add(listener);
 		}
 	}
 
