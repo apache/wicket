5f8b9fe07b6581cebb5f88a2b00e657e292dc31c
Tobias Soloschenko <tsoloschenko@apache.org>
2016-03-10 14:03:19 +0100

    Wicket Metrics - Time measurement / license header

--- wicket-metrics/src/main/java/org/apache/wicket/metrics/WicketMetrics.java
+++ wicket-metrics/src/main/java/org/apache/wicket/metrics/WicketMetrics.java
@@ -1,7 +1,24 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.apache.wicket.metrics;
 
 import com.codahale.metrics.JmxReporter;
 import com.codahale.metrics.MetricRegistry;
+import com.codahale.metrics.Timer.Context;
 
 /**
  * Base aspect provides access to the metric registry
@@ -16,7 +33,7 @@ public class WicketMetrics
 
 	private static boolean enabled = true;
 
-	private static final String JMX_PREFIX = "ApacheWicket/";
+	private static final String PREFIX = "ApacheWicket/";
 
 	/**
 	 * Gets the metric registry
@@ -43,7 +60,40 @@ public class WicketMetrics
 	{
 		if (WicketMetrics.enabled)
 		{
-			getMetricRegistry().meter(JMX_PREFIX + name).mark();
+			getMetricRegistry().meter(PREFIX + name).mark();
+		}
+	}
+
+	/**
+	 * Gets a timer context
+	 * 
+	 * @param name
+	 *            the name of the timer context
+	 * @return the timer context
+	 */
+	protected Context context(String name)
+	{
+		if (WicketMetrics.enabled)
+		{
+			return getMetricRegistry().timer(PREFIX + name).time();
+		}
+		else
+		{
+			return null;
+		}
+	}
+
+	/**
+	 * Stops the contex quietly
+	 * 
+	 * @param context
+	 *            the context to stop
+	 */
+	public void stopQuietly(Context context)
+	{
+		if (context != null)
+		{
+			context.stop();
 		}
 	}
 
