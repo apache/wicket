a0eeddda865b56d30fc1b17c3a4a41eb46f40ccf
Andrea Del Bene <adelbene@apache.org>
2016-03-12 19:52:00 +0100

    Revert "Introduced MarkupContainer#childrenStream to leverage Java 8 Stream when accessing children components"
    This reverts commit 73ac8c7e6da3ff9dd0244ad66c63944ba3d64c78.
    
    Conflicts:
    	wicket-util/src/main/java/org/apache/wicket/util/NestedIterator.java

--- wicket-core/src/main/java/org/apache/wicket/MarkupContainer.java
+++ wicket-core/src/main/java/org/apache/wicket/MarkupContainer.java
@@ -24,8 +24,6 @@ import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
-import java.util.stream.Stream;
-import java.util.stream.StreamSupport;
 
 import org.apache.commons.collections4.map.LinkedMap;
 import org.apache.wicket.core.util.string.ComponentStrings;
@@ -47,7 +45,6 @@ import org.apache.wicket.model.IComponentInheritedModel;
 import org.apache.wicket.model.IModel;
 import org.apache.wicket.model.IWrapModel;
 import org.apache.wicket.settings.DebugSettings;
-import org.apache.wicket.util.NestedIterator;
 import org.apache.wicket.util.lang.Args;
 import org.apache.wicket.util.lang.Classes;
 import org.apache.wicket.util.lang.Generics;
@@ -2175,19 +2172,5 @@ public abstract class MarkupContainer extends Component implements Iterable<Comp
 	protected void addDequeuedComponent(Component component, ComponentTag tag)
 	{
 		add(component);
-	}
-	
-	/**
-	 * Creates a {@code java.util.stream.Stream} returning all the children (included nested ones)
-	 * of the this container.
-	 * 
-	 * @return a stream returning children.
-	 */
-	public Stream<Component> childrenStream()
-	{
-		NestedIterator<Component> nestedIterator = new NestedIterator<>(this);
-		Iterable<Component> iterable = () -> nestedIterator;
-		
-		return StreamSupport.stream(iterable.spliterator(), false);
 	}
 }
