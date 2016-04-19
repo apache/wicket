20cf8ee341a0aab994c9e9a102687a9a3e88e27a
Martin Tzvetanov Grigorov <mgrigorov@apache.org>
2016-04-19 23:56:49 +0200

    WICKET-6148 Remove AjaxEventBehavior#onCheckEvent() before Wicket 8.0.0

--- wicket-core/src/main/java/org/apache/wicket/ajax/AjaxEventBehavior.java
+++ wicket-core/src/main/java/org/apache/wicket/ajax/AjaxEventBehavior.java
@@ -112,9 +112,21 @@ public abstract class AjaxEventBehavior extends AbstractDefaultAjaxBehavior
 	 * 
 	 * @param event
 	 *      the event this behavior will be attached to
+	 * @deprecated Wicket 8 Remove this method for Wicket 8.0.0
 	 */
+	@Deprecated
 	protected void onCheckEvent(final String event)
 	{
+		if (event.startsWith("on"))
+		{
+			String shortName = event.substring(2);
+			throw new IllegalArgumentException(
+					String.format("Since version 6.0.0 Wicket uses JavaScript event registration so there is no need of the leading " +
+									"'on' in the event name '%s'. Please use just '%s'. Wicket 8.x won't manipulate the provided event " +
+									"names so the leading 'on' may break your application."
+							, event, shortName));
+		}
+
 	}
 
 	/**
@@ -129,16 +141,6 @@ public abstract class AjaxEventBehavior extends AbstractDefaultAjaxBehavior
 		{
 			if (Strings.isEmpty(evt) == false)
 			{
-				if (evt.startsWith("on"))
-				{
-					String shortName = evt.substring(2);
-					// TODO Wicket 8 Change this to throw an error in the milestone/RC versions and remove it for the final version
-					LOGGER.warn("Since version 6.0.0 Wicket uses JavaScript event registration so there is no need of the leading " +
-							"'on' in the event name '{}'. Please use just '{}'. Wicket 8.x won't manipulate the provided event " +
-							"names so the leading 'on' may break your application."
-							, evt, shortName);
-					evt = shortName;
-				}
 				cleanedEvents.add(evt);
 			}
 		}
@@ -146,10 +148,6 @@ public abstract class AjaxEventBehavior extends AbstractDefaultAjaxBehavior
 		return Strings.join(" ", cleanedEvents);
 	}
 
-	/**
-	 * 
-	 * @see org.apache.wicket.ajax.AbstractDefaultAjaxBehavior#respond(AjaxRequestTarget)
-	 */
 	@Override
 	protected final void respond(final AjaxRequestTarget target)
 	{
