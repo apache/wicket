841c961c7324224bda129aa36c1c89d6a84c937d
Andrea Del Bene <adelbene@apache.org>
2016-06-20 18:49:29 +0200

    Revert "WICKET-6187 Enclosures rendered twice in derived component"
    This reverts commit 084590a07b80b3cb05d268965cc6b15f75afe95c.

--- wicket-core/src/main/java/org/apache/wicket/markup/parser/filter/InlineEnclosureHandler.java
+++ wicket-core/src/main/java/org/apache/wicket/markup/parser/filter/InlineEnclosureHandler.java
@@ -67,6 +67,12 @@ public final class InlineEnclosureHandler extends AbstractMarkupFilter
 	private Deque<ComponentTag> enclosures;
 
 	/**
+	 * InlineEnclosures are not removed after render as other auto-components,
+	 * thus they have to have a stable id.
+	 */
+	private int counter;
+
+	/**
 	 * Construct.
 	 */
 	public InlineEnclosureHandler()
@@ -109,8 +115,7 @@ public final class InlineEnclosureHandler extends AbstractMarkupFilter
 				{
 					if (Strings.isEmpty(htmlId))
 					{
-						String id = getWicketNamespace() + "_" + INLINE_ENCLOSURE_ID_PREFIX + 
-							getRequestUniqueId();
+						String id = getWicketNamespace() + "_" + INLINE_ENCLOSURE_ID_PREFIX + (counter++);
 						tag.setId(id);
 					}
 					else
