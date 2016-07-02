6e184f3f298e1f21c62d188d423b8a00b29e9850
Martin Tzvetanov Grigorov <mgrigorov@apache.org>
2016-07-02 14:15:00 +0200

    WICKET-6194 Add Tomcat 8.5+ implementation of PushBuilder

---
+++ wicket-experimental/wicket-http2/wicket-http2-tomcat/src/main/java/org/apache/wicket/http2/markup/head/Tomcat85PushBuilder.java
@@ -0,0 +1,42 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.wicket.http2.markup.head;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.apache.wicket.request.Request;
+import org.apache.wicket.request.cycle.RequestCycle;
+
+/**
+ *
+ */
+public class Tomcat85PushBuilder implements PushBuilder
+{
+	@Override
+	public void push(HttpServletRequest httpServletRequest, String... paths)
+	{
+		Request request = RequestCycle.get().getRequest();
+		HttpServletRequest httpRequest = (HttpServletRequest) request.getContainerRequest();
+		org.apache.catalina.connector.Request tomcatRequest = (org.apache.catalina.connector.Request) httpRequest;
+		org.apache.catalina.servlet4preview.http.PushBuilder pushBuilder = tomcatRequest.getPushBuilder();
+		for (String path : paths)
+		{
+			pushBuilder.path(path);
+		}
+		pushBuilder.push();
+	}
+}
