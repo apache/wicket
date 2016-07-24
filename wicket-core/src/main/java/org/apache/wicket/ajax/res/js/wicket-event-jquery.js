d380b638dd122fbe148b6a7adf1aebc10b9a0d41
Sven Meier <svenmeier@apache.org>
2016-07-24 20:50:48 +0200

    WICKET-6108 revert Event.fix(): passed event should always be fixed

--- wicket-core/src/main/java/org/apache/wicket/ajax/res/js/wicket-event-jquery.js
+++ wicket-core/src/main/java/org/apache/wicket/ajax/res/js/wicket-event-jquery.js
@@ -199,7 +199,7 @@
 			 * If no event is given as argument (IE), window.event is returned.
 			 */
 			fix: function (evt) {
-				return evt || jQuery.event.fix(window.event);
+				return jQuery.event.fix(evt || window.event);
 			},
 
 			fire: function (element, event) {
