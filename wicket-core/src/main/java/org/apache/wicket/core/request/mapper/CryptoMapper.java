c422446e0fe83ed6dc4422d92d4b168a67b85845
Martin Tzvetanov Grigorov <mgrigorov@apache.org>
2016-08-26 17:05:54 +0200

    WICKET-6234 Log the decrypted url in CryptoMapper for debugging purposes

--- wicket-core/src/main/java/org/apache/wicket/core/request/mapper/CryptoMapper.java
+++ wicket-core/src/main/java/org/apache/wicket/core/request/mapper/CryptoMapper.java
@@ -401,6 +401,8 @@ public class CryptoMapper implements IRequestMapperDelegate
 			url = decryptRequestListenerParameter(request, encryptedUrl);
 		}
 
+		log.debug("Url '{}' has been decrypted to '{}'", encryptedUrl, url);
+
 		return url;
 	}
 
