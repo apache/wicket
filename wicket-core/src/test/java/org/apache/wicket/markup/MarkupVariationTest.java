f1f34994c6637f9d49245204350a57a5dae76396
Andrea Del Bene <adelbene@apache.org>
2016-08-29 14:49:52 +0200

    Revert "WICKET-6231 wicket:enclosure and getVariation()."
    This reverts commit 961d6cde4fe6f434a44385a6dc5f5b187816f169.

--- wicket-core/src/test/java/org/apache/wicket/markup/MarkupVariationTest.java
+++ wicket-core/src/test/java/org/apache/wicket/markup/MarkupVariationTest.java
@@ -25,7 +25,6 @@ import org.apache.wicket.markup.html.form.Form;
 import org.apache.wicket.markup.html.panel.Panel;
 import org.apache.wicket.util.resource.IResourceStream;
 import org.apache.wicket.util.resource.StringResourceStream;
-import org.apache.wicket.util.tester.FormTester;
 import org.apache.wicket.util.tester.WicketTestCase;
 import org.junit.Test;
 
@@ -62,20 +61,6 @@ public class MarkupVariationTest extends WicketTestCase
 	{
 		return (MarkupContainer) tester.getComponentFromLastRenderedPage("p");
 	}
-	
-	/**
-	 * https://issues.apache.org/jira/browse/WICKET-6231
-	 */
-	@Test
-	public void changeVariationBeforeRendering() throws Exception
-	{
-		tester.startPage(new VariationPage());
-		FormTester formTester = tester.newFormTester("p:a_form");
-		
-		formTester.submit();
-		
-		tester.assertContainsNot("One");
-	}
 
 	private static class VariationPage extends WebPage implements IMarkupResourceStreamProvider
 	{
@@ -110,22 +95,14 @@ public class MarkupVariationTest extends WicketTestCase
 				@Override
 				public void onClick(AjaxRequestTarget target)
 				{
-					changeVariation();
+					variation = "one".equals(variation) ? "two" : "one";
 					target.add(VariationPanel.this);
 				}
 			});
 
 			add(new Label("simpleLabel", "Label"));
 
-			add(new Form<Void>("a_form")
-			{
-				@Override
-				protected void onSubmit()
-				{
-					super.onSubmit();
-					changeVariation();
-				}
-			});
+			add(new Form<Void>("a_form"));
 
 			add(new Label("child", "Inline Enclosure child text"));
 			add(new Label("nestedChild", "Nested Inline Enclosure child text"));
@@ -136,11 +113,6 @@ public class MarkupVariationTest extends WicketTestCase
 		public String getVariation()
 		{
 			return variation;
-		}
-
-		private void changeVariation()
-		{
-			variation = "one".equals(variation) ? "two" : "one";
 		}
 	}
 }
