5b5e25c9c89ef175e9d0799752bfdaa5205379df
Martin Tzvetanov Grigorov <mgrigorov@apache.org>
2017-01-25 23:21:10 +0100

    WICKET-6310 Add licence, fix typo and optimize imports

--- wicket-core/src/test/java/org/apache/wicket/protocol/http/SessionDestroyTest.java
+++ wicket-core/src/test/java/org/apache/wicket/protocol/http/SessionDestroyTest.java
@@ -1,3 +1,19 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.apache.wicket.protocol.http;
 
 import static org.hamcrest.Matchers.is;
@@ -11,7 +27,6 @@ import java.util.Locale;
 import org.apache.wicket.mock.MockWebRequest;
 import org.apache.wicket.request.Url;
 import org.apache.wicket.util.tester.WicketTestCase;
-import org.junit.Assert;
 import org.junit.Test;
 
 public class SessionDestroyTest extends WicketTestCase
@@ -20,7 +35,7 @@ public class SessionDestroyTest extends WicketTestCase
 	 * https://issues.apache.org/jira/browse/WICKET-6310
 	 */
 	@Test
-	public void whenSessionIsDestroyed_thenItShouldResetItState()
+	public void whenSessionIsDestroyed_thenItShouldResetItsState()
 	{
 		final Locale locale = Locale.ENGLISH;
 		MockWebRequest request = new MockWebRequest(Url.parse("/"))
