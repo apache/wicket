24500049ddeedfb6d536f07326f002e923c72a33
Martin Tzvetanov Grigorov <mgrigorov@apache.org>
2017-04-03 11:19:02 +0200

    WICKET-6351 Form.anyFormComponentError performance degradation

--- wicket-core/src/main/java/org/apache/wicket/markup/html/form/Form.java
+++ wicket-core/src/main/java/org/apache/wicket/markup/html/form/Form.java
@@ -1025,8 +1025,7 @@ public class Form<T> extends WebMarkupContainer
 			public void component(final Component component, final IVisit<Void> visit)
 			{
 				Form<?> form = (Form<?>)component;
-				if (form.isEnabledInHierarchy() && form.isVisibleInHierarchy() &&
-					form.wantSubmitOnParentFormSubmit())
+				if (form.wantSubmitOnParentFormSubmit() && form.isEnabledInHierarchy() && form.isVisibleInHierarchy())
 				{
 					form.setFlag(FLAG_SUBMITTED, true);
 					return;
@@ -1168,8 +1167,7 @@ public class Form<T> extends WebMarkupContainer
 			@Override
 			public void component(final Component component, final IVisit<Boolean> visit)
 			{
-				if (component.isVisibleInHierarchy() && component.isEnabledInHierarchy() &&
-					component.hasErrorMessage())
+				if (component.hasErrorMessage() && component.isVisibleInHierarchy() && component.isEnabledInHierarchy())
 				{
 					visit.stop(true);
 				}
