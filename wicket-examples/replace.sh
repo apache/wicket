f7688bff9a93fa04c4fb70ac50474ec92448b9a3
Andrea Del Bene <adelbene@apache.org>
2017-10-30 16:46:09 +0100

    Added migration script

--- wicket-examples/replace.sh
+++ wicket-examples/replace.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-for fl in $*; do
-  echo $fl;
+grep -r -H -l --include=*.html "<span wicket:id=\"mainNavigation\"/>"  . | while read line; do
+   cat $line | sed -e 's/<span wicket:id=\"mainNavigation\"/>/<wicket:extend>/g' | sed -e 's/<\/body>/<\/body>\n<\/wicket:extend>/g' >> tmpfile ; mv tmpfile $line
 done
