a6e5935e4505459913a31fe66099ad84edad3787
Andrea Del Bene <adelbene@apache.org>
2017-10-31 20:47:13 +0100

    Other examples migrated

--- wicket-examples/replace.sh
+++ wicket-examples/replace.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-grep -r -H -l --include=*.html "<span wicket:id=\"mainNavigation\"></span>"  . | while read line; do
-   cat $line | sed -e 's/<span wicket:id=\"mainNavigation\"\/><\/span>/<wicket:extend>/g' | sed -e 's/<\/body>/<\/wicket:extend>\n<\/body>/g' >> tmpfile ; mv tmpfile $line
+grep -r -H -l --include=*.html "<span wicket:id=\"mainNavigation\" />"  . | while read line; do
+   cat $line | sed -e 's/<span wicket:id=\"mainNavigation\" \/>/<wicket:extend>/g' | sed -e 's/<\/body>/<\/wicket:extend>\n<\/body>/g' >> tmpfile ; mv tmpfile $line
 done
