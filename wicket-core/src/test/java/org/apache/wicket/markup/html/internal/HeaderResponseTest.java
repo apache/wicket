ebc97e31d09dba1b5a1e01c730831a40405f3deb
Maxim Solodovnik <solomax666@gmail.com>
2018-04-03 21:23:48 +0700

    [WICKET-6546] HTML comments are removed from inline CSS

--- wicket-core/src/test/java/org/apache/wicket/markup/html/internal/HeaderResponseTest.java
+++ wicket-core/src/test/java/org/apache/wicket/markup/html/internal/HeaderResponseTest.java
@@ -136,8 +136,8 @@ public class HeaderResponseTest
 	public void conditionalRenderCSSContent()
 	{
 		headerResponse.render(CssHeaderItem.forCSS(".className { font-size: 10px}", "id", "lt IE 8"));
-		String expected = "<!--[if lt IE 8]><style type=\"text/css\" id=\"id\"><!--\n" +
-				".className { font-size: 10px}--></style>\n" +
+		String expected = "<!--[if lt IE 8]><style type=\"text/css\" id=\"id\">\n" +
+				".className { font-size: 10px}</style>\n" +
 				"<![endif]-->\n";
 		String actual = headerResponse.getResponse().toString();
 		Assert.assertEquals(expected, actual);
