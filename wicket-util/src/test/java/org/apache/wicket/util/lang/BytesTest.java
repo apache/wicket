b171f46d1d8eaec0dda3f0c313177a18c1099ca5
Sven Meier <svenmeier@apache.org>
2018-08-29 17:36:52 +0200

    WICKET-6580 use standard units KB,MB,GB,TB

--- wicket-util/src/test/java/org/apache/wicket/util/lang/BytesTest.java
+++ wicket-util/src/test/java/org/apache/wicket/util/lang/BytesTest.java
@@ -148,11 +148,11 @@ public class BytesTest extends Assert
 	public void testToString()
 	{
 		assertEquals("1 bytes", Bytes.bytes(1).toString());
-		assertEquals("1K", Bytes.bytes(1024).toString());
-		assertEquals("1M", Bytes.bytes(1024 * 1024L).toString());
-		assertEquals("1G", Bytes.bytes(1024 * 1024 * 1024L).toString());
-		assertEquals("1T", Bytes.bytes(1024 * 1024 * 1024 * 1024L).toString());
-		assertEquals("1.5K", Bytes.bytes(1024 * 1.5).toString());
+		assertEquals("1KB", Bytes.bytes(1024).toString());
+		assertEquals("1MB", Bytes.bytes(1024 * 1024L).toString());
+		assertEquals("1GB", Bytes.bytes(1024 * 1024 * 1024L).toString());
+		assertEquals("1TB", Bytes.bytes(1024 * 1024 * 1024 * 1024L).toString());
+		assertEquals("1.5KB", Bytes.bytes(1024 * 1.5).toString());
 
 		assertEquals("1 bytes", Bytes.bytes(1).toString(Locale.GERMAN));
 	}
