bcf516a3928f7db8af87f046de12450bafe74a7f
Martin Tzvetanov Grigorov <mgrigorov@apache.org>
2019-02-05 17:41:37 +0200

    Fix javadoc build issue:
    [ERROR] /tmp/w712/apache-wicket-7.12.0/wicket-core/src/main/java/org/apache/wicket/page/PartialPageUpdate.java:307: error - JavaScript found in documentation comment.
    [ERROR] Use --allow-script-in-comments to allow use of JavaScript.

--- wicket-core/src/main/java/org/apache/wicket/page/PartialPageUpdate.java
+++ wicket-core/src/main/java/org/apache/wicket/page/PartialPageUpdate.java
@@ -299,7 +299,7 @@ public abstract class PartialPageUpdate
 	protected abstract void writeHeader(Response response, String encoding);
 
 	/**
-	 * Writes header contribution (<link/> or <script/>) to the response.
+	 * Writes header contribution (&lt;link/&gt; or &lt;script/&gt;) to the response.
 	 *
 	 * @param response
 	 *      the response to write to
