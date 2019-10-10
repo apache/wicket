7a740a24f901d23bb6c3813337bd6797d43a1e33
Martin Tzvetanov Grigorov <mgrigorov@apache.org>
2019-10-10 13:21:12 +0300

    WICKET-6708 Fix compilation issue after git cherry-pick

--- wicket-extensions/src/test/java/org/apache/wicket/extensions/markup/html/form/select/SelectTestPage.java
+++ wicket-extensions/src/test/java/org/apache/wicket/extensions/markup/html/form/select/SelectTestPage.java
@@ -35,7 +35,7 @@ public class SelectTestPage extends WebPage
 		form = new Form<>("form");
 		add(form);
 
-		select = new Select<>("select", new Model<>(null));
+		select = new Select<>("select", new Model<String>(null));
 		form.add(select);
 
 		select.add(new SelectOption<>("option0", new Model<>("OPTION_0")));
