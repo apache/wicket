b6af906c2730f5b7871cae75777f19a1d8809bb1
Andrea Del Bene <an.delbene@gmail.com>
2019-10-25 12:01:07 +0200

    WICKET-6708 Made lowercase conversion local insentive, as did for WICKET-6614

--- wicket-core/src/main/java/org/apache/wicket/markup/html/form/FormComponent.java
+++ wicket-core/src/main/java/org/apache/wicket/markup/html/form/FormComponent.java
@@ -815,7 +815,7 @@ public abstract class FormComponent<T> extends LabeledWebMarkupContainer impleme
 		}
 
 		final IRequestParameters parameters;
-		switch (method.toLowerCase())
+		switch (method.toLowerCase(Locale.ROOT))
 		{
 			case Form.METHOD_POST:
 				parameters = request.getPostParameters();
