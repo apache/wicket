ccd5eb69b87e820e5d04b387fc565da20e3345f6
Martin Tzvetanov Grigorov <mgrigorov@apache.org>
2020-04-21 23:38:26 +0300

    WICKET-6764 RedirectToUrlException change the second question mark in URL from "?" to "%3F"
    (cherry picked from commit 6c589d2bf9ea69d8ffaac647669c4cd35b1f2dd5)

--- wicket-request/src/test/java/org/apache/wicket/request/http/handler/RedirectRequestHandlerTest.java
+++ wicket-request/src/test/java/org/apache/wicket/request/http/handler/RedirectRequestHandlerTest.java
@@ -17,6 +17,7 @@
 package org.apache.wicket.request.http.handler;
 
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 import javax.servlet.http.HttpServletResponse;
@@ -26,7 +27,6 @@ import org.apache.wicket.request.http.WebRequest;
 import org.apache.wicket.request.http.WebResponse;
 import org.junit.Before;
 import org.junit.Test;
-import org.mockito.Mockito;
 
 /**
  * RedirectRequestHandlerTest
@@ -46,19 +46,16 @@ public class RedirectRequestHandlerTest
 		when(requestCycle.getRequest()).thenReturn(webRequest);
 	}
 
-	/**
-	 * permenanentlyMovedShouldSetLocationHeader()
-	 */
 	@Test
-	public void permenanentlyMovedShouldSetLocationHeader()
+	public void permanentlyMovedShouldSetLocationHeader()
 	{
 		RedirectRequestHandler handler = new RedirectRequestHandler(REDIRECT_URL,
 			HttpServletResponse.SC_MOVED_PERMANENTLY);
 
 		handler.respond(requestCycle);
 
-		Mockito.verify(webResponse).setStatus(HttpServletResponse.SC_MOVED_PERMANENTLY);
-		Mockito.verify(webResponse).setHeader("Location", REDIRECT_URL);
+		verify(webResponse).setStatus(HttpServletResponse.SC_MOVED_PERMANENTLY);
+		verify(webResponse).setHeader("Location", REDIRECT_URL);
 	}
 
 	/**
@@ -77,7 +74,7 @@ public class RedirectRequestHandlerTest
 
 		handler.respond(requestCycle);
 
-		Mockito.verify(webResponse).sendRedirect(REDIRECT_URL);
+		verify(webResponse).sendRedirect(REDIRECT_URL);
 	}
 
 	/**
@@ -91,8 +88,8 @@ public class RedirectRequestHandlerTest
 
 		handler.respond(requestCycle);
 
-		Mockito.verify(webResponse).setStatus(HttpServletResponse.SC_SEE_OTHER);
-		Mockito.verify(webResponse).setHeader("Location", REDIRECT_URL);
+		verify(webResponse).setStatus(HttpServletResponse.SC_SEE_OTHER);
+		verify(webResponse).setHeader("Location", REDIRECT_URL);
 	}
 
 	/**
@@ -108,7 +105,38 @@ public class RedirectRequestHandlerTest
 
 		handler.respond(requestCycle);
 
-		Mockito.verify(webResponse).setStatus(HttpServletResponse.SC_SEE_OTHER);
-		Mockito.verify(webResponse).setHeader("Ajax-Location", REDIRECT_URL);
+		verify(webResponse).setStatus(HttpServletResponse.SC_SEE_OTHER);
+		verify(webResponse).setHeader("Ajax-Location", REDIRECT_URL);
+	}
+
+	/**
+	 * https://issues.apache.org/jira/browse/WICKET-6764
+	 */
+	@Test
+	public void movedPermanentlyAndModeRedirect_shouldSendRedirect()
+	{
+		RedirectRequestHandler handler = new RedirectRequestHandler(REDIRECT_URL,
+		                                                            HttpServletResponse.SC_MOVED_PERMANENTLY);
+		handler.mode(RedirectRequestHandler.Mode.REDIRECT);
+
+		handler.respond(requestCycle);
+
+		verify(webResponse).sendRedirect(REDIRECT_URL);
+	}
+
+	/**
+	 * https://issues.apache.org/jira/browse/WICKET-6764
+	 */
+	@Test
+	public void movedTemporarilyAndModeStatus_shouldSetLocation()
+	{
+		RedirectRequestHandler handler = new RedirectRequestHandler(REDIRECT_URL,
+		                                                            HttpServletResponse.SC_MOVED_TEMPORARILY);
+		handler.mode(RedirectRequestHandler.Mode.STATUS);
+
+		handler.respond(requestCycle);
+
+		verify(webResponse).setStatus(HttpServletResponse.SC_MOVED_TEMPORARILY);
+		verify(webResponse).setHeader("Location", REDIRECT_URL);
 	}
 }
