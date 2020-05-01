091d0e0bd3751004a52899caf64175b2057edae4
Emond Papegaaij <emond.papegaaij@topicus.nl>
2020-05-01 09:08:56 +0200

    WICKET-6774: minor performance improvements, fixed header

--- wicket-core/src/main/java/org/apache/wicket/ComponentState.java
+++ wicket-core/src/main/java/org/apache/wicket/ComponentState.java
@@ -1,12 +1,18 @@
 /*
- * Licensed to the Apache Software Foundation (ASF) under one or more contributor license
- * agreements. See the NOTICE file distributed with this work for additional information regarding
- * copyright ownership. The ASF licenses this file to You under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance with the License. You may obtain a
- * copy of the License at http://www.apache.org/licenses/LICENSE-2.0 Unless required by applicable
- * law or agreed to in writing, software distributed under the License is distributed on an "AS IS"
- * BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License
- * for the specific language governing permissions and limitations under the License.
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 package org.apache.wicket;
 
@@ -210,8 +216,7 @@ abstract class ComponentState implements Serializable
 		}
 		else if (modelSet)
 		{
-			stateBehavior = null;
-			stateMetaData = null;
+			return model;
 		}
 		else if (state instanceof MetaDataEntry || state instanceof MetaDataEntry[])
 		{
@@ -254,9 +259,7 @@ abstract class ComponentState implements Serializable
 		}
 		else
 		{
-			stateModel = null;
-			stateBehavior = state;
-			stateMetaData = null;
+			return addBehaviors(component, state, behaviorsToAdd);
 		}
 
 		return createState(state, stateModel,
@@ -290,9 +293,7 @@ abstract class ComponentState implements Serializable
 		}
 		else
 		{
-			stateModel = null;
-			stateBehavior = state;
-			stateMetaData = null;
+			return removeBehaviors(component, state, behaviorsToRemove);
 		}
 
 		return createState(state, stateModel,
@@ -319,9 +320,7 @@ abstract class ComponentState implements Serializable
 		}
 		else if (state instanceof MetaDataEntry || state instanceof MetaDataEntry[])
 		{
-			stateModel = null;
-			stateBehavior = null;
-			stateMetaData = state;
+			return setMetaData(state, key, data);
 		}
 		else
 		{
