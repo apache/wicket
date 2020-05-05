6291469772a518a8d2ecc97163cde370ea0f2aed
Martin Tzvetanov Grigorov <mgrigorov@apache.org>
2020-05-05 11:57:41 +0300

    WICKET-6779 Upgrade JQuery 3 to 3.5.1

--- testing/wicket-js-tests/Gruntfile.js
+++ testing/wicket-js-tests/Gruntfile.js
@@ -117,7 +117,7 @@ module.exports = function(grunt) {
 			        urls: [
 			        	'http://localhost:38888/test/js/all.html?1.12.4',
 			        	'http://localhost:38888/test/js/all.html?2.2.4',
-			        	'http://localhost:38888/test/js/all.html?3.3.1'
+			        	'http://localhost:38888/test/js/all.html?3.5.1'
 			        ]
 			    }
 			},
@@ -130,7 +130,7 @@ module.exports = function(grunt) {
 					urls: [
 						'http://localhost:38888/test/js/amd.html?1.12.4',
 						'http://localhost:38888/test/js/amd.html?2.2.4',
-						'http://localhost:38888/test/js/amd.html?3.3.1'
+						'http://localhost:38888/test/js/amd.html?3.5.1'
 					]
 				}
 			}
