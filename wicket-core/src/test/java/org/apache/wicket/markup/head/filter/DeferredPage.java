77618961508f7a799c3cb7b40d4a89eed054b0c0
Sven Meier <svenmeier@apache.org>
2022-02-02 22:01:55 +0100

    WICKET-6953 defer non-ajax only

--- wicket-core/src/test/java/org/apache/wicket/markup/head/filter/DeferredPage.java
+++ wicket-core/src/test/java/org/apache/wicket/markup/head/filter/DeferredPage.java
@@ -18,6 +18,7 @@ package org.apache.wicket.markup.head.filter;
 
 import org.apache.wicket.ajax.AjaxEventBehavior;
 import org.apache.wicket.ajax.AjaxRequestTarget;
+import org.apache.wicket.markup.html.WebMarkupContainer;
 import org.apache.wicket.markup.html.WebPage;
 
 /**
@@ -27,6 +28,8 @@ public class DeferredPage extends WebPage
 {
 	private static final long serialVersionUID = 1L;
 
+	public AjaxEventBehavior behavior;
+	
 	/**
 	 * Construct.
 	 * 
@@ -34,12 +37,16 @@ public class DeferredPage extends WebPage
 	 */
 	public DeferredPage()
 	{
-		add(new AjaxEventBehavior("click") {
+		WebMarkupContainer c = new WebMarkupContainer("c");
+		add(c);
+
+		c.add(behavior = new AjaxEventBehavior("click") {
 			private static final long serialVersionUID = 1L;
 
 			@Override
 			protected void onEvent(AjaxRequestTarget target)
 			{
+				target.add(c);
 			}
 		});
 	}
