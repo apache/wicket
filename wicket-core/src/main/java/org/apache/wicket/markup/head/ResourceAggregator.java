981cb4684c155281a4a532a274b2564168017848
Andrea Del Bene <adelbene@apache.org>
2022-06-29 16:07:18 +0200

    WICKET-6991 using a separate StringBuilder for OnLoadHeaderItemS

--- wicket-core/src/main/java/org/apache/wicket/markup/head/ResourceAggregator.java
+++ wicket-core/src/main/java/org/apache/wicket/markup/head/ResourceAggregator.java
@@ -347,43 +347,45 @@ public class ResourceAggregator extends DecoratingHeaderResponse
 	private void renderCombinedEventScripts()
 	{
 		// make a rough estimate of the size to which this StringBuilder will grow
-		int length = domReadyItemsToBeRendered.size() * 256;
-		StringBuilder combinedScript = new StringBuilder(length);
+		int domReadyLength = domReadyItemsToBeRendered.size() * 256;
+		StringBuilder domReadScript = new StringBuilder(domReadyLength);
 		for (HeaderItem curItem : domReadyItemsToBeRendered)
 		{
 			if (markItemRendered(curItem))
 			{
-				combinedScript.append('\n');
+				domReadScript.append('\n');
 				if (curItem instanceof OnDomReadyHeaderItem)
 				{
-					combinedScript.append(((OnDomReadyHeaderItem)curItem).getJavaScript());
+					domReadScript.append(((OnDomReadyHeaderItem)curItem).getJavaScript());
 				} else if (curItem instanceof OnEventHeaderItem)
 				{
-					combinedScript.append(((OnEventHeaderItem)curItem).getCompleteJavaScript());
+					domReadScript.append(((OnEventHeaderItem)curItem).getCompleteJavaScript());
 				}
-				combinedScript.append(';');
+				domReadScript.append(';');
 			}
 		}
-		if (combinedScript.length() > 0)
+		if (domReadScript.length() > 0)
 		{
-			combinedScript.append("\nWicket.Event.publish(Wicket.Event.Topic.AJAX_HANDLERS_BOUND);\n");
-			getRealResponse().render(OnDomReadyHeaderItem.forScript(combinedScript));
+			domReadScript.append("\nWicket.Event.publish(Wicket.Event.Topic.AJAX_HANDLERS_BOUND);\n");
+			getRealResponse().render(OnDomReadyHeaderItem.forScript(domReadScript));
 		}
 
-		combinedScript.setLength(0);
+		int onLoadLength = loadItemsToBeRendered.size() * 256;
+        StringBuilder onLoadScript = new StringBuilder(onLoadLength);
+		
 		for (OnLoadHeaderItem curItem : loadItemsToBeRendered)
 		{
 			if (markItemRendered(curItem))
 			{
-				combinedScript.append('\n');
-				combinedScript.append(curItem.getJavaScript());
-				combinedScript.append(';');
+			    onLoadScript.append('\n');
+			    onLoadScript.append(curItem.getJavaScript());
+			    onLoadScript.append(';');
 			}
 		}
-		if (combinedScript.length() > 0)
+		if (onLoadScript.length() > 0)
 		{
 			getRealResponse().render(
-				OnLoadHeaderItem.forScript(combinedScript.append('\n')));
+				OnLoadHeaderItem.forScript(onLoadScript.append('\n')));
 		}
 	}
 
