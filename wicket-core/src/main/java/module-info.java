5fb05aa60ab5f3e8e9aac85adc01b0df61d2bf28
Maxim Solodovnik <solomax666@gmail.com>
2023-06-17 08:48:55 +0700

    Module names are corrected

--- wicket-core/src/main/java/module-info.java
+++ wicket-core/src/main/java/module-info.java
@@ -23,8 +23,8 @@ module org.apache.wicket.core {
     requires org.apache.wicket.request;
     requires org.apache.commons.io;
     requires org.apache.commons.collections4;
-    requires commons.fileupload2.core;
-    requires commons.fileupload2.jakarta;
+    requires org.apache.commons.fileupload2.core;
+    requires org.apache.commons.fileupload2.jakarta;
     requires org.slf4j;
     requires static jakarta.servlet;
     requires org.danekja.jdk.serializable.functional;
