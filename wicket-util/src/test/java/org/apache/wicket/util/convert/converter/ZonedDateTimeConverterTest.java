2d19d344c9e08796c83405eb597aaff3cae47d40
Martin Tzvetanov Grigorov <mgrigorov@apache.org>
2023-09-11 10:35:34 +0300

    Fix failing tests on JDK 21
    The problem is explained at https://bugs.openjdk.org/browse/JDK-8304925
    
    Update Byte-Buddy to fix failing tests due to Mockito not able to create
    mocks
    
    Signed-off-by: Martin Tzvetanov Grigorov <mgrigorov@apache.org>

--- wicket-util/src/test/java/org/apache/wicket/util/convert/converter/ZonedDateTimeConverterTest.java
+++ wicket-util/src/test/java/org/apache/wicket/util/convert/converter/ZonedDateTimeConverterTest.java
@@ -36,16 +36,34 @@ public class ZonedDateTimeConverterTest
 	private final ZoneId zoneUTC = ZoneId.of("Etc/UTC");
 
 	@Test
-	public void convertToString() {
+	@EnabledOnJre({JRE.JAVA_17, JRE.JAVA_18, JRE.JAVA_19})
+	public void convertToString_upto_jdk19() {
 		ZonedDateTimeConverter converter = new ZonedDateTimeConverter();
 		String date = converter.convertToString(ZonedDateTime.of(2016, 7, 11, 1, 2, 3, 0, zoneUCT), Locale.ENGLISH);
 		assertEquals("Jul 11, 2016, 1:02:03 AM Coordinated Universal Time", date);
 	}
 
 	@Test
-	public void convertToObject() {
+	@EnabledOnJre({JRE.JAVA_20, JRE.JAVA_21}) // See https://bugs.openjdk.org/browse/JDK-8304925
+	public void convertToString_jdk20_and_newer() {
+		ZonedDateTimeConverter converter = new ZonedDateTimeConverter();
+		String date = converter.convertToString(ZonedDateTime.of(2016, 7, 11, 1, 2, 3, 0, zoneUCT), Locale.ENGLISH);
+		assertEquals("Jul 11, 2016, 1:02:03 AM Coordinated Universal Time", date);
+	}
+
+	@Test
+	@EnabledOnJre({JRE.JAVA_17, JRE.JAVA_18, JRE.JAVA_19})
+	public void convertToObject_upto_jdk19() {
 		ZonedDateTimeConverter converter = new ZonedDateTimeConverter();
 		ZonedDateTime date = converter.convertToObject("Jul 11, 2016, 1:02:03 AM Coordinated Universal Time", Locale.ENGLISH);
+		assertEquals(ZonedDateTime.of(2016, 7, 11, 1, 2, 3, 0, zoneUTC), date);
+	}
+
+	@Test
+	@EnabledOnJre({JRE.JAVA_20, JRE.JAVA_21}) // See https://bugs.openjdk.org/browse/JDK-8304925
+	public void convertToObject_jdk20_and_newer() {
+		ZonedDateTimeConverter converter = new ZonedDateTimeConverter();
+		ZonedDateTime date = converter.convertToObject("Jul 11, 2016, 1:02:03 AM Coordinated Universal Time", Locale.ENGLISH);
 		assertEquals(ZonedDateTime.of(2016, 7, 11, 1, 2, 3, 0, zoneUTC), date);
 	}
 
