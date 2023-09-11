5d6c8821032bcf3d524d027fdc632e2f2c73ac71
Martin Grigorov <martin-g@users.noreply.github.com>
2023-09-11 11:27:47 +0300

    Build and test with JDK 17 & 21 (#607)
    * Build and test with JDK 17 & 21
    
    Update Maven in CI to 3.9.4 (latest stable)
    
    Signed-off-by: Martin Tzvetanov Grigorov <mgrigorov@apache.org>
    
    * Bump Mockito to 5.5.0
    
    Signed-off-by: Martin Tzvetanov Grigorov <mgrigorov@apache.org>
    
    * Do not use Mockito to create a mock of java.io.File class
    
    It fails on JdK 21 with:
    ```
    [ERROR]   FilesTest.dontWaitTooMuchIfCantDelete:96 Mockito
    Mockito cannot mock this class: class java.io.File.
    
    If you're not sure why you're getting this error, please open an issue on GitHub.
    
    Java               : 21
    JVM vendor name    : Oracle Corporation
    JVM vendor version : 21+35-2513
    JVM name           : OpenJDK 64-Bit Server VM
    JVM version        : 21+35-2513
    JVM info           : mixed mode, sharing
    OS name            : Linux
    OS version         : 6.2.0-10018-tuxedo
    
    You are seeing this disclaimer because Mockito is configured to create inlined mocks.
    You can learn about inline mocks and their limitations under item #39 of the Mockito class javadoc.
    
    Underlying exception : org.mockito.exceptions.base.MockitoException: Could not modify all classes [interface java.io.Serializable, class java.lang.Object, class java.io.File, interface java.lang.Comparable]
    ```
    
    Signed-off-by: Martin Tzvetanov Grigorov <mgrigorov@apache.org>
    
    * Fix failing tests on JDK 21
    
    The problem is explained at https://bugs.openjdk.org/browse/JDK-8304925
    
    Update Byte-Buddy to fix failing tests due to Mockito not able to create
    mocks
    
    Signed-off-by: Martin Tzvetanov Grigorov <mgrigorov@apache.org>
    
    * Update org.openrewrite.recipe:rewrite-recipe-bom to support Java 21
    
    Signed-off-by: Martin Tzvetanov Grigorov <mgrigorov@apache.org>
    
    * Test against JDK 22-ea too
    
    Signed-off-by: Martin Tzvetanov Grigorov <mgrigorov@apache.org>
    
    * 22-ea is not yet available at adoptium.net
    
    Test against latest stable - JDK 20
    
    Signed-off-by: Martin Tzvetanov Grigorov <mgrigorov@apache.org>
    
    * Add a Maven profile for JDK 21
    
    Fix the version of EA
    Update maven-javadoc-plugin
    
    Signed-off-by: Martin Tzvetanov Grigorov <mgrigorov@apache.org>
    
    ---------
    
    Signed-off-by: Martin Tzvetanov Grigorov <mgrigorov@apache.org>

--- wicket-util/src/test/java/org/apache/wicket/util/convert/converter/LocalDateTimeConverterTest.java
+++ wicket-util/src/test/java/org/apache/wicket/util/convert/converter/LocalDateTimeConverterTest.java
@@ -18,6 +18,8 @@ package org.apache.wicket.util.convert.converter;
 
 import org.apache.wicket.util.convert.ConversionException;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.condition.EnabledOnJre;
+import org.junit.jupiter.api.condition.JRE;
 
 import java.time.LocalDateTime;
 import java.util.Locale;
@@ -30,16 +32,34 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class LocalDateTimeConverterTest
 {
 	@Test
-	public void convertToString() {
+	@EnabledOnJre({JRE.JAVA_17, JRE.JAVA_18, JRE.JAVA_19})
+	public void convertToString_upto_jdk19() {
 		LocalDateTimeConverter converter = new LocalDateTimeConverter();
 		String date = converter.convertToString(LocalDateTime.of(2016, 7, 11, 1, 2, 3), Locale.ENGLISH);
 		assertEquals("Jul 11, 2016, 1:02:03 AM", date);
 	}
 
 	@Test
-	public void convertToObject() {
+	@EnabledOnJre({JRE.JAVA_20, JRE.JAVA_21}) // See https://bugs.openjdk.org/browse/JDK-8304925
+	public void convertToString_jdk20_and_newer() {
+		LocalDateTimeConverter converter = new LocalDateTimeConverter();
+		String date = converter.convertToString(LocalDateTime.of(2016, 7, 11, 1, 2, 3), Locale.ENGLISH);
+		assertEquals("Jul 11, 2016, 1:02:03 AM", date);
+	}
+
+	@Test
+	@EnabledOnJre({JRE.JAVA_17, JRE.JAVA_18, JRE.JAVA_19})
+	public void convertToObject_upto_jdk19() {
 		LocalDateTimeConverter converter = new LocalDateTimeConverter();
 		LocalDateTime date = converter.convertToObject("Jul 11, 2016, 1:02:03 AM", Locale.ENGLISH);
+		assertEquals(date, LocalDateTime.of(2016, 7, 11, 1, 2, 3));
+	}
+
+	@Test
+	@EnabledOnJre({JRE.JAVA_20, JRE.JAVA_21}) // See https://bugs.openjdk.org/browse/JDK-8304925
+	public void convertToObject_jdk20_and_newer() {
+		LocalDateTimeConverter converter = new LocalDateTimeConverter();
+		LocalDateTime date = converter.convertToObject("Jul 11, 2016, 1:02:03 AM", Locale.ENGLISH);
 		assertEquals(date, LocalDateTime.of(2016, 7, 11, 1, 2, 3));
 	}
 	
