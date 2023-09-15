fb14db0b3f8febe3e25e5466329c99b256e463a0
Martin Grigorov <martin-g@users.noreply.github.com>
2023-09-15 12:23:14 +0300

    WICKET-7073: Update JQuery to 3.7.1 (#646)
    Signed-off-by: Martin Tzvetanov Grigorov <mgrigorov@apache.org>

--- testing/wicket-js-tests/Gruntfile.js
+++ testing/wicket-js-tests/Gruntfile.js
@@ -107,7 +107,7 @@ module.exports = function(grunt) {
 			all: {
 				options: {
 					urls: [
-						'http://localhost:38887/test/js/all.html?3.6.4'
+						'http://localhost:38887/test/js/all.html?3.7.1'
 					]
 				}
 			},
@@ -118,7 +118,7 @@ module.exports = function(grunt) {
 			amd: {
 				options: {
 					urls: [
-						'http://localhost:38887/test/js/amd.html?3.6.4'
+						'http://localhost:38887/test/js/amd.html?3.7.1'
 					]
 				}
 			}
