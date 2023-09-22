c2b529f7544adb12001dc49ebd590750d1a62eb8
sre <s.reifegerste@mapchart.com>
2023-09-22 14:05:57 +0200

    fixes, now building.

--- wicket-core-tests/bck/module-info.java
+++
@@ -1,4 +0,0 @@
-module org.apache.wicket.core.tests {}
-// otherwise 31mExecution default-testCompile of goal org.apache.maven.plugins:maven-compiler-plugin:3.10.1:test
-// Compile failed: Can't compile test sources when main sources are missing a module descriptor
-//
