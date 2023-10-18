117b6b789789677f6c65400ba328cd0edcf543ff
Andrea Del Bene <adelbene@apache.org>
2023-10-18 13:19:02 +0200

    WICKET-7081 added packages to open

--- wicket-util/src/main/java/module-info.java
+++ wicket-util/src/main/java/module-info.java
@@ -53,4 +53,9 @@ module org.apache.wicket.util {
     exports org.apache.wicket.util.visit;
     exports org.apache.wicket.util.watch;
     exports org.apache.wicket.util.xml;
+
+
+    //opening packages to allow resources access
+    opens org.apache.wicket.util.markup.xhtml;
+    opens org.apache.wicket.util.license;
 }
