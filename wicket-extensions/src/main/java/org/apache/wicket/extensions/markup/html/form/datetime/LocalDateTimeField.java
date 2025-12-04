9ff52425a3a90ea5286558865975222379fb5ddc
Johan Stuyts <j.stuyts@javathinker.com>
2025-12-04 20:42:53 +0100

    WICKET-7144: added option for `FormComponentPanel` implementations to indicate they want to process their children on Ajax requests.

--- wicket-extensions/src/main/java/org/apache/wicket/extensions/markup/html/form/datetime/LocalDateTimeField.java
+++ wicket-extensions/src/main/java/org/apache/wicket/extensions/markup/html/form/datetime/LocalDateTimeField.java
@@ -20,11 +20,64 @@ import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
 
+import org.apache.wicket.Component;
+import org.apache.wicket.ajax.form.AjaxFormComponentUpdatingBehavior;
+import org.apache.wicket.markup.html.form.FormComponent;
 import org.apache.wicket.model.IModel;
 
 /**
  * Works on a {@link java.time.LocalDateTime} object. See {@link AbstractDateTimeField} for
  * further details.
+ * <p>
+ * If you want to Ajaxify this component with an {@link AjaxFormComponentUpdatingBehavior}, it be done in 2 ways:
+ * </p>
+ * <ul>
+ *     <li>
+ *         On <code>LocalDateTimeField</code>: easy, less code, larger requests, and (unfortunately) excessive requests.
+ *         <p>
+ *         Create a subclass and:
+ *         <ul>
+ *             <li>
+ *                 Set {@link #WANT_CHILDREN_TO_PROCESS_INPUT_IN_AJAX_UPDATE} to <code>true</code>.
+ *             </li>
+ *             <li>
+ *                 Add the <code>AjaxFormComponentUpdatingBehavior</code> with event <code>"input change"</code> to it.
+ *             </li>
+ *             <li>
+ *                 Override {@link #newTimeField(String, IModel)} and also set
+ *                 {@link #WANT_CHILDREN_TO_PROCESS_INPUT_IN_AJAX_UPDATE} to <code>true</code> on the component created
+ *                 by the superclass.
+ *             </li>
+ *         </ul>
+ *     </li>
+ *     <li>
+ *         On the descendent form components: cumbersone, quite a bit of code, but few, smallest possible requests.
+ *         <p>
+ *         Create a subclass and:
+ *         <ul>
+ *             <li>
+ *                 Override {@link AbstractDateTimeField#newDateField(String, IModel)} and add a
+ *                 <code>AjaxFormComponentUpdatingBehavior</code> with event <code>"input change"</code> to the
+ *                 component created by the superclass.
+ *                 <p>
+ *                 {@link IModel#setObject(Object)} of the date field is a no-op. So use
+ *                 {@link FormComponent#getConvertedInput()} to get the submitted date, and update the model object of
+ *                 this field manually.
+ *             </li>
+ *             <li>
+ *                 Override {@link Component#onInitialize()} and
+ *                 {@link AbstractDateTimeField#getTimeField() get the timefield}. Use
+ *                 {@link TimeField#getHoursField()}, {@link TimeField#getMinutesField()} and
+ *                 {@link TimeField#getAmOrPmChoice()} to get the subfields, and add
+ *                 <code>AjaxFormComponentUpdatingBehavior</code> with event <code>"change"</code> to them.
+ *                 <p>
+ *                 {@link IModel#setObject(Object)} of these fields is also a no-op. So again use
+ *                 <code>getConvertedInput()</code> to get the submitted values, and update the model object of these
+ *                 fields manually.
+ *             </li>
+ *         </ul>
+ *     </li>
+ * </ul>
  *  
  * @author eelcohillenius
  */
@@ -57,7 +110,8 @@ public class LocalDateTimeField extends AbstractDateTimeField<LocalDateTime>
 	}
 
 	@Override
-	protected LocalDateTime createTemporal(LocalDate date, LocalTime time) {
+	protected LocalDateTime createTemporal(LocalDate date, LocalTime time)
+	{
 		return LocalDateTime.of(date, time);
 	}
 
