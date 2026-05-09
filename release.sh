040931bdac8bff0028d687b233354081c77852fb
Andrea Del Bene <adelbene@apache.org>
2026-05-09 20:19:16 +0200

    more attempt to solve multiple repository problem

--- release.sh
+++ release.sh
@@ -484,7 +484,7 @@ mvn -q clean -Pall
 
 # package and assemble the release
 echo "Prepare the release"
-mvn --batch-mode release:prepare -X -l $log -DpreparationGoals="clean" -Dtag=$tag -Papache-release,release
+mvn --batch-mode release:prepare -X -l $log -DpreparationGoals="clean" -Dtag=$tag -Daether.connector.basic.parallelPut=false -Papache-release,release
 if [ $? -ne 0 ] ; then
     fail "ERROR: mvn release:prepare was not successful"
 fi
