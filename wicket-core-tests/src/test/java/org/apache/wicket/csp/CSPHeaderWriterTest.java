c106a81350c405dc0b31b1ecebae1457f2c9daab
Schäfer, H.H. (Hans Hosea) <HansHosea.Schaefer@ing.de>
2026-09-08 21:18:19 +0200

    Simplified test.
    (cherry picked from commit fedc172255b5dbd9c87c7d1ca26bed4f3b6012f3)

--- wicket-core-tests/src/test/java/org/apache/wicket/csp/CSPHeaderWriterTest.java
+++ wicket-core-tests/src/test/java/org/apache/wicket/csp/CSPHeaderWriterTest.java
@@ -45,6 +45,8 @@ import static org.apache.wicket.csp.CSPDirectiveSrcValue.SELF;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.Optional;
+
 class CSPHeaderWriterTest extends WicketTestCase
 {
 
@@ -127,7 +129,7 @@ class CSPHeaderWriterTest extends WicketTestCase
 	}
 
 	@Test
-	void addCspDirectiveToStatelessPageEvenIfWrappedInMultipleIRequestHandlerDelegates()
+	void addCspDirectiveToPageEvenIfWrappedInMultipleIRequestHandlerDelegates()
 	{
 		tester.getApplication().mount(new MountedMapper("withdelegate/page", NoopMockPage.class)
 		{
@@ -135,14 +137,10 @@ class CSPHeaderWriterTest extends WicketTestCase
 			public IRequestHandler mapRequest(final Request request)
 			{
 				final IRequestHandler requestHandler = super.mapRequest(request);
-				final IRequestHandler result;
-				if (requestHandler instanceof RenderPageRequestHandler renderPageRequestHandler && NoopMockPage.class.equals(renderPageRequestHandler.getPageClass()))
-				{
-					result = new NoopIRequestHandlerDelegate(new NoopIRequestHandlerDelegate(renderPageRequestHandler));
-				} else {
-					result = requestHandler;
-				}
-				return result;
+				return Optional.ofNullable(requestHandler)
+				  .map(NoopIRequestHandlerDelegate::new)
+				  .map(NoopIRequestHandlerDelegate::new) // wrap twice
+				  .orElse(null);
 			}
 		});
 		tester.startPage(NoopMockPage.class);
