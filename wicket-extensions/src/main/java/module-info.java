9e4401f3f4d7a3228791a7cff573f0ce9010bbfb
Andrea Del Bene <andrea.delbene@host.it>
2026-09-11 10:57:29 +0200

    WICKET-7203: Fixed dependency declaration of jackson-databind in module-info.java. Is optional now

--- wicket-extensions/src/main/java/module-info.java
+++ wicket-extensions/src/main/java/module-info.java
@@ -19,7 +19,7 @@ module org.apache.wicket.extensions {
     requires java.desktop;
     requires static jakarta.servlet;
     requires org.slf4j;
-    requires tools.jackson.databind;
+    requires static tools.jackson.databind;
     requires com.github.openjson;
     requires org.apache.commons.fileupload2.core;
     requires org.apache.wicket.util;
